919d6001bdc311d46daa55fff639338ea231c2de
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-24 14:03:44 +0000

    Add package feed2fedi/3.1.2/feed2fedi-3.1.2.tar.gz

---
+++ packages/feed2fedi/feed2fedi-3.1.2.tar.gz/feed2fedi-3.1.2/src/feed2fedi/__init__.py
@@ -0,0 +1,15 @@
+"""Module specific values and logic."""
+
+from datetime import timedelta
+from datetime import timezone
+from importlib.metadata import version
+from typing import Final
+
+__version__: Final[str] = version(__package__)
+
+DISPLAY_NAME: Final[str] = "Feed2Fedi"
+WEBSITE: Final[str] = "https://codeberg.org/MarvinsMastodonTools/feed2fedi"
+
+POST_RECORDER_SQLITE_DB: Final[str] = "cache.sqlite"
+FILE_ENCODING: Final[str] = "UTF-8"
+UTC = timezone(offset=timedelta(hours=0))
