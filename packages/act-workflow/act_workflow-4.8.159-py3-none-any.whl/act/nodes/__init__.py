365797aaf32f8b7c915baabd2f23caec45ebd6ee
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-24 05:04:32 +0000

    Add package act-workflow/4.8.159/act_workflow-4.8.159-py3-none-any.whl

---
+++ packages/act-workflow/act_workflow-4.8.159-py3-none-any.whl/act/nodes/__init__.py
@@ -0,0 +1,9 @@
+
+from .chatmodels_node import ChatModelsNode
+from .setvariable_node import SetVariableNode
+from .slack_node import SlackNode
+from .openai_node import OpenAINode
+from .slack_node import SlackNode
+from .openai_node import OpenAINode
+from .data_transformation_node import DataTransformationNode
+from .aggregatenode_node import AggregateNode
