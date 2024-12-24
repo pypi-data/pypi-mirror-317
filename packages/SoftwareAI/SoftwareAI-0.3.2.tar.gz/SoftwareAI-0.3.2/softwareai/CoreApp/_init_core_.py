1005d3588fe17591f31ba73208cbf1427432b9b0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-24 04:04:22 +0000

    Add package SoftwareAI/0.3.2/SoftwareAI-0.3.2.tar.gz

---
+++ packages/SoftwareAI/SoftwareAI-0.3.2.tar.gz/SoftwareAI-0.3.2/softwareai/CoreApp/_init_core_.py
@@ -0,0 +1,11 @@
+# IMPORT SoftwareAI Core 
+from .SoftwareAI.Core import (
+        AutenticateAgent,
+        ResponseAgent,
+        python_functions,
+        Agent_files,
+        Agent_files_update,
+        OpenAIKeysinit,
+        FirebaseKeysinit
+)
+#########################################
