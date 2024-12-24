2e5b38017fa151554b475188e0cf4b9e6786df40
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-24 03:07:40 +0000

    Add package opteryx/0.19.0a912/opteryx-0.19.0a912-cp312-cp312-macosx_10_15_universal2.whl

---
+++ packages/opteryx/opteryx-0.19.0a912-cp312-cp312-macosx_10_15_universal2.whl/opteryx/virtual_datasets/__init__.py
@@ -0,0 +1,22 @@
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+import opteryx.virtual_datasets.astronaut_data as astronauts
+import opteryx.virtual_datasets.derived_data as derived
+import opteryx.virtual_datasets.no_table_data as no_table
+import opteryx.virtual_datasets.planet_data as planets
+import opteryx.virtual_datasets.satellite_data as satellites
+import opteryx.virtual_datasets.variables_data as variables
+from opteryx.virtual_datasets import missions
+from opteryx.virtual_datasets import statistics
+from opteryx.virtual_datasets import stop_words
+from opteryx.virtual_datasets import user
