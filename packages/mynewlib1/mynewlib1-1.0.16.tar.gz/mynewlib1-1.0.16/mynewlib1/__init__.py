88f7782a17f6dc5f72dc90b9713b1a0f69e83b63
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-24 05:03:37 +0000

    Add package mynewlib1/1.0.16/mynewlib1-1.0.16.tar.gz

---
+++ packages/mynewlib1/mynewlib1-1.0.16.tar.gz/mynewlib1-1.0.16/mynewlib1/__init__.py
@@ -0,0 +1,14 @@
+if __name__ == '__main__':
+    from pi_collections import *
+else:
+    from mini_math.pi.pi_collections import *
+
+if __name__ == '__main__':
+    from operations import *
+
+else:
+    from math.operations import *
+
+
+def help_mi():
+    print("эта библиотека состоит из двух модулей, 1-все вычисления с факториалами(дискретная математика), 2 модуль считает значение строки")
