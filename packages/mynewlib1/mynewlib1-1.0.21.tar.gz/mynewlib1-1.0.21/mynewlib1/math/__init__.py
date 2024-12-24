d26505313fd2536b61127f0f1f321b83fc223bd4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-24 05:04:25 +0000

    Add package mynewlib1/1.0.21/mynewlib1-1.0.21.tar.gz

---
+++ packages/mynewlib1/mynewlib1-1.0.21.tar.gz/mynewlib1-1.0.21/mynewlib1/math/__init__.py
@@ -0,0 +1,70 @@
+
+
+if __name__ == '__main__':
+    from operations import *
+
+else:
+    from mynewlib1.math.operations import *
+
+
+
+
+
+def test_factorial(comments=True):
+    if comments:
+        print('test factorial...')
+    assert factorial(3) == 6
+
+    if comments:
+        print('factorial completed')
+
+def test_permutation(comments=True):
+    if comments:
+        print('test permutation...')
+    assert permutation(4) == 24
+
+    if comments:
+        print('permutation completed')
+
+def test_compination(comments=True):
+    if comments:
+        print('test compination...')
+    assert compination(5,3) == 10
+
+    if comments:
+        print('compination completed')
+
+def test_binkof(comments=True):
+    if comments:
+        print('test binkof...')
+    assert binkof(100,2) == 4950
+
+    if comments:
+        print('binkof completed')
+
+def test_gamma(comments=True):
+    if comments:
+        print('test gamma...')
+    assert gamma(4) == 6
+
+    if comments:
+        print('gamma completed')
+
+def test_summaf(comments=True):
+    if comments:
+        print('test 5...')
+    assert summaf(3) == 9
+
+    if comments:
+        print('summaf completed')
+
+test_factorial()
+test_permutation()
+test_compination()
+test_binkof()
+test_gamma()
+test_summaf()
+
+
+
+
