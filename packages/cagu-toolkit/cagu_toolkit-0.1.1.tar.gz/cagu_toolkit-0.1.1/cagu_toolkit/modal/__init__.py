a70b2fa31ecddb3513bf00faea7f45a88cdfbc76
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-24 04:03:49 +0000

    Add package cagu-toolkit/0.1.1/cagu_toolkit-0.1.1.tar.gz

---
+++ packages/cagu-toolkit/cagu_toolkit-0.1.1.tar.gz/cagu_toolkit-0.1.1/cagu_toolkit/modal/__init__.py
@@ -0,0 +1,9 @@
+from cagu_toolkit.modal.exchange_rate import ExchangeRate
+from cagu_toolkit.modal.sku import Sku
+from cagu_toolkit.modal.category import Category
+from cagu_toolkit.modal.product import Product
+from cagu_toolkit.modal.supplier import Supplier
+from cagu_toolkit.modal.sku_freight_estimation import SkuFreightEstimation, EstimationType
+from cagu_toolkit.modal.logistics_channel import LogisticsChannel
+
+__all__ = ["Category", "LogisticsChannel", "Supplier", "ExchangeRate", "Product", "Supplier", "Sku", "SkuFreightEstimation", "EstimationType"]
