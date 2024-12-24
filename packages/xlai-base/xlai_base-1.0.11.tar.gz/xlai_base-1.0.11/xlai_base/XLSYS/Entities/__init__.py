88d0c22ef4712c75ba3cc8c299e9e92376efa45d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-24 16:04:34 +0000

    Add package xlai-base/1.0.11/xlai_base-1.0.11.tar.gz

---
+++ packages/xlai-base/xlai_base-1.0.11.tar.gz/xlai_base-1.0.11/xlai_base/XLSYS/Entities/__init__.py
@@ -0,0 +1,8 @@
+# coding=utf8
+
+from .XLDataStructure import XLDE, CREATE_SOURCE_DE, XLDataUtil, XLLinkDE
+from .XLDataMark import XLDataMarkUtil, XLDM, XLLinkDM
+from .RunEntiy import RunTraceE
+from .Storage import XLCGlobalInfo, XLContext
+from .XLExceptionEntities import XLBaseException, XLExceptionEnum
+
