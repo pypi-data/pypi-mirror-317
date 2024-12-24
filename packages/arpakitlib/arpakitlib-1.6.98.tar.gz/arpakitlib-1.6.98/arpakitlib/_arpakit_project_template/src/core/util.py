dfb0dfc4774a7a9607216d7d4acfa685dcfc749e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-24 14:03:34 +0000

    Add package arpakitlib/1.6.98/arpakitlib-1.6.98.tar.gz

---
+++ packages/arpakitlib/arpakitlib-1.6.98.tar.gz/arpakitlib-1.6.98/arpakitlib/_arpakit_project_template/src/core/util.py
@@ -0,0 +1,52 @@
+import asyncio
+from functools import lru_cache
+
+from arpakitlib.ar_file_storage_in_dir_util import FileStorageInDir
+from arpakitlib.ar_logging_util import setup_normal_logging
+from src.core.settings import get_cached_settings
+
+
+def setup_logging():
+    setup_normal_logging(log_filepath=get_cached_settings().log_filepath)
+
+
+def create_cache_file_storage_in_dir() -> FileStorageInDir:
+    return FileStorageInDir(dirpath=get_cached_settings().cache_dirpath)
+
+
+@lru_cache()
+def get_cached_cache_file_storage_in_dir() -> FileStorageInDir:
+    return create_cache_file_storage_in_dir()
+
+
+def create_media_file_storage_in_dir() -> FileStorageInDir:
+    return FileStorageInDir(dirpath=get_cached_settings().media_dirpath)
+
+
+@lru_cache()
+def get_cached_media_file_storage_in_dir() -> FileStorageInDir:
+    return create_media_file_storage_in_dir()
+
+
+def create_dump_file_storage_in_dir() -> FileStorageInDir:
+    return FileStorageInDir(dirpath=get_cached_settings().dump_dirpath)
+
+
+@lru_cache()
+def get_cached_dump_file_storage_in_dir() -> FileStorageInDir:
+    return create_dump_file_storage_in_dir()
+
+
+def __example():
+    get_cached_cache_file_storage_in_dir().init()
+    get_cached_media_file_storage_in_dir().init()
+    get_cached_dump_file_storage_in_dir().init()
+
+
+async def __async_example():
+    pass
+
+
+if __name__ == '__main__':
+    __example()
+    asyncio.run(__async_example())
