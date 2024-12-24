8dcfd54ce1192c107e97084bd9f9c6c640d63360
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-24 14:03:45 +0000

    Add package math-formulas/1.0.1/math_formulas-1.0.1.tar.gz

---
+++ packages/math-formulas/math_formulas-1.0.1.tar.gz/math_formulas-1.0.1/setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup, find_packages
+
+def readme():
+    with open('README.md', 'r', encoding="utf8") as f:
+        return f.read()
+setup(
+    name='math_formulas',
+    version='1.0.1',
+    author='Yugay A.V.',
+    author_email='knifecalledlust33@gmail.com',
+    description='',
+    long_description=readme(),
+    long_description_content_type='text/markdown',
+    url='',
+    packages=find_packages('.'),
+    install_requires=[''],
+    classifiers=[
+        'Programming Language :: Python :: 3.11',
+        'License :: OSI Approved :: MIT License',
+    ],
+    keywords='example, math',
+    project_urls={
+    },
+    python_requires='>=3.7'
+)
