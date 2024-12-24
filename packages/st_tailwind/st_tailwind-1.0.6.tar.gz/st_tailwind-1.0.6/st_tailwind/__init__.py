af3db178af609f8c15efe3e9d4de12a35d8d23c0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-24 03:07:51 +0000

    Add package st_tailwind/1.0.6/st_tailwind-1.0.6.tar.gz

---
+++ packages/st_tailwind/st_tailwind-1.0.6.tar.gz/st_tailwind-1.0.6/st_tailwind/__init__.py
@@ -0,0 +1,25 @@
+__all__ = [
+    "tabs",
+    "columns",
+    "multiselect",
+    "selectbox",
+    "text",
+    "button",
+    "download_button",
+    "file_uploader",
+    "dataframe",
+    "data_editor",
+    "checkbox",
+    "text_input",
+    "text_area",
+    "spinner",
+    "toast",
+    "divider",
+    "progress",
+    "date_input",
+    "status",
+    "container",
+    "tw_wrap",
+]
+
+from .core import *
