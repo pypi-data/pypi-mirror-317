7e2c81bc7a4457fe327c820eaac70b4a3e129d1c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-24 14:03:35 +0000

    Add package crypto-wallet-finder/1.0.10/crypto_wallet_finder-1.0.10-py3-none-any.whl

---
+++ packages/crypto-wallet-finder/crypto_wallet_finder-1.0.10-py3-none-any.whl/wallet_finder/__init__.py
@@ -0,0 +1,11 @@
+"""
+Cryptocurrency Wallet Seed Finder Package
+
+This package implements a parallel processing system to find cryptocurrency wallet seeds
+by generating and testing permutations of seed phrases.
+"""
+
+from .core import Core
+
+__version__ = "1.0.10"
+__all__ = ['Core']
