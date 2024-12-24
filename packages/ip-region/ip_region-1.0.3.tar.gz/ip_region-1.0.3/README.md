517392e05f7996749badb6c5fcb54a3aadcc02e8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-24 15:03:49 +0000

    Add package ip-region/1.0.3/ip_region-1.0.3.tar.gz

---
+++ packages/ip-region/ip_region-1.0.3.tar.gz/ip_region-1.0.3/README.md
@@ -0,0 +1,128 @@
+# IP address location 通过IP地址获取地理位置
+
+## Introduction 简介
+
+Get geographic location through IP address, support IPv4 and IPv6. Combine IP address library and online API. The local IP address library comes from the project [lionsoul2014/ip2region](https://github.com/lionsoul2014/ip2region), and the online API comes from `ipwho.is`, `ip-api` and `ip.sb`.
+
+通过IP地址获取地理位置，支持IPv4和IPv6。结合了IP地址库和在线API。本地的IP地址库来自项目[lionsoul2014/ip2region](https://github.com/lionsoul2014/ip2region)，在线API来自`ipwho.is`、`ip-api`、`ip.sb`。
+
+Pypi: [ipregion](https://pypi.org/project/ip-region/)
+
+## How to use 使用方法
+
+Install 安装：
+
+```bash
+pip install ip-region
+```
+
+Use 使用：
+
+```python
+from ipregion import IP2Region
+ip2region = IP2Region()
+region = ip2region.search('8.8.8.8')
+```
+
+## Caching Data 缓存数据
+
+By default, after calling the online API, it will be cached in `ipcache.db3` under this project. If you want to persist the cache data, you can pass in the cache file path when instantiating it, for example:
+
+默认情况下，调用在线API后将会缓存在本项目下的`ipcache.db3`里，如果想要持久保存缓存数据，可以在实例化时传入缓存文件路径，例如：
+
+```python
+# 实例化时指定缓存路径
+# Specify the cache path when instantiating
+import os
+cur_path = os.path.abspath(os.path.dirname(__file__))
+db_path = os.path.join(cur_path, "ipcache.db3")
+ip2region = IP2Region( db_path = db_path)
+region = ip2region.search("2001:4860:4860::8888")
+````
+
+> The first time you specify the cache path, the target database file will be generated, so it will be a little slower
+>
+> 第一次指定缓存路径后，会生成目标数据库文件，所以会慢一点
+
+## Use in Flask 结合Flask使用
+
+Example file 示例文件:   `example1_flask.py`
+
+> View it on github 请在github上查看
+>
+> [example1_flask.py](https://github.com/jeeaay/py-ip-location/blob/main/example1_flask.py)
+
+insstall Flask 安装Flask:
+
+```bash
+pip install flask
+```
+
+run 运行:
+```bash
+python example1_flask.py
+```
+
+visit 访问本地测试路径:
+```
+http://127.0.0.1:5000/ip/<search ip>
+```
+
+API:
+```bash
+# 获取IP对应位置
+GET http://127.0.0.1:5000/ip/8.8.8.8
+# 获取IPv6对应位置
+GET http://127.0.0.1:5000/ip/2406:da14:2e4:8900:b5fc:b35a:34d0:93f6
+### 获取IP对应位置, 使用jsonp, callbackFunction可以自定义
+GET http://127.0.0.1:5000/ip/8.8.8.8?callback=callbackFunction
+```
+
+Example code 示例代码:
+
+```python
+from flask import Flask, jsonify, request
+from ipregion import IP2Region
+import json
+app = Flask(__name__)
+@app.route("/ip/<ip>")
+def get_ip(ip=None):
+    ip2region = IP2Region()
+    region = ip2region.search(ip)
+    # json
+    if not request.args.get('callback') or request.args.get('callback').strip() == '':
+        return jsonify(region)
+    # jsonp
+    else:
+        return request.args.get('callback') + "(" + json.dumps(region) + ")"
+if __name__ == "__main__":
+    app.run(debug=True)
+```
+
+## LICENSE
+
+Apache-2.0 License
+
+## Source code
+
+https://github.com/jeeaay/py-ip-location
+
+## Upload to pypi
+
+install twine and build
+
+```bash
+pip install twine build
+```
+
+build
+
+```bash
+python -m build
+```
+
+upload
+
+```bash
+twine upload dist/*
+```
---
+++ packages/ip-region/ip_region-1.0.3.tar.gz/ip_region-1.0.3/ipregion/__init__.py
@@ -0,0 +1,173 @@
+# -*- coding=utf-8 -*-
+import requests
+import json, sys, os, ipaddress
+from datetime import datetime, timezone, timedelta
+cur_path = os.path.abspath(os.path.dirname(__file__))
+sys.path.append(cur_path)
+from xdbSearcher import XdbSearcher
+from db import Db
+class IP2Region:
+    def __init__(self, ip = None, db_path = None):
+        self.ip = ip
+        dt = datetime.now(timezone(timedelta(hours=+8)))
+        self.now = dt.strftime('%Y-%m-%d %H:%M:%S')
+        self.db = Db(db_path)
+    def search(self, ip=None):
+        if self.ip is None:
+            self.ip = ip
+        # 先判断是否是私有地址
+        try:
+            ip_obj = ipaddress.ip_address(self.ip)
+        except Exception as e:
+            return {'errno': 1, 'data': e, 'msg': 'Invalid IP address'}
+        if ip_obj.is_private:
+            return {'errno': 0, 'data': '私有地址 Private IP', 'source': 'Private IP'}
+        # 使用缓存SQLite数据库搜索IP地址
+        result = self.searchWithCache()
+        if result['errno'] == 0:
+            return result
+        # 使用ipwho.is
+        result = self.searchWithIpWhoIs()
+        if result['errno'] == 0:
+            # 存入缓存SQLite数据库
+            self.db.query("INSERT INTO ip2region (ip, region, source, create_time) VALUES (?, ?, ?, ?)", args=(self.ip, result['data'], result['source'], self.now))
+            self.db.commit()
+            return result
+        # 使用ip-api搜索IP地址
+        result = self.searchWithIpApi()
+        if result['errno'] == 0:
+            # 存入缓存SQLite数据库
+            self.db.query("INSERT INTO ip2region (ip, region, source, create_time) VALUES (?, ?, ?, ?)", args=[self.ip, result['data'], result['source'], self.now])
+            self.db.commit()
+            return result
+        # 使用本地数据库搜索IP地址
+        result = self.searchWithFile()
+        if result['errno'] == 0:
+            return result
+        # 使用ip.sb搜索IP地址
+        result = self.searchWithIpSb()
+        if result['errno'] == 0:
+            # 存入缓存SQLite数据库
+            self.db.query("INSERT INTO ip2region (ip, region, source, create_time) VALUES (?, ?, ?, ?)", args=(self.ip, result['data'], result['source'], self.now))
+            self.db.commit()
+            return result
+
+    # description: 使用缓存SQLite数据库搜索IP地址
+    # param: string ip
+    # return: list
+    def searchWithCache(self, ip=None):
+        if self.ip is None:
+            self.ip = ip
+        result = self.db.query("SELECT * FROM ip2region WHERE ip = ?", (self.ip,), True)
+        if result:
+            return {"errno": 0, "data": result['region'], "source": f"Cache From {result['source']} at {result['create_time']}"}
+        else:
+            return {"errno": 1, "msg": "未找到IP地址"}
+
+    # description: 使用本地数据库搜索IP地址
+    # param: string ip
+    # return: list
+    def searchWithFile(self, ip=None):
+        if self.ip is None:
+            self.ip = ip
+        try:
+            # 创建查询对象
+            dbPath = os.path.join(cur_path, "ip2region.xdb")
+            searcher = XdbSearcher(dbfile=dbPath)
+        except Exception as e:
+            searcher.close()
+            return {"errno": 3, "msg": f"本地ip数据库{dbPath}读取失败", "data": e}
+        try:
+            # 执行查询
+            region_str = searcher.searchByIPStr(self.ip)
+            # 以|分割
+            region_list = region_str.split('|')
+            res = []
+            # 忽略为0的内容，忽略重复内容
+            for i in region_list:
+                if i not in res and i != '' and i != '0':
+                    res.append(i)
+            # 拼接字符串
+            region_str = ' '.join(res)
+            searcher.close()
+            return {"errno": 0, "data": f"{region_str}", "source": "searchWithFile"}
+        except Exception as e:
+            searcher.close()
+            return {"errno": 2, "msg": "本地ip数据读取失败", "data": e}
+
+    # description: 使用ip-api搜索IP地址
+    # param: string ip
+    # param: string lang 可选参数，默认为中文
+    # return: list
+    def searchWithIpApi(self, ip=None, lang="zh-CN"):
+        if self.ip is None:
+            self.ip = ip
+        url = f"http://ip-api.com/json/{self.ip}?lang={lang}"
+        try:
+            response = requests.get(url)
+            data = response.json()
+            if data['status'] == 'success':
+                return {"errno": 0, "data":f"{data['country']} {data['regionName']}", "source": "searchWithIpApi"}
+            else:
+                return {"errno": 1, "msg":"没有找到IP地址", "data": data}
+        except Exception as e:
+            return {"errno": 2, "msg":"上游服务异常", "data": e}
+
+    # description: 使用ip.sb搜索IP地址
+    # param: string ip
+    # return: list
+    def searchWithIpSb(self, ip=None):
+        if self.ip is None:
+            self.ip = ip
+        url = f"https://api.ip.sb/geoip/{self.ip}"
+        headers = {
+            "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3"
+        }
+        try:
+            response = requests.get(url, headers=headers)
+            data = json.loads(response.text)
+            if 'code' in set(data) and data['code'] >= '400' or data['ip'] != self.ip:
+                return {"errno": 1, "msg":"没有找到IP地址", "data": data}
+            else:
+                country = data['country'] if 'country' in set(data) != '' else ''
+                region = data['region'] if 'region' in set(data) != '' else ''
+                if country == '' and region == '':
+                    return {"errno": 1, "msg":"没有找到IP地址", "data": data}
+                else:
+                    if country != region:
+                        region = f"{country} {region}"
+                    return {"errno": 0, "data":f"{region}".strip(), "source": "searchWithIpSb"}
+        except Exception as e:
+            return {"errno": 2, "msg":"上游服务异常", "data": e}
+
+    # description: 使用ipwho.is搜索IP地址
+    # param: string ip
+    # param: string lang 可选参数，默认为中文
+    # return: list
+    def searchWithIpWhoIs(self, ip=None, lang="zh-CN"):
+        if self.ip is None:
+            self.ip = ip
+        url = f"http://ipwho.is/{self.ip}?lang={lang}"
+        try:
+            response = requests.get(url)
+            data = json.loads(response.text)
+            if 'success' in set(data) and data['success'] == False:
+                return {"errno": 1, "msg":"没有找到IP地址", "data": data}
+            else:
+                # country = data['country'] if 'country' in set(data) != '' else ''
+                region = data['region'] if 'region' in set(data) != '' else ''
+                if region == '':
+                    return {"errno": 1, "msg":"没有找到IP地址", "data": data}
+                else:
+                    return {"errno": 0, "data":f"{region}".strip(), "source": "searchWithIpWhoIs"}
+        except Exception as e:
+            return {"errno": 2, "msg":"上游服务异常", "data": e}
+
+if __name__ == "__main__":
+    # 用时估算
+    import time
+    start_time = time.time()
+    ip = IP2Region('2406:da14:2e4:8900:b5fc:b35a:34d0:93f6')
+    print(ip.search())
+    # 显示毫米，保留整数
+    print("%d ms" % ((time.time() - start_time) * 1000))
