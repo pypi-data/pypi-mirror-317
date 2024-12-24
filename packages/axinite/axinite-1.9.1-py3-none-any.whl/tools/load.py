96fbbc0d01f023a471ea9ed6141608a455868d34
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-24 15:03:31 +0000

    Add package axinite/1.9.1/axinite-1.9.1-py3-none-any.whl

---
+++ packages/axinite/axinite-1.9.1-py3-none-any.whl/tools/load.py
@@ -0,0 +1,69 @@
+import axinite as ax
+import axinite.tools as axtools
+from axinite.tools import AxiniteArgs
+import json
+from numba import jit
+_jit = jit
+
+def load(args: AxiniteArgs, path: str = "", dont_change_args: bool = False, jit: bool = True):
+    if jit == False: args.action = lambda t, **kwargs: print(f"Timestep {t} ({((t / args.limit) * 100).value:.2f}% complete)", end="\r")
+
+    bodies = ax.load(*args.unpack(), t=args.t, modifier=args.modifier, action=args.action)
+    print(f"\nFinished with {len(bodies[0].r)} timesteps")
+
+    _bodies = []
+    for i, body in enumerate(bodies):
+        _bodies.append(args.bodies[i])
+        for j, r in body.r.items():
+            _bodies[i].r[j] = r
+            _bodies[i].v[j] = body.v[j]
+    if path == "": 
+        if not dont_change_args:
+            args.t = args.limit
+            args.bodies = _bodies
+        return _bodies
+    else: 
+        with open(path, 'w+') as f:
+            data = {
+                "name": args.name,
+                "delta": args.delta.value,
+                "limit": args.limit.value,
+                "t": args.t.value,
+                "radius_multiplier": args.radius_multiplier,
+                "bodies": []
+            }
+
+            for body in _bodies: 
+                body_data = {
+                    "name": body.name,
+                    "mass": body.mass.value,
+                    "radius": body.radius.value,
+                    "r": {k: [v.x.value, v.y.value, v.z.value] for k, v in body.r.items()},
+                    "v": {k: [v.x.value, v.y.value, v.z.value] for k, v in body.v.items()}
+                }
+                if body.color != None:
+                    body_data["color"] = body.color
+                if body.retain != None:
+                    body_data["retain"] = body.retain
+                if body.light != None:
+                    body_data["light"] = body.light
+
+                data["bodies"].append(body_data)
+
+            if args.radius_multiplier is not None:
+                data["radius_multiplier"] = args.radius_multiplier
+
+            if args.rate is not None:
+                data["rate"] = args.rate
+
+            if args.retain is not None:
+                data["retain"] = args.retain
+            
+            if args.frontend_args != {}:
+                data["frontend_args"] = args.frontend_args
+
+            json.dump(data, f, indent=4)
+            if not dont_change_args:
+                args.t = args.limit
+                args.bodies = _bodies
+            return _bodies
