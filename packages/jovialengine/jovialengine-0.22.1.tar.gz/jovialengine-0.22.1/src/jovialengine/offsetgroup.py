ccd9a5f8203c81f9446b472fe7a2d43e964d74a6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-24 05:04:37 +0000

    Add package jovialengine/0.22.1/jovialengine-0.22.1.tar.gz

---
+++ packages/jovialengine/jovialengine-0.22.1.tar.gz/jovialengine-0.22.1/src/jovialengine/offsetgroup.py
@@ -0,0 +1,23 @@
+import pygame
+
+from . import utility
+
+
+class OffsetGroup(pygame.sprite.LayeredUpdates):
+    """A sprite group for drawing sprites, in layers, offset by some amount.
+    For best results, sprite.rect should be an FRect."""
+    def draw(self, surface: pygame.Surface, offset: pygame.typing.IntPoint=(0,0)):
+        sprite_sequence = [
+            (
+                sprite.image,
+                (
+                    round(sprite.rect.x) + offset[0],
+                    round(sprite.rect.y) + offset[1],
+                )
+            )
+            for sprite
+            in self.sprites()
+        ]
+        surface.fblits(sprite_sequence)
+        self.lostsprites = []
+        return self.lostsprites
