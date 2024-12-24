8b5400356aee94d6f6c680caf7ab11fa3e2de504
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-24 14:03:50 +0000

    Add package frr-ospf-api-stream-client/1.0.4/frr_ospf_api_stream_client-1.0.4-py3-none-any.whl

---
+++ packages/frr-ospf-api-stream-client/frr_ospf_api_stream_client-1.0.4-py3-none-any.whl/api_stream_client/archival.py
@@ -0,0 +1,22 @@
+import json
+import os
+from datetime import datetime
+
+
+def archive_event_message(event: dict, path_prefix: str):
+    event_time = datetime.fromtimestamp(event["timestamp"] / 1000)
+    file_path = os.path.join(path_prefix, event_time.strftime("%Y/%m/%d/%H") + ".jsonl")
+    os.makedirs(os.path.dirname(file_path), exist_ok=True)
+    with open(file_path, "a") as f:
+        f.write(json.dumps(event) + "\n")
+
+
+def archive_db_snapshot(api_payload: dict, path_prefix: str):
+    api_snapshot_time = datetime.fromtimestamp(api_payload["updated"] / 1000)
+    file_path = os.path.join(
+        path_prefix,
+        api_snapshot_time.strftime("%Y/%m/%d/%H/%M") + ".json",
+    )
+    os.makedirs(os.path.dirname(file_path), exist_ok=True)
+    with open(file_path, "w") as f:
+        f.write(json.dumps(api_payload))
