81e4efe87510aa1561f05c6589cf8051cefe1edc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-24 15:03:58 +0000

    Add package explorationViewer/0.1/explorationViewer-0.1-py3-none-any.whl

---
+++ packages/explorationViewer/explorationViewer-0.1-py3-none-any.whl/explorationViewer/__init__.py
@@ -0,0 +1,15 @@
+"""
+- Authors: Peter Mawhorter
+- Consulted:
+- Date: 2023-7-14
+- Purpose: Visualize exploration traces from the `exploration` library.
+
+See ../README.md (or
+[https://pypi.org/project/explorationViewer/](https://pypi.org/project/explorationViewer/))
+for project README.
+"""
+
+__version__ = "0.1"
+
+# Imports define what's available when you do `import explorationViewer`
+from . import viewer # noqa
