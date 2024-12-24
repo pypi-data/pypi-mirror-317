014bf5e028bc992902b22486149c0275a9da455e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-24 16:04:43 +0000

    Add package datav-client/1.1.0.5/datav_client-1.1.0.5.tar.gz

---
+++ packages/datav-client/datav_client-1.1.0.5.tar.gz/datav_client-1.1.0.5/setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='datav_client',
+    version='1.1.0.5',
+    description='datav爬取数据通用组件客户端',
+    author='python之父·博思之光·杨瑞',
+    packages=['datav_client'],
+    install_requires=[
+        'pypinyin',
+        'Style',
+        'Flask'
+    ],
+)
