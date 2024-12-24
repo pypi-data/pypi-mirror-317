c13c4a3f7a4bed04f4c93aa6d13af1bcdd2ff3bd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-24 04:04:01 +0000

    Add package increase/0.173.0/increase-0.173.0-py3-none-any.whl

---
+++ packages/increase/increase-0.173.0-py3-none-any.whl/increase/types/oauth_connection_list_params.py
@@ -0,0 +1,37 @@
+# File generated from our OpenAPI spec by Stainless. See CONTRIBUTING.md for details.
+
+from __future__ import annotations
+
+from typing import List
+from typing_extensions import Literal, TypedDict
+
+__all__ = ["OAuthConnectionListParams", "Status"]
+
+
+class OAuthConnectionListParams(TypedDict, total=False):
+    cursor: str
+    """Return the page of entries after this one."""
+
+    limit: int
+    """Limit the size of the list that is returned.
+
+    The default (and maximum) is 100 objects.
+    """
+
+    oauth_application_id: str
+    """The identifier of the OAuth Application to filter by."""
+
+    status: Status
+
+
+_StatusReservedKeywords = TypedDict(
+    "_StatusReservedKeywords",
+    {
+        "in": List[Literal["active", "inactive"]],
+    },
+    total=False,
+)
+
+
+class Status(_StatusReservedKeywords, total=False):
+    pass
