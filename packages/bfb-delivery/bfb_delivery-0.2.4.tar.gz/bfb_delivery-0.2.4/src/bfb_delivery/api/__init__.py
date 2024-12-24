d570e52ff91bfe74dfc9bbabc91949bb8f0a6cd5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-24 16:04:00 +0000

    Add package bfb-delivery/0.2.4/bfb_delivery-0.2.4.tar.gz

---
+++ packages/bfb-delivery/bfb_delivery-0.2.4.tar.gz/bfb_delivery-0.2.4/src/bfb_delivery/api/__init__.py
@@ -0,0 +1,3 @@
+"""Public and internal APIs."""
+
+from bfb_delivery.api.public import combine_route_tables, split_chunked_route
---
+++ packages/bfb-delivery/bfb_delivery-0.2.4.tar.gz/bfb_delivery-0.2.4/src/bfb_delivery/lib/constants.py
@@ -0,0 +1,9 @@
+"""Constants used in the project."""
+
+from typing import Final
+
+
+class Columns:
+    """Column name constants."""
+
+    DRIVER: Final[str] = "driver"
