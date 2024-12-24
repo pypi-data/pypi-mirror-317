f2a75bcc47e8814521343b497a756b2581397b20
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-24 04:04:33 +0000

    Add package awmt-proto-dsp-tools/0.1.0/awmt_proto_dsp_tools-0.1.0-py3-none-any.whl

---
+++ packages/awmt-proto-dsp-tools/awmt_proto_dsp_tools-0.1.0-py3-none-any.whl/awmt/proto_dsp_tools/example.py
@@ -0,0 +1,12 @@
+"""
+Example main file
+"""
+
+def prompt() -> None:
+    print('Hello World!')
+
+def add_one(number: int) -> int:
+    return number + 1
+
+if __name__ == "__main__":
+    prompt()
