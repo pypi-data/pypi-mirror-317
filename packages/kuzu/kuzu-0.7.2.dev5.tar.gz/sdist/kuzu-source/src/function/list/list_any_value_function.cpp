7447869066298cdefd9b87507fd1d1fa2dff5922
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-24 16:04:10 +0000

    Add package kuzu/0.7.2.dev5/kuzu-0.7.2.dev5.tar.gz

---
+++ packages/kuzu/kuzu-0.7.2.dev5.tar.gz/sdist/kuzu-source/src/function/list/list_any_value_function.cpp
@@ -0,0 +1,48 @@
+#include "common/type_utils.h"
+#include "function/list/vector_list_functions.h"
+#include "function/scalar_function.h"
+
+using namespace kuzu::common;
+
+namespace kuzu {
+namespace function {
+
+struct ListAnyValue {
+    template<typename T>
+    static void operation(common::list_entry_t& input, T& result, common::ValueVector& inputVector,
+        common::ValueVector& resultVector) {
+        auto inputValues = common::ListVector::getListValues(&inputVector, input);
+        auto inputDataVector = common::ListVector::getDataVector(&inputVector);
+        auto numBytesPerValue = inputDataVector->getNumBytesPerValue();
+
+        for (auto i = 0u; i < input.size; i++) {
+            if (!(inputDataVector->isNull(input.offset + i))) {
+                resultVector.copyFromVectorData(reinterpret_cast<uint8_t*>(&result),
+                    inputDataVector, inputValues);
+                break;
+            }
+            inputValues += numBytesPerValue;
+        }
+    }
+};
+
+static std::unique_ptr<FunctionBindData> bindFunc(ScalarBindFuncInput input) {
+    auto scalarFunction = ku_dynamic_cast<ScalarFunction*>(input.definition);
+    const auto& resultType = ListType::getChildType(input.arguments[0]->dataType);
+    TypeUtils::visit(resultType.getPhysicalType(), [&scalarFunction]<typename T>(T) {
+        scalarFunction->execFunc =
+            ScalarFunction::UnaryExecNestedTypeFunction<list_entry_t, T, ListAnyValue>;
+    });
+    return FunctionBindData::getSimpleBindData(input.arguments, resultType);
+}
+
+function_set ListAnyValueFunction::getFunctionSet() {
+    function_set result;
+    result.push_back(
+        std::make_unique<ScalarFunction>(name, std::vector<LogicalTypeID>{LogicalTypeID::LIST},
+            LogicalTypeID::ANY, nullptr, nullptr, bindFunc));
+    return result;
+}
+
+} // namespace function
+} // namespace kuzu
---
+++ packages/kuzu/kuzu-0.7.2.dev5.tar.gz/sdist/kuzu-source/src/function/map/map_values_function.cpp
@@ -0,0 +1,27 @@
+#include "function/map/functions/map_values_function.h"
+
+#include "function/map/vector_map_functions.h"
+#include "function/scalar_function.h"
+
+using namespace kuzu::common;
+
+namespace kuzu {
+namespace function {
+
+static std::unique_ptr<FunctionBindData> bindFunc(ScalarBindFuncInput input) {
+    auto resultType = LogicalType::LIST(MapType::getValueType(input.arguments[0]->dataType).copy());
+    return FunctionBindData::getSimpleBindData(input.arguments, std::move(resultType));
+}
+
+function_set MapValuesFunctions::getFunctionSet() {
+    auto execFunc =
+        ScalarFunction::UnaryExecNestedTypeFunction<list_entry_t, list_entry_t, MapValues>;
+    function_set functionSet;
+    functionSet.push_back(
+        make_unique<ScalarFunction>(name, std::vector<LogicalTypeID>{LogicalTypeID::MAP},
+            LogicalTypeID::LIST, execFunc, nullptr, bindFunc));
+    return functionSet;
+}
+
+} // namespace function
+} // namespace kuzu
