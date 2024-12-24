0d6f394cc0bbe5dc6aae579e38f79d1c3adad929
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-24 04:04:24 +0000

    Add package omserv/0.0.0.dev189/omserv-0.0.0.dev189.tar.gz

---
+++ packages/omserv/omserv-0.0.0.dev189.tar.gz/omserv-0.0.0.dev189/omserv/nginx/configs.py
@@ -0,0 +1,69 @@
+"""
+TODO:
+ - omnibus/jmespath
+
+https://nginx.org/en/docs/dev/development_guide.html
+https://nginx.org/en/docs/dev/development_guide.html#config_directives
+https://nginx.org/en/docs/example.html
+
+https://github.com/yandex/gixy
+"""
+import collections.abc
+import dataclasses as dc
+import typing as ta
+
+from omlish import check
+from omlish import lang
+from omlish.text.indent import IndentWriter
+
+
+@dc.dataclass()
+class Items(lang.Final):
+    lst: list['Item']
+
+    @classmethod
+    def of(cls, obj: ta.Any) -> 'Items':
+        if isinstance(obj, Items):
+            return obj
+        return cls([Item.of(e) for e in check.isinstance(obj, list)])
+
+
+@dc.dataclass()
+class Item(lang.Final):
+    name: str
+    args: list[str] | None = None
+    block: Items | None = None
+
+    @classmethod
+    def of(cls, obj: ta.Any) -> 'Item':
+        if isinstance(obj, Item):
+            return obj
+        args = check.isinstance(check.not_isinstance(obj, str), collections.abc.Sequence)
+        name, args = check.isinstance(args[0], str), args[1:]
+        if args and not isinstance(args[-1], str):
+            block, args = Items.of(args[-1]), args[:-1]
+        else:
+            block = None
+        return Item(name, [check.isinstance(e, str) for e in args], block=block)
+
+
+def render(wr: IndentWriter, obj: ta.Any) -> None:
+    if isinstance(obj, Item):
+        wr.write(obj.name)
+        for e in obj.args or ():
+            wr.write(' ')
+            wr.write(e)
+        if obj.block:
+            wr.write(' {\n')
+            with wr.indent():
+                render(wr, obj.block)
+            wr.write('}\n')
+        else:
+            wr.write(';\n')
+
+    elif isinstance(obj, Items):
+        for e2 in obj.lst:
+            render(wr, e2)
+
+    else:
+        raise TypeError(obj)
