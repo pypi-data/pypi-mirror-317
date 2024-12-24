d27d7fa96f6197f86ba19c9329a657c3290e95ea
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-24 03:07:46 +0000

    Add package pycityagent/2.0.0a13/pycityagent-2.0.0a13.tar.gz

---
+++ packages/pycityagent/pycityagent-2.0.0a13.tar.gz/pycityagent-2.0.0a13/pycityagent/simulation/agentgroup.py
@@ -0,0 +1,151 @@
+import asyncio
+import json
+import logging
+import uuid
+import ray
+from uuid import UUID
+from pycityagent.agent import Agent
+from pycityagent.economy.econ_client import EconomyClient
+from pycityagent.environment.simulator import Simulator
+from pycityagent.llm.llm import LLM
+from pycityagent.llm.llmconfig import LLMConfig
+from pycityagent.message import Messager
+from typing import Any
+
+@ray.remote
+class AgentGroup:
+    def __init__(self, agents: list[Agent], config: dict, exp_id: str|UUID):
+        self.agents = agents
+        self.config = config
+        self.exp_id = exp_id
+        self._uuid = uuid.uuid4()
+        self.messager = Messager(
+            hostname=config["simulator_request"]["mqtt"]["server"],
+            port=config["simulator_request"]["mqtt"]["port"],
+            username=config["simulator_request"]["mqtt"].get("username", None),
+            password=config["simulator_request"]["mqtt"].get("password", None),
+        )
+        self.initialized = False
+        self.id2agent = {}
+        # Step:1 prepare LLM client
+        llmConfig = LLMConfig(config["llm_request"])
+        logging.info("-----Creating LLM client in remote...")
+        self.llm = LLM(llmConfig)
+
+        # Step:2 prepare Simulator
+        logging.info("-----Creating Simulator in remote...")
+        self.simulator = Simulator(config["simulator_request"])
+
+        # Step:3 prepare Economy client
+        if "economy" in config["simulator_request"]:
+            logging.info("-----Creating Economy client in remote...")
+            self.economy_client = EconomyClient(
+                config["simulator_request"]["economy"]["server"]
+            )
+        else:
+            self.economy_client = None
+
+        for agent in self.agents:
+            agent.set_exp_id(self.exp_id)
+            agent.set_llm_client(self.llm)
+            agent.set_simulator(self.simulator)
+            if self.economy_client is not None:
+                agent.set_economy_client(self.economy_client)
+            agent.set_messager(self.messager)
+
+    async def init_agents(self):
+        for agent in self.agents:
+            await agent.bind_to_simulator()
+        self.id2agent = {agent._uuid: agent for agent in self.agents}
+        await self.messager.connect()
+        if self.messager.is_connected():
+            await self.messager.start_listening()
+            for agent in self.agents:
+                agent.set_messager(self.messager)
+                topic = f"exps/{self.exp_id}/agents/{agent._uuid}/agent-chat"
+                await self.messager.subscribe(topic, agent)
+                topic = f"exps/{self.exp_id}/agents/{agent._uuid}/user-chat"
+                await self.messager.subscribe(topic, agent)
+                topic = f"exps/{self.exp_id}/agents/{agent._uuid}/user-survey"
+                await self.messager.subscribe(topic, agent)
+                topic = f"exps/{self.exp_id}/agents/{agent._uuid}/gather"
+                await self.messager.subscribe(topic, agent)
+        self.initialized = True
+        self.message_dispatch_task = asyncio.create_task(self.message_dispatch())
+        
+    async def gather(self, content: str):
+        results = {}
+        for agent in self.agents:
+            results[agent._uuid] = await agent.memory.get(content)
+        return results
+
+    async def update(self, target_agent_uuid: str, target_key: str, content: Any):
+        agent = self.id2agent[target_agent_uuid]
+        await agent.memory.update(target_key, content)
+
+    async def message_dispatch(self):
+        while True:
+            if not self.messager.is_connected():
+                logging.warning("Messager is not connected. Skipping message processing.")
+
+            # Step 1: 获取消息
+            messages = await self.messager.fetch_messages()
+            logging.info(f"Group {self._uuid} received {len(messages)} messages")
+
+            # Step 2: 分发消息到对应的 Agent
+            for message in messages:
+                topic = message.topic.value
+                payload = message.payload
+
+                # 添加解码步骤，将bytes转换为str
+                if isinstance(payload, bytes):
+                    payload = payload.decode("utf-8")
+                    payload = json.loads(payload)
+
+                # 提取 agent_id（主题格式为 "exps/{exp_id}/agents/{agent_uuid}/{topic_type}"）
+                _, _, _, agent_uuid, topic_type = topic.strip("/").split("/")
+                    
+                if uuid.UUID(agent_uuid) in self.id2agent:
+                    agent = self.id2agent[uuid.UUID(agent_uuid)]
+                    # topic_type: agent-chat, user-chat, user-survey, gather
+                    if topic_type == "agent-chat":
+                        await agent.handle_agent_chat_message(payload)
+                    elif topic_type == "user-chat":
+                        await agent.handle_user_chat_message(payload)
+                    elif topic_type == "user-survey":
+                        await agent.handle_user_survey_message(payload)
+                    elif topic_type == "gather":
+                        await agent.handle_gather_message(payload)
+
+            await asyncio.sleep(1)
+
+    async def step(self):
+        if not self.initialized:
+            await self.init_agents()
+
+        tasks = [agent.run() for agent in self.agents]
+        await asyncio.gather(*tasks)
+
+    async def run(self, day: int = 1):
+        """运行模拟器
+
+        Args:
+            day: 运行天数,默认为1天
+        """
+        try:
+            # 获取开始时间
+            start_time = await self.simulator.get_time()
+            start_time = int(start_time)
+            # 计算结束时间（秒）
+            end_time = start_time + day * 24 * 3600  # 将天数转换为秒
+
+            while True:
+                current_time = await self.simulator.get_time()
+                current_time = int(current_time)
+                if current_time >= end_time:
+                    break
+                await self.step()
+
+        except Exception as e:
+            logging.error(f"模拟器运行错误: {str(e)}")
+            raise
