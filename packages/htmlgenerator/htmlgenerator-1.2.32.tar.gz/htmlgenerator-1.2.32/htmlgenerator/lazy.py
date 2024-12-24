90dcb37194b1885c5aa3a7701d6726a205fb0065
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-24 15:04:00 +0000

    Add package htmlgenerator/1.2.32/htmlgenerator-1.2.32.tar.gz

---
+++ packages/htmlgenerator/htmlgenerator-1.2.32.tar.gz/htmlgenerator-1.2.32/htmlgenerator/lazy.py
@@ -0,0 +1,194 @@
+from __future__ import annotations
+
+import inspect
+import typing
+import warnings
+
+
+def make_lazy(func):
+    """
+    Decorator which allows any function to be converted to a lazy
+    ContextFunction object if any of the passed arguments are of type Lazy. If
+    no argument is of type Lazy, the function is called directly.
+    """
+
+    def lazy_wrapper(*args, **kwargs):
+        if any(isinstance(arg, Lazy) for arg in args + tuple(kwargs.values())):
+            return F(
+                lambda c: func(
+                    *[resolve_lazy(arg, c) for arg in args],
+                    **{k: resolve_lazy(v, c) for k, v in kwargs.items()},
+                )
+            )
+        return func(*args, **kwargs)
+
+    return lazy_wrapper
+
+
+def resolve_lazy(value: typing.Any, context: dict):
+    """Shortcut to resolve a value in case it is a Lazy value"""
+
+    while isinstance(value, Lazy):
+        value = value.resolve(context)
+    return value
+
+
+def resolve_lookup(
+    context: dict, lookup: str, call_functions: bool = True
+) -> typing.Any:
+    """
+    Helper function to extract a value out of a context-dict.
+    A lookup string can access attributes, dict-keys, methods without
+    parameters and indexes by using the dot-accessor (e.g. ``person.name``)
+    This is based on the implementation of the variable lookup of the django
+    template system:
+    https://github.com/django/django/blob/master/django/template/base.py
+    """
+    current = context
+    for bit in lookup.split("."):
+        try:
+            current = current[bit]
+        except (TypeError, AttributeError, KeyError, ValueError, IndexError):
+            try:
+                current = getattr(current, bit)
+            except (TypeError, AttributeError):
+                # Reraise if the exception was raised by a @property
+                if not isinstance(current, dict) and bit in dir(current):
+                    raise
+                try:  # list-index lookup
+                    current = current[int(bit)]
+                except (
+                    IndexError,  # list index out of range
+                    ValueError,  # invalid literal for int()
+                    KeyError,  # current is a dict without `int(bit)` key
+                    TypeError,
+                ):  # unsubscriptable object
+                    return None
+                    # raise LookupError(
+                    # "Failed lookup for key " "[%s] in %r", (bit, current)
+                    # )  # missing attribute
+        if callable(current) and call_functions:
+            try:  # method call (assuming no args required)
+                current = current()
+            except TypeError:
+                signature = inspect.signature(current)  # type: ignore
+                try:
+                    signature.bind()
+                except TypeError:  # arguments *were* required
+                    # but we continue because we might use an attribute on the
+                    # object instead of calling it
+                    pass
+                else:
+                    raise
+
+    return current
+
+
+class Lazy:
+    """Lazy values will be evaluated at render time via the resolve method."""
+
+    def __deepcopy__(self, memo):
+        # deep copy not supported, sorry
+        return self
+
+    def __getattr__(self, name):
+        return ContextFunction(lambda c: getattr(self.resolve(c), name))
+
+    def __call__(self, *args, **kwargs):
+        def resolve_call(context):
+            value = self.resolve(context)
+            # value should in theory always be callable
+            # however, older versions of htmlgenerator
+            # use resolve_lookup which will try to call
+            # any attributes it encounters
+            # __callable__ marks this object as callable
+            # and will therefore be called inside resolve_lookup
+            # even though the resolved value is not callable
+            if callable(value):
+                return value(*args, **kwargs)
+            return value
+
+        return ContextFunction(resolve_call)
+
+    def __getitem__(self, name):
+        return ContextFunction(lambda c: self.resolve(c)[name])
+
+    def __iter__(self):
+        raise NotImplementedError("Can not iterate over a lazy value")
+
+    def resolve(self, context: dict) -> typing.Any:
+        raise NotImplementedError("Lazy needs to be subclassed")
+
+    def __bool__(self):
+        return F(lambda c: bool(resolve_lazy(self, c)))
+
+    def __or__(self, other):  # this is the `|` operator, but will act like `or`
+        return F(lambda c: resolve_lazy(self, c) or resolve_lazy(other, c))
+
+    def __and__(self, other):  # this is the `&` operator but will act like `and`
+        return F(lambda c: resolve_lazy(self, c) and resolve_lazy(other, c))
+
+    def __not__(self):  # this is the `not` operator
+        return F(lambda c: not resolve_lazy(self, c))
+
+
+class ContextValue(Lazy):
+    def __init__(self, value: str):
+        self.value = value
+
+    def resolve(self, context: dict) -> typing.Any:
+        if isinstance(self.value, str):
+            return resolve_lookup(context, self.value)
+        return self.value
+
+    def __repr__(self):
+        return f"{self.__class__.__name__}(value='{self.value}')"
+
+
+class ContextFunction(Lazy):
+    """Call a function a render time, usefull for calculation of more complex"""
+
+    def __init__(self, func: typing.Callable[[dict], typing.Any]):
+        assert callable(func), "ContextFunction needs to be callable"
+        self.func = func
+
+    def resolve(self, context: dict) -> typing.Any:
+        return self.func(context)
+
+    def __repr__(self):
+        return f"{self.__class__.__name__}(func='{self.func}')"
+
+
+C = ContextValue
+F = ContextFunction
+
+
+def getattr_lazy(lazyobject: Lazy, attr: str) -> F:
+    """
+    Takes a lazy object and returns a new lazy object which will
+    resolve the attribute on the object
+    """
+    warnings.warn(
+        "getattr_lazy should no longer be used. Lazy objects support "
+        "now direct access to attributes of lazy objects"
+    )
+
+    def wrapper(c):
+        ret = getattr(resolve_lazy(lazyobject, c), attr)
+        return ret() if callable(ret) else ret
+
+    return F(wrapper)
+
+
+def lazify(func):
+    """Automatically wraps any function with a ContextFunction and resolves all arguments on call"""
+
+    def lazywrapper(*args, **kwargs):
+        return F(
+            lambda c: func(
+                *[resolve_lazy(arg, c) for arg in args],
+                **{k: resolve_lazy(v, c) for k, v in kwargs.items()},
+            )
+        )
+
+    return lazywrapper
---
+++ packages/htmlgenerator/htmlgenerator-1.2.32.tar.gz/htmlgenerator-1.2.32/setup.py
@@ -0,0 +1,45 @@
+from setuptools import find_packages, setup  # type: ignore
+
+with open("README.md") as f:
+    long_description = f.read()
+
+with open("htmlgenerator/__init__.py") as f:
+    # magic n stuff
+    version = (
+        [i for i in f.readlines() if "__version__" in i][-1]
+        .split("=", 1)[1]
+        .strip()
+        .strip('"')
+    )
+
+setup(
+    name="htmlgenerator",
+    description="Declarative HTML templating system with lazy rendering",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/basxsoftwareassociation/htmlgenerator",
+    author="basx Software Association",
+    author_email="sam@basx.dev",
+    version=version,
+    license="New BSD License",
+    packages=find_packages(),
+    zip_safe=False,
+    include_package_data=True,
+    extras_require={
+        "all": ["black", "beautifulsoup4", "lxml;platform_system!='Windows'"]
+    },
+    entry_points={
+        "console_scripts": [
+            "convertfromhtml = htmlgenerator.contrib.convertfromhtml:main",
+        ],
+    },
+    classifiers=[
+        "Development Status :: 4 - Beta",
+        "Environment :: Console",
+        "Environment :: Web Environment",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: BSD License",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 3 :: Only",
+    ],
+)
