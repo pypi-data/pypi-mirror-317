7d3327a0c9601c9f499e779117ceba53143f7a16
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-24 05:03:35 +0000

    Add package airflow-provider-sap-hana/1.0.0/airflow_provider_sap_hana-1.0.0-py3-none-any.whl

---
+++ packages/airflow-provider-sap-hana/airflow_provider_sap_hana-1.0.0-py3-none-any.whl/airflow_provider_sap_hana/__init__.py
@@ -0,0 +1,14 @@
+__version__ = "1.0.0"
+
+
+def get_provider_info():
+    return {
+        "package-name": "airflow-provider-sap-hana",
+        "name": "SAP HANA Airflow Provider",
+        "description": "An Airflow provider to connect to SAP HANA",
+        "connection-types": [{
+            "connection-type": "hana",
+            "hook-class-name": "airflow_provider_sap_hana.hooks.hana.SapHanaHook",
+        }],
+        "versions": [__version__]
+    }
