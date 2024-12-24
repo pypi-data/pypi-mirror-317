c2f3ba7f4510acea241c10b1f1a7d32ea4a9f5e0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-24 04:04:08 +0000

    Add package loclx-colab/0.0.6/loclx_colab-0.0.6.tar.gz

---
+++ packages/loclx-colab/loclx_colab-0.0.6.tar.gz/loclx_colab-0.0.6/loclx_colab/__init__.py
@@ -0,0 +1,6 @@
+from .loclx import http_tunnel,login
+from .utils2 import is_loclx_installed, install_deb_package,create_config_file
+
+
+# You can specify which functions should be available when someone imports your package
+# __all__ = ['http_tunnel', 'login', 'is_loclx_installed', 'install_deb_package', 'create_config_file']
---
+++ packages/loclx-colab/loclx_colab-0.0.6.tar.gz/loclx_colab-0.0.6/tests/test.py
@@ -0,0 +1,4 @@
+
+from loclx_colab import http_tunnel,login
+
+http_tunnel(port=1234,access_token="8jfZMBrtwnZXjZwrujAzU7jBkTYxIhunXwSmflDy")
