906084f38e02e84765e3311ed2b5fd34f5c14ef5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-24 05:04:15 +0000

    Add package mynewlib1/1.0.15/mynewlib1-1.0.15-py3-none-any.whl

---
+++ packages/mynewlib1/mynewlib1-1.0.15-py3-none-any.whl/mynewlib1/__init__.py
@@ -0,0 +1,9 @@
+
+from mini_math.pi.pi_collections import *
+
+
+from math.operations import *
+
+
+def help_mi():
+    print("эта библиотека состоит из двух модулей, 1-все вычисления с факториалами(дискретная математика), 2 модуль считает значение строки")
