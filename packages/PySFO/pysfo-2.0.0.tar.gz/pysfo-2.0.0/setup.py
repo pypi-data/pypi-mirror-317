6050ce3c339e4a10e209461e8f8358bbb0e70e89
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-24 03:07:28 +0000

    Add package PySFO/2.0.0/pysfo-2.0.0.tar.gz

---
+++ packages/PySFO/pysfo-2.0.0.tar.gz/pysfo-2.0.0/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="PySFO",
+    version="2.0.0",
+    author="FlipFlapsRU",
+    description="Get SFO!",
+    packages=find_packages(),
+    python_requires='>=3.7',
+)
