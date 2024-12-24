6cb94401a96a2d461036b325dc2db2cc2c07649e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-24 16:04:38 +0000

    Add package fuando-example-stainless/0.1.0a1/fuando_example_stainless-0.1.0a1-py3-none-any.whl

---
+++ packages/fuando-example-stainless/fuando_example_stainless-0.1.0a1-py3-none-any.whl/example_stainless/types/pet_update_params.py
@@ -0,0 +1,34 @@
+# File generated from our OpenAPI spec by Stainless. See CONTRIBUTING.md for details.
+
+from __future__ import annotations
+
+from typing import List, Iterable
+from typing_extensions import Literal, Required, Annotated, TypedDict
+
+from .._utils import PropertyInfo
+from .category_param import CategoryParam
+
+__all__ = ["PetUpdateParams", "Tag"]
+
+
+class PetUpdateParams(TypedDict, total=False):
+    name: Required[str]
+
+    photo_urls: Required[Annotated[List[str], PropertyInfo(alias="photoUrls")]]
+
+    id: int
+
+    category: CategoryParam
+
+    first: str
+
+    status: Literal["available", "pending", "sold"]
+    """pet status in the store"""
+
+    tags: Iterable[Tag]
+
+
+class Tag(TypedDict, total=False):
+    id: int
+
+    name: str
