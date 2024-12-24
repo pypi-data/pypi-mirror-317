6498e5f2500ac6c952eeca915b5bc0cfa1375176
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-24 16:04:43 +0000

    Add package test_locatr/0.1.0/test_locatr-0.1.0-py3-none-any.whl

---
+++ packages/test_locatr/test_locatr-0.1.0-py3-none-any.whl/python_client/__init__.py
@@ -0,0 +1,9 @@
+from python_client.exceptions import *
+from python_client.schema import (
+    LocatrSeleniumSettings,
+    LocatrCdpSettings,
+    PluginType,
+    LlmSettings,
+    LlmProvider,
+)
+from python_client.locatr import Locatr
