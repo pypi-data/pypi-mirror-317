a12c8a4ae13ac05faaa530b232d99497f549b9d2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-24 04:04:33 +0000

    Add package skytemple-rust/1.8.5/skytemple_rust-1.8.5-cp312-cp312-macosx_11_0_arm64.whl

---
+++ packages/skytemple-rust/skytemple_rust-1.8.5-cp312-cp312-macosx_11_0_arm64.whl/skytemple_rust/st_bpl.pyi
@@ -0,0 +1,42 @@
+#  Copyright 2021-2022 Capypara and the SkyTemple Contributors
+#
+#  This file is part of SkyTemple.
+#
+#  SkyTemple is free software: you can redistribute it and/or modify
+#  it under the terms of the GNU General Public License as published by
+#  the Free Software Foundation, either version 3 of the License, or
+#  (at your option) any later version.
+#
+#  SkyTemple is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU General Public License for more details.
+#
+#  You should have received a copy of the GNU General Public License
+#  along with SkyTemple.  If not, see <https://www.gnu.org/licenses/>.
+from range_typed_integers import u16
+from typing import List, Sequence
+
+class BplAnimationSpec:
+    duration_per_frame: u16
+    number_of_frames: u16
+
+    def __init__(self, duration_per_frame: u16, number_of_frames: u16): ...
+
+class Bpl:
+    number_palettes: u16
+    has_palette_animation: bool
+    palettes: Sequence[Sequence[int]]
+    animation_specs: Sequence[BplAnimationSpec]
+    animation_palette: Sequence[Sequence[int]]
+
+    def __init__(self, data: bytes) -> None: ...
+    def import_palettes(self, palettes: List[List[int]]) -> None: ...
+    def apply_palette_animations(self, frame: int) -> List[List[int]]: ...
+    def is_palette_affected_by_animation(self, pal_idx: int) -> bool: ...
+    def get_real_palettes(self) -> List[List[int]]: ...
+    def set_palettes(self, palettes: List[List[int]]) -> None: ...
+
+class BplWriter:
+    def __new__(cls): ...
+    def write(self, model: Bpl) -> bytes: ...
