443b1ff4254bbe53828daee75faba8b5c1fefefc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-24 16:04:29 +0000

    Add package pyglossary/5.0.2/pyglossary-5.0.2.tar.gz

---
+++ packages/pyglossary/pyglossary-5.0.2.tar.gz/pyglossary-5.0.2/pyglossary/sort_keys_types.py
@@ -0,0 +1,52 @@
+from __future__ import annotations
+
+from collections.abc import Callable
+from typing import TYPE_CHECKING, Any, Protocol, TypeAlias
+
+if TYPE_CHECKING:
+	from pyglossary.icu_types import T_Collator
+
+SortKeyType: TypeAlias = Callable[
+	[list[str]],
+	Any,
+]
+
+SQLiteSortKeyType: TypeAlias = list[tuple[str, str, SortKeyType]]
+
+
+class SortKeyMakerType(Protocol):
+	def __call__(
+		self,
+		sortEncoding: str = "utf-8",
+		**kwargs,  # noqa: ANN003
+	) -> SortKeyType: ...
+
+
+class SQLiteSortKeyMakerType(Protocol):
+	def __call__(
+		self,
+		sortEncoding: str = "utf-8",
+		**kwargs,  # noqa: ANN003
+	) -> SQLiteSortKeyType: ...
+
+
+class LocaleSortKeyMakerType(Protocol):
+	def __call__(
+		self,
+		collator: T_Collator,  # noqa: F821
+	) -> SortKeyMakerType: ...
+
+
+class LocaleSQLiteSortKeyMakerType(Protocol):
+	def __call__(
+		self,
+		collator: T_Collator,  # noqa: F821
+	) -> SQLiteSortKeyMakerType: ...
+
+
+__all__ = [
+	"LocaleSortKeyMakerType",
+	"SQLiteSortKeyType",
+	"SortKeyMakerType",
+	"SortKeyType",
+]
