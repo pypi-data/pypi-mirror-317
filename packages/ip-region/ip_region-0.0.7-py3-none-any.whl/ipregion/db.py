18ace0c7d6e09aa9bf4de541bf7301363bff1908
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-24 14:03:45 +0000

    Add package ip-region/0.0.7/ip_region-0.0.7-py3-none-any.whl

---
+++ packages/ip-region/ip_region-0.0.7-py3-none-any.whl/ipregion/db.py
@@ -0,0 +1,22 @@
+import sqlite3, os
+# 这里需要添加一个 如果没有缓存数据库，将空数据库重命名为
+class Db:
+    def __init__(self, db_file = "ipcache.db3"):
+        # 当前路径
+        cur_path = os.path.abspath(os.path.dirname(__file__))
+        # 数据库路径
+        db_path = os.path.join(cur_path, db_file)
+        # 示例数据库路径
+        example_db_path = os.path.join(cur_path, "ipcache.sample.db3")
+        # 如果没有缓存数据库，将空数据库重命名为
+        if not os.path.exists(db_path):
+            os.rename(example_db_path, db_path)
+        self.conn = sqlite3.connect(db_path, check_same_thread = False)
+        self.cursor = self.conn.cursor()
+    def query(self, query, args=(), one=False):
+        cur = self.cursor.execute(query, args)
+        rv = [dict((cur.description[idx][0], value)
+                    for idx, value in enumerate(row)) for row in cur.fetchall()]
+        return (rv[0] if rv else None) if one else rv
+    def commit(self):
+        self.conn.commit()
