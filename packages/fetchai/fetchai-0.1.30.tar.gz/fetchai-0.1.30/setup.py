7b72666e828edb884292ecb85d97fd1485f9b60d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-24 06:03:42 +0000

    Add package fetchai/0.1.30/fetchai-0.1.30.tar.gz

---
+++ packages/fetchai/fetchai-0.1.30.tar.gz/fetchai-0.1.30/setup.py
@@ -0,0 +1,40 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="fetchai",
+    version="0.1.30",
+    packages=find_packages(),  # Automatically find all packages in the folder
+    install_requires=[
+        "bech32>=1.2.0,<2.0",
+        "ecdsa>=0.19.0,<1.0",
+        "pydantic>=2.7.4,<3.0",
+        "requests>=2.32.3,<3.0",
+        "httpx>=0.23.0,<1.0",
+        "mnemonic>=0.21",
+        "click>=8.1.2,<9.0",
+        "python-dotenv>=1.0.1",
+    ],
+    entry_points={
+        "console_scripts": [
+            "fetchai-cli = fetchai.cli:cli"  # Link to the main `cli` function in cli.py
+        ]
+    },
+    extras_require={
+        "dev": [
+            "black",
+        ],
+    },
+    description="Find the right AI at the right time and register your AI to be discovered.",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    url="https://github.com/flockx-official/fetchai",
+    author="Devon Bleibtrey",
+    author_email="bleib1dj@gmail.com",
+    license="MIT",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.8",
+)
