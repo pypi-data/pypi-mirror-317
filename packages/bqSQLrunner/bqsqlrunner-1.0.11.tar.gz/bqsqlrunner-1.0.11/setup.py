e2aa2356631b0bae34ed083a2361453b776a40b1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-24 03:07:44 +0000

    Add package bqSQLrunner/1.0.11/bqsqlrunner-1.0.11.tar.gz

---
+++ packages/bqSQLrunner/bqsqlrunner-1.0.11.tar.gz/bqsqlrunner-1.0.11/setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="bqSQLrunner",  # Name of your framework
+    version="1.0.11",  # Version of your framework
+    author="Martin Birkholz / Iulian",
+    author_email="iulian.glavan@metro.digital",
+    description="A Python framework for running SQL steps on BigQuery, with support for variables and dependencies. Developed by Martin Birkholz and maintained by Iulian.",
+    packages=find_packages(),  # Automatically finds `framework` as a package
+    include_package_data=True,
+    install_requires=[
+        "google-cloud-bigquery>=2.0.0",
+    ],
+    python_requires=">=3.7",
+)
+ 
