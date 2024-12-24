44a2b8e9912d9a4e7c84e65427bf7b47ac4539bc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-24 04:03:55 +0000

    Add package bblocks-data-importers/0.2.0/bblocks_data_importers-0.2.0.tar.gz

---
+++ packages/bblocks-data-importers/bblocks_data_importers-0.2.0.tar.gz/bblocks_data_importers-0.2.0/src/bblocks_data_importers/__init__.py
@@ -0,0 +1,10 @@
+# read version from installed package
+from importlib.metadata import version
+
+from bblocks_data_importers.who.ghed import GHED
+from bblocks_data_importers.imf.weo import WEO
+from bblocks_data_importers.wfp.wfp import WFPFoodSecurity, WFPInflation
+from bblocks_data_importers.world_bank.wb_api import WorldBank
+from bblocks_data_importers.world_bank.ids import InternationalDebtStatistics
+
+__version__ = version("bblocks_data_importers")
