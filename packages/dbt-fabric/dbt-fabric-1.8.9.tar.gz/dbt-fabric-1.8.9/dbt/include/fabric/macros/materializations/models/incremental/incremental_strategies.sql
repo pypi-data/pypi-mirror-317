507821d76d2f75b68e2e8ccd58a9d4a224b623cc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-24 14:03:27 +0000

    Add package dbt-fabric/1.8.9/dbt-fabric-1.8.9.tar.gz

---
+++ packages/dbt-fabric/dbt-fabric-1.8.9.tar.gz/dbt-fabric-1.8.9/dbt/include/fabric/macros/materializations/models/incremental/incremental_strategies.sql
@@ -0,0 +1,10 @@
+{% macro fabric__get_incremental_default_sql(arg_dict) %}
+
+    {% if arg_dict["unique_key"] %}
+        {% do return(get_incremental_delete_insert_sql(arg_dict)) %}
+    {% else %}
+        -- Incremental Append will insert data into target table.
+        {% do return(get_incremental_append_sql(arg_dict)) %}
+    {% endif %}
+
+{% endmacro %}
