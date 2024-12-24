3e161d17d1ec2224a1707727d3d6133e2a928f98
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-24 05:04:41 +0000

    Add package gradio-webrtc/0.0.27/gradio_webrtc-0.0.27-py3-none-any.whl

---
+++ packages/gradio-webrtc/gradio_webrtc-0.0.27-py3-none-any.whl/gradio_webrtc/__init__.py
@@ -0,0 +1,42 @@
+from .credentials import (
+    get_hf_turn_credentials,
+    get_turn_credentials,
+    get_twilio_turn_credentials,
+)
+from .reply_on_pause import AlgoOptions, ReplyOnPause, SileroVadOptions
+from .reply_on_stopwords import ReplyOnStopWords
+from .speech_to_text import stt, stt_for_chunks
+from .utils import (
+    AdditionalOutputs,
+    Warning,
+    WebRTCError,
+    aggregate_bytes_to_16bit,
+    async_aggregate_bytes_to_16bit,
+    audio_to_bytes,
+    audio_to_file,
+    audio_to_float32,
+)
+from .webrtc import AsyncStreamHandler, StreamHandler, WebRTC
+
+__all__ = [
+    "AsyncStreamHandler",
+    "AlgoOptions",
+    "AdditionalOutputs",
+    "aggregate_bytes_to_16bit",
+    "async_aggregate_bytes_to_16bit",
+    "audio_to_bytes",
+    "audio_to_file",
+    "audio_to_float32",
+    "get_hf_turn_credentials",
+    "get_twilio_turn_credentials",
+    "get_turn_credentials",
+    "ReplyOnPause",
+    "ReplyOnStopWords",
+    "SileroVadOptions",
+    "stt",
+    "stt_for_chunks",
+    "StreamHandler",
+    "WebRTC",
+    "WebRTCError",
+    "Warning",
+]
