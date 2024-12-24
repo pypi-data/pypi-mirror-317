91a709d171a0d9fa8a0de958b2b44a13e2f487de
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-24 04:04:11 +0000

    Add package mistaker/0.3.2/mistaker-0.3.2-py3-none-any.whl

---
+++ packages/mistaker/mistaker-0.3.2-py3-none-any.whl/mistaker/__init__.py
@@ -0,0 +1,11 @@
+from .word import Word
+from .name import Name
+from .date import Date
+from .number import Number
+from .address import Address
+from .generator import Generator
+from .constants import ErrorType
+
+__version__ = "0.3.2"
+
+__all__ = ["Word", "Name", "Date", "Number", "Address", "Generator", "ErrorType"]
