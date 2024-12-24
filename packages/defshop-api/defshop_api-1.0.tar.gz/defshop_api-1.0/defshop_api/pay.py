16ef524789c69174890a492e0857e805bce86cdf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-24 15:04:00 +0000

    Add package defshop-api/1.0/defshop_api-1.0.tar.gz

---
+++ packages/defshop-api/defshop_api-1.0.tar.gz/defshop_api-1.0/defshop_api/pay.py
@@ -0,0 +1,15 @@
+import requests
+
+def send_bill(owner_id, name, summ):
+    url = 'http://194.87.94.28:81/add_bill'
+    payload = {
+        'owner_id': owner_id,
+        'name': name,
+        'sum': summ
+    }
+    response = requests.post(url, json=payload)
+    if response.status_code == 200:
+        data = response.json()
+        return data['link']
+    else:
+        print("Ошибка при отправке запроса:", response.status_code)
