ac6ca77b1b0b176262b81a4e4c8a1f7926deb287
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-24 03:07:18 +0000

    Add package at-common-functions/1.0.7/at_common_functions-1.0.7-py3-none-any.whl

---
+++ packages/at-common-functions/at_common_functions-1.0.7-py3-none-any.whl/at_common_functions/example.py
@@ -0,0 +1,8 @@
+def echo(*, in_msg: str) -> str:
+    return in_msg
+
+def reverse(*, in_msg: str) -> str:
+    return in_msg[::-1]
+
+def add_integers(*, num1: int, num2: int) -> int:
+    return num1 + num2
