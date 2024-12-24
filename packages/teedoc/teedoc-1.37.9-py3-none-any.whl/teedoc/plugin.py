40765185fe23617413743775bef20b3ab70e70f4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-24 16:04:10 +0000

    Add package teedoc/1.37.9/teedoc-1.37.9-py3-none-any.whl

---
+++ packages/teedoc/teedoc-1.37.9-py3-none-any.whl/teedoc/plugin.py
@@ -0,0 +1,294 @@
+import os
+import tempfile
+
+class Plugin_Base:
+    '''
+        call sequence:
+            __init__
+            on_init
+                (for parse in docs/pages/blog)
+                on_parse_start
+                on_add_html_header_items
+                on_add_html_footer_js_items
+                on_html_template
+                on_html_template_i18n_dir
+                    (new multiprocess or threads)
+                    on_new_process_init (only multiprocess)
+                    on_parse_files / on_parse_pages / on_parse_blog
+                    on_js_vars
+                    on_add_navbar_items
+                    on_render_vars
+                    on_new_process_del (only multiprocess)
+                on_parse_end
+            on_htmls
+            on_copy_files
+            on_del
+            __del__
+    '''
+    name = "markdown-plugin"
+    desc = "markdown plugin for teedoc"
+    defautl_config = {
+    }
+
+    def __init__(self, config, doc_src_path, site_config, logger = None, multiprocess = True, **kw_args):
+        '''
+            @attention DO NOT overwrite this function, if have to, please call super().__init__() first
+            @config a dict object
+            @logger teedoc.logger.Logger object
+            @multiprocess multiple process mode is enabled or not,
+                          in multiple process mode, all vars have a copy,
+                          but thread mode them share the memory, so, be careful to use variables in new threads or new process
+        '''
+        self._pid = os.getpid()
+        self.on_init(config, doc_src_path, site_config, logger, multiprocess = multiprocess, **kw_args)
+
+    def on_init(self, config, doc_src_path, site_config, logger, multiprocess = True, **kw_args):
+        '''
+            @config a dict object
+            @logger teedoc.logger.Logger object
+            @multiprocess multiple process mode is enabled or not,
+                          in multiple process mode, all vars have a copy,
+                          but thread mode them share the memory, so, be careful to use variables in new threads or new process
+        '''
+        pass
+
+    def on_parse_start(self, type_name, url, dirs, doc_config, new_config):
+        '''
+            call when start parse one doc
+            @type_name canbe "doc" "page" "blog"
+            #url doc url, e.g. /get_started/zh/
+            @doc_config config of doc, get from config.json or config.yaml
+            @new_config this plugin's config from doc_config
+        '''
+        # can update plugin config from site_config with new_config by teedoc.utils.update_config
+        # self.new_config = copy.deepcopy(self.config)
+        # self.new_config = update_config(self.new_config, new_config)
+        pass
+
+    def on_parse_end(self):
+        pass
+
+    def on_copy_files(self):
+        '''
+            copy file to out directory, when file changes, it also will be called
+            @return dict object, keyword is url, value is file path
+                    {
+                        "/static/css/theme-default.css": "{}/theme-default.css".format(assets_abs_path)
+                    }
+                    how to get assets_abs_path see teedoc-plugin-theme-default
+        '''
+        return {}
+
+    def on_htmls(self, htmls_files, htmls_pages, htmls_blog=None):
+        '''
+            update htmls, may not all html, just partially, DO NOT change params' value, read only, or will lead to other plugins error
+            htmls_files: {
+                "/get_started/zh":{
+                    "url":{
+                                "title": "",
+                                "desc": "",
+                                "keywords": [],
+                                "body": html,
+                                "url": "",
+                                "raw": ""
+                          }
+                }
+            }
+            htmls_blog: {
+                "/blog/":{
+                    "url":{
+                                "title": "",
+                                "desc": "",
+                                "keywords": [],
+                                "body": html,
+                                "tags": [],
+                                "url": "",
+                                "raw": "",
+                                "date": date,
+                                "ts": 12344566,
+                                "author": author,
+                                "brief": "",
+                                "metadata": {}
+                          }
+                }
+            }
+        '''
+        return True
+
+    def __del__(self):
+        # DO NOT implement this function, use on_end() instead !!!!!! this functioin may be called multi times
+        if os.getpid() == self._pid:
+            self.on_del()
+
+    def on_del(self):
+        pass
+
+    def on_html_template(self, type_name):
+        '''
+            call when render html
+            @type_name canbe "doc" "page" "blog"
+            @return None or template file abs path,
+                    and teedoc will auto search templates in the dir of this file
+        '''
+        return None
+
+    def on_add_html_header_items(self, type_name):
+        '''
+            add js(/or other tags) items to head tag, should be a html tag, e.g.
+                <link rel="stylesheet" href="..." type="text/css"/>
+                <script src="..."></script>
+            @type_name canbe "doc" "page" "blog"
+        '''
+        return []
+    
+    def on_add_html_footer_js_items(self, type_name):
+        '''
+            add js(/or other tags) items to end of body, should be absolute file path or a html tag or a dict object, e.g.
+                ['<script src="..."></script>', '/home/abc/test.js', {
+                    "path": "/home/abc/test.js",
+                    "options": ["async"]
+                }]
+            @type_name canbe "doc" "page" "blog"
+        '''
+        return []
+
+    def on_html_template_i18n_dir(self, type_name):
+        '''
+            html template's i18n dir, dir generated by:
+              * babel(http://babel.pocoo.org/en/latest/cmdline.html)
+              * gettext tool like official https://www.gnu.org/software/gettext/
+              * others like powdit(https://github.com/vslavik/poedit)
+            dir structure should be like:
+            ```
+                locales
+                    ├── messages.pot
+                    ├── zh_CN
+                    │   └── LC_MESSAGES
+                    │       └── messages.po
+                    └── zh_TW
+                        └── LC_MESSAGES
+                            └── messages.po
+            ```
+            generate by babel for example:
+            ```
+                mkdir -p locales
+                pybabel extract -F babel.cfg -o locales/messages.pot ./
+                pybabel init -i locales/messages.pot -d locales -l zh_CN
+                pybabel compile -d locales -l zh_CN
+            ```
+            babel.cfg:
+            ```
+                [python: **.py]
+
+                [jinja2: **.html]
+                extensions=jinja2.ext.i18n
+            ```
+            @type_name canbe "doc" "page" "blog"
+            @return i18n translate dir, default locales relative to plugin dir
+        '''
+        return os.path.join(self.module_path, "locales") # self.module_path set when object created
+
+
+    ############### run in new process start ######################
+
+    def on_new_process_init(self):
+        '''
+            for multiple processing, for below func, will be called in new process,
+            every time create a new process, this func will be invoke
+            @attention only call in multiple process mode on, thread mode not call this func
+                        in multiple process mode, all vars have a copy, but thread mode them share the memory, so, be careful
+        '''
+        pass
+
+    def on_new_process_del(self):
+        '''
+            for multiple processing, for below func, will be called in new process,
+            every time exit a new process, this func will be invoke
+            @attention only call in multiple process mode on, thread mode not call this func
+                        in multiple process mode, all vars have a copy, but thread mode them share the memory, so, be careful
+        '''
+        pass
+        
+
+    def on_parse_files(self, files):
+        '''
+            result = {
+                "ok": False,
+                "msg": "",
+                "htmls": {
+                    "file1_path": {
+                                    "title": "",
+                                    "desc": "",
+                                    "keywords": [],
+                                    "tags": [],
+                                    "body": "",
+                                    "toc": "", # just empty, toc generated by js but not python
+                                    "metadata": {
+                                        "title":"",
+                                        "keywords": "1, 2, 3",
+                                        "desc": "",
+                                        "tags": "1, 2, 3",
+                                        "id": "",
+                                        "class": ", , , ",
+                                        "show_source": "true"
+                                        },
+                                    "raw": content
+                                 }
+            }
+        '''
+        # result = {
+        #     "ok": False,
+        #     "msg": "",
+        #     "htmls": OrderedDict()
+        # }
+        return None
+    
+    def on_parse_pages(self, pages):
+        return None
+
+    def on_parse_blog(self, pages):
+        return None
+
+    def on_add_navbar_items(self):
+        '''
+            @return list items(navbar item, e.g. "<a href=></a>")
+        '''
+        return []
+
+    def on_js_vars(self):
+        '''
+            return the vars you wish store in head tag of html,
+            to use these vars in your js, e.g. `var conf=js_vars["teedoc-plugin-ad-hint"];`
+        '''
+        return {}
+
+    def on_render_vars(self, vars : dict):
+        '''
+            call when render html page by jinja2
+            @vars  dict object, you can add variables or change variables in this callback
+            @return dict object
+        '''
+        return vars
+
+    ############### run in new process end ######################
+
+    ####################### utils ###############################
+    def update_file_var(self, files, vars, temp_dir):
+        for url, path in files.items():
+            with open(path, encoding='utf-8') as f:
+                content = f.read()
+                for k, v in vars.items():
+                    if v is None:
+                        v = "null"
+                    content = content.replace("${}{}{}".format("{", k.strip(), "}"), str(v))
+                temp_path = os.path.join(temp_dir, os.path.basename(path))
+                with open(temp_path, "w", encoding='utf-8') as fw:
+                    fw.write(content)
+                files[url] = temp_path
+        return files
+
+    def get_temp_dir(self):
+        self.temp_dir = os.path.join(tempfile.gettempdir(), self.name)
+        if not os.path.exists(self.temp_dir):
+            os.makedirs(self.temp_dir)
+        return self.temp_dir
---
+++ packages/teedoc/teedoc-1.37.9-py3-none-any.whl/teedoc/utils.py
@@ -0,0 +1,436 @@
+import re, os
+from collections import OrderedDict
+import shutil
+import requests
+import subprocess
+from datetime import datetime
+
+has_git = False
+
+def sidebar_summary2dict(content):
+    '''
+        convert gitbook summary.md to dict
+    '''
+    def parse_line(line, start=""):
+        match = re.findall(r"\[(.*)\]\((.*)\)", line, flags=re.M)
+        if not match:
+            return line, "#", "url"
+        label = match[0][0].strip()
+        url = match[0][1].strip()
+        if url.endswith(".md") or url.endswith(".ipynb"):
+            url_type = "file"
+        else:
+            url_type = "url"
+        return label, url, url_type
+
+    # remove comment
+    def re_del(c):
+        return ""
+    content = re.sub(r'<!--.*?-->', re_del, content, flags=re.M|re.S)
+    # remove h1
+    content = re.sub(r"^# .*", re_del, content, flags=re.I)
+
+    content = content.split("\n")
+
+    level_mark = None
+    sidebar_items = []
+    for i, line in enumerate(content):
+        if line.strip() == "":
+            continue
+        if level_mark is None:
+            if line.startswith("\t* ") or line.startswith("\t- "):
+                level_mark = "\t"
+            elif line.startswith(" * ") or line.startswith(" - "):
+                level_mark = " "
+            elif line.startswith("  * ") or line.startswith("  - "):
+                level_mark = "  "
+            elif line.startswith("    * ") or line.startswith("    - "):
+                level_mark = "    "
+            if not level_mark is None:
+                print("level symbol:=={}==".format(level_mark))
+        if line.startswith("## "): # label
+            item = {
+                "label": line.strip()[3:]
+            }
+            sidebar_items.append(item)
+        elif line.startswith("* ") or line.startswith("- "): # link
+            label, url, url_type = parse_line(line[2:])
+            item = {
+                "label": label
+            }
+            item[url_type] = url
+            sidebar_items.append(item)
+        elif line.startswith("{}* ".format(level_mark)) or line.startswith("{}- ".format(level_mark)): # level 2 label
+            label, url, url_type = parse_line(line[len(level_mark)+2:])
+            item = {
+                "label": label,
+                url_type: url
+            }
+            if not "items" in sidebar_items[-1]:
+                sidebar_items[-1]["items"] = []
+            sidebar_items[-1]["items"].append(item)
+        elif line.startswith("{}{}* ".format(level_mark, level_mark)) or line.startswith("{}{}- ".format(level_mark, level_mark)): # level 3 label
+            label, url, url_type = parse_line(line[len(level_mark) * 2 + 2:])
+            item = {
+                "label": label,
+                url_type: url
+            }
+            if not "items" in sidebar_items[-1]["items"][-1]:
+                sidebar_items[-1]["items"][-1]["items"] = []
+            sidebar_items[-1]["items"][-1]["items"].append(item)
+        elif line.startswith("{}{}{}* ".format(level_mark, level_mark, level_mark)) or line.startswith("{}{}{}- ".format(level_mark, level_mark, level_mark)): # level 4 label
+            label, url, url_type = parse_line(line[len(level_mark) * 3 + 2:])
+            item = {
+                "label": label,
+                url_type: url
+            }
+            if not "items" in sidebar_items[-1]["items"][-1]["items"][-1]:
+                sidebar_items[-1]["items"][-1]["items"][-1]["items"] = []
+            sidebar_items[-1]["items"][-1]["items"][-1]["items"].append(item)
+        elif line.startswith("{}{}{}{}* ".format(level_mark, level_mark, level_mark, level_mark)) or line.startswith("{}{}{}{}- ".format(level_mark, level_mark, level_mark, level_mark)): # level 5 label
+            label, url, url_type = parse_line(line[len(level_mark) * 4 + 2:])
+            item = {
+                "label": label,
+                url_type: url
+            }
+            if not "items" in sidebar_items[-1]["items"][-1]["items"][-1]["items"][-1]:
+                sidebar_items[-1]["items"][-1]["items"][-1]["items"][-1]["items"] = []
+            sidebar_items[-1]["items"][-1]["items"][-1]["items"][-1]["items"].append(item)
+    sidebar = {
+        "items": sidebar_items
+    }
+    return sidebar
+
+def update_config(old, update, level = 0, ignore=[]):
+    '''
+        update config by new config, merge a new config
+        if value of key is list and item is dict, new config will append,
+        but if dict has "id" key, it will be update, e.g.
+        ```
+            "g": [
+                {
+                    "id": "id0",
+                    "h": 123
+                },
+                {
+                    "h": 123
+                }
+            ]
+        ```
+        update with
+        ```
+            "g":[
+                {
+                    "id": "id0",
+                    "h": 789
+                },
+                {
+                    "h": 234
+                }
+            ]
+        ```
+        will be 
+        ```
+            'g': [
+                {
+                   'id': 'id0',
+                   'h': 789
+                }, {
+                   'h': 123
+                }, {
+                   'h': 234
+                }
+            ]
+        ```
+    '''
+    new = old.copy()
+    for key in update.keys():
+        if key in ignore:
+            continue
+        if not key in old:
+            new[key] = update[key]
+            continue
+        if type(update[key]) == dict:
+            new[key] = update_config(old[key], update[key], level + 1)
+        elif type(update[key]) == list and len(update[key]) > 0 and type(update[key][0]) == dict:
+            # some dict in list, add item, or replace if exist id
+            # convert list to OrderedDict
+            old_list_item = OrderedDict()
+            for i, item in enumerate(old[key]):
+                if "id" in item:
+                    old_list_item[item["id"]] = item
+                else:
+                    old_list_item[str(i)] = item
+            # update item
+            for i, item in enumerate(update[key]):
+                if "id" in item:
+                    if "id" in old_list_item and type(old_list_item[item["id"]]) == dict:
+                        old_list_item[item["id"]] = update_config(old_list_item[item["id"]], item, level + 1)
+                    else:
+                        old_list_item[item["id"]] = item
+                else:
+                    old_list_item["n{}".format(i)] = item
+            # convert back to list
+            items = []
+            for id in old_list_item:
+                items.append(old_list_item[id])
+            new[key] = items
+        else:
+            new[key] = update[key]
+    return new
+
+
+def check_sidebar_diff(a, b, urla, urlb, dira, dirb, doc_root, log):
+    '''
+        @a {
+            "file_path": {
+                "curr": (url, label),     # e.g. ('/get_started/zh/index.html', 'teedoc 简介')
+                "previous": (url, label),
+                "next": (url, label),
+            }
+        }
+        @b {
+            "file_path": {
+                "file": "sidebar file item",  # e.g. install/README.md
+                "curr": (url, label),         # e.g. ('/get_started/zh/index.html', 'teedoc 简介')
+                "previous": (url, label),
+                "next": (url, label),
+            }
+        }
+        @urla, urlb e.g. "/get_started/zh/", "/get_started/en/"
+    '''
+    for f in a:
+        w = False
+        itema = a[f]
+        k = f.replace(dira, dirb)
+        if not k in b:
+            w = True
+        else:
+            itemb = b[k]
+            if itema["file"] != itemb["file"]:
+                w = True
+        if w:
+            log.w(f'doc <{dira.replace(doc_root, "")}> and translate <{dirb.replace(doc_root, "")}> sidebar item [{itema["file"]}-"{itema["curr"][1]}"] different')
+
+
+def get_url_by_file_rel(file_path, doc_url = "", rel = False):
+    '''
+        @rel relative url
+    '''
+    url = os.path.splitext(file_path)[0]
+    tmp = os.path.split(url)
+    if tmp[1].lower() == "readme":
+        url = "{}/index".format(tmp[0])
+        if url.startswith("/"):
+            url = url[1:]
+    url = url + ".html"
+    if rel:
+        return url
+    if(doc_url.endswith("/")):
+        url = "{}{}".format(doc_url, url)
+    else:
+        url = "{}/{}".format(doc_url, url)
+    return url
+
+def get_file_path_by_url(url, doc_root, route, translate):
+    if not url.startswith("/"):
+        url = "/" + url
+    map = {}
+    for k, v in route.items():
+        if type(v) != dict:
+            raise Exception(f"site_config error, route {k}'s value should be dict")
+        for _url, _path in v.items():
+            map[_url] = _path
+    for k, v in translate.items():
+        for _url, trans in v.items():
+            for item in trans:
+                map[item["url"]] = item["src"]
+    def find_url_dir(url, map):
+        # /, /, /soft/maixpy3/, /soft/maixpy3/api/
+        url_dir = "/".join(url[:-1].split("/")[:-1]) + "/" # map: /, /, /soft/maixpy3/, /soft/maixpy3/
+        if url_dir in map:
+            return url_dir
+        if url_dir == "/":
+            raise Exception("site_config route no url '/' item")
+        url_dir = find_url_dir(url_dir, map)
+        return url_dir
+    
+    def get_src_file_path(base_name, dir):
+        suportted_ext = ["md", "ipynb"] # TODO: this should be update by plugins
+        path = None
+        for ext in suportted_ext:
+            _path = os.path.join(dir, base_name) + "." + ext
+            if os.path.exists(_path):
+                path = _path
+                break
+        if not path: # find other ext files with same file name
+            file_name = os.path.basename(base_name)
+            for name in os.listdir(os.path.dirname(os.path.join(dir, base_name))):
+                if name.lower().startswith(file_name.lower()):
+                    base_name_dir = os.path.dirname(base_name)
+                    if base_name_dir:
+                        base_name = base_name_dir + "/" + name
+                    else:
+                        base_name = name
+                    path = os.path.join(dir, base_name)
+                    break
+        if path:
+            path = path.replace("\\", "/")
+        return path
+
+    if url in map: # /, /maixpy/, /soft/maixpy3/
+        path = get_src_file_path("index", map[url][1])
+        if not path:
+            path = get_src_file_path("readme", map[url][1])
+    else: # /index.html, /maixpy, /soft/maixpy3/start.html, /soft/maixpy3/api/start.html
+        url_dir = find_url_dir(url, map)
+        dir = map[url_dir][1]
+        base_name = url.replace(url_dir, "") # index.html, maixpy, start.html, api/start.html
+        base_name = os.path.splitext(base_name)[0]
+        path = get_src_file_path(base_name, dir)
+    return path
+
+
+def convert_file_tag_items(items, out_dir, plugin_name):
+    '''
+        @items list, abs path or str, or dict:{
+                    "path": "/home/abc/test.js",
+                    "options": ["async"]
+                }
+        @out_dir copy items to `save_dir/plugin_name/`
+    '''
+    new = []
+    save_dir = os.path.join(out_dir, plugin_name)
+    os.makedirs(save_dir, exist_ok=True)
+    for item in items:
+        options = []
+        if type(item) == dict:
+            path = item["path"]
+            options = item["options"]
+        else:
+            path = item
+        if os.path.exists(path):
+            name = os.path.basename(path)
+            url = f'/{plugin_name}/{name}'
+            shutil.copyfile(path, os.path.join(save_dir, name))
+            file_type = os.path.splitext(name)[1][1:]
+            if file_type == "js":
+                item = f'<script src="{url}"'
+                for opt in options:
+                    item += f' {opt}'
+                item += '></script>'
+            elif file_type == "css":
+                item = f'<link rel="stylesheet" href="{url}"'
+                for opt in options:
+                    item += f' {opt}'
+                item += 'type="text/css"/>'
+        new.append(item)
+    return new
+
+def download_file(url, save_path):
+    '''
+        @url download url
+        @save_path save file to `save_path`
+    '''
+    if not os.path.exists(save_path):
+        os.makedirs(os.path.dirname(save_path), exist_ok=True)
+    with open(save_path, "wb") as f:
+        res = requests.get(url)
+        if res.status_code != 200:
+            raise Exception("Download file: {} failed".format(url))
+        f.write(res.content)
+
+def get_file_last_modify_time(file_path, git=True):
+    last_edit_time = None
+    if has_git and git:
+        cmd = ["git", "log", "-1", "--format=%cd", "--date", "iso8601-strict", f"{file_path}"]
+        p = subprocess.Popen(cmd, stdout=subprocess.PIPE, stderr=subprocess.PIPE, shell=False)
+        output, err = p.communicate()
+        if p.returncode == 0:
+            date_str = output.decode("utf-8").strip()
+            if date_str:
+                if date_str.endswith("Z"):
+                    date_str = date_str[:-1] + "+00:00"
+                last_edit_time = datetime.fromisoformat(date_str)
+    if not last_edit_time: # this time is not accurate, just for outside of git repository's file
+        last_edit_time = datetime.fromtimestamp(os.stat(file_path).st_mtime)
+    return last_edit_time
+
+def check_git():
+    global has_git
+    cmd = ["git", "--version"]
+    try:
+        p = subprocess.Popen(cmd, stdout=subprocess.PIPE, stderr=subprocess.PIPE, shell=False)
+        output, err = p.communicate()
+        if p.returncode != 0:
+            has_git = False
+            return False
+    except Exception:
+        has_git = False
+        return False
+    has_git = True
+    return True
+
+def get_sub_dirs(dir, trans_info):
+    '''
+        @trans_info  translate info in site_config
+        dir and dirs are all absolute path
+        e.g. [{'url': '/get_started/en/', 'src': ['docs/get_started/en', '/home/neucrack/projects/teedoc/examples/local_test/docs/get_started/en']}]
+    '''
+    sub_dirs = []
+    for info in trans_info:
+        d = info["src"][1]
+        if d.startswith(dir):
+            sub_dirs.append(d)
+    return sub_dirs
+
+def find_plugin_in_dir(dir, name):
+    '''
+        @dir plugins dir
+        @name plugin name
+        @return plugin path or None
+    '''
+    for dir_name in os.listdir(dir):
+        if dir_name == name:
+            return os.path.join(dir, dir_name)
+    return None
+
+if __name__ == "__main__":
+    a = {
+        "a": 1,
+        "b": 2,
+        "c": {
+            "d": 3,
+            "e": {
+                "f": 4
+            }
+        },
+        "g": [
+            {
+                "id": "id0",
+                "h": 123
+            },
+            {
+                "h": 123
+            }
+        ]
+    }
+
+    b = {
+        "c": {
+            "e": 5
+        },
+        "a": [1, 2, 3],
+        "g":[
+            {
+                "id": "id0",
+                "h": 789
+            },
+            {
+                "h": 234
+            }
+        ]
+    }
+    c = update_config(a, b)
+    print(c)
+
