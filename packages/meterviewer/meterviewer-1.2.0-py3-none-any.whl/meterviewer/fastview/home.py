c49d139216ccdc8dacabde2373e294f0f1214f96
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-24 16:04:05 +0000

    Add package meterviewer/1.2.0/meterviewer-1.2.0-py3-none-any.whl

---
+++ packages/meterviewer/meterviewer-1.2.0-py3-none-any.whl/meterviewer/fastview/home.py
@@ -0,0 +1,13 @@
+import streamlit as st
+
+main_text = """
+This tool-sets is built for quick process meter image data.
+"""
+
+
+def main():
+  st.title("Meter data viewer")
+  st.text(main_text)
+
+
+main()
