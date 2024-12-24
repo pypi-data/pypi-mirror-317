d7f0fdb8828322404d2dd0c0637b53907f4cd22c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-24 16:04:44 +0000

    Add package lyrebird/3.1.2/lyrebird-3.1.2.tar.gz

---
+++ packages/lyrebird/lyrebird-3.1.2.tar.gz/lyrebird-3.1.2/lyrebird/mock/handlers/http_data_helper/__init__.py
@@ -0,0 +1,86 @@
+from collections import OrderedDict
+from . import content_encoding, content_type
+from lyrebird.utils import CaseInsensitiveDict
+import json
+
+origin2flow_handlers = OrderedDict({
+    'Content-Encoding': content_encoding,
+    'Content-Type': content_type
+})
+
+flow2origin_handlers = OrderedDict({
+    'Content-Type': content_type,
+    'Content-Encoding': content_encoding
+})
+
+
+class DataHelper:
+
+    @staticmethod
+    def origin2flow(origin_obj, output=None, chain=None):
+        if not origin_obj:
+            return
+
+        _data = origin_obj.data
+        if not _data:
+            return
+
+        # Read raw headers, support the request from extra mock 9999 port
+        if 'Proxy-Raw-Headers' in origin_obj.headers:
+            _origin_headers = json.loads(origin_obj.headers['Proxy-Raw-Headers'])
+            raw_headers = CaseInsensitiveDict(_origin_headers)
+        else:
+            raw_headers = origin_obj.headers
+
+        for headers_key, func in origin2flow_handlers.items():
+            headers_val = raw_headers.get(headers_key, '')
+            _data = func.origin2flow(headers_val, _data, chain=chain)
+
+        if output:
+            output['data'] = _data
+        else:
+            return _data
+
+    @staticmethod
+    def flow2origin(flow_obj, output=None, chain=None):
+        _data = flow_obj.get('data')
+        if _data is None:
+            return
+
+        if chain:
+            for func in chain[::-1]:
+                _data = func.flow2origin(_data)
+        else:
+            for headers_key, func in flow2origin_handlers.items():
+                headers_val = flow_obj['headers'].get(headers_key, '')
+                _data = func.flow2origin(headers_val, _data)
+
+        if output:
+            output.data = _data
+        else:
+            return _data
+
+    @staticmethod
+    def origin2string(origin_obj, output=None):
+        if not origin_obj:
+            return
+
+        _data = origin_obj.data
+        if not _data:
+            return
+
+        # Read raw headers, support the request from extra mock 9999 port
+        if 'Proxy-Raw-Headers' in origin_obj.headers:
+            _origin_headers = json.loads(origin_obj.headers['Proxy-Raw-Headers'])
+            raw_headers = CaseInsensitiveDict(_origin_headers)
+        else:
+            raw_headers = origin_obj.headers
+
+        for headers_key, func in origin2flow_handlers.items():
+            headers_val = raw_headers.get(headers_key, '')
+            _data = func.origin2string(headers_val, _data)
+
+        if output:
+            output['data'] = _data
+        else:
+            return _data
---
+++ packages/lyrebird/lyrebird-3.1.2.tar.gz/lyrebird-3.1.2/setup.py
@@ -0,0 +1,52 @@
+import runpy
+import os
+from setuptools import setup, find_packages
+
+here = os.path.abspath(os.path.dirname(__file__))
+
+VERSION = runpy.run_path(
+    os.path.join(here, "lyrebird", "version.py")
+)["VERSION"]
+
+
+def read_requirements(name):
+    with open(os.path.join(here, name), encoding='utf-8') as f:
+        require_str = f.read()
+        return require_str.split()
+
+
+with open(os.path.join(here, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
+
+
+setup(
+    name='lyrebird',
+    version=VERSION,
+    packages=['lyrebird'],
+    url='https://github.com/meituan/lyrebird',
+    author='HBQA',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    include_package_data=True,
+    zip_safe=False,
+    classifiers=[
+        "Programming Language :: Python :: 3 :: Only",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+        "Programming Language :: Python :: 3.11",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: MacOS",
+        "Operating System :: Microsoft :: Windows",
+    ],
+    entry_points={
+        'console_scripts': [
+            'lyrebird = lyrebird.manager:main'
+        ]
+    },
+    install_requires=read_requirements('requirements.txt.lock'),
+    extras_require={
+        'dev': read_requirements('requirements.dev.txt')
+    }
+)
