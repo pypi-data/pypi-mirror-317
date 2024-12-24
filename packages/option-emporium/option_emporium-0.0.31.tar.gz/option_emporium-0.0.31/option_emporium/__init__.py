98b60119a89308bc9e0d459aa47f2df876a383e6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-24 04:04:20 +0000

    Add package option-emporium/0.0.31/option_emporium-0.0.31.tar.gz

---
+++ packages/option-emporium/option_emporium-0.0.31.tar.gz/option_emporium-0.0.31/option_emporium/__init__.py
@@ -0,0 +1,17 @@
+from .calendar_calculations import (
+    fc32,
+    calendar_calculations,
+    calculate_fb_spread,
+    calculate_cal_spread,
+    calculate_spreads,
+    required_column_check,
+)
+
+__all__ = [
+    "fc32",
+    "calendar_calculations",
+    "calculate_fb_spread",
+    "calculate_cal_spread",
+    "calculate_spreads",
+    "required_column_check",
+]
