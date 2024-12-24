8ab0056cfead81892d69b03f34d1b6fec0a60152
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-24 05:04:18 +0000

    Add package hatch-buildext/0.1.0/hatch_buildext-0.1.0-py3-none-any.whl

---
+++ packages/hatch-buildext/hatch_buildext-0.1.0-py3-none-any.whl/hatch_buildext/__init__.py
@@ -0,0 +1,15 @@
+# SPDX-FileCopyrightText: 2024-present tunnelworks <git@tunnelworks.org>
+#
+# SPDX-License-Identifier: Apache-2.0
+
+
+from hatch_buildext.core import Macro
+from hatch_buildext.hooks import hatch_register_build_hook
+from hatch_buildext.plugin import ExtensionBuildHook
+
+
+__all__ = (
+    "hatch_register_build_hook",
+    "ExtensionBuildHook",
+    "Macro",
+)
