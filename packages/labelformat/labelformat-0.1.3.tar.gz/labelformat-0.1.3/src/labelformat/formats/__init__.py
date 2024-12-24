a3a9112a91201f46a2d0938ab528a005ac550e02
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-24 03:07:19 +0000

    Add package labelformat/0.1.3/labelformat-0.1.3.tar.gz

---
+++ packages/labelformat/labelformat-0.1.3.tar.gz/labelformat-0.1.3/src/labelformat/formats/__init__.py
@@ -0,0 +1,37 @@
+from labelformat.formats.coco import (
+    COCOInstanceSegmentationInput,
+    COCOInstanceSegmentationOutput,
+    COCOObjectDetectionInput,
+    COCOObjectDetectionOutput,
+)
+from labelformat.formats.kitti import (
+    KittiObjectDetectionInput,
+    KittiObjectDetectionOutput,
+)
+from labelformat.formats.labelbox import LabelboxObjectDetectionInput
+from labelformat.formats.lightly import (
+    LightlyObjectDetectionInput,
+    LightlyObjectDetectionOutput,
+)
+from labelformat.formats.pascalvoc import (
+    PascalVOCObjectDetectionInput,
+    PascalVOCObjectDetectionOutput,
+)
+from labelformat.formats.yolov5 import (
+    YOLOv5ObjectDetectionInput,
+    YOLOv5ObjectDetectionOutput,
+)
+from labelformat.formats.yolov6 import (
+    YOLOv6ObjectDetectionInput,
+    YOLOv6ObjectDetectionOutput,
+)
+from labelformat.formats.yolov7 import (
+    YOLOv7ObjectDetectionInput,
+    YOLOv7ObjectDetectionOutput,
+)
+from labelformat.formats.yolov8 import (
+    YOLOv8InstanceSegmentationInput,
+    YOLOv8InstanceSegmentationOutput,
+    YOLOv8ObjectDetectionInput,
+    YOLOv8ObjectDetectionOutput,
+)
