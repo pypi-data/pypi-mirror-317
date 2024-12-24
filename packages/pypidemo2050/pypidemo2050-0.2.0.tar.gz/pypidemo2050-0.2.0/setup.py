59ed9b34389fe1cdce840e702f3f310cdf83dfdd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-24 03:07:14 +0000

    Add package pypidemo2050/0.2.0/pypidemo2050-0.2.0.tar.gz

---
+++ packages/pypidemo2050/pypidemo2050-0.2.0.tar.gz/pypidemo2050-0.2.0/setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="pypidemo2050",                      # 项目名称
+    version="0.2.0",                        # 项目版本
+    author="Leo Wang",                     # 作者名
+    author_email="your_email@example.com",  # 作者邮箱
+    description="A sample Python module",  # 简短描述
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    url="https://github.com/yourusername/my_project",  # 项目主页
+    packages=find_packages(),               # 自动发现模块
+    classifiers=[                           # 分类标签
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.11.5',                # Python 版本要求
+    install_requires=[                      # 项目依赖
+        "poetry-core",
+        "poetry.core.masonry.api"
+    ],
+)
+
+
+
