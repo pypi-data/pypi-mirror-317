ea9dd750fd8fb4f7883804c57c55a65003357d79
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-24 05:03:48 +0000

    Add package mynewlib1/1.0.17/mynewlib1-1.0.17.tar.gz

---
+++ packages/mynewlib1/mynewlib1-1.0.17.tar.gz/mynewlib1-1.0.17/setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="mynewlib1",
+    version="1.0.17",
+    test_suite='tests',
+    author="Romitsina Spelov",
+    author_email="romitsina.a.r@gmail.com",
+    description="Библиотека для работы с факториалами и вычисления чисел из формул типа str",
+    long_description=open("README.md", encoding="utf-8").read(),
+    long_description_content_type="text/markdown",
+    url="https://github.com/Anromit/mylib/tree/main/mylib",
+    packages=find_packages(),
+    license='example',
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.12",
+)
