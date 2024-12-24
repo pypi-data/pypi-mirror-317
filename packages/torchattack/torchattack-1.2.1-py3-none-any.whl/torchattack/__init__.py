084d3a48f330c1af9fce14b2b38810005b3c1256
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-24 15:03:33 +0000

    Add package torchattack/1.2.1/torchattack-1.2.1-py3-none-any.whl

---
+++ packages/torchattack/torchattack-1.2.1-py3-none-any.whl/torchattack/__init__.py
@@ -0,0 +1,111 @@
+from torchattack.admix import Admix
+from torchattack.att import ATT
+from torchattack.attack_model import AttackModel
+from torchattack.bia import BIA
+from torchattack.cda import CDA
+from torchattack.create_attack import create_attack
+from torchattack.decowa import DeCoWA
+from torchattack.deepfool import DeepFool
+from torchattack.difgsm import DIFGSM
+from torchattack.dr import DR
+from torchattack.fgsm import FGSM
+from torchattack.fia import FIA
+from torchattack.gama import GAMA
+from torchattack.geoda import GeoDA
+from torchattack.ilpd import ILPD
+from torchattack.ltp import LTP
+from torchattack.mifgsm import MIFGSM
+from torchattack.naa import NAA
+from torchattack.nifgsm import NIFGSM
+from torchattack.pgd import PGD
+from torchattack.pgdl2 import PGDL2
+from torchattack.pna_patchout import PNAPatchOut
+from torchattack.sinifgsm import SINIFGSM
+from torchattack.ssa import SSA
+from torchattack.ssp import SSP
+from torchattack.tgr import TGR
+from torchattack.tifgsm import TIFGSM
+from torchattack.vdc import VDC
+from torchattack.vmifgsm import VMIFGSM
+from torchattack.vnifgsm import VNIFGSM
+
+__version__ = '1.2.1'
+
+__all__ = [
+    # Helper function to create an attack by its name
+    'create_attack',
+    # Optional but recommended model wrapper
+    'AttackModel',
+    # All supported attacks
+    'Admix',
+    'ATT',
+    'BIA',
+    'CDA',
+    'DeCoWA',
+    'DeepFool',
+    'DIFGSM',
+    'DR',
+    'FGSM',
+    'FIA',
+    'GAMA',
+    'GeoDA',
+    'ILPD',
+    'LTP',
+    'MIFGSM',
+    'NAA',
+    'NIFGSM',
+    'PGD',
+    'PGDL2',
+    'PNAPatchOut',
+    'SINIFGSM',
+    'SSA',
+    'SSP',
+    'TGR',
+    'TIFGSM',
+    'VDC',
+    'VMIFGSM',
+    'VNIFGSM',
+]
+
+GRADIENT_NON_VIT_ATTACKS = {
+    'Admix': Admix,
+    'DeCoWA': DeCoWA,
+    'DIFGSM': DIFGSM,
+    'DR': DR,
+    'FGSM': FGSM,
+    'FIA': FIA,
+    'ILPD': ILPD,
+    'MIFGSM': MIFGSM,
+    'NAA': NAA,
+    'NIFGSM': NIFGSM,
+    'PGD': PGD,
+    'PGDL2': PGDL2,
+    'SINIFGSM': SINIFGSM,
+    'SSA': SSA,
+    'SSP': SSP,
+    'TIFGSM': TIFGSM,
+    'VMIFGSM': VMIFGSM,
+    'VNIFGSM': VNIFGSM,
+}
+GRADIENT_VIT_ATTACKS = {
+    'TGR': TGR,
+    'VDC': VDC,
+    'PNAPatchOut': PNAPatchOut,
+    'ATT': ATT,
+}
+GENERATIVE_ATTACKS = {
+    'BIA': BIA,
+    'CDA': CDA,
+    'GAMA': GAMA,
+    'LTP': LTP,
+}
+NON_EPS_ATTACKS = {
+    'GeoDA': GeoDA,
+    'DeepFool': DeepFool,
+}
+SUPPORTED_ATTACKS = (
+    GRADIENT_NON_VIT_ATTACKS
+    | GRADIENT_VIT_ATTACKS
+    | GENERATIVE_ATTACKS
+    | NON_EPS_ATTACKS
+)
---
+++ packages/torchattack/torchattack-1.2.1-py3-none-any.whl/torchattack/_attack.py
@@ -0,0 +1,98 @@
+from abc import ABC, abstractmethod
+from typing import Any, Callable
+
+import torch
+import torch.nn as nn
+
+from torchattack.attack_model import AttackModel
+
+
+class Attack(ABC):
+    """The base class for all attacks."""
+
+    def __init__(
+        self,
+        model: nn.Module | AttackModel | None,
+        normalize: Callable[[torch.Tensor], torch.Tensor] | None,
+        device: torch.device | None,
+    ) -> None:
+        super().__init__()
+
+        self.model = (
+            # If model is an AttackModel, use the model attribute
+            model.model
+            if isinstance(model, AttackModel)
+            # If model is a nn.Module, use the model itself
+            else model
+            if model is not None
+            # Otherwise, use an empty nn.Sequential acting as a dummy model
+            else nn.Sequential()
+        )
+
+        # Set device to given or defaults to cuda if available
+        is_cuda = torch.cuda.is_available()
+        self.device = device if device else torch.device('cuda' if is_cuda else 'cpu')
+
+        # If normalize is None, use identity function
+        self.normalize = normalize if normalize else lambda x: x
+
+    @abstractmethod
+    def forward(self, *args: Any, **kwds: Any) -> Any:
+        pass
+
+    def __call__(self, *args: Any, **kwds: Any) -> Any:
+        return self.forward(*args, **kwds)
+
+    def __repr__(self) -> str:
+        name = self.__class__.__name__
+
+        def repr_map(k: str, v: Any) -> str:
+            if isinstance(v, float):
+                return f'{k}={v:.3f}'
+            if k in ['model', 'normalize', 'feature_layer', 'hooks', 'generator']:
+                return f'{k}={v.__class__.__name__}'
+            if isinstance(v, torch.Tensor):
+                return f'{k}={v.shape}'
+            return f'{k}={v}'
+
+        args = ', '.join(repr_map(k, v) for k, v in self.__dict__.items())
+        return f'{name}({args})'
+
+    def __eq__(self, other: Any) -> bool:
+        if not isinstance(other, Attack):
+            return False
+
+        eq_name_attrs = [
+            'model',
+            'normalize',
+            'lossfn',
+            'feature_layer',  # FIA
+            'hooks',  # PNAPatchOut, TGR, VDC
+            'perceptual_criteria',  # SSP
+            'sub_basis',  # GeoDA
+            'generator',  # BIA, CDA
+        ]
+        for attr in eq_name_attrs:
+            if not (hasattr(self, attr) and hasattr(other, attr)):
+                continue
+            if (
+                getattr(self, attr).__class__.__name__
+                != getattr(other, attr).__class__.__name__
+            ):
+                return False
+
+        for attr in self.__dict__:
+            if attr in eq_name_attrs:
+                continue
+            self_val = getattr(self, attr)
+            other_val = getattr(other, attr)
+            
+            if isinstance(self_val, torch.Tensor):
+                if not isinstance(other_val, torch.Tensor):
+                    return False
+                if not torch.equal(self_val, other_val):
+                    return False
+            elif self_val != other_val:
+                return False
+                
+        return True
