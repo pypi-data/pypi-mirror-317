b08760596449cd02314bfe2dd89239495890179f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-24 03:08:25 +0000

    Add package ecommerceutils/1.0.0/ecommerceutils-1.0.0.tar.gz

---
+++ packages/ecommerceutils/ecommerceutils-1.0.0.tar.gz/ecommerceutils-1.0.0/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='ecommerceutils',
+    version='1.0.0',
+    packages=find_packages(),
+    install_requires=[],
+    author='Pandiselvi',
+    description='A Python package for eCommerce utilities',
+    python_requires='>=3.6',
+)
