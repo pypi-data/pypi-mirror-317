74d7e149f2ab913def20c7086dcdb30fce42a897
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-24 04:03:54 +0000

    Add package dozersim/0.0.6/dozersim-0.0.6-py3-none-any.whl

---
+++ packages/dozersim/dozersim-0.0.6-py3-none-any.whl/dozersim/sources/constant.py
@@ -0,0 +1,28 @@
+from __future__ import annotations
+
+from dataclasses import dataclass, field
+
+import numpy as np
+
+from dozersim.path.path import Path
+from dozersim.settings.settings import Settings
+from dozersim.modelling.model import Model
+
+
+@dataclass(slots=True)
+class ConstantSettings(Settings):
+    Target: str = None
+    Magnitude: float = None
+
+
+def evaluate_constant(paths: tuple[Path], constant: ConstantSettings, model: Model):
+    path, = paths
+
+    time = path.variables.time
+    profile = np.full(time.shape, constant.Magnitude)
+    path.variables.add[constant.Target](profile, model, f"{model.name}")
+
+
+class Constant(Model):
+    def __init__(self, name: str = 'constant', settings=ConstantSettings(), eval_fun=evaluate_constant):
+        super().__init__(name=name, settings=settings, eval_fun=eval_fun)
