ce0098438fb2b4ade9a07d9373d5a1753657706e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-24 06:04:07 +0000

    Add package fluctlight/0.0.1/fluctlight-0.0.1.tar.gz

---
+++ packages/fluctlight/fluctlight-0.0.1.tar.gz/fluctlight-0.0.1/setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="fluctlight",
+    version="0.0.1",
+    author="Your Name",
+    author_email="your.email@example.com",
+    description="A package template",
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    url="https://github.com/yourusername/fluctlight",
+    packages=find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.9',
+)
