4fae7e6347e3edd1bfda1b0f7369ceb65452bdca
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-24 06:04:11 +0000

    Add package jvcore/0.0.2/jvcore-0.0.2-py3-none-any.whl

---
+++ packages/jvcore/jvcore-0.0.2-py3-none-any.whl/core/reasoner_base.py
@@ -0,0 +1,26 @@
+from abc import ABC, abstractmethod
+from typing import Dict
+from typing import TypedDict
+
+class QueryDescription:
+    pass #todo
+
+class SkillParameters(TypedDict):
+    commandName: str
+    parameters: Dict[str,any]
+
+class SkillDescription():
+    pass
+    
+class Reasoner(ABC):
+    @abstractmethod
+    def selectSkill(self, skills: list(SkillDescription), queries: list[QueryDescription], utterance: str) -> SkillParameters:
+        '''Selects requested skill based on utterance and list of available skills. 
+        Should return index of selected skill in provided list of skills'''
+        pass
+    
+    @abstractmethod
+    def getPrompt(self, skills: Skills, utterance: str) -> int:
+        '''Selects requested skill based on utterance and list of available skills. 
+        Should return index of selected skill in provided list of skills'''
+        pass
