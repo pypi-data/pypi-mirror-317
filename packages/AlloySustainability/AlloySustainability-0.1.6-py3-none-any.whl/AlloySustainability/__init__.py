4a2bb690957e16a4abb13fae518225ab79517bf0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-24 04:04:57 +0000

    Add package AlloySustainability/0.1.6/AlloySustainability-0.1.6-py3-none-any.whl

---
+++ packages/AlloySustainability/AlloySustainability-0.1.6-py3-none-any.whl/AlloySustainability/__init__.py
@@ -0,0 +1,8 @@
+"""
+AlloySustainability Package
+
+This package provides tools to compute and visualize sustainability indicators for alloys.
+"""
+
+__version__ = "0.1.6"
+__author__ = "Stephane GORSSE"
