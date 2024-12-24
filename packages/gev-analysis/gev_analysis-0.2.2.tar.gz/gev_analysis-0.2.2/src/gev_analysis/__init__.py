0a91be00b5b22bfad4422a6fba6d0570277fa470
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-24 06:04:09 +0000

    Add package gev-analysis/0.2.2/gev_analysis-0.2.2.tar.gz

---
+++ packages/gev-analysis/gev_analysis-0.2.2.tar.gz/gev_analysis-0.2.2/src/gev_analysis/__init__.py
@@ -0,0 +1,6 @@
+from .data_handler import DataHandler
+from .normality_var import NormalityVaR
+from .gev_analysis import GEVExtremeValueAnalysis
+from .gpd_analysis import GPDExtremeValueAnalysis
+
+__all__ = ["DataHandler", "NormalityVaR", "GEVExtremeValueAnalysis", "GPDExtremeValueAnalysis"]
