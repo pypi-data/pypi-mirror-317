aabe2bfed0a568fc4056a4f6895f37733dc3a8f5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-24 05:04:00 +0000

    Add package skytemple-rust/1.8.5/skytemple_rust-1.8.5-cp311-cp311-win_amd64.whl

---
+++ packages/skytemple-rust/skytemple_rust-1.8.5-cp311-cp311-win_amd64.whl/skytemple_rust/st_script_var_table.pyi
@@ -0,0 +1,44 @@
+#  Copyright 2020-2023 Capypara and the SkyTemple Contributors
+#
+#  This file is part of SkyTemple.
+#
+#  SkyTemple is free software: you can redistribute it and/or modify
+#  it under the terms of the GNU General Public License as published by
+#  the Free Software Foundation, either version 3 of the License, or
+#  (at your option) any later version.
+#
+#  SkyTemple is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU General Public License for more details.
+#
+#  You should have received a copy of the GNU General Public License
+#  along with SkyTemple.  If not, see <https://www.gnu.org/licenses/>.
+from range_typed_integers import u16, u32, i16
+from typing import Sequence
+
+COUNT_GLOBAL_VARS: u32
+COUNT_LOCAL_VARS: u32
+DEFINITION_STRUCT_SIZE: int
+
+class ScriptVariableDefinition:
+    id: int
+    type: u16
+    unk1: u16
+    memoffset: u16
+    bitshift: u16
+    nbvalues: u16
+    default: i16
+    name: str
+
+class ScriptVariableTables:
+    globals: Sequence[ScriptVariableDefinition]
+    locals: Sequence[ScriptVariableDefinition]
+
+    def __init__(
+        self,
+        arm9: bytes,
+        global_start: u32,
+        local_start: u32,
+        subtract_from_name_addrs: u32,
+    ): ...
