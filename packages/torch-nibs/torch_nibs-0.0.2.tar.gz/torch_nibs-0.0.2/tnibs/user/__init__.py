f43a5f1581a4a8b2c75620b13f25c693a9a16d03
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-24 14:03:35 +0000

    Add package torch-nibs/0.0.2/torch_nibs-0.0.2.tar.gz

---
+++ packages/torch-nibs/torch_nibs-0.0.2.tar.gz/torch_nibs-0.0.2/tnibs/user/__init__.py
@@ -0,0 +1,30 @@
+# import this when doing interactive stuff
+
+# useful libs
+import numpy as np
+import torch
+import seaborn as sns
+import matplotlib.pyplot as plt
+import torcheval.metrics as ms
+import torch.utils.data as td
+
+# info
+import logging
+import importlib
+from pprint import pprint
+from tnibs.utils import *
+if is_notebook():
+    from IPython.display import display, clear_output
+
+from tnibs.data import *
+from tnibs.data.utils.dfs import *
+from tnibs.modules.modules import *
+from tnibs.metric import *
+from tnibs.train import *
+
+# Device configuration
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+if torch.cuda.is_available():
+    print(f"Using: {device}. Device: {torch.cuda.get_device_name()}")
+
+
