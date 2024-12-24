93b1de74da7b7303a4eda8fb6ae643e0d2987f88
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-24 15:03:39 +0000

    Add package ip-region/1.0.2/ip_region-1.0.2.tar.gz

---
+++ packages/ip-region/ip_region-1.0.2.tar.gz/ip_region-1.0.2/setup.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python
+#-*- coding:utf-8 -*-
+
+#############################################
+# File Name: setup.py
+# Author: Jeay
+# Mail: admin@jeay.net
+# Created Time:  2024-12-20 15:09:02
+#############################################
+
+
+from setuptools import setup, find_packages
+filepath = 'README.md'
+setup(
+    name = "ip-region",
+    version = "1.0.2",
+    keywords = ["ip loaction", "ip country", "ip region", "IP address loaction", "IP geographic location"],
+    description = "Get geographic location through IP address, support IPv4 and IPv6. Combine IP address library and online API.",
+    long_description = open(filepath, encoding='utf-8').read(),
+    long_description_content_type='text/markdown',
+    license = "Apache-2.0 License",
+
+    url = "https://github.com/jeeaay/py-ip-location",
+    author = "Jeay",
+    author_email = "admin@jeay.net",
+
+    packages = find_packages(),
+    include_package_data = True,
+    platforms = "any",
+    install_requires = ["requests"]
+)
