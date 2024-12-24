6535ec33eb669ccce3ac669da3ccdeddeb398caa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-24 14:03:32 +0000

    Add package UQPyL/2.0.10/UQPyL-2.0.10-cp37-cp37m-manylinux_2_17_x86_64.manylinux2014_x86_64.whl

---
+++ packages/UQPyL/UQPyL-2.0.10-cp37-cp37m-manylinux_2_17_x86_64.manylinux2014_x86_64.whl/UQPyL/__init__.py
@@ -0,0 +1,13 @@
+from . import problems, surrogates, optimization, sensibility, DoE, utility
+
+__version__ = "2.0.10"
+__author__ = "wmtSky"
+
+__all__=[
+    "problems",
+    "surrogates",
+    "optimization",
+    "sensibility",
+    "DoE",
+    "utility"
+]
---
+++ packages/UQPyL/UQPyL-2.0.10-cp37-cp37m-manylinux_2_17_x86_64.manylinux2014_x86_64.whl/UQPyL/utility/__init__.py
@@ -0,0 +1,18 @@
+from .scalers import MinMaxScaler, StandardScaler, Scaler
+from .model_selections import KFold, RandSelect
+from .polynomial_features import PolynomialFeatures
+from .grid_search import GridSearch
+from .metrics import r_square, rank_score
+from .verbose import Verbose
+__all__=[
+    'Scaler',
+    'MinMaxScaler',
+    'StandardScaler',
+    'KFold',
+    'RandSelect',
+    'PolynomialFeatures',
+    'r_square',
+    'rank_score',
+    'GridSearch',
+    'Verbose'
+]
