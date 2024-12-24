45c5bfbb5d022caf58df6b1df126c86089322a18
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-24 04:03:52 +0000

    Add package craft-ai-sdk/0.59.0rc1/craft_ai_sdk-0.59.0rc1.tar.gz

---
+++ packages/craft-ai-sdk/craft_ai_sdk-0.59.0rc1.tar.gz/craft_ai_sdk-0.59.0rc1/craft_ai_sdk/constants.py
@@ -0,0 +1,33 @@
+from strenum import LowercaseStrEnum
+from enum import auto
+
+
+class DEPLOYMENT_EXECUTION_RULES(LowercaseStrEnum):
+
+    """Enumeration for deployments execution rules."""
+
+    ENDPOINT = auto()
+    PERIODIC = auto()
+
+
+class DEPLOYMENT_MODES(LowercaseStrEnum):
+
+    """Enumeration for deployments modes."""
+
+    LOW_LATENCY = auto()
+    ELASTIC = auto()
+
+
+class DEPLOYMENT_STATUS(LowercaseStrEnum):
+
+    """Enumeration for deployments status."""
+
+    CREATION_PENDING = auto()
+    UP = auto()
+    CREATION_FAILED = auto()
+    DOWN_RETRYING = auto()
+    STANDBY = auto()
+    DISABLED = auto()
+
+
+CREATION_REQUESTS_RETRY_INTERVAL = 10
