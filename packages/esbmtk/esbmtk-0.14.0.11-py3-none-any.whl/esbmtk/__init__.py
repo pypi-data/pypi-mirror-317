5575ccbd4c98fb6d8cb406fcbdb26111687c11f2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-24 05:03:33 +0000

    Add package esbmtk/0.14.0.11/esbmtk-0.14.0.11-py3-none-any.whl

---
+++ packages/esbmtk/esbmtk-0.14.0.11-py3-none-any.whl/esbmtk/__init__.py
@@ -0,0 +1,25 @@
+from __future__ import annotations
+import numpy as np
+from pint import UnitRegistry
+ureg = UnitRegistry(on_redefinition="ignore")
+ureg.enable_contexts("chemistry")
+Q_ = ureg.Quantity
+ureg.define("Sverdrup = 1e6 * meter **3 / second = Sv = Sverdrups")
+ureg.define("Mol = 1 * mol / liter = M")
+ureg.define("fraction = [] = frac")
+ureg.define("percent = 1e-2 frac = pct")
+ureg.define("permil = 1e-3 fraction")
+ureg.define("ppm = 1e-6 fraction")
+from .esbmtk_base import *
+from .esbmtk import *
+from .extended_classes import *
+from .connections import *
+from .utility_functions import *
+from .sealevel import *
+from .ode_backend import *
+from .seawater import *
+from .post_processing import *
+from .processes import *
+from .carbonate_chemistry import *
+
+np.seterr(invalid="ignore")
