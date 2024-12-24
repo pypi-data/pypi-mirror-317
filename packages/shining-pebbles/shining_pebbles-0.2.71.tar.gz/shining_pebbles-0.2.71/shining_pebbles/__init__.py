f5945b676f2081cca9766efdc8f39549337f40d6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-24 16:04:05 +0000

    Add package shining-pebbles/0.2.71/shining_pebbles-0.2.71.tar.gz

---
+++ packages/shining-pebbles/shining_pebbles-0.2.71.tar.gz/shining_pebbles-0.2.71/shining_pebbles/__init__.py
@@ -0,0 +1,7 @@
+import logging
+
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger(__name__)
+logger.info("shining_pebbles package initialized")
+
+from .collection_of_utils import *
