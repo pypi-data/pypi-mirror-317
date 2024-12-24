dfa206c3787fccb429ccb40a45bd730338c2a3b1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-24 16:04:24 +0000

    Add package sysstra/0.1.3/sysstra-0.1.3.tar.gz

---
+++ packages/sysstra/sysstra-0.1.3.tar.gz/sysstra-0.1.3/setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="sysstra",  # Your package name (must be unique on PyPI)
+    version="0.1.3",  # Initial release version
+    description="Official Python Library for Sysstra Algo Trading",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    author="Anurag Singh Kushwah",
+    author_email="anurag786kushwah@gmail.com",
+    url="https://github.com/sysstra/sysstra",  # Link to source code
+    packages=find_packages(),  # Automatically find sub-packages
+    install_requires=[  # Dependencies
+        "requests",
+        "numpy",
+        "pandas_ta",
+        "redis",
+    ],
+    classifiers=[  # Additional metadata (check PyPI classifiers)
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.6",  # Minimum Python version
+)
