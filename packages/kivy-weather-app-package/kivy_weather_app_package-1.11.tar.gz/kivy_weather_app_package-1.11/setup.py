377e28136b163751f12759d543a97fe41f623f42
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-24 05:03:36 +0000

    Add package kivy-weather-app-package/1.11/kivy_weather_app_package-1.11.tar.gz

---
+++ packages/kivy-weather-app-package/kivy_weather_app_package-1.11.tar.gz/kivy_weather_app_package-1.11/setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup, find_packages
+setup(
+    name="kivy_weather_app_package",
+    version="1.11",
+    author="Daniel Kříž",
+    license="MIT",
+    packages=find_packages(),
+    include_package_data=True,
+    install_requires=[
+        "kivy",
+    ],
+    entry_points={
+        'console_scripts': [
+            'kivy_weather_app_package = kivy_weather_app_package.main:main'
+        ]
+    },
+    package_data={
+        '': ['kivy_weather_app_package/*'],
+    },
+)
