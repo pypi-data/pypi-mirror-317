a1d659144fd8815ba9f228719df2304ec2ef6926
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-24 04:03:49 +0000

    Add package crick/0.0.8/crick-0.0.8.tar.gz

---
+++ packages/crick/crick-0.0.8.tar.gz/crick-0.0.8/README.rst
@@ -0,0 +1,27 @@
+Crick
+=====
+
+|Build Status| |Conda Badge| |PyPI Badge|
+
+Crick is a fast library of approximate and streaming algorithms. It is still a
+work in progress, use at your own risk.
+
+Installation
+------------
+
+Crick is available via ``conda`` or ``pip``:
+
+::
+
+   # Install with conda
+   $ conda install crick -c conda-forge
+
+   # Install with pip
+   $ pip install crick
+
+.. |Build Status| image:: https://github.com/dask/crick/actions/workflows/tests.yml/badge.svg
+   :target: https://github.com/dask/crick/actions/workflows/tests.yml
+.. |PyPI Badge| image:: https://img.shields.io/pypi/v/crick.svg
+   :target: https://pypi.python.org/pypi/crick
+.. |Conda Badge| image:: https://anaconda.org/conda-forge/crick/badges/version.svg
+   :target: https://anaconda.org/conda-forge/crick
---
+++ packages/crick/crick-0.0.8.tar.gz/crick-0.0.8/setup.py
@@ -0,0 +1,68 @@
+import os
+import sys
+
+import numpy as np
+from Cython import Tempita as tempita
+from Cython.Build import cythonize
+from setuptools import setup
+from setuptools.extension import Extension
+
+import versioneer
+
+compile_args = dict(
+    library_dirs=[
+        os.path.abspath(os.path.join(np.get_include(), "..", "lib")),
+    ],
+    include_dirs=[np.get_include(), "crick/klib"],
+    libraries=["npymath"],
+)
+
+if "--debug" in sys.argv:
+    sys.argv.remove("--debug")
+    compile_args["undef_macros"] = ["NDEBUG"]
+    compile_args["extra_compile_args"] = ["-O0"]
+
+
+def generate_code(templates):
+    """Generate code from template files"""
+    for template in templates:
+        # template extension must be .in
+        assert template.endswith(".in")
+        outfile = template[:-3]
+
+        if os.path.exists(outfile) and (
+            not os.path.exists(template)
+            or os.stat(template).st_mtime < os.stat(outfile).st_mtime
+        ):
+            # If output is present but template isn't, or if template is not
+            # updated, then no need to generate
+            continue
+
+        with open(template) as f:
+            tmpl = f.read()
+        output = tempita.sub(tmpl)
+
+        with open(outfile, "w") as f:
+            f.write(output)
+
+
+templates = ["crick/space_saving_stubs.c.in"]
+
+generate_code(templates)
+
+extensions = [
+    Extension("crick.tdigest", ["crick/tdigest.pyx"], **compile_args),
+    Extension("crick.space_saving", ["crick/space_saving.pyx"], **compile_args),
+    Extension("crick.stats", ["crick/stats.pyx"], **compile_args),
+    Extension(
+        "crick.numpy_version",
+        ["crick/numpy_version.pyx"],
+        extra_compile_args=[f'-DNUMPY_VERSION="{np.__version__}"'],
+    ),
+]
+
+setup(
+    version=versioneer.get_version(),
+    cmdclass=versioneer.get_cmdclass(),
+    ext_modules=cythonize(extensions),
+)
