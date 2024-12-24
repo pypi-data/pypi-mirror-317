f7ae5eccd09cf18cd6b5fc60f4a6042f6d8105cb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-24 05:04:21 +0000

    Add package light-database/0.2.6/light_database-0.2.6-cp310-cp310-win_amd64.whl

---
+++ packages/light-database/light_database-0.2.6-cp310-cp310-win_amd64.whl/light_database/hbase/thrift/constants.pyi
@@ -0,0 +1,53 @@
+# (generated with --quick)
+
+import light_database.hbase.thrift.ttypes
+import sys
+from thrift.transport import TTransport
+from typing import Any, Type
+
+TAppend: Type[light_database.hbase.thrift.ttypes.TAppend]
+TApplicationException: Any
+TAuthorization: Type[light_database.hbase.thrift.ttypes.TAuthorization]
+TBloomFilterType: Type[light_database.hbase.thrift.ttypes.TBloomFilterType]
+TCellVisibility: Type[light_database.hbase.thrift.ttypes.TCellVisibility]
+TColumn: Type[light_database.hbase.thrift.ttypes.TColumn]
+TColumnFamilyDescriptor: Type[light_database.hbase.thrift.ttypes.TColumnFamilyDescriptor]
+TColumnIncrement: Type[light_database.hbase.thrift.ttypes.TColumnIncrement]
+TColumnValue: Type[light_database.hbase.thrift.ttypes.TColumnValue]
+TCompareOperator: Type[light_database.hbase.thrift.ttypes.TCompareOperator]
+TCompressionAlgorithm: Type[light_database.hbase.thrift.ttypes.TCompressionAlgorithm]
+TConsistency: Type[light_database.hbase.thrift.ttypes.TConsistency]
+TDataBlockEncoding: Type[light_database.hbase.thrift.ttypes.TDataBlockEncoding]
+TDelete: Type[light_database.hbase.thrift.ttypes.TDelete]
+TDeleteType: Type[light_database.hbase.thrift.ttypes.TDeleteType]
+TDurability: Type[light_database.hbase.thrift.ttypes.TDurability]
+TException: Any
+TFilterByOperator: Type[light_database.hbase.thrift.ttypes.TFilterByOperator]
+TFrozenDict: Any
+TGet: Type[light_database.hbase.thrift.ttypes.TGet]
+THRegionInfo: Type[light_database.hbase.thrift.ttypes.THRegionInfo]
+THRegionLocation: Type[light_database.hbase.thrift.ttypes.THRegionLocation]
+TIOError: Type[light_database.hbase.thrift.ttypes.TIOError]
+TIllegalArgument: Type[light_database.hbase.thrift.ttypes.TIllegalArgument]
+TIncrement: Type[light_database.hbase.thrift.ttypes.TIncrement]
+TKeepDeletedCells: Type[light_database.hbase.thrift.ttypes.TKeepDeletedCells]
+TLogQueryFilter: Type[light_database.hbase.thrift.ttypes.TLogQueryFilter]
+TLogType: Type[light_database.hbase.thrift.ttypes.TLogType]
+TMessageType: Any
+TMutation: Type[light_database.hbase.thrift.ttypes.TMutation]
+TNamespaceDescriptor: Type[light_database.hbase.thrift.ttypes.TNamespaceDescriptor]
+TOnlineLogRecord: Type[light_database.hbase.thrift.ttypes.TOnlineLogRecord]
+TProtocolException: Any
+TPut: Type[light_database.hbase.thrift.ttypes.TPut]
+TReadType: Type[light_database.hbase.thrift.ttypes.TReadType]
+TResult: Type[light_database.hbase.thrift.ttypes.TResult]
+TRowMutations: Type[light_database.hbase.thrift.ttypes.TRowMutations]
+TScan: Type[light_database.hbase.thrift.ttypes.TScan]
+TServerName: Type[light_database.hbase.thrift.ttypes.TServerName]
+TTableDescriptor: Type[light_database.hbase.thrift.ttypes.TTableDescriptor]
+TTableName: Type[light_database.hbase.thrift.ttypes.TTableName]
+TThriftServerType: Type[light_database.hbase.thrift.ttypes.TThriftServerType]
+TTimeRange: Type[light_database.hbase.thrift.ttypes.TTimeRange]
+TType: Any
+all_structs: Any
+fix_spec: Any
