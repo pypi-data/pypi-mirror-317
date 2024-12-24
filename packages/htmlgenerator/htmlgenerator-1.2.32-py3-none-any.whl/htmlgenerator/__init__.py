52d2bc6d1b97b9e73cd18182045db2e60fdff7a6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-24 15:04:00 +0000

    Add package htmlgenerator/1.2.32/htmlgenerator-1.2.32-py3-none-any.whl

---
+++ packages/htmlgenerator/htmlgenerator-1.2.32-py3-none-any.whl/htmlgenerator/__init__.py
@@ -0,0 +1,13 @@
+from .base import *  # noqa
+from .htmltags import *  # noqa
+from .lazy import *  # noqa
+from .safestring import mark_safe  # noqa
+
+__version__ = "1.2.32"
+
+
+DEBUG: bool = False
+"Turning on this flag will add html attributes with information "
+"about the source of the generated html output",
+# not sure whether this is a smart idea, but environment variables
+# do not really seem better...
