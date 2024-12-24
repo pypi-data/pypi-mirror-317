86ca9c0fb79a434d4b50882dff39cbb47245e61e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-24 03:08:32 +0000

    Add package exact_multiset_cover/1.5.0/exact_multiset_cover-1.5.0.tar.gz

---
+++ packages/exact_multiset_cover/exact_multiset_cover-1.5.0.tar.gz/exact_multiset_cover-1.5.0/build.py
@@ -0,0 +1,26 @@
+from setuptools import Extension
+
+import numpy
+
+
+# define the extension module
+src_dir = "src/"
+exact_cover_impl = Extension(
+    "exact_cover_impl",
+    sources=[
+        src_dir + "exact_cover.c",
+        src_dir + "dlx.c",
+        src_dir + "sparse_matrix.c",
+        src_dir + "quad_linked_list.c",
+    ],
+    include_dirs=[src_dir, numpy.get_include()],
+)
+
+
+def build(setup_kwargs):
+    setup_kwargs.update(
+        {
+            "packages": ["exact_multiset_cover"],
+            "ext_modules": [exact_cover_impl],
+        }
+    )
