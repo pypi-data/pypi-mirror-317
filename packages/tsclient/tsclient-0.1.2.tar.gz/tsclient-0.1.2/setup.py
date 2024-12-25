5e7b95bba2cc8bf983b9e1f0d5e5795b8e0f6858
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-25 15:04:25 +0000

    Add package tsclient/0.1.2/tsclient-0.1.2.tar.gz

---
+++ packages/tsclient/tsclient-0.1.2.tar.gz/tsclient-0.1.2/setup.py
@@ -0,0 +1,33 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="tsclient",
+    version="0.1.2",
+    author="Talkscriber",
+    author_email="info@talkscriber.com",
+    description="Talkscriber Python Client",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    url="https://github.com/Talkscriber/ts-client",
+    packages=find_packages(where="src"),
+    package_dir={"": "src"},
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Intended Audience :: Developers",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.7",
+    install_requires=[
+        "ffmpeg-python==0.2.0",
+        "numpy>=1.22.3,<1.23.0",
+        "PyAudio==0.2.11",
+        "scipy>=1.8.0,<1.11.0",
+        "websocket-client==1.6.0",
+    ],
+)
---
+++ packages/tsclient/tsclient-0.1.2.tar.gz/tsclient-0.1.2/src/tsclient/__init__.py
@@ -0,0 +1,11 @@
+"""
+Talkscriber Python Client
+~~~~~~~~~~~~~~~~~~~~~~~~
+
+A Python client for interacting with the Talkscriber API.
+
+:copyright: (c) 2024 Talkscriber
+:license: MIT, see LICENSE for more details.
+"""
+
+__version__ = "0.1.2"  # Updated to match setup.py
