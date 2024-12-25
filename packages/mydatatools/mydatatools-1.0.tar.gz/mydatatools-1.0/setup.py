87aafe5711e3499a363e058f923494afe63ae31e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-25 14:03:40 +0000

    Add package mydatatools/1.0/mydatatools-1.0.tar.gz

---
+++ packages/mydatatools/mydatatools-1.0.tar.gz/mydatatools-1.0/setup.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""
+mydatatools包打包脚本
+"""
+
+from setuptools import setup, find_packages
+
+
+if __name__ == '__main__':
+    setup(
+        name='mydatatools',
+        version='1.0',
+        author='zhengquantao80',
+        author_email='zhengquantao80@gmail.com',
+        long_description=__doc__,
+        packages=find_packages(),
+        include_package_data=True,
+        zip_safe=False,
+        exclude_package_data={'': ['.gitignore']},
+        install_requires=[
+            'pandas==2.2.2',
+            'SQLAlchemy==2.0.34',
+            'PyMySQL==1.1.1',
+            'paramiko==3.4.1',
+            'loguru==0.7.2',
+            'taos-ws-py==0.3.2'
+        ]
+    )
+
+
+
