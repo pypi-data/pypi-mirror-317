035d79283758df1ccd8c9197d41f907cce37a649
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-25 05:03:59 +0000

    Add package openskill/6.0.2/openskill-6.0.2.tar.gz

---
+++ packages/openskill/openskill-6.0.2.tar.gz/openskill-6.0.2/openskill/__init__.py
@@ -0,0 +1,16 @@
+from typing import List
+
+# Public API
+__all__: List[str] = []
+
+
+# Metadata
+__version__ = "6.0.2"
+__author__ = "Vivek Joshy"
+__email__ = "vivekjoshy97@gmail.com"
+__copyright__ = "Copyright 2023 - 2024, Vivek Joshy"
+__credits__ = ["Philihp Busby"]
+__deprecated__ = False
+__license__ = "MIT"
+__maintainer__ = "Vivek Joshy"
+__status__ = "Production"
