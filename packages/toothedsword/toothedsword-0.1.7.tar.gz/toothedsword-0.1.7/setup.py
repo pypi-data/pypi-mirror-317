207d3f3580a672ed08666656f60250cee4658804
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-25 14:04:11 +0000

    Add package toothedsword/0.1.7/toothedsword-0.1.7.tar.gz

---
+++ packages/toothedsword/toothedsword-0.1.7.tar.gz/toothedsword-0.1.7/setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="toothedsword",
+    version="0.1.7",
+    description="代码工程化",
+    packages=find_packages(),
+    python_requires=">=3.6",
+    include_package_data=True,
+
+    package_data={
+        "toothedsword": ["base/*.json", "tctb/*.json", "ningxia/*.json"],
+                 },
+    install_requires=[
+        # "numpy>=1.12.0",
+    ],
+)
+
