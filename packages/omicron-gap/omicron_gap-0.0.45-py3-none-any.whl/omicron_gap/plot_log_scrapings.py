ea7b928b19e005781ca0f99a8c719929c0761a23
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-25 04:04:22 +0000

    Add package omicron-gap/0.0.45/omicron_gap-0.0.45-py3-none-any.whl

---
+++ packages/omicron-gap/omicron_gap-0.0.45-py3-none-any.whl/omicron_gap/plot_log_scrapings.py
@@ -0,0 +1,129 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# vim: nu:ai:ts=4:sw=4
+
+#
+#  Copyright (C) 2024 Joseph Areeda <joseph.areeda@ligo.org>
+#
+#  This program is free software: you can redistribute it and/or modify
+#  it under the terms of the GNU General Public License as published by
+#  the Free Software Foundation, either version 3 of the License, or
+#  (at your option) any later version.
+#
+#  This program is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU General Public License for more details.
+#
+#  You should have received a copy of the GNU General Public License
+#  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+
+""""""
+
+import time
+
+import numpy
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+start_time = time.time()
+
+import argparse
+import logging
+from pathlib import Path
+import re
+import subprocess
+import sys
+import traceback
+
+try:
+    from ._version import __version__
+except ImportError:
+    __version__ = '0.0.0'
+
+__author__ = 'joseph areeda'
+__email__ = 'joseph.areeda@ligo.org'
+__process_name__ = Path(__file__).name
+
+logger = None
+
+
+def parser_add_args(parser):
+    """
+    Set up command parser
+    :param argparse.ArgumentParser parser:
+    :return: None but parser object is updated
+    """
+    parser.add_argument('-v', '--verbose', action='count', default=1,
+                        help='increase verbose output')
+    parser.add_argument('-V', '--version', action='version',
+                        version=__version__)
+    parser.add_argument('-q', '--quiet', default=False, action='store_true',
+                        help='show only fatal errors')
+    parser.add_argument('infiles', nargs='+', type=Path,
+                        help='One or more CSV files from omicron-log-scanner')
+
+
+def main():
+    global logger
+
+    logging.basicConfig()
+    logger = logging.getLogger(__process_name__)
+    logger.setLevel(logging.DEBUG)
+
+    parser = argparse.ArgumentParser(description=__doc__, prog=__process_name__,
+                                     formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+    parser_add_args(parser)
+    args = parser.parse_args()
+    verbosity = 0 if args.quiet else args.verbose
+
+    if verbosity < 1:
+        logger.setLevel(logging.CRITICAL)
+    elif verbosity < 2:
+        logger.setLevel(logging.INFO)
+    else:
+        logger.setLevel(logging.DEBUG)
+
+    # debugging?
+    logger.debug(f'{__process_name__} version: {__version__} called with arguments:')
+    for k, v in args.__dict__.items():
+        logger.debug('    {} = {}'.format(k, v))
+
+    data = None
+    for infile in args.infiles:
+        atable = np.genfromtxt(infile, delimiter=',', names=True, autostrip=True, dtype=None,
+                               encoding='utf-8')
+        if data is None:
+            data = atable
+        else:
+            data = np.hstack((data, atable))
+    logger.info(f'Total jobs: {data.size}')
+
+    local_jobs = data[data[:]['Local'] == 1]
+    vanilla_jobs = data[data[:]['Local'] == 0]
+
+    # percentiles
+    pctls = [0, 25, 50, 75, 90, 95, 100]
+    for jobls, lst_name in [(local_jobs, 'local'), (vanilla_jobs, 'vanilla')]:
+        print(f'{lst_name} universe: {len(jobls)} jobs:')
+        for pct in pctls:
+            print(f'{pct}%, {np.percentile(jobls[:]["Qtime"], pct)}')
+
+    # Plot histogram of queue times
+
+
+if __name__ == "__main__":
+    try:
+        main()
+    except (ValueError, TypeError, OSError, NameError, ArithmeticError, RuntimeError) as ex:
+        print(ex, file=sys.stderr)
+        traceback.print_exc(file=sys.stderr)
+
+    if logger is None:
+        logging.basicConfig()
+        logger = logging.getLogger(__process_name__)
+        logger.setLevel(logging.DEBUG)
+    # report our run time
+    logger.info(f'Elapsed time: {time.time() - start_time:.1f}s')
