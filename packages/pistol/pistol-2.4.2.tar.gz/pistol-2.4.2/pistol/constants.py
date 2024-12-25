35029ef63c0adb09304fa1074b6141a443063aa6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-25 05:04:17 +0000

    Add package pistol/2.4.2/pistol-2.4.2.tar.gz

---
+++ packages/pistol/pistol-2.4.2.tar.gz/pistol-2.4.2/pistol/constants.py
@@ -0,0 +1,26 @@
+import os, platform
+
+from pathlib import Path
+from prompt_toolkit.styles import Style
+
+DIR: Path = Path(__file__).parent
+STORAGE_PATH: Path = DIR / "storage"
+SYS_ROOT: str = os.path.abspath(os.sep)
+EP_MODULE: str = str(DIR).removeprefix(SYS_ROOT).replace("\\", "/").replace("/", ".")
+PLATFORM: str = platform.system().lower()
+STYLE = Style.from_dict({
+    'yellow': 'bold fg:yellow',
+    'magenta': 'fg:magenta',
+    'blue': 'bold fg:blue',
+    'reset': '',
+})
+JSON_FRAME: str = """
+{
+    "cmd_history": [],
+    "cd_history": [],
+    "aliases": {},
+    "props": {},
+    "last_location": "",
+    "cache": []
+}
+"""
