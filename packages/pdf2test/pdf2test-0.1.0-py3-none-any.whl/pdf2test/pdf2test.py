4867564ecf4885461686b2580deb3b0f20f4afe3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-25 03:06:59 +0000

    Add package pdf2test/0.1.0/pdf2test-0.1.0-py3-none-any.whl

---
+++ packages/pdf2test/pdf2test-0.1.0-py3-none-any.whl/pdf2test/pdf2test.py
@@ -0,0 +1,11 @@
+# pdf2test/pdf2test.py
+
+from .pdf2test import Converter
+
+
+class Converter:
+    def convert(path):
+        print("pdf.text  test")
+
+# 在 __init__.py 中导入 Converter 类，使得它可以通过包名直接访问
+# pdf2test/__init__.py
