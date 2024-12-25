636ea91b88ddbccaf3c51a522683f539a1dff09b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-25 14:04:02 +0000

    Add package ozon-api-client/0.0.9/ozon_api_client-0.0.9.tar.gz

---
+++ packages/ozon-api-client/ozon_api_client-0.0.9.tar.gz/ozon_api_client-0.0.9/ozon_api_client/api/__init__.py
@@ -0,0 +1,28 @@
+from __future__ import absolute_import
+
+# flake8: noqa
+
+# import apis into api package
+from ozon_api_client.api.analytics_api_api import AnalyticsAPIApi
+from ozon_api_client.api.barcode_api_api import BarcodeAPIApi
+from ozon_api_client.api.brand_api_api import BrandAPIApi
+from ozon_api_client.api.cancellation_api_api import CancellationAPIApi
+from ozon_api_client.api.category_api_api import CategoryAPIApi
+from ozon_api_client.api.certification_api_api import CertificationAPIApi
+from ozon_api_client.api.chat_api_api import ChatAPIApi
+from ozon_api_client.api.fbo_api import FBOApi
+from ozon_api_client.api.fbs_api import FBSApi
+from ozon_api_client.api.fbsr_fbs_marks_api import FBSrFBSMarksApi
+from ozon_api_client.api.finance_api_api import FinanceAPIApi
+from ozon_api_client.api.polygon_api_api import PolygonAPIApi
+from ozon_api_client.api.prices_stocks_api_api import PricesStocksAPIApi
+from ozon_api_client.api.pricing_strategy_api_api import PricingStrategyAPIApi
+from ozon_api_client.api.product_api_api import ProductAPIApi
+from ozon_api_client.api.promos_api import PromosApi
+from ozon_api_client.api.rfbs_returns_api_api import RFBSReturnsAPIApi
+from ozon_api_client.api.report_api_api import ReportAPIApi
+from ozon_api_client.api.return_api_api import ReturnAPIApi
+from ozon_api_client.api.returns_api_api import ReturnsAPIApi
+from ozon_api_client.api.seller_rating_api import SellerRatingApi
+from ozon_api_client.api.supplier_api_api import SupplierAPIApi
+from ozon_api_client.api.warehouse_api_api import WarehouseAPIApi
