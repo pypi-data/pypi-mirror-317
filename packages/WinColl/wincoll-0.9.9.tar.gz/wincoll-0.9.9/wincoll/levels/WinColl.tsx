389797c448e86d23a7191dd78d6b4966c2f24bb1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-25 15:04:25 +0000

    Add package WinColl/0.9.9/wincoll-0.9.9.tar.gz

---
+++ packages/WinColl/wincoll-0.9.9.tar.gz/wincoll-0.9.9/wincoll/levels/WinColl.tsx
@@ -0,0 +1,34 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.8" tiledversion="1.8.2" name="WinColl" tilewidth="16" tileheight="16" tilecount="10" columns="0">
+ <grid orientation="orthogonal" width="1" height="1"/>
+ <tile id="9">
+  <image width="16" height="16" source="Gap.png"/>
+ </tile>
+ <tile id="10">
+  <image width="16" height="16" source="Brick.png"/>
+ </tile>
+ <tile id="11">
+  <image width="16" height="16" source="Safe.png"/>
+ </tile>
+ <tile id="12">
+  <image width="16" height="16" source="Diamond.png"/>
+ </tile>
+ <tile id="13">
+  <image width="16" height="16" source="Blob.png"/>
+ </tile>
+ <tile id="14">
+  <image width="16" height="16" source="Earth.png"/>
+ </tile>
+ <tile id="15">
+  <image width="16" height="16" source="Rock.png"/>
+ </tile>
+ <tile id="16">
+  <image width="16" height="16" source="Key.png"/>
+ </tile>
+ <tile id="17">
+  <image width="16" height="16" source="Win.png"/>
+ </tile>
+ <tile id="18">
+  <image width="16" height="16" source="Gap.png"/>
+ </tile>
+</tileset>
