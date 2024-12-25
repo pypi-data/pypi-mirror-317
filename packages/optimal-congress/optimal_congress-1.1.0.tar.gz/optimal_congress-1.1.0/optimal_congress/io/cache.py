fd1cc0a1584fc6092dbe23a513b8d113b8c126f1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-25 04:04:23 +0000

    Add package optimal-congress/1.1.0/optimal_congress-1.1.0.tar.gz

---
+++ packages/optimal-congress/optimal_congress-1.1.0.tar.gz/optimal_congress-1.1.0/optimal_congress/io/cache.py
@@ -0,0 +1,146 @@
+"""IO operations on local cache."""
+
+import json
+
+from optimal_congress.config import (
+    DIR_EVENTS_CACHE,
+    DIR_RATINGS_CACHE,
+    DIR_ROOMS_CACHE,
+)
+from optimal_congress.schema import Event, Rating, Room
+
+
+def save_events(events: set[Event]) -> None:
+    """Save events to cache.
+
+    Note: This function overwrites all cached events.
+    """
+    # create events directory if it doesn't exist
+    DIR_EVENTS_CACHE.mkdir(parents=True, exist_ok=True)
+
+    # empty cache
+    for file in DIR_EVENTS_CACHE.glob("*.json"):
+        file.unlink()
+
+    # save events
+    for event in events:
+        with open(DIR_EVENTS_CACHE / f"event_{event.id}.json", "w") as f:
+            f.write(event.model_dump_json())
+
+
+def save_rooms(rooms: set[Room]) -> None:
+    """Save rooms to cache.
+
+    Note: This function overwrites all cached rooms.
+    """
+    # create rooms directory if it doesn't exist
+    DIR_ROOMS_CACHE.mkdir(parents=True, exist_ok=True)
+
+    # empty cache
+    for file in DIR_ROOMS_CACHE.glob("*.json"):
+        file.unlink()
+
+    # save rooms
+    for room in rooms:
+        with open(DIR_ROOMS_CACHE / f"room_{room.id}.json", "w") as f:
+            f.write(room.model_dump_json())
+
+
+def save_ratings(ratings: set[Rating]) -> None:
+    """Save ratings to cache.
+
+    Note: This does not replace cached rating for the same event.
+    """
+    # create ratings directory if it doesn't exist
+    DIR_RATINGS_CACHE.mkdir(parents=True, exist_ok=True)
+
+    # save ratings
+    for rating in ratings:
+        with open(
+            DIR_RATINGS_CACHE / f"rating_{rating.event_id}_{rating.timestamp}.json",
+            "w",
+        ) as f:
+            f.write(rating.model_dump_json())
+
+
+# OPTIONAL: replace by save_ratings
+def save_rating(rating: Rating) -> None:
+    """Save an individual rating to cache.
+
+    Note: This does not replace cached rating for the same event.
+    """
+    # create ratings directory if it doesn't exist
+    DIR_RATINGS_CACHE.mkdir(parents=True, exist_ok=True)
+
+    # save ratings
+    with open(
+        DIR_RATINGS_CACHE / f"rating_{rating.event_id}_{rating.timestamp}.json",
+        "w",
+    ) as f:
+        f.write(rating.model_dump_json())
+
+
+def load_events(exit_if_empty: bool) -> set[Event]:
+    """Load events from disk.
+
+    Args:
+        exit_if_empty: Exit if no events are found, and give instructions.
+    Returns:
+        List of events.
+    """
+    # create events directory if it doesn't exist
+    DIR_EVENTS_CACHE.mkdir(parents=True, exist_ok=True)
+
+    # load events
+    events_files = list(DIR_EVENTS_CACHE.glob("*.json"))
+    events = {Event(**json.loads(open(file).read())) for file in events_files}
+
+    # exit if no events are found
+    if exit_if_empty and len(events) == 0:
+        print("\nNo events found! Run `fetch` command to load events from API.")
+        exit()
+    return events
+
+
+def load_rooms(exit_if_empty: bool) -> set[Room]:
+    """Load rooms from disk.
+
+    Args:
+        exit_if_empty: Exit if no events are found, and give instructions.
+    Returns:
+        List of rooms.
+    """
+    # create rooms directory if it doesn't exist
+    DIR_ROOMS_CACHE.mkdir(parents=True, exist_ok=True)
+
+    # load rooms
+    rooms_files = list(DIR_ROOMS_CACHE.glob("*.json"))
+    rooms = {Room(**json.loads(open(file).read())) for file in rooms_files}
+
+    # exit if no events are found
+    if exit_if_empty and len(rooms) == 0:
+        print("\nNo rooms found! Run `fetch` command to load room info from API.")
+        exit()
+    return rooms
+
+
+def load_ratings(exit_if_empty: bool) -> set[Rating]:
+    """Load all ratings from disk.
+
+    Args:
+        exit_if_empty: Exit if no ratings are found, and give instructions.
+    Returns:
+        List of ratings.
+    """
+    # create ratings directory if it doesn't exist
+    DIR_RATINGS_CACHE.mkdir(parents=True, exist_ok=True)
+
+    # load ratings
+    ratings_files = list(DIR_RATINGS_CACHE.glob("*.json"))
+    ratings = {Rating(**json.loads(open(file).read())) for file in ratings_files}
+
+    # exit if no events are found
+    if exit_if_empty and len(ratings) == 0:
+        print("\nNo ratings found! Run `rate` command to rate events.")
+        exit()
+    return ratings
---
+++ packages/optimal-congress/optimal_congress-1.1.0.tar.gz/optimal_congress-1.1.0/optimal_congress/optimize.py
@@ -0,0 +1,82 @@
+"""Schedule optimization."""
+
+import logging
+
+from pulp import PULP_CBC_CMD, LpMaximize, LpProblem, LpStatus, LpVariable
+
+from optimal_congress.schema import Event, EventRating, events_overlap
+
+
+def optimize_schedule(
+    event_ratings: set[EventRating],
+) -> set[Event]:
+    """
+    Optimize the schedule of events based on ratings.
+
+    Args:
+        events_ratings: Tuples of events and matching ratings.
+    Returns:
+        Scheduled events.
+    Raises:
+        ValueError: If no optimal solution is found.
+    """
+    # unpack events and ratings
+    events = [event_rating.event for event_rating in event_ratings]
+    ratings = [event_rating.rating for event_rating in event_ratings]
+
+    # sanity check
+    assert len(events) == len(ratings)
+
+    # define problem
+    prob = LpProblem(name="OptimalCongress", sense=LpMaximize)
+
+    # define decision variables (binary vector of same length as events)
+    lp_vars = [
+        LpVariable(
+            name=event.slug,
+            cat="Binary",
+        )
+        for event in events
+    ]
+
+    # objective function: maximize sum of ratings for scheduled events
+    prob += sum(lp_var * rating.score for lp_var, rating in zip(lp_vars, ratings))
+
+    # constraints: no overlapping events can be scheduled
+    for i, event_i in enumerate(events):
+        for j, event_j in enumerate(events):
+            if i >= j:
+                # avoid double counting, and comparing event to itself
+                continue
+            if events_overlap(event_i, event_j):
+                # rename event names to make work with pulp
+                event_i_name = event_i.slug.replace("-", "_")
+                event_j_name = event_j.slug.replace("-", "_")
+                constraint_name = f"overlap_{event_i_name}_{event_j_name}"
+                prob += (lp_vars[i] + lp_vars[j] <= 1, constraint_name)
+
+    logging.debug("\nProblem:")
+    logging.debug(prob)
+
+    # solve problem
+    prob.solve(PULP_CBC_CMD(msg=False))
+
+    # check if optimal solution was found
+    optimal = LpStatus[prob.status] == "Optimal"
+    if not optimal:
+        raise ValueError("No optimal solution found.")
+
+    logging.debug("solution:")
+    for var in prob.variables():
+        logging.debug(f"{var.name}: {var.varValue}")
+
+    # extract scheduled events
+    scheduled_event_names: set[str] = {
+        var.name for var in prob.variables() if var.varValue == 1
+    }
+    scheduled_events: set[Event] = {
+        event
+        for event in events
+        if event.slug.replace("-", "_") in scheduled_event_names
+    }
+    return scheduled_events
