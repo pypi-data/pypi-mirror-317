5676680dbee4e97bbb5e7294d515e2846777b77a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-25 03:06:31 +0000

    Add package gepeto/1.0.8/gepeto-1.0.8.tar.gz

---
+++ packages/gepeto/gepeto-1.0.8.tar.gz/gepeto-1.0.8/README.md
@@ -0,0 +1,6 @@
+# gepeto
+pip install gepeto
+
+# to update pypi package
+python -m build
+python -m twine upload dist/*
---
+++ packages/gepeto/gepeto-1.0.8.tar.gz/gepeto-1.0.8/gepeto/__init__.py
@@ -0,0 +1,4 @@
+from .team.team import Team
+from .team.schema import Agent, Response, Result
+
+__all__ = ["Team", "Agent", "Response", "Result"]
