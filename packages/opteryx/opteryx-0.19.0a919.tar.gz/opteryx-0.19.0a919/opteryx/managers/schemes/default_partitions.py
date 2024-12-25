a2272f917f72243169795ad7dfe8b0867fb3329e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-25 03:06:42 +0000

    Add package opteryx/0.19.0a919/opteryx-0.19.0a919.tar.gz

---
+++ packages/opteryx/opteryx-0.19.0a919.tar.gz/opteryx-0.19.0a919/opteryx/managers/schemes/default_partitions.py
@@ -0,0 +1,31 @@
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+import datetime
+from typing import Callable
+from typing import List
+from typing import Optional
+
+from opteryx.managers.schemes import BasePartitionScheme
+
+
+class DefaultPartitionScheme(BasePartitionScheme):
+    def get_blobs_in_partition(
+        self,
+        *,
+        blob_list_getter: Callable,
+        prefix: str,
+        start_date: Optional[datetime.datetime],
+        end_date: Optional[datetime.datetime],
+        **kwargs,
+    ) -> List[str]:
+        return blob_list_getter(prefix=prefix)
