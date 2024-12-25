c628110a73a2768cb78630afa7fa698f2fb9c0f2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-25 05:04:09 +0000

    Add package gultron/1.1.1/gultron-1.1.1-py3-none-any.whl

---
+++ packages/gultron/gultron-1.1.1-py3-none-any.whl/gultron/utils.py
@@ -0,0 +1,11 @@
+import subprocess
+
+def install_clipboard_tool():
+    """Installs necessary clipboard tools on Linux if they are not available."""
+    from gultron.logs import LOGGER
+    try:
+        LOGGER.info("Installing clipboard tool...")
+        subprocess.check_call(["sudo", "apt-get", "install", "-y", "xclip"])
+        LOGGER.info("xclip installed successfully.")
+    except subprocess.CalledProcessError:
+        LOGGER.error("Failed to install clipboard tool. Please install it manually.")
