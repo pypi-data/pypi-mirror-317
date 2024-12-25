921e0e51bb4d60081b3ef0283025b272f139d7b7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-25 14:03:37 +0000

    Add package tapflow/0.2.50/tapflow-0.2.50.tar.gz

---
+++ packages/tapflow/tapflow-0.2.50.tar.gz/tapflow-0.2.50/setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup, find_packages
+with open('requirements.txt') as f:
+    required = f.read().splitlines()
+
+setup(
+    name='tapflow',
+    version='0.2.50',
+    packages=find_packages(),
+    install_requires=required,
+    include_package_data=True,
+    package_data={
+        '': ['*.md', '*.txt', '*.sh', "*.py", '*.project'],
+    },
+    entry_points={
+        'console_scripts': [
+            'tap=tapflow.cli.tap:main',
+        ],
+    },
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.6',
+)
