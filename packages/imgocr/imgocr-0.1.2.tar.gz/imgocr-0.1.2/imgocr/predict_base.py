e97aa25156da70592e5c8754497adc2fce4ddb3a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-25 15:04:11 +0000

    Add package imgocr/0.1.2/imgocr-0.1.2.tar.gz

---
+++ packages/imgocr/imgocr-0.1.2.tar.gz/imgocr-0.1.2/imgocr/predict_base.py
@@ -0,0 +1,54 @@
+import onnxruntime as ort
+
+
+class PredictBase:
+
+    def get_onnx_session(self, model_path, use_gpu=False, gpu_id=0):
+        # Use GPU if requested and available
+        if use_gpu:
+            providers = [
+                (
+                    "CUDAExecutionProvider",
+                    {"device_id": gpu_id, "cudnn_conv_algo_search": "DEFAULT"},
+                )
+            ]
+        else:
+            providers = ["CPUExecutionProvider"]
+        # Create the ONNX inference session with the determined providers
+        onnx_session = ort.InferenceSession(model_path, sess_options=None, providers=providers)
+
+        return onnx_session
+
+    def get_output_name(self, onnx_session):
+        """
+        output_name = onnx_session.get_outputs()[0].name
+        :param onnx_session:
+        :return:
+        """
+        output_name = []
+        for node in onnx_session.get_outputs():
+            output_name.append(node.name)
+        return output_name
+
+    def get_input_name(self, onnx_session):
+        """
+        input_name = onnx_session.get_inputs()[0].name
+        :param onnx_session:
+        :return:
+        """
+        input_name = []
+        for node in onnx_session.get_inputs():
+            input_name.append(node.name)
+        return input_name
+
+    def get_input_feed(self, input_name, image_numpy):
+        """
+        input_feed={self.input_name: image_numpy}
+        :param input_name:
+        :param image_numpy:
+        :return:
+        """
+        input_feed = {}
+        for name in input_name:
+            input_feed[name] = image_numpy
+        return input_feed
---
+++ packages/imgocr/imgocr-0.1.2.tar.gz/imgocr-0.1.2/tests/test_diff_input_img.py
@@ -0,0 +1,63 @@
+# -*- coding: utf-8 -*-
+"""
+@author:XuMing(xuming624@qq.com)
+@description: 
+"""
+
+import sys
+import unittest
+import os
+from pathlib import Path
+sys.path.append('..')
+from imgocr.ppocr_onnx import ImgOcr
+
+pwd_path = os.path.abspath(os.path.dirname(__file__))
+
+
+class TestImgOcr(unittest.TestCase):
+
+    def setUp(self):
+        # Initialize the ImgOcr instance with default parameters
+        self.m = ImgOcr()
+        self.img_path = os.path.join(pwd_path, 'data/11.jpg')
+
+    def test_ocr_img_path(self):
+        result = self.m.ocr(self.img_path)
+        print(result)
+        self.assertIsInstance(result, list)
+        self.assertGreater(len(result), 0)
+
+    def test_ocr_img_pil(self):
+        from PIL import Image
+        img = Image.open(self.img_path)
+        result = self.m.ocr(img)
+        print(result)
+        self.assertIsInstance(result, list)
+        self.assertGreater(len(result), 0)
+
+    def test_ocr_img_np(self):
+        import cv2
+        img = cv2.imread(self.img_path)
+        result = self.m.ocr(img)
+        print(result)
+        self.assertIsInstance(result, list)
+        self.assertGreater(len(result), 0)
+
+    def test_ocr_img_bytes(self):
+        with open(self.img_path, 'rb') as f:
+            img_bytes = f.read()
+        result = self.m.ocr(img_bytes)
+        print(result)
+        self.assertIsInstance(result, list)
+        self.assertGreater(len(result), 0)
+
+    def test_ocr_img_pathlib(self):
+        img_path = Path(self.img_path)
+        result = self.m.ocr(img_path)
+        print(result)
+        self.assertIsInstance(result, list)
+        self.assertGreater(len(result), 0)
+
+
+if __name__ == '__main__':
+    unittest.main()
