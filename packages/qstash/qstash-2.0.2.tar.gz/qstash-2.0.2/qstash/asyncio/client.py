f2360d8057dd5027ff8686141d88316b3c8b33ca
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-25 04:04:05 +0000

    Add package qstash/2.0.2/qstash-2.0.2.tar.gz

---
+++ packages/qstash/qstash-2.0.2.tar.gz/qstash-2.0.2/qstash/asyncio/client.py
@@ -0,0 +1,54 @@
+from typing import Literal, Optional, Union
+
+from qstash.asyncio.chat import AsyncChatApi
+from qstash.asyncio.dlq import AsyncDlqApi
+from qstash.asyncio.event import AsyncEventApi
+from qstash.asyncio.http import AsyncHttpClient
+from qstash.asyncio.message import AsyncMessageApi
+from qstash.asyncio.queue import AsyncQueueApi
+from qstash.asyncio.schedule import AsyncScheduleApi
+from qstash.asyncio.signing_key import AsyncSigningKeyApi
+from qstash.asyncio.url_group import AsyncUrlGroupApi
+from qstash.http import RetryConfig
+
+
+class AsyncQStash:
+    def __init__(
+        self,
+        token: str,
+        *,
+        retry: Optional[Union[Literal[False], RetryConfig]] = None,
+        base_url: Optional[str] = None,
+    ) -> None:
+        """
+        :param token: The authorization token from the Upstash console.
+        :param retry: Configures how the client should retry requests.
+        """
+        http = AsyncHttpClient(
+            token,
+            retry,
+            base_url,
+        )
+        self.message = AsyncMessageApi(http)
+        """Message api."""
+
+        self.url_group = AsyncUrlGroupApi(http)
+        """Url group api."""
+
+        self.queue = AsyncQueueApi(http)
+        """Queue api."""
+
+        self.schedule = AsyncScheduleApi(http)
+        """Schedule api."""
+
+        self.signing_key = AsyncSigningKeyApi(http)
+        """Signing key api."""
+
+        self.event = AsyncEventApi(http)
+        """Event api."""
+
+        self.dlq = AsyncDlqApi(http)
+        """Dlq (Dead Letter Queue) api."""
+
+        self.chat = AsyncChatApi(http)
+        """Chat api."""
