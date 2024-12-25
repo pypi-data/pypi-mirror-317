faf436884823d4ff3c703da42bf6812605d9d4d0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-25 03:07:02 +0000

    Add package django-musicbrainz-connector/0.0.3/django_musicbrainz_connector-0.0.3-py3-none-any.whl

---
+++ packages/django-musicbrainz-connector/django_musicbrainz_connector-0.0.3-py3-none-any.whl/django_musicbrainz_connector/models/__init__.py
@@ -0,0 +1,37 @@
+from django_musicbrainz_connector.models.artist_credit import ArtistCredit
+from django_musicbrainz_connector.models.language import Language
+from django_musicbrainz_connector.models.link import Link
+from django_musicbrainz_connector.models.link_type import LinkType
+from django_musicbrainz_connector.models.medium import Medium
+from django_musicbrainz_connector.models.medium_format import MediumFormat
+from django_musicbrainz_connector.models.recording import Recording
+from django_musicbrainz_connector.models.recording_work_link import RecordingWorkLink
+from django_musicbrainz_connector.models.release import Release
+from django_musicbrainz_connector.models.release_group import ReleaseGroup
+from django_musicbrainz_connector.models.release_group_primary_type import ReleaseGroupPrimaryType
+from django_musicbrainz_connector.models.release_packaging import ReleasePackaging
+from django_musicbrainz_connector.models.release_status import ReleaseStatus
+from django_musicbrainz_connector.models.script import Script
+from django_musicbrainz_connector.models.track import Track
+from django_musicbrainz_connector.models.work import Work
+from django_musicbrainz_connector.models.work_type import WorkType
+
+__all__ = [
+    "ArtistCredit",
+    "Language",
+    "Link",
+    "LinkType",
+    "Medium",
+    "MediumFormat",
+    "Recording",
+    "RecordingWorkLink",
+    "Release",
+    "ReleaseGroup",
+    "ReleaseGroupPrimaryType",
+    "ReleasePackaging",
+    "ReleaseStatus",
+    "Script",
+    "Track",
+    "Work",
+    "WorkType",
+]
