dedd9af6d493bcf782a5cd295976c36b3b94d0de
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-25 15:04:12 +0000

    Add package cam-cli-tool/0.2.13/cam_cli_tool-0.2.13-py3-none-any.whl

---
+++ packages/cam-cli-tool/cam_cli_tool-0.2.13-py3-none-any.whl/cam/main.py
@@ -0,0 +1,12 @@
+from controller import controller
+
+
+def main():
+    message = controller()
+
+    if message is not None:
+        print(f'{message}')
+
+
+if __name__ == "__main__":
+    main()
