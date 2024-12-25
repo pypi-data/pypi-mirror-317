1bc74265c97e4b14e2c4594f7e20466d35b9cada
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-25 14:04:08 +0000

    Add package deploy-remote/0.0.1/deploy_remote-0.0.1.tar.gz

---
+++ packages/deploy-remote/deploy_remote-0.0.1.tar.gz/deploy_remote-0.0.1/deploy_remote/__version__.py
@@ -0,0 +1,10 @@
+# -*-coding:utf-8-*-
+# Author: Eason.Deng
+# Github: https://github.com/holbos-deng
+# Email: 2292861292@qq.com
+# CreateDate: 2022/5/23 14:42
+# Description:
+
+VERSION = (0, 0, 1)
+
+__version__ = '.'.join(map(str, VERSION))
