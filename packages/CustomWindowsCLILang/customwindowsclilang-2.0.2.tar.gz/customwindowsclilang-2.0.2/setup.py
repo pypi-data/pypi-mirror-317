e284b8d81c5ae5b8711ba6b2fbbd6082f700b40a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-25 14:04:11 +0000

    Add package CustomWindowsCLILang/2.0.2/customwindowsclilang-2.0.2.tar.gz

---
+++ packages/CustomWindowsCLILang/customwindowsclilang-2.0.2.tar.gz/customwindowsclilang-2.0.2/setup.py
@@ -0,0 +1,49 @@
+# from setuptools import setup, find_packages  # Ensure this is imported
+
+# setup(
+#     name="CustomWindowsCLILang",  # New package name
+#     version="2.0.1",
+#     description="A custom CLI programming language for handling Windows tasks.",
+#     long_description=open("README.md", "r", encoding="utf-8").read(),
+#     long_description_content_type="text/markdown",
+#     author="vinit",
+#     author_email="vineetp6@gmail.com",
+#     url="https://github.com/vineetp6/CustomWindowsCLILang",  # Update GitHub repo URL if applicable
+#     packages=find_packages(),
+#     entry_points={
+#         "console_scripts": [
+#             "customwincli=cli_interpreter:main",  # New CLI command
+#         ],
+#     },
+#     install_requires=[],
+#     classifiers=[
+#         "Programming Language :: Python :: 3",
+#         "Operating System :: Microsoft :: Windows",
+#         "License :: OSI Approved :: MIT License",
+#     ],
+#     python_requires=">=3.7",
+#     license="MIT",
+# )
+
+
+from setuptools import setup, find_packages
+
+setup(
+    name="CustomWindowsCLILang",
+    version="2.0.2",  # Increment the version
+    description="A custom CLI language for managing Windows tasks",
+    author="vinit",
+    author_email="vineetp6@gmail.com",
+    packages=find_packages(),
+    entry_points={
+        'console_scripts': [
+            'open=core.cli_interpreter:main',  # Maps `open` to `main` function in `cli_interpreter.py`
+        ],
+    },
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Operating System :: Microsoft :: Windows",
+    ],
+    python_requires='>=3.6',
+)
+
