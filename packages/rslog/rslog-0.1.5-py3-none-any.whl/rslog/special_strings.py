8224c887b6e12ef3ba7b46b4d46afde21bf37bdf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-25 04:03:46 +0000

    Add package rslog/0.1.5/rslog-0.1.5-py3-none-any.whl

---
+++ packages/rslog/rslog-0.1.5-py3-none-any.whl/rslog/special_strings.py
@@ -0,0 +1,45 @@
+error_strings = [
+    "error",
+    "failed",
+    "exception",
+    "critical",
+    "fatal",
+    "unhandled",
+    "panic",
+    "abort",
+    "traceback",
+    "corrupt",
+]
+
+warning_strings = [
+    "warning",
+    "unsupported",
+    "not implemented",
+    "unable",
+    "unknown",
+    "unrecognized",
+    "deprecated",
+    "obsolete",
+    "outdated",
+    "old",
+    "invalid",
+    "incompatible",
+    "unstable",
+    "experimental",
+    "untrusted",
+    "unverified",
+    "unconfirmed",
+    "untested",
+    "unreliable",
+]
+
+
+success_strings = [
+    "success",
+    "completed",
+    "done",
+    "finished",
+    "succeeded",
+    "passed",
+    "good",
+]
