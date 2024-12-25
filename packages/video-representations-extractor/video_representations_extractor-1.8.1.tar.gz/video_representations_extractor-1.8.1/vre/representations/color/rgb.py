ad8ead49b4051823fe699010bd89b8eed9a4ab24
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-25 03:06:44 +0000

    Add package video-representations-extractor/1.8.1/video_representations_extractor-1.8.1.tar.gz

---
+++ packages/video-representations-extractor/video_representations_extractor-1.8.1.tar.gz/video_representations_extractor-1.8.1/vre/representations/color/rgb.py
@@ -0,0 +1,33 @@
+"""RGB representation."""
+from __future__ import annotations
+import numpy as np
+from overrides import overrides
+
+from vre.utils import VREVideo, MemoryData
+from vre.representations import (
+    Representation, ReprOut, ComputeRepresentationMixin, NpIORepresentation, NormedRepresentationMixin)
+
+class RGB(Representation, ComputeRepresentationMixin, NpIORepresentation, NormedRepresentationMixin):
+    """Basic RGB representation."""
+    def __init__(self, *args, **kwargs):
+        Representation.__init__(self, *args, **kwargs)
+        ComputeRepresentationMixin.__init__(self)
+        NpIORepresentation.__init__(self)
+        NormedRepresentationMixin.__init__(self)
+        assert len(self.dependencies) == 0, self.dependencies
+        self.output_dtype = "uint8"
+
+    @overrides
+    def compute(self, video: VREVideo, ixs: list[int]):
+        assert self.data is None, f"[{self}] data must not be computed before calling this"
+        self.data = ReprOut(frames=video[ixs], output=MemoryData(video[ixs]), key=ixs) # video[ixs] is cached
+
+    @overrides
+    def make_images(self, data: ReprOut) -> np.ndarray:
+        y = self.unnormalize(data.output) if self.normalization is not None else self.data.output
+        return y.astype(np.uint8)
+
+    @property
+    @overrides
+    def n_channels(self) -> int:
+        return 3
---
+++ packages/video-representations-extractor/video_representations_extractor-1.8.1.tar.gz/video_representations_extractor-1.8.1/vre/representations/edges/canny.py
@@ -0,0 +1,42 @@
+"""Canny edge detector representation."""
+import numpy as np
+from overrides import overrides
+
+from vre.representations import (Representation, ReprOut, ComputeRepresentationMixin,
+                                 NpIORepresentation, NormedRepresentationMixin)
+from vre.utils import VREVideo, MemoryData
+from vre.utils.cv2_utils import cv2_Canny
+
+class Canny(Representation, ComputeRepresentationMixin, NpIORepresentation, NormedRepresentationMixin):
+    """Canny edge detector representation."""
+    def __init__(self, threshold1: float, threshold2: float, aperture_size: int, l2_gradient: bool, **kwargs):
+        Representation.__init__(self, **kwargs)
+        ComputeRepresentationMixin.__init__(self)
+        NpIORepresentation.__init__(self)
+        NormedRepresentationMixin.__init__(self)
+        self.threshold1 = threshold1
+        self.threshold2 = threshold2
+        self.aperture_size = aperture_size
+        self.l2_gradient = l2_gradient
+
+    @overrides
+    def compute(self, video: VREVideo, ixs: list[int]):
+        assert self.data is None, f"[{self}] data must not be computed before calling this"
+        self.data = ReprOut(frames=video[ixs], key=ixs,
+                            output=MemoryData([self._make_one(frame) for frame in video[ixs]]))
+
+    @overrides
+    def make_images(self, data: ReprOut) -> np.ndarray:
+        assert self.data is not None, f"[{self}] data must be first computed using compute()"
+        return (255 * self.data.output.repeat(3, axis=-1)).astype(np.uint8)
+
+    @property
+    @overrides
+    def n_channels(self) -> int:
+        return 1
+
+    def _make_one(self, x: np.ndarray) -> np.ndarray:
+        res = cv2_Canny(x, threshold1=self.threshold1, threshold2=self.threshold2,
+                        apertureSize=self.aperture_size, L2gradient=self.l2_gradient)
+        res = np.float32(res) / 255
+        return res[..., None]
