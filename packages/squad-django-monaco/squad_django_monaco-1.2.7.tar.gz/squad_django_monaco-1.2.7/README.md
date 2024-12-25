31a2c6758d2e11cd27eceb2c52fb17ef6c937a10
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-25 05:04:07 +0000

    Add package squad-django-monaco/1.2.7/squad_django_monaco-1.2.7.tar.gz

---
+++ packages/squad-django-monaco/squad_django_monaco-1.2.7.tar.gz/squad_django_monaco-1.2.7/README.md
@@ -0,0 +1,11 @@
+# Django Monaco Editor
+
+This works with django 1.9 & 1.11
+
+## Installation
+
+To install the package by `pip` run following command
+
+```sh
+$ pip install squad-django-monaco
+```
---
+++ packages/squad-django-monaco/squad_django_monaco-1.2.7.tar.gz/squad_django_monaco-1.2.7/setup.py
@@ -0,0 +1,25 @@
+import os
+from setuptools import setup, find_packages
+
+README = open(
+    os.path.join(os.path.abspath(os.path.dirname(__file__)), "README.md")
+).read()
+
+setup(
+    name="squad-django-monaco",
+    version="1.2.7",
+    packages=["monaco"],
+    description="Monaco editor widgets in the Django Admin",
+    include_package_data=True,
+    long_description=README,
+    long_description_content_type="text/markdown",
+    author="Ayush Shanker",
+    keywords=[
+        "monaco",
+    ],
+    platforms=["OS Independent"],
+    license="MIT",
+    install_requires=[
+        "Django<=5.2",
+    ],
+)
