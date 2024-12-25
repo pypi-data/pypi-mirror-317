a06d62a3fc8fb995aeb5cf88deb257bba66cb674
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-25 04:04:25 +0000

    Add package tmuxp/1.50.1/tmuxp-1.50.1-py3-none-any.whl

---
+++ packages/tmuxp/tmuxp-1.50.1-py3-none-any.whl/tmuxp/cli/ls.py
@@ -0,0 +1,28 @@
+"""CLI for ``tmuxp ls`` subcommand."""
+
+import argparse
+import os
+import typing as t
+
+from tmuxp.workspace.constants import VALID_WORKSPACE_DIR_FILE_EXTENSIONS
+from tmuxp.workspace.finders import get_workspace_dir
+
+
+def create_ls_subparser(
+    parser: argparse.ArgumentParser,
+) -> argparse.ArgumentParser:
+    """Augment :class:`argparse.ArgumentParser` with ``ls`` subcommand."""
+    return parser
+
+
+def command_ls(
+    parser: t.Optional[argparse.ArgumentParser] = None,
+) -> None:
+    """Entrypoint for ``tmuxp ls`` subcommand."""
+    tmuxp_dir = get_workspace_dir()
+    if os.path.exists(tmuxp_dir) and os.path.isdir(tmuxp_dir):
+        for f in sorted(os.listdir(tmuxp_dir)):
+            stem, ext = os.path.splitext(f)
+            if os.path.isdir(f) or ext not in VALID_WORKSPACE_DIR_FILE_EXTENSIONS:
+                continue
+            print(stem)
