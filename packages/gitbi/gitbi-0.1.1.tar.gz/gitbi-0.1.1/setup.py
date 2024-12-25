0d5c4518a6f1a6d5eb676a3e2fde9ef98b03c9b2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-25 15:04:28 +0000

    Add package gitbi/0.1.1/gitbi-0.1.1.tar.gz

---
+++ packages/gitbi/gitbi-0.1.1.tar.gz/gitbi-0.1.1/setup.py
@@ -0,0 +1,22 @@
+import os
+from setuptools import setup, find_packages
+
+setup(
+    name='gitbi',
+    version='0.1.1',
+    packages=find_packages(exclude=('example', 'example.*')),
+    install_requires=[
+    ],
+    author='Reuven Peleg',
+    author_email='your.email@example.com',
+    description='A tool for automating git bisect operations',
+    long_description=open('README.md', 'r', encoding='utf-8').read() if os.path.exists('README.md') else '',
+    long_description_content_type='text/markdown',
+    url='https://github.com/R-Peleg/python_git_bisector',
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.6',
+)
