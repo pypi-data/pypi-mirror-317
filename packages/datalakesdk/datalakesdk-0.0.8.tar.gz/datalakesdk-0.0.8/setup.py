1dbff093b04e3bebcfc5a19fb42a79847347cdab
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-25 14:03:43 +0000

    Add package datalakesdk/0.0.8/datalakesdk-0.0.8.tar.gz

---
+++ packages/datalakesdk/datalakesdk-0.0.8.tar.gz/datalakesdk-0.0.8/setup.py
@@ -0,0 +1,23 @@
+import setuptools
+from setuptools import find_packages
+
+setuptools.setup(name='datalakesdk',
+                 version="0.0.8",
+                 description='DataLake SDK',
+                 long_description=open('README.md').read().strip(),
+                 long_description_content_type="text/markdown",
+                 author='ivan',
+                 author_email='ivan.liu@anker-in.com',
+                 url='',
+                 # py_modules=['sdk'],
+                install_requires=[
+                    "requests==2.31.0",
+                    "pydantic==2.9.2",
+                    "hachoir==3.3.0",
+                    "httpx==0.27.1"
+                ],
+                 license='MIT License',
+                 zip_safe=False,
+                 keywords='',
+                 packages=find_packages()
+)
