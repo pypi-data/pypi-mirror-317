2a53e1807f1f7249f600b306b6806e616785977a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-25 15:04:16 +0000

    Add package hello-world-programm/0.0.1/hello-world-programm-0.0.1.tar.gz

---
+++ packages/hello-world-programm/hello-world-programm-0.0.1.tar.gz/hello-world-programm-0.0.1/setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="hello-world-programm",
+    version="0.0.1",
+    author="Gaurav Chaudhary",
+    description='a hello world description example',
+    packages=find_packages(),
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent'
+    ],
+)
