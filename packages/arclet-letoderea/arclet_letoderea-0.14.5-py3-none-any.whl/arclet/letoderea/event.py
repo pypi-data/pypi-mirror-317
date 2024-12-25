8ca5d7a996a58374eb205efe05b10e7a8183471f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-25 14:04:11 +0000

    Add package arclet-letoderea/0.14.5/arclet_letoderea-0.14.5-py3-none-any.whl

---
+++ packages/arclet-letoderea/arclet_letoderea-0.14.5-py3-none-any.whl/arclet/letoderea/event.py
@@ -0,0 +1,13 @@
+from __future__ import annotations
+
+from typing import Any, Final, Protocol, runtime_checkable
+
+from .typing import Contexts, CtxItem
+
+
+@runtime_checkable
+class BaseEvent(Protocol):
+    async def gather(self, context: Contexts) -> Any: ...
+
+
+EVENT: Final[CtxItem[BaseEvent]] = CtxItem("event")
