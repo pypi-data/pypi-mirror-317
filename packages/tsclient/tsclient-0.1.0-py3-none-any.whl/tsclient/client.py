9be92309e4bbf2d5d0050c1838693590d58cec61
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-25 14:04:13 +0000

    Add package tsclient/0.1.0/tsclient-0.1.0-py3-none-any.whl

---
+++ packages/tsclient/tsclient-0.1.0-py3-none-any.whl/tsclient/client.py
@@ -0,0 +1,12 @@
+class TalkScriberClient:
+    """Main client class for interacting with Talkscriber API."""
+    
+    def __init__(self, api_key=None):
+        self.api_key = api_key
+        
+    def connect(self):
+        """Establish connection to Talkscriber service."""
+        pass
+    
+    def disconnect(self):
+        """Close connection to Talkscriber service.""" 
