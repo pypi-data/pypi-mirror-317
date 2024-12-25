5d0d37b8dc7135c21da45e98531f97937bb512af
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-25 05:04:07 +0000

    Add package font-rename-fm/0.1.1/font_rename_fm-0.1.1.tar.gz

---
+++ packages/font-rename-fm/font_rename_fm-0.1.1.tar.gz/font_rename_fm-0.1.1/font_rename_fm/__init__.py
@@ -0,0 +1,23 @@
+# font_rename_fm/__init__.py
+
+(
+    SUCCESS,
+    DIR_ERROR,
+    FILE_ERROR,
+    DB_READ_ERROR,
+    DB_WRITE_ERROR,
+    JSON_ERROR,
+    ID_ERROR,
+    ITEM_NOT_FOUND_ERROR,
+) = range(8)
+
+ERRORS = {
+    DIR_ERROR: "config directory error",
+    FILE_ERROR: "config file error",
+    DB_READ_ERROR: "database read error",
+    DB_WRITE_ERROR: "database write error",
+    ID_ERROR: "expense id error",
+    ITEM_NOT_FOUND_ERROR: "item not found error",
+}
+
+from .rename import *
