9ac05c4546b128fe4a80323bf950869b804e5a82
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-25 14:04:18 +0000

    Add package streamlit-mui-elements/0.0.13/streamlit_mui_elements-0.0.13.tar.gz

---
+++ packages/streamlit-mui-elements/streamlit_mui_elements-0.0.13.tar.gz/streamlit_mui_elements-0.0.13/setup.py
@@ -0,0 +1,39 @@
+from setuptools import setup, find_packages
+
+# Read the content of your README.md file
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setup(
+    name="streamlit-mui-elements",
+    version="0.0.13",
+    author="Tom Jose",
+    author_email="tomj0311@gmail.com",
+    description="A Streamlit component for Material-UI elements",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/your_username/streamlit-mui-elements",  # Update with your repository URL
+    packages=find_packages(),
+    include_package_data=True,
+    package_data={
+        "streamlit_mui_elements": ["frontend/build/**/*"],
+    },
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: Apache Software License",  # Set to Apache 2.0
+        "Operating System :: OS Independent",
+    ],
+    license="Apache 2.0",  # Specify the license explicitly
+    python_requires=">=3.6",
+    install_requires=[
+        # By definition, a Custom Component depends on Streamlit.
+        # If your component has other Python dependencies, list
+        # them here.
+        "streamlit>=1.0.0",
+    ],
+    extras_require={
+        "devel": [
+            "wheel",
+        ]
+    }
+)
