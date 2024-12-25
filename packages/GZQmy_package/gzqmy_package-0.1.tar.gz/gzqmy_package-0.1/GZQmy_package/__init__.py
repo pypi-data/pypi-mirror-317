299aec9d70df01c74112afd6f8d787ecbbba2074
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-25 03:06:59 +0000

    Add package GZQmy_package/0.1/gzqmy_package-0.1.tar.gz

---
+++ packages/GZQmy_package/gzqmy_package-0.1.tar.gz/gzqmy_package-0.1/GZQmy_package/__init__.py
@@ -0,0 +1,11 @@
+"""
+This is the GZQmy_package package.
+
+Description of the functionality provided by the package.
+
+Author: Zq G
+Version: 0.1
+"""
+__version__ = "0.1"
+
+from .module1 import add, subtract
---
+++ packages/GZQmy_package/gzqmy_package-0.1.tar.gz/gzqmy_package-0.1/GZQmy_package/module1.py
@@ -0,0 +1,7 @@
+# my_package/module1.py
+
+def add(a, b):
+    return a + b
+
+def subtract(a, b):
+    return a - b
