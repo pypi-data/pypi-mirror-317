1aacddce6fe7b948bd3053614ccf61d85039d5d0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-25 03:06:36 +0000

    Add package nlp-data/0.4.3/nlp_data-0.4.3-py3-none-any.whl

---
+++ packages/nlp-data/nlp_data-0.4.3-py3-none-any.whl/nlp_data/document/__init__.py
@@ -0,0 +1,4 @@
+from .api import APIDoc, APIDocList, APIParam
+from .embedding import EmbeddingDoc, EmbeddingDocList
+from .nlu import NLUDoc, NLUDocList
+from .dialogue import DialogueDoc, DialogueDocList, FucntionCallDialogueDoc, FucntionCallDialogueDocList
