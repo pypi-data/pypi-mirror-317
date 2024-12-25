10676e6a363c3eb511a8e4aea3473446b93ceece
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-25 15:04:22 +0000

    Add package flax-pilot/0.2.0/flax_pilot-0.2.0.tar.gz

---
+++ packages/flax-pilot/flax_pilot-0.2.0.tar.gz/flax_pilot-0.2.0/setup.py
@@ -0,0 +1,28 @@
+from setuptools import setup, find_packages
+
+
+setup(
+    name='flax-pilot',
+    version='0.2.0',
+    author='Nithish M',
+    author_email='nithishm2206@gmail.com',
+    description='A Simplistic trainer for Flax',
+    long_description=open('README.md', encoding='utf-8').read(),
+    long_description_content_type='text/markdown',
+    url='https://github.com/NITHISHM2410/flax-pilot',
+    packages=find_packages(include=['fpilot', 'fpilot.*']),
+    install_requires=[
+        "jax[cuda12]==0.4.37",
+        'flax==0.10.2',
+        'optax==0.2.4',
+        'numpy',
+        'tqdm',
+        'mergedeep',
+        'orbax-checkpoint==0.7.0',
+    ],
+    classifiers=[
+        "Programming Language :: Python :: 3.10",
+        'License :: OSI Approved :: MIT License',
+    ],
+    python_requires='>=3.10',
+)
