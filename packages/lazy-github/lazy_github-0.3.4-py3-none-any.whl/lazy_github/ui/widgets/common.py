256625dba0922c2e92e46af86e51d850da64f890
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-25 15:04:28 +0000

    Add package lazy-github/0.3.4/lazy_github-0.3.4-py3-none-any.whl

---
+++ packages/lazy-github/lazy_github-0.3.4-py3-none-any.whl/lazy_github/ui/widgets/common.py
@@ -0,0 +1,202 @@
+from asyncio import Lock
+from typing import Awaitable, Callable, Iterable
+
+from textual import on, work
+from textual.app import ComposeResult
+from textual.containers import Container, Vertical
+from textual.events import Blur
+from textual.widgets import DataTable, Footer, Input
+
+from lazy_github.lib.bindings import LazyGithubBindings
+
+TABLE_POPULATION_FUNCTION = Callable[[int, int], Awaitable[list[tuple[str | int, ...]]]]
+
+
+class LazyGithubFooter(Footer):
+    def __init__(self) -> None:
+        super().__init__(show_command_palette=False)
+
+
+class _VimLikeDataTable(DataTable[str | int]):
+    "An data table for LazyGithub that provides some more vim-like bindings"
+
+    BINDINGS = [
+        LazyGithubBindings.SELECT_ENTRY,
+        LazyGithubBindings.TABLE_DOWN,
+        LazyGithubBindings.TABLE_PAGE_DOWN,
+        LazyGithubBindings.TABLE_CURSOR_UP,
+        LazyGithubBindings.TABLE_PAGE_UP,
+        LazyGithubBindings.TABLE_SCROLL_RIGHT,
+        LazyGithubBindings.TABLE_PAGE_RIGHT,
+        LazyGithubBindings.TABLE_SCROLL_LEFT,
+        LazyGithubBindings.TABLE_PAGE_LEFT,
+        LazyGithubBindings.TABLE_SCROLL_TOP,
+        LazyGithubBindings.TABLE_SCROLL_BOTTOM,
+        LazyGithubBindings.TABLE_PAGE_LEFT,
+        LazyGithubBindings.TABLE_PAGE_RIGHT,
+    ]
+
+
+class ToggleableSearchInput(Input):
+    def _on_blur(self, event: Blur) -> None:
+        if not self.value.strip():
+            # If we lose focus and the content is empty, hide it
+            self.can_focus = False
+            self.display = False
+        return super()._on_blur(event)
+
+
+class SearchableDataTable(Vertical):
+    BINDINGS = [LazyGithubBindings.SEARCH_TABLE]
+
+    DEFAULT_CSS = """
+    ToggleableSearchInput {
+        margin-bottom: 1;
+    }
+    """
+
+    def __init__(
+        self, table_id: str, search_input_id: str, sort_key: str, *args, reverse_sort: bool = False, **kwargs
+    ) -> None:
+        super().__init__(*args, **kwargs)
+        self.table = _VimLikeDataTable(id=table_id)
+        self.search_input = ToggleableSearchInput(placeholder="Search...", id=search_input_id)
+        self.search_input.display = False
+        self.search_input.can_focus = False
+        self.sort_key = sort_key
+        self.reverse_sort = reverse_sort
+        self._rows_cache: list[tuple[str | int, ...]] = []
+
+    def sort(self):
+        self.table.sort(self.sort_key, reverse=self.reverse_sort)
+
+    def compose(self) -> ComposeResult:
+        yield self.search_input
+        yield self.table
+
+    async def action_focus_search(self) -> None:
+        self.search_input.can_focus = True
+        self.search_input.display = True
+        self.search_input.focus()
+
+    def clear_rows(self):
+        """Removes all rows currently displayed and tracked in this table"""
+        self._rows_cache = []
+        self.table.clear()
+
+    def add_row(self, *cells: str | int, key: str | None = None) -> None:
+        self._rows_cache.append(tuple(cells))
+        self.table.add_row(*cells, key=key)
+
+    def add_rows(self, rows: Iterable[tuple[str | int, ...]], keys: Iterable[str] | None = None) -> None:
+        """Add new rows to the currently displayed table and cache"""
+        self._rows_cache.extend(rows)
+        # TODO: Should this actually call handle_submitted_search so that new rows which don't match criteria aren't
+        # shown?
+        if keys:
+            for row, key in zip(rows, keys):
+                self.table.add_row(*row, key=key)
+        else:
+            self.table.add_rows(rows)
+        self.sort()
+
+    def set_rows(self, rows: list[tuple[str | int, ...]]) -> None:
+        """Override the set of rows contained in this table. This will remove any existing rows"""
+        self._rows_cache = rows
+        self.change_displayed_rows(rows)
+
+    def change_displayed_rows(self, rows: Iterable[tuple[str | int, ...]]) -> None:
+        """Change which rows are currently displayed in the table"""
+        self.table.clear()
+        self.table.add_rows(rows)
+        self.sort()
+
+    @on(Input.Submitted)
+    async def handle_submitted_search(self) -> None:
+        search_query = self.search_input.value.strip().lower()
+        filtered_rows: Iterable[tuple[str | int, ...]] = []
+        for row in self._rows_cache:
+            if search_query in str(row).lower() or not search_query:
+                filtered_rows.append(row)
+
+        self.change_displayed_rows(filtered_rows)
+        self.table.focus()
+
+
+class LazilyLoadedDataTable(SearchableDataTable):
+    """A searchable data table that is lazily loaded when you have viewed the currently loaded data"""
+
+    def __init__(
+        self,
+        table_id: str,
+        search_input_id: str,
+        sort_key: str,
+        load_function: TABLE_POPULATION_FUNCTION | None,
+        batch_size: int,
+        *args,
+        load_more_data_buffer: int = 5,
+        reverse_sort: bool = False,
+        **kwargs,
+    ) -> None:
+        super().__init__(table_id, search_input_id, sort_key, *args, reverse_sort=reverse_sort, **kwargs)
+        self.fetch_lock = Lock()
+        self.load_function = load_function
+        self.batch_size = batch_size
+        self.load_more_data_buffer = load_more_data_buffer
+        self.current_batch = 0
+
+        # We initialize this to true and set it to false later if we believe we've run out of data to load from the load
+        # function.
+        self.can_load_more = True
+
+    async def initialize(self) -> None:
+        if not self.load_function:
+            return
+
+        initial_data = await self.load_function(self.batch_size, self.current_batch)
+        self.set_rows(initial_data)
+
+        if len(initial_data) == 0:
+            self.can_load_more = False
+
+    def change_load_function(self, new_load_function: TABLE_POPULATION_FUNCTION | None) -> None:
+        self.load_function = new_load_function
+
+    @work
+    async def load_more_data(self, row_highlighted: DataTable.RowHighlighted) -> None:
+        async with self.fetch_lock:
+            rows_remaining = len(self._rows_cache) - row_highlighted.cursor_row
+            if not (self.can_load_more and self.load_function):
+                return
+
+            if rows_remaining > self.load_more_data_buffer:
+                return
+
+            additional_data = await self.load_function(self.batch_size, self.current_batch + 1)
+            self.current_batch += 1
+            if len(additional_data) == 0:
+                self.can_load_more = False
+
+            self.add_rows(additional_data)
+
+    @on(DataTable.RowHighlighted)
+    async def check_highlighted_row_boundary(self, row_highlighted: DataTable.RowHighlighted) -> None:
+        self.load_more_data(row_highlighted)
+
+
+class LazyGithubContainer(Container):
+    """
+    Base container class for focusible containers within the Lazy Github UI
+    """
+
+    DEFAULT_CSS = """
+    LazyGithubContainer {
+        display: block;
+        border: solid $primary-lighten-3;
+    }
+
+    LazyGithubContainer:focus-within {
+        min-height: 40%;
+        border: solid $success;
+    }
+    """
---
+++ packages/lazy-github/lazy_github-0.3.4-py3-none-any.whl/lazy_github/ui/widgets/repositories.py
@@ -0,0 +1,145 @@
+import asyncio
+from typing import Dict, Iterable
+
+from textual import on, work
+from textual.app import ComposeResult
+from textual.coordinate import Coordinate
+from textual.widgets import DataTable
+
+import lazy_github.lib.github.repositories as repos_api
+from lazy_github.lib.bindings import LazyGithubBindings
+from lazy_github.lib.constants import IS_FAVORITED, favorite_string, private_string
+from lazy_github.lib.context import LazyGithubContext
+from lazy_github.lib.github.backends.protocol import GithubApiRequestFailed
+from lazy_github.lib.logging import lg
+from lazy_github.lib.messages import RepoSelected
+from lazy_github.models.github import Repository
+from lazy_github.ui.screens.lookup_repository import LookupRepositoryModal
+from lazy_github.ui.widgets.common import LazyGithubContainer, SearchableDataTable
+
+
+def _repo_to_row(repo: Repository) -> tuple[str, ...]:
+    favorited = favorite_string(repo.full_name in LazyGithubContext.config.repositories.favorites)
+    private = private_string(repo.private)
+    return (favorited, repo.owner.login, repo.name, private)
+
+
+class ReposContainer(LazyGithubContainer):
+    BINDINGS = [
+        LazyGithubBindings.TOGGLE_FAVORITE_REPO,
+        LazyGithubBindings.LOOKUP_REPOSITORY,
+    ]
+
+    def __init__(self, *args, **kwargs) -> None:
+        super().__init__(*args, **kwargs)
+        self.repos: Dict[str, Repository] = {}
+        self.favorite_column_index = -1
+        self.owner_column_index = 1
+        self.name_column_index = 1
+        self.private_column_index = 1
+
+    def compose(self) -> ComposeResult:
+        self.border_title = "[1] Repositories"
+        yield SearchableDataTable(
+            id="searchable_repos_table",
+            table_id="repos_table",
+            search_input_id="repo_search",
+            sort_key="favorite",
+        )
+
+    @property
+    def searchable_table(self) -> SearchableDataTable:
+        return self.query_one("#searchable_repos_table", SearchableDataTable)
+
+    @property
+    def table(self) -> DataTable:
+        return self.searchable_table.table
+
+    async def on_mount(self) -> None:
+        # Setup the table
+        self.table.cursor_type = "row"
+        self.table.add_column(IS_FAVORITED, key="favorite")
+        self.table.add_column("Owner", key="owner")
+        self.table.add_column("Name", key="name")
+        self.table.add_column("Private", key="private")
+
+        self.favorite_column_index = self.table.get_column_index("favorite")
+        self.owner_column_index = self.table.get_column_index("owner")
+        self.name_column_index = self.table.get_column_index("name")
+        self.private_column_index = self.table.get_column_index("private")
+
+        self.load_repos()
+
+    async def get_selected_repo(self) -> Repository:
+        current_row = self.table.cursor_row
+        owner = self.table.get_cell_at(Coordinate(current_row, self.owner_column_index))
+        repo_name = self.table.get_cell_at(Coordinate(current_row, self.name_column_index))
+        full_name = f"{owner}/{repo_name}"
+        return self.repos[full_name]
+
+    async def add_repo_to_table(self, repo: Repository) -> None:
+        self.repos[repo.full_name] = repo
+        self.searchable_table.add_row(*_repo_to_row(repo), repo.full_name)
+
+    @work
+    async def action_lookup_repository(self) -> None:
+        if repository := await self.app.push_screen_wait(LookupRepositoryModal()):
+            await self.add_repo_to_table(repository)
+            self.post_message(RepoSelected(repository))
+
+    @work
+    async def set_repositories(self, repos: Iterable[Repository]) -> None:
+        self.repos = {}
+        self.table.clear()
+        rows = []
+        for repo in repos:
+            rows.append(_repo_to_row(repo))
+            self.repos[repo.full_name] = repo
+        self.searchable_table.set_rows(rows)
+
+        # If the current user's directory is a git repo and they don't already have a git repo selected, try and mark
+        # that repo as the current repo
+        if LazyGithubContext.current_directory_repo and not LazyGithubContext.current_repo:
+            if repo := self.repos.get(LazyGithubContext.current_directory_repo):
+                self.post_message(RepoSelected(repo))
+
+    @work
+    async def load_repos(self) -> None:
+        # Loading the repos associated with the current account
+        repos: list[Repository] = []
+        try:
+            repos = await repos_api.list_all()
+        except GithubApiRequestFailed:
+            lg.exception("Error fetching repositories from Github API")
+
+        # Loading any additionally tracked repos
+        additional_repos_to_fetch = LazyGithubContext.config.repositories.additional_repos_to_track
+        additional_repos = await asyncio.gather(
+            *[repos_api.get_repository_by_name(full_repo_name) for full_repo_name in additional_repos_to_fetch]
+        )
+        repos.extend(filter(None, additional_repos))
+        self.set_repositories(repos)
+
+    async def action_toggle_favorite_repo(self):
+        repo = await self.get_selected_repo()
+        # Update the config to add/remove the favorite
+        with LazyGithubContext.config.to_edit() as config:
+            favorited = repo.full_name in config.repositories.favorites
+            if favorited:
+                lg.info(f"Unfavoriting repo {repo.full_name}")
+                config.repositories.favorites.remove(repo.full_name)
+            else:
+                lg.info(f"Favoriting repo {repo.full_name}")
+                config.repositories.favorites.append(repo.full_name)
+
+        # Flip the state of the favorited column in the UI
+        favorite_coord = Coordinate(self.table.cursor_row, self.favorite_column_index)
+        updated_favorited = repo.full_name in config.repositories.favorites
+        self.table.update_cell_at(favorite_coord, favorite_string(updated_favorited))
+        self.table.sort()
+
+    @on(DataTable.RowSelected, "#repos_table")
+    async def repo_selected(self):
+        # Bubble a message up indicating that a repo was selected
+        repo = await self.get_selected_repo()
+        self.post_message(RepoSelected(repo))
