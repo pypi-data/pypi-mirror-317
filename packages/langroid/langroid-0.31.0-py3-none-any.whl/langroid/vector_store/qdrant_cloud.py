e989ecf111e9f0d24cf3cbd79d0edac526d241d6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-25 04:04:04 +0000

    Add package langroid/0.31.0/langroid-0.31.0-py3-none-any.whl

---
+++ packages/langroid/langroid-0.31.0-py3-none-any.whl/langroid/vector_store/qdrant_cloud.py
@@ -0,0 +1,6 @@
+from qdrant_client import QdrantClient
+
+qdrant_client = QdrantClient(
+    url="https://644cabc3-4141-4734-91f2-0cc3176514d4.us-east-1-0.aws.cloud.qdrant.io:6333",
+    api_key="-7XJ3zw",
+)
