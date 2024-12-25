73eaa11ba80b4ff58fb0aacbe809d3040c29bff1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-25 14:04:08 +0000

    Add package afraz-pack/0.2/afraz_pack-0.2.tar.gz

---
+++ packages/afraz-pack/afraz_pack-0.2.tar.gz/afraz_pack-0.2/README.md
@@ -0,0 +1,3 @@
+# mypackage
+
+This is a simple example package to demonstrate how to publish to PyPI.
---
+++ packages/afraz-pack/afraz_pack-0.2.tar.gz/afraz_pack-0.2/setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="afraz_pack",  
+    version="0.2",
+    packages=find_packages(),
+    install_requires=[],
+    author="Afraz Ahmed A",
+    author_email="afraz@gmail.com",
+    description="Afraz Ahmed A is a Python developer working with QcodesInfotech. We will launch our package very soon.",
+    url="https://github.com/Afraz/mypackage",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    keywords=["hello", "world", "example", "simple", "tutorial", "python", "beginner", "learning", "demo", "QCoDes"],
+)
