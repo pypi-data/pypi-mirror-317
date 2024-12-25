25f1f810b6247c035891385f20201bfe78544162
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-25 15:04:10 +0000

    Add package cam-cli-tool/0.2.15/cam_cli_tool-0.2.15.tar.gz

---
+++ packages/cam-cli-tool/cam_cli_tool-0.2.15.tar.gz/cam_cli_tool-0.2.15/setup.py
@@ -0,0 +1,35 @@
+from setuptools import setup, find_packages  # type: ignore
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setup(
+    name="cam-cli-tool",
+    version="0.2.15",
+    author="shailesh",
+    author_email="shaileshpandit141@gmail.com.com",
+    description=("""
+        cam CLI is a command-line tool for creating boilerplate code for various types of applications
+        including vanilla JavaScript frontend web app, Python module, Flask app and etc.
+    """),
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/shaileshpandit141/cam-cli-tool.git",
+    packages=find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.8',
+    install_requires=[
+        "livereload>=2.5"
+    ],
+    entry_points={
+        "console_scripts": [
+            "cam=cam.main:main",  # cam CLI command.
+        ],
+    },
+    include_package_data=True,
+    zip_safe=False,
+)
