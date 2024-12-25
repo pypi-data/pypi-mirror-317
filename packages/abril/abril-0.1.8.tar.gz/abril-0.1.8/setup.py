11ee287be05ff3084e75473cfb0583c9414f0d37
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-25 14:03:42 +0000

    Add package abril/0.1.8/abril-0.1.8.tar.gz

---
+++ packages/abril/abril-0.1.8.tar.gz/abril-0.1.8/setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup
+
+setup(
+    name="abril",
+    version="0.1.8",
+    package_dir={"": "src"},
+    packages=["abril"],
+    install_requires=[
+        "torch>=1.7.1",
+        "pillow>=7.1.2",
+        "numpy>=1.18.5",
+        "clip",  # Just specify clip as a regular dependency
+    ],
+    dependency_links=[
+        "https://github.com/openai/CLIP/tarball/master#egg=clip"  # Use this format for git dependencies
+    ],
+)
