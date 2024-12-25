ee2c78ac7abb705bdffe89aede39d841d17603a5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-25 03:06:51 +0000

    Add package repo-reasoner/0.0.3/repo_reasoner-0.0.3-py3-none-any.whl

---
+++ packages/repo-reasoner/repo_reasoner-0.0.3-py3-none-any.whl/src/cli.py
@@ -0,0 +1,60 @@
+# The MIT License (MIT)
+#
+# Copyright (c) 2024 Dzhovidon Vakhidov
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included
+# in all copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NON-INFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
+
+"""
+Cli runner.
+"""
+from typing import Optional
+import typer
+from typer.cli import app
+
+from src import NAME
+
+
+@app.command()
+def filter(
+        repositories: str = typer.Option(
+            ..., "--repositories", help="Path to the input repositories CSV file."
+        ),
+
+):
+    """
+    Filter repositories.
+    """
+    print(repositories)
+    print(NAME)
+
+# Run it.
+@app.callback()
+def main(
+        # pylint: disable=unused-argument
+        version: Optional[bool] = typer.Option(
+            None,
+            "--version",
+            help="Show the application's version and exit.",
+            is_eager=True,
+        )
+) -> None:
+    f"""
+    {NAME}
+    """
+    return
