71a15986ef0ac3886fe74c1116d0eea87dfba4f8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-25 14:04:14 +0000

    Add package seq-optimizer/0.1.0/seq_optimizer-0.1.0-cp310-cp310-manylinux_2_17_x86_64.manylinux2014_x86_64.whl

---
+++ packages/seq-optimizer/seq_optimizer-0.1.0-cp310-cp310-manylinux_2_17_x86_64.manylinux2014_x86_64.whl/seq_optimizer/__init__.py
@@ -0,0 +1,3 @@
+from .core import find_longest_common_sequence as longest_common
+
+__version__ = "0.1.0"
---
+++ packages/seq-optimizer/seq_optimizer-0.1.0-cp310-cp310-manylinux_2_17_x86_64.manylinux2014_x86_64.whl/seq_optimizer/core.py
@@ -0,0 +1,5 @@
+from seq_optimizer import _seq_optimizer
+from typing import List
+
+def find_longest_common_sequence(seq: List[List[int]]) -> List[int]:
+    return _seq_optimizer.find_longest_common_sequence(seq)
