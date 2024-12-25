3f7121eeaf724623cb11e674e55a33e587aa53e5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-25 05:04:09 +0000

    Add package procyon-py/0.3.2/procyon_py-0.3.2.tar.gz

---
+++ packages/procyon-py/procyon_py-0.3.2.tar.gz/procyon_py-0.3.2/setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='procyon-py',
+    version='0.3.2',
+    description='A terminal based UI library for Python',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    url='https://github.com/evanlaube/procyon-py',
+    author='Evan Laube',
+    author_email='laubeevan@gmail.com',
+    license='GPL 3.0',
+    packages=find_packages(),
+    install_requires=[
+
+    ],
+    extras_require={
+        'windows': [
+            'windows-curses',
+        ],
+        'dev': [
+            'pytest',
+        ],
+    },
+    test_suite='tests',
+)
