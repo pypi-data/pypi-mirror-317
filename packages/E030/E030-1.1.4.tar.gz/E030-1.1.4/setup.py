2699e77f894b6f2625260b9b84607012aaf84913
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-25 15:04:10 +0000

    Add package E030/1.1.4/E030-1.1.4.tar.gz

---
+++ packages/E030/E030-1.1.4.tar.gz/E030-1.1.4/setup.py
@@ -0,0 +1,36 @@
+from setuptools import find_packages, setup
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    LONG_DESCRIPTION = fh.read()
+
+VERSION = '1.1.4' #Muy importante, deberéis ir cambiando la versión de vuestra librería según incluyáis nuevas funcionalidades
+PACKAGE_NAME = 'E030' #Debe coincidir con el nombre de la carpeta 
+AUTHOR = 'Alex Henrry Palomino Encinas' #Modificar con vuestros datos
+AUTHOR_EMAIL = 'sagep_peru@hotmail.com' #Modificar con vuestros datos
+URL = 'https://www.ceint.info' #Modificar con vuestros datos
+
+LICENSE = 'MIT' #Tipo de licencia
+DESCRIPTION = 'Librería para procesamiento de información según la NTE E.030 de Perú' #Descripción corta
+#LONG_DESCRIPTION = (HERE / "README.md").read_text(encoding='utf-8') #Referencia al documento README con una descripción más elaborada
+LONG_DESC_TYPE = "text/markdown"
+
+
+#Paquetes necesarios para que funcione la libreía. Se instalarán a la vez si no lo tuvieras ya instalado
+INSTALL_REQUIRES = [
+      'pandas', 'numpy', 'matplotlib', 'plotly', 'comtypes'
+      ]
+
+setup(
+    name=PACKAGE_NAME,
+    version=VERSION,
+    description=DESCRIPTION,
+    long_description=LONG_DESCRIPTION,
+    long_description_content_type=LONG_DESC_TYPE,
+    author=AUTHOR,
+    author_email=AUTHOR_EMAIL,
+    url=URL,
+    install_requires=INSTALL_REQUIRES,
+    license=LICENSE,
+    packages=find_packages(),
+    include_package_data=True
+)
