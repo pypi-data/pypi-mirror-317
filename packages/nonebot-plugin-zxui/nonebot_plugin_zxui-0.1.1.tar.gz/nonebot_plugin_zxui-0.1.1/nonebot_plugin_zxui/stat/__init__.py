6f46a6858eb6d0fc9e05271321ece4408a9e47be
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-25 14:04:01 +0000

    Add package nonebot-plugin-zxui/0.1.1/nonebot_plugin_zxui-0.1.1.tar.gz

---
+++ packages/nonebot-plugin-zxui/nonebot_plugin_zxui-0.1.1.tar.gz/nonebot_plugin_zxui-0.1.1/nonebot_plugin_zxui/stat/__init__.py
@@ -0,0 +1,36 @@
+from datetime import datetime
+
+import nonebot
+from nonebot.adapters import Bot
+from nonebot.drivers import Driver
+from zhenxun_utils.log import logger
+from zhenxun_utils.platform import PlatformUtils
+
+from ..models.bot_connect_log import BotConnectLog
+from ..models.bot_console import BotConsole
+
+driver: Driver = nonebot.get_driver()
+
+
+@driver.on_bot_connect
+async def _(bot: Bot):
+    logger.debug(f"Bot: {bot.self_id} 建立连接...")
+    await BotConnectLog.create(
+        bot_id=bot.self_id, platform=bot.adapter, connect_time=datetime.now(), type=1
+    )
+    if not await BotConsole.exists(bot_id=bot.self_id):
+        await BotConsole.create(
+            bot_id=bot.self_id, platform=PlatformUtils.get_platform(bot)
+        )
+
+
+@driver.on_bot_disconnect
+async def _(bot: Bot):
+    logger.debug(f"Bot: {bot.self_id} 断开连接...")
+    await BotConnectLog.create(
+        bot_id=bot.self_id, platform=bot.adapter, connect_time=datetime.now(), type=0
+    )
+
+
+from .chat_history import *  # noqa: F403
+from .statistics import *  # noqa: F403
