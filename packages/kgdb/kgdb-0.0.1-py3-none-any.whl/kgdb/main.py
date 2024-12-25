4686d4b9ff503738d34c897a9ce24c2389349c90
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-25 03:06:46 +0000

    Add package kgdb/0.0.1/kgdb-0.0.1-py3-none-any.whl

---
+++ packages/kgdb/kgdb-0.0.1-py3-none-any.whl/kgdb/main.py
@@ -0,0 +1,13 @@
+
+import argparse
+
+
+def main():
+    parser = argparse.ArgumentParser()
+    # parser.add_argument("path", type=str)
+    # args = parser.parse_args()
+    print("hello kgdb!")
+
+
+if __name__ == "__main__":
+    main()
