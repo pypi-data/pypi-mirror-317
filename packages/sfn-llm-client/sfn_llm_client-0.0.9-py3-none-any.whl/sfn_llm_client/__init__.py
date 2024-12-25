99b65d7e9bf33a781924180f1ee67a2672910304
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-25 03:07:17 +0000

    Add package sfn-llm-client/0.0.9/sfn_llm_client-0.0.9-py3-none-any.whl

---
+++ packages/sfn-llm-client/sfn_llm_client-0.0.9-py3-none-any.whl/sfn_llm_client/__init__.py
@@ -0,0 +1,56 @@
+__version__ = "0.0.9"
+
+from sfn_llm_client.utils.base_llm_client import BaseLLMClient
+
+# load api clients
+try:
+    from sfn_llm_client.llm_api_client.base_llm_api_client import BaseLLMAPIClient, LLMAPIClientConfig, ChatMessage, Role
+    from sfn_llm_client.llm_api_client.llm_api_client_factory import LLMAPIClientFactory, LLMAPIClientType
+    # load base-api clients
+    try:
+        from sfn_llm_client.llm_api_client.ai21_client import AI21Client
+        from sfn_llm_client.llm_api_client.aleph_alpha_client import AlephAlphaClient
+        from sfn_llm_client.llm_api_client.google_client import GoogleClient, MessagePrompt
+    except ImportError:
+        pass
+    # load apis with different dependencies
+    try:
+        from sfn_llm_client.llm_api_client.openai_client import OpenAIClient
+    except ImportError:
+        pass
+    try:
+        from sfn_llm_client.llm_api_client.huggingface_client import HuggingFaceClient
+    except ImportError:
+        pass
+    try:
+        from sfn_llm_client.llm_api_client.anthropic_client import AnthropicClient
+    except ImportError:
+        pass
+    try:
+        from sfn_llm_client.llm_api_client.cortex_client import CortexClient
+    except ImportError:
+        pass
+    try:
+        from sfn_llm_client.llm_cost_calculation.openai_cost_calculation import openai_cost_calculation
+    except ImportError:
+        pass
+    try:
+        from sfn_llm_client.llm_cost_calculation.snowflake_cortex_cost_calculation import snowflake_cortex_cost_calculation
+    except ImportError:
+        pass
+    try:
+        from sfn_llm_client.llm_cost_calculation.anthropic_cost_calculation import anthropic_cost_calculation
+    except ImportError:
+        pass
+except ImportError:
+    pass
+# load local clients
+try:
+    from sfn_llm_client.llm_client.local_client import LocalClient, LocalClientConfig
+except ImportError:
+    pass
+# load sync support
+try:
+    from sfn_llm_client.sync.sync_llm_api_client_factory import init_sync_llm_api_client
+except ImportError:
+    pass
