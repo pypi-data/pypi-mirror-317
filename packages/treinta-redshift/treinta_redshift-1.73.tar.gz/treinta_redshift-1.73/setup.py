af6d8a0e39af6e7c1b583c565a491d5bcc55dde4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-25 05:04:12 +0000

    Add package treinta-redshift/1.73/treinta_redshift-1.73.tar.gz

---
+++ packages/treinta-redshift/treinta_redshift-1.73.tar.gz/treinta_redshift-1.73/setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='treinta_redshift',
+    version='1.73',  # Incremento de la versión
+    author='cristian.pinela',
+    author_email='cristian.pinela@treinta.co',
+    packages=find_packages(),
+    license='LICENSE',
+    description='paquete que permite interactuar pandas con redshift',
+    # Asegurarse de que README.md no tiene errores de sintaxis
+    long_description=open('README.md').read(),
+    # Especifica que la descripción larga está en Markdown
+    long_description_content_type='text/markdown',
+    install_requires=[
+        'boto3',
+        'botocore',
+        'numpy',
+        'pandas',
+        'pyarrow'
+    ]
+)
