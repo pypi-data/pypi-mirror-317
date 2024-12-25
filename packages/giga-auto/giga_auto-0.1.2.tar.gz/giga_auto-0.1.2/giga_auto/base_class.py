7a9c8e7ae9f485560f55afb7174db450d0ad8258
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-25 14:04:10 +0000

    Add package giga-auto/0.1.2/giga_auto-0.1.2.tar.gz

---
+++ packages/giga-auto/giga_auto-0.1.2.tar.gz/giga_auto-0.1.2/giga_auto/base_class.py
@@ -0,0 +1,12 @@
+from giga_auto.request import RequestBase
+
+
+class ApiBase(RequestBase):
+    def __init__(self, **env):
+        self.host = env['host']
+        _host = self.host
+        if env.get('business') and env['business'] == 'b2b':
+            self.host_route = env['host_route']
+            _host = self.host_route
+        super().__init__(_host, env['business'] if 'business' in env else '')
+        self.headers = env['headers']
