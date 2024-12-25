0c434e1db4754cce467e85a7aac2a91413fbbf5b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-25 04:03:46 +0000

    Add package ai-search-api/0.0.8/ai_search_api-0.0.8.tar.gz

---
+++ packages/ai-search-api/ai_search_api-0.0.8.tar.gz/ai_search_api-0.0.8/ai_search_api/__init__.py
@@ -0,0 +1,15 @@
+import ai_search_api.ai_search as ai
+
+def update():
+    import ai_search_api.update
+    return "updating..."
+    
+def request(text):
+    # set the request
+    ai.req = text
+    ai.run()
+    results = ai.result
+    ai.result = []
+
+    # print the results
+    return results
