f24b016b0367b8005bfad4c905cc2e6ed9b74958
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-25 04:04:22 +0000

    Add package syncmaster-commons/0.0.22/syncmaster_commons-0.0.22-py3-none-any.whl

---
+++ packages/syncmaster-commons/syncmaster_commons-0.0.22-py3-none-any.whl/syncmaster_commons/abstract/__init__.py
@@ -0,0 +1,8 @@
+from .baseclass import SMBaseClass, ThirdPartyPayloadConsumedByAgent
+from .enforcers import EnforceDocStringBaseClass
+
+__all__ = [
+    "SMBaseClass",
+    "ThirdPartyPayloadConsumedByAgent",
+    "EnforceDocStringBaseClass",
+]
