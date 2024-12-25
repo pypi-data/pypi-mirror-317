e7a8016ba1b568c55e2860ac27d364c7a94e23fb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-25 04:03:53 +0000

    Add package carrier-api/1.14.2/carrier_api-1.14.2.tar.gz

---
+++ packages/carrier-api/carrier_api-1.14.2.tar.gz/carrier_api-1.14.2/src/carrier_api/const.py
@@ -0,0 +1,39 @@
+from enum import Enum, StrEnum
+
+INFINITY_API_BASE_URL = "https://www.app-api.ing.carrier.com"
+INFINITY_API_CONSUMER_KEY = "8j30j19aj103911h"
+INFINITY_API_CONSUMER_SECRET = "0f5ur7d89sjv8d45"
+
+
+class SystemModes(Enum):
+    OFF = "off"
+    COOL = "cool"
+    HEAT = "heat"
+    AUTO = "auto"
+    FAN_ONLY = "fanonly"
+
+
+class ActivityNames(Enum):
+    HOME = "home"
+    AWAY = "away"
+    SLEEP = "sleep"
+    WAKE = "wake"
+    MANUAL = "manual"
+    VACATION = "vacation"
+
+
+class FanModes(Enum):
+    OFF = "off"
+    LOW = "low"
+    MED = "med"
+    HIGH = "high"
+
+
+class TemperatureUnits(Enum):
+    CELSIUS = "C"
+    FAHRENHEIT = "F"
+
+class HeatSourceTypes(StrEnum):
+    IDU_ONLY = "idu only"
+    ODU_ONLY = "odu only"
+    SYSTEM = "system"
