4c4d62c31c83019ddddd14588ec9e38afa4ea80c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-25 03:06:37 +0000

    Add package opteryx/0.19.0a919/opteryx-0.19.0a919-cp311-cp311-macosx_10_15_universal2.whl

---
+++ packages/opteryx/opteryx-0.19.0a919-cp311-cp311-macosx_10_15_universal2.whl/opteryx/shared/async_memory_pool.py
@@ -0,0 +1,45 @@
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+"""
+This is the async wrapper around the memory pool, it is used by the
+async blob readers to move data to the synchronous code.
+"""
+
+import asyncio
+
+from opteryx.compiled.structures import MemoryPool
+
+
+class AsyncMemoryPool:
+    def __init__(self, pool: MemoryPool):
+        self.pool: MemoryPool = pool
+        self.lock = asyncio.Lock()
+
+    async def commit(self, data: bytes) -> int:
+        async with self.lock:
+            return self.pool.commit(data)
+
+    async def read(self, ref_id: int) -> bytes:
+        """
+        In an async environment, we much more certain the bytes will be overwritten
+        if we don't materialize them so we always create a copy.
+        """
+        async with self.lock:
+            return self.pool.read(ref_id, zero_copy=False)
+
+    async def release(self, ref_id: int):
+        async with self.lock:
+            self.pool.release(ref_id)
+
+    def size(self):
+        return self.pool.size
