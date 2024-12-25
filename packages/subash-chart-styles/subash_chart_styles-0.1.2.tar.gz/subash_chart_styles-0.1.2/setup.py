112aa0d8080349521a4d6665116aae61197a72f7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-25 15:04:07 +0000

    Add package subash-chart-styles/0.1.2/subash_chart_styles-0.1.2.tar.gz

---
+++ packages/subash-chart-styles/subash_chart_styles-0.1.2.tar.gz/subash_chart_styles-0.1.2/setup.py
@@ -0,0 +1,25 @@
+# setup.py
+from setuptools import setup, find_packages
+
+setup(
+    name='subash_chart_styles',  # Name of the package
+    version='0.1.2',             # Version of the package
+    description='A Python package for generating styled market cap charts',
+    long_description=open('README.md').read(),  # Read the long description from the README
+    long_description_content_type='text/markdown',
+    author='Your Name',
+    author_email='your.email@example.com',
+    url='https://github.com/yourusername/subash_chart_styles',  # Replace with your GitHub URL
+    packages=find_packages(),  # Automatically find packages in the directory
+    install_requires=[
+        'matplotlib',  # Required dependencies
+        'psycopg2',    # For PostgreSQL access
+        'slugify',     # For creating slugs
+    ],
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.6',
+)
