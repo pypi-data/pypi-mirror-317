f5254099274bab0bcc587e12d932248c36e54d12
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-25 14:03:37 +0000

    Add package langgraph_log_parser/0.1.10/langgraph_log_parser-0.1.10-py3-none-any.whl

---
+++ packages/langgraph_log_parser/langgraph_log_parser-0.1.10-py3-none-any.whl/langgraph_log_parser/load_events.py
@@ -0,0 +1,28 @@
+import pandas as pd
+import pm4py
+
+#1
+def load_event_log(file_path: str) -> pd.DataFrame:
+    """
+    Load CSV data into a formatted PM4Py DataFrame.
+
+    :param file_path: Path to the CSV file containing the event log.
+    :type file_path: str
+    :return: PM4Py formatted DataFrame.
+    :rtype: pd.DataFrame
+
+    **Example:**
+
+    >>> csv_output = "files/examples.csv"
+    >>> event_log = load_event_log(csv_output)
+    Event log loaded and formated from file: files/examples.csv
+    """
+    # Ładowanie CSV do pandas DataFrame
+    df = pd.read_csv(file_path)
+
+    # Formatowanie DataFrame dla PM4Py
+    df['timestamp'] = pd.to_datetime(df['timestamp'])
+    df['end_timestamp'] = pd.to_datetime(df['end_timestamp'])
+
+    print(f"Event log loaded and formated from file: {file_path}")
+    return pm4py.format_dataframe(df, case_id='case_id', activity_key='activity', timestamp_key='timestamp')
