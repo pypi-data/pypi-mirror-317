7731c5b98d0c9f90a26f6832e3d16e4edf11e33a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-25 04:04:22 +0000

    Add package fastapi-csrf-protect/1.0.0/fastapi_csrf_protect-1.0.0.tar.gz

---
+++ packages/fastapi-csrf-protect/fastapi_csrf_protect-1.0.0.tar.gz/fastapi_csrf_protect-1.0.0/tests/__init__.py
@@ -0,0 +1,55 @@
+#!/usr/bin/env python3
+# Copyright (C) 2020-2024 All rights reserved.
+# FILENAME:    ~~/tests/__init__.py
+# VERSION:     1.0.0
+# CREATED:     2020-11-26 18:50
+# AUTHOR:      Sitt Guruvanich <aekazitt+github@gmail.com>
+# DESCRIPTION:
+#
+# HISTORY:
+# *************************************************************
+
+### Standard packages ###
+from pytest import fixture
+
+### Third-party packages ###
+from fastapi import Depends, FastAPI, Request
+from fastapi.responses import JSONResponse
+from fastapi.testclient import TestClient
+
+### Local modules ###
+from fastapi_csrf_protect import CsrfProtect
+from fastapi_csrf_protect.exceptions import CsrfProtectError
+
+
+@fixture
+def test_client() -> TestClient:
+  """
+  Sets up a FastAPI TestClient wrapped around an App implementing Context/Headers extension pattern
+
+  ---
+  :returns: TestClient
+  """
+  app = FastAPI()
+
+  @app.get("/gen-token", response_class=JSONResponse)
+  def generate(csrf_protect: CsrfProtect = Depends()):
+    csrf_token, signed_token = csrf_protect.generate_csrf_tokens()
+    response: JSONResponse = JSONResponse(
+      status_code=200, content={"detail": "OK", "csrf_token": csrf_token}
+    )
+    csrf_protect.set_csrf_cookie(signed_token, response)
+    return response
+
+  @app.get("/protected", response_class=JSONResponse)
+  async def protected(request: Request, csrf_protect: CsrfProtect = Depends()):
+    await csrf_protect.validate_csrf(request)
+    response: JSONResponse = JSONResponse(status_code=200, content={"detail": "OK"})
+    csrf_protect.unset_csrf_cookie(response)  # prevent token reuse
+    return response
+
+  @app.exception_handler(CsrfProtectError)
+  def csrf_protect_error_handler(request: Request, exc: CsrfProtectError):
+    return JSONResponse(status_code=exc.status_code, content={"detail": exc.message})
+
+  return TestClient(app)
---
+++ packages/fastapi-csrf-protect/fastapi_csrf_protect-1.0.0.tar.gz/fastapi_csrf_protect-1.0.0/tests/token_expired.py
@@ -0,0 +1,47 @@
+#!/usr/bin/env python3
+# Copyright (C) 2020-2024 All rights reserved.
+# FILENAME:    ~~/tests/token_expired.py
+# VERSION:     1.0.0
+# CREATED:     2020-11-26 16:14
+# AUTHOR:      Sitt Guruvanich <aekazitt+github@gmail.com>
+# DESCRIPTION:
+#
+# HISTORY:
+# *************************************************************
+
+### Standard packages ###
+from time import sleep
+
+### Third-party packages ###
+from fastapi.testclient import TestClient
+
+### Local modules ###
+from . import test_client
+from fastapi_csrf_protect import CsrfProtect
+
+
+def test_validate_token_expired(test_client: TestClient, max_age: int = 2):
+  ### Loads Config ###
+  @CsrfProtect.load_config
+  def get_configs():
+    return [("secret_key", "secret"), ("max_age", max_age)]
+
+  ### Generate token ###
+  response = test_client.get("/gen-token")
+
+  ### Assertion ###
+  assert response.status_code == 200
+
+  ### Extract `csrf_token` from response to be set as next request's header ###
+  csrf_token: str = response.json().get("csrf_token", None)
+  headers: dict = {"X-CSRF-Token": csrf_token} if csrf_token is not None else {}
+
+  ### Delays ###
+  sleep(max_age + 1)
+
+  ### Get protected contents ###
+  response = test_client.get("/protected", headers=headers)
+
+  ### Assertions ###
+  assert response.status_code == 400
+  assert response.json() == {"detail": "Missing Cookie: `fastapi-csrf-token`."}
