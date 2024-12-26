9446054a89201710dd5dbe8fd5562e748c0d6777
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-26 03:08:08 +0000

    Add package LTPpyTools/0.1.1/LTPpyTools-0.1.1-py3-none-any.whl

---
+++ packages/LTPpyTools/LTPpyTools-0.1.1-py3-none-any.whl/LTPpyTools/__init__.py
@@ -0,0 +1,12 @@
+"""this is Tools"""
+"""by LTPLAX"""
+"""version 0.1.0"""
+"""Please use Tools.init before use"""
+from .music import music
+__all__=["music"]
+
+def init():
+    print("LTPpyTools 0.1 Normal mode ready(this ver only have music\ntype:Tools.music.init())")
+
+
+
