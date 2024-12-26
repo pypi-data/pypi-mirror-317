1baf553e86eed1dd8740cf8cd16dc441b8f260d9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-26 03:07:56 +0000

    Add package mongo-schema-V2/0.1.5/mongo-schema-V2-0.1.5.tar.gz

---
+++ packages/mongo-schema-V2/mongo-schema-V2-0.1.5.tar.gz/mongo-schema-V2-0.1.5/setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="mongo-schema-V2",
+    version="0.1.5",
+    description="A Python SDK for exporting MongoDB schema metadata",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    author="Karan Bhatia",
+    author_email="karanbhatia.kb27@gmail.com",
+    url="https://github.com/karanbhatiakb/mongo-schema-V2/tree/master",
+    packages=find_packages(),
+    install_requires=["pymongo", "bson"],
+    entry_points={
+        "console_scripts": [
+            "mongo-schema-V2=mongo_schema.cli:main",
+        ],
+    },
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
