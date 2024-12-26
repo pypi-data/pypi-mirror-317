908d7b4afd72a6852e993290c217871ace6c4535
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-26 14:04:34 +0000

    Add package xorbits/0.8.2/xorbits-0.8.2-cp310-cp310-macosx_10_9_universal2.whl

---
+++ packages/xorbits/xorbits-0.8.2-cp310-cp310-macosx_10_9_universal2.whl/xorbits/_mars/deploy/yarn/supervisor.py
@@ -0,0 +1,52 @@
+# Copyright 2022-2023 XProbe Inc.
+# derived from copyright 1999-2021 Alibaba Group Holding Ltd.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+import os
+
+from ..oscar.supervisor import SupervisorCommandRunner
+from .config import MarsSupervisorConfig
+from .core import YarnServiceMixin
+
+
+class YarnSupervisorCommandRunner(YarnServiceMixin, SupervisorCommandRunner):
+    service_name = MarsSupervisorConfig.service_name
+    web_service_name = MarsSupervisorConfig.web_service_name
+
+    def __call__(self, *args, **kwargs):
+        os.environ["MARS_CONTAINER_IP"] = self.get_container_ip()
+        return super().__call__(*args, **kwargs)
+
+    def parse_args(self, parser, argv, environ=None):
+        args = super().parse_args(parser, argv, environ=environ)
+        self.config["web"]["host"] = args.endpoint.split(":", 1)[0]
+        self.config["cluster"]["backend"] = "yarn"
+        return args
+
+    async def start_services(self):
+        self.register_endpoint()
+
+        await super().start_services()
+
+        from ...services.web import OscarWebAPI
+
+        web_api = await OscarWebAPI.create(self.args.endpoint)
+        web_endpoint = await web_api.get_web_address()
+        self.register_endpoint(self.web_service_name, web_endpoint)
+
+
+main = YarnSupervisorCommandRunner()
+
+if __name__ == "__main__":  # pragma: no branch
+    main()
---
+++ packages/xorbits/xorbits-0.8.2-cp310-cp310-macosx_10_9_universal2.whl/xorbits/numpy/__init__.py
@@ -0,0 +1,133 @@
+# Copyright 2022-2023 XProbe Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+import inspect
+from typing import Any, Callable, Dict, Optional
+
+from ..utils import is_numpy_2
+
+if is_numpy_2():
+    from numpy.exceptions import AxisError
+    from numpy.lib._index_tricks_impl import ndindex
+else:
+    from numpy import AxisError
+    from numpy.lib.index_tricks import ndindex
+
+from numpy import (
+    bool_,
+    bytes_,
+    character,
+    complex64,
+    complex128,
+    complexfloating,
+    datetime64,
+    double,
+    dtype,
+    e,
+    errstate,
+    finfo,
+    flexible,
+    float16,
+    float32,
+    float64,
+    floating,
+    generic,
+    inexact,
+    inf,
+    int8,
+    int16,
+    int32,
+    int64,
+    int_,
+    intc,
+    integer,
+    intp,
+    nan,
+    newaxis,
+    number,
+    object_,
+    pi,
+    signedinteger,
+    str_,
+    timedelta64,
+    uint,
+    uint8,
+    uint16,
+    uint32,
+    uint64,
+    unsignedinteger,
+    void,
+)
+
+from ..core.utils.fallback import unimplemented_func
+
+
+def _install():
+    from .mars_adapters import _install as _install_mars_methods
+    from .numpy_adapters import _install as _install_numpy_methods
+
+    _install_mars_methods()
+    _install_numpy_methods()
+
+
+try:
+    import warnings
+
+    # suppress numpy warnings on types
+    with warnings.catch_warnings():
+        warnings.simplefilter("ignore", DeprecationWarning)
+        warnings.simplefilter("ignore", FutureWarning)
+        # noinspection PyUnresolvedReferences
+        from numpy import bool, float, int, object  # type: ignore[attr-defined]
+except ImportError:  # pragma: no cover
+    pass
+finally:
+    del warnings
+
+
+from . import fft, linalg, random, special
+from .core import ndarray
+
+
+def __dir__():
+    from .mars_adapters import MARS_TENSOR_CALLABLES, MARS_TENSOR_OBJECTS
+    from .numpy_adapters.core import NUMPY_MEMBERS
+
+    return (
+        list(MARS_TENSOR_CALLABLES.keys())
+        + list((MARS_TENSOR_OBJECTS.keys()))
+        + list(NUMPY_MEMBERS.keys())
+    )
+
+
+def __getattr__(name: str):
+    from .mars_adapters import MARS_TENSOR_CALLABLES, MARS_TENSOR_OBJECTS
+    from .numpy_adapters.core import NUMPY_MEMBERS
+
+    if name in MARS_TENSOR_CALLABLES:
+        return MARS_TENSOR_CALLABLES[name]
+    elif name in MARS_TENSOR_OBJECTS:
+        return MARS_TENSOR_OBJECTS[name]
+    else:
+        import numpy
+
+        if not hasattr(numpy, name):
+            raise AttributeError(name)
+        elif name in NUMPY_MEMBERS:
+            return NUMPY_MEMBERS[name]
+        else:  # pragma: no cover
+            if inspect.ismethod(getattr(numpy, name)):
+                return unimplemented_func
+            else:
+                raise AttributeError(name)
