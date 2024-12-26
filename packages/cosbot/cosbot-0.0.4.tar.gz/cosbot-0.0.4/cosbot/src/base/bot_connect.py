b5acf78210326ecc75c7c5f993019da1f78a82ec
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-26 03:07:53 +0000

    Add package cosbot/0.0.4/cosbot-0.0.4.tar.gz

---
+++ packages/cosbot/cosbot-0.0.4.tar.gz/cosbot-0.0.4/cosbot/src/base/bot_connect.py
@@ -0,0 +1,50 @@
+"""
+Copyright 2025 Vitaliy Zarubin
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+"""
+import time
+
+from cosbot.src.base.bot_connect_exceptions import BotWSConnectException, BotApiConnectException
+from cosbot.src.base.bot_reconnect import get_reconnect, clear_reconnect
+from cosbot.src.base.bot_rest_api import BotRestApi
+from cosbot.src.base.bot_websocket import BotWebsocket
+from cosbot.src.base.utils import is_app_close
+
+
+def connect_sleep():
+    rec = get_reconnect()
+    # limit connects
+    if rec >= 60:
+        print('Отсутствует подключение.')
+        exit(1)
+    print('> Ошибка соединения, переподключение ({}s)...'.format(rec))
+    time.sleep(rec)
+
+
+def connect(token: str):
+    try:
+        BotWebsocket(BotRestApi(token).get_cookies(), lambda: clear_reconnect())
+    except BotWSConnectException:
+        connect_sleep()
+        connect(token)
+    except BotApiConnectException:
+        connect_sleep()
+        connect(token)
+    except (Exception,):
+        print('Отсутствует подключение.')
+        exit(1)
+
+    # reconnect if close
+    if not is_app_close:
+        connect(token)
---
+++ packages/cosbot/cosbot-0.0.4.tar.gz/cosbot-0.0.4/setup.py
@@ -0,0 +1,45 @@
+import setuptools
+
+long_description = """
+**Cosbot** предназначено для взаимодействия с [Aurora Bot](https://aurora-bot.keygenqt.com/).
+
+> Приложение в разработке!
+
+### Feature
+
+- Позволяет реализовать в Telegram режим команд.
+- Отвечать на вопросы.
+
+### Roadmap
+
+- Режим умных команд.
+- Фреймворк click.
+- Другие улучшения.
+"""
+
+setuptools.setup(
+    name='cosbot',
+    version='0.0.4',
+    author='Vitaliy Zarubin',
+    author_email='keygenqt@gmail.com',
+    description='An application that simplifies the life of an application developer for the Aurora OS.',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    url="https://aurora-bot.keygenqt.com/",
+    packages=setuptools.find_packages(exclude=['*tests.*', '*tests']),
+    include_package_data=True,
+    py_modules=['colors'],
+    install_requires=[
+        'Telethon>=1.38.1',
+        'setuptools>=75.6.0',
+        'websocket-client>=1.8.0',
+        'requests>=2.32.3',
+        'aurora-cli>=3.2.10',
+        'beautifulsoup4>=4.12.3',
+    ],
+    python_requires='>=3.8.10',
+    entry_points="""
+        [console_scripts]
+        cosbot = cosbot.__main__
+    """
+)
