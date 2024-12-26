3eb20253de79291eef957bc000719342ef39a674
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-26 04:04:44 +0000

    Add package django-seo-optimizer/1.0.1/django_seo_optimizer-1.0.1-py3-none-any.whl

---
+++ packages/django-seo-optimizer/django_seo_optimizer-1.0.1-py3-none-any.whl/seo_optimizer/__init__.py
@@ -0,0 +1,12 @@
+"""
+Django SEO Optimizer - A powerful SEO optimization package
+Created by avixiii (https://avixiii.com)
+"""
+from .version import VERSION
+
+__version__ = VERSION
+__author__ = "avixiii"
+__email__ = "contact@avixiii.com"
+__website__ = "https://avixiii.com"
+
+default_app_config = 'seo_optimizer.apps.SEOOptimizerConfig'
