329356c676a74b2eef84e1f7c03f0cae2abb88ac
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-26 03:07:59 +0000

    Add package matrix-operations-amber/0.1.0/matrix_operations_amber-0.1.0.tar.gz

---
+++ packages/matrix-operations-amber/matrix_operations_amber-0.1.0.tar.gz/matrix_operations_amber-0.1.0/test/test_operations.py
@@ -0,0 +1,31 @@
+import unittest
+from matrix_operations.operations import add_matrices, multiply_matrices
+
+class TestMatrixOperations(unittest.TestCase):
+
+    def test_add_matrices(self):
+        matrix1 = [[1, 2], [3, 4]]
+        matrix2 = [[5, 6], [7, 8]]
+        expected_result = [[6, 8], [10, 12]]
+        self.assertEqual(add_matrices(matrix1, matrix2), expected_result)
+
+    def test_multiply_matrices(self):
+        matrix1 = [[1, 2], [3, 4]]
+        matrix2 = [[5, 6], [7, 8]]
+        expected_result = [[19, 22], [43, 50]]
+        self.assertEqual(multiply_matrices(matrix1, matrix2), expected_result)
+
+    def test_add_matrices_invalid_dimensions(self):
+        matrix1 = [[1, 2, 3], [4, 5, 6]]
+        matrix2 = [[7, 8], [9, 10]]
+        with self.assertRaises(ValueError):
+            add_matrices(matrix1, matrix2)
+
+    def test_multiply_matrices_invalid_dimensions(self):
+        matrix1 = [[1, 2, 3], [4, 5, 6]]
+        matrix2 = [[7, 8, 9], [10, 11, 12]]
+        with self.assertRaises(ValueError):
+            multiply_matrices(matrix1, matrix2)
+
+if __name__ == '__main__':
+    unittest.main()
