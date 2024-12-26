9b48c0e2f413f5ee54480e79643f6341d4940a4d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-26 03:08:15 +0000

    Add package casify/0.2.21/casify-0.2.21-py3-none-any.whl

---
+++ packages/casify/casify-0.2.21-py3-none-any.whl/casify/__init__.py
@@ -0,0 +1,37 @@
+from .equation import solve, Solve
+from .likning import løs, Løs
+from .function import function, Function
+from .funksjon import funksjon, Funksjon
+from .algebra import (
+    expand,
+    factor,
+    utvid,
+    Utvid,
+    faktoriser,
+    Faktoriser,
+    div,
+    Div,
+    polynomdivisjon,
+    Polynomdivisjon,
+)
+
+__all__ = [
+    "solve",
+    "løs",
+    "Solve",
+    "Løs",
+    "function",
+    "Function",
+    "funksjon",
+    "Funksjon",
+    "expand",
+    "factor",
+    "utvid",
+    "Utvid",
+    "faktoriser",
+    "Faktoriser",
+    "div",
+    "Div",
+    "polynomdivisjon",
+    "Polynomdivisjon",
+]
