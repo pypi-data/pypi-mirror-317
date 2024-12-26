a5f64dba47e21d1b43d97aa47896e885a4ca6f9f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-26 14:04:38 +0000

    Add package seut/0.0.2/seut-0.0.2.tar.gz

---
+++ packages/seut/seut-0.0.2.tar.gz/seut-0.0.2/setup.py
@@ -0,0 +1,16 @@
+# setup.py
+from setuptools import setup, find_packages
+
+setup(
+    name='seut',
+    version='0.0.2',
+    packages=find_packages(),
+    description='Seut is Socicaly Network For System.',
+    author='Carnation Studio',
+    author_email='carnation.scr.offical@gmail.com',
+    url='https://github.com/SugiSaku8/Seut',
+    classifiers=[
+        'Programming Language :: Python :: 3',
+    ],
+    python_requires='>=3.6',
+)
