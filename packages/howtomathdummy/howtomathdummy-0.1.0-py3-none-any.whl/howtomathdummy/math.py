e0a562e6acfe01a5b673817ac7c340f8c0540761
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-26 16:03:56 +0000

    Add package howtomathdummy/0.1.0/howtomathdummy-0.1.0-py3-none-any.whl

---
+++ packages/howtomathdummy/howtomathdummy-0.1.0-py3-none-any.whl/howtomathdummy/math.py
@@ -0,0 +1,15 @@
+class Math:
+    '''Math module contains two functionalities
+    addition function: adds numbers
+    subtraction function: calculates diffrence
+    '''
+    def add(self, a: int, b: int) -> int:
+        '''Returns sum of two numbers'''
+        return a+b
+
+    def sub(self, a:int, b: int) -> int:
+        '''Returns difference of two numbers'''
+        return a-b
+
+
+
