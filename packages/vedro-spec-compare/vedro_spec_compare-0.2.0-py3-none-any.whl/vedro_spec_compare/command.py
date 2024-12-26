7c1a8bdb1d40660d5cf9d8fde657fae7f89237f4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-26 15:04:44 +0000

    Add package vedro-spec-compare/0.2.0/vedro_spec_compare-0.2.0-py3-none-any.whl

---
+++ packages/vedro-spec-compare/vedro_spec_compare-0.2.0-py3-none-any.whl/vedro_spec_compare/command.py
@@ -0,0 +1,28 @@
+import argparse
+
+from .coverage import coverage
+from .discrepancy import discrepancy
+
+
+def command() -> None:
+    parser = argparse.ArgumentParser(description='vedro-spec-compare commands')
+    subparsers = parser.add_subparsers(help='Available commands', required=True)
+
+    coverage_parser = subparsers.add_parser('coverage', help='Generate coverage report')
+    coverage_parser.add_argument('golden_spec_path', type=str, help='Path to the golden OpenAPI spec')
+    coverage_parser.add_argument('testing_spec_path', type=str, help='Path to the testing OpenAPI spec')
+    coverage_parser.add_argument(
+        '--report-path', type=str, help='The path of the coverage report', default='coverage.html'
+    )
+    coverage_parser.set_defaults(func=coverage)
+
+    discrepancy_parser = subparsers.add_parser('discrepancy', help='Generate discrepancy report')
+    discrepancy_parser.add_argument('golden_spec_path', type=str, help='Path to the golden OpenAPI spec')
+    discrepancy_parser.add_argument('testing_spec_path', type=str, help='Path to the testing OpenAPI spec')
+    discrepancy_parser.add_argument(
+        '--report-path', type=str, help='The path of the discrepancy report', default='discrepancy.html'
+    )
+    discrepancy_parser.set_defaults(func=discrepancy)
+
+    args = parser.parse_args()
+    args.func(args)
