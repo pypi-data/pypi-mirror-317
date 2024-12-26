6c5d4ee0dcb6199d040d8d9c47195c7effb99d8b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-26 14:04:23 +0000

    Add package act-workflow/4.8.177/act_workflow-4.8.177.tar.gz

---
+++ packages/act-workflow/act_workflow-4.8.177.tar.gz/act_workflow-4.8.177/act/nodes/__init__.py
@@ -0,0 +1,8 @@
+
+from .chatmodels_node import ChatModelsNode
+from .slack_node import SlackNode
+from .openai_node import OpenAINode
+from .slack_node import SlackNode
+from .openai_node import OpenAINode
+from .data_transformation_node import DataTransformationNode
+from .aggregatenode_node import AggregateNode
