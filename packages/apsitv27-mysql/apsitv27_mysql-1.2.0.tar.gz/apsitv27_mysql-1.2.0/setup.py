8bf06e97e2434c05b502bc1b6682273e1a8271aa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-26 03:07:59 +0000

    Add package apsitv27-mysql/1.2.0/apsitv27_mysql-1.2.0.tar.gz

---
+++ packages/apsitv27-mysql/apsitv27_mysql-1.2.0.tar.gz/apsitv27_mysql-1.2.0/setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="apsitv27-mysql",
+    version="1.2.0",
+    packages=find_packages(),
+    include_package_data=True,  # Include data files specified in MANIFEST.in
+    entry_points={
+        "console_scripts": [
+            "apsitv27-mysql=apsitv27_mysql.main:build_and_run_mysql_container"
+        ],
+    },
+    install_requires=[
+        "docker",
+        "setuptools>=65.0.0",
+    ],
+    # Add the Dockerfile and entrypoint.sh here
+    package_data={
+        "apsitv27_mysql": ["Dockerfile", "entrypoint.sh","install_docker.sh", "install_docker.bat"]
+    },
+)
