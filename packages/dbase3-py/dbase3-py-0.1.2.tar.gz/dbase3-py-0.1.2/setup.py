f4f33e8158fc25bdb14b6fd45082b680951fc126
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-26 03:08:00 +0000

    Add package dbase3-py/0.1.2/dbase3-py-0.1.2.tar.gz

---
+++ packages/dbase3-py/dbase3-py-0.1.2.tar.gz/dbase3-py-0.1.2/setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='dbase3-py',
+    version='0.1.2',
+    description='A Python library to manipulate DBase III database files',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    author='Domingo E. Savoretti',
+    author_email='esavoretti@gmail.com',
+    url='https://github.com/sandy98/dbase3-py',
+    packages=find_packages(),
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.6',
+)
