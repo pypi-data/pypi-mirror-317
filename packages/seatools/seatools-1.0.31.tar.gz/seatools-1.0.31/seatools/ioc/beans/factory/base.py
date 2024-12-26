ca503647a4c201b983fdc937ead44a36ad044cee
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-26 16:03:58 +0000

    Add package seatools/1.0.31/seatools-1.0.31.tar.gz

---
+++ packages/seatools/seatools-1.0.31.tar.gz/seatools-1.0.31/seatools/ioc/beans/factory/base.py
@@ -0,0 +1,28 @@
+import abc
+from typing import Union, Type, Callable, Any
+
+
+class BeanFactory(abc.ABC):
+    """Bean工厂抽象类"""
+
+    def __init__(self, **kwargs):
+        pass
+
+    @abc.abstractmethod
+    def get_bean(self, name: str = None, required_type: Union[Type, Callable] = None):
+        """获取bean方法."""
+        raise NotImplementedError
+
+    def get_beans(self, required_type: Type):
+        """获取某个类型的bean列表方法."""
+        raise NotImplementedError
+
+    @abc.abstractmethod
+    def register_bean(self, name: str, cls, primary: bool = False, order: int = 0, **kwargs) -> Any:
+        """bean注册方法."""
+        raise NotImplementedError
+
+    @abc.abstractmethod
+    def init(self, **kwargs):
+        """创建bean的初始化方法."""
+        raise NotImplementedError
