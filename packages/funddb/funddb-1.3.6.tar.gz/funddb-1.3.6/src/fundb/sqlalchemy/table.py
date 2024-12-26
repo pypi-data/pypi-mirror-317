38cd5c096bc91e8586b6abfcc0813755ad2e4560
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-26 14:04:19 +0000

    Add package funddb/1.3.6/funddb-1.3.6.tar.gz

---
+++ packages/funddb/funddb-1.3.6.tar.gz/funddb-1.3.6/src/fundb/sqlalchemy/table.py
@@ -0,0 +1,64 @@
+import traceback
+from datetime import datetime
+
+from funutil import getLogger
+from sqlalchemy import String, select, update
+from sqlalchemy.dialects.postgresql import insert
+from sqlalchemy.orm import DeclarativeBase, Mapped, Session, mapped_column
+
+logger = getLogger("fundb")
+
+
+class BaseTable(DeclarativeBase):
+    uid: Mapped[str] = mapped_column(String(128), comment="唯一ID", unique=True)
+
+    gmt_modified: Mapped[datetime] = mapped_column(
+        comment="修改时间", default=datetime.now, onupdate=datetime.now
+    )
+
+    gmt_create: Mapped[datetime] = mapped_column(
+        comment="创建时间", default=datetime.now
+    )
+    id: Mapped[int] = mapped_column(
+        primary_key=True, comment="主键", autoincrement=True
+    )
+
+    def _get_uid(self):
+        raise NotImplementedError
+
+    def _to_dict(self) -> dict:
+        raise NotImplementedError
+
+    def _child(self):
+        raise NotImplementedError
+
+    def to_dict(self) -> dict:
+        res = self._to_dict()
+        res.update(
+            {
+                "uid": self._get_uid(),
+            }
+        )
+        for key in list(res.keys()):
+            if res[key] is None:
+                res.pop(key)
+        return res
+
+    def exists(self, session: Session):
+        sql = select(self._child()).where(self._child().uid == self.uid)
+        return session.execute(sql).first() is not None
+
+    def upsert(self, session: Session, update_data=False):
+        try:
+            if not self.exists(session):
+                logger.debug(f"uid={self.uid} not exists, insert it.")
+                session.execute(insert(self._child()).values(**self.to_dict()))
+            elif update_data:
+                logger.debug(f"uid={self.uid} exists, update it.")
+                session.execute(
+                    update(self._child())
+                    .where(self._child().uid == self.uid)
+                    .values(**self.to_dict())
+                )
+        except Exception as e:
+            logger.error(f"upsert error: {e}:{traceback.format_exc()}")
