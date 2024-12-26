4b72a79b13b5f8f61a4c27deb4394d4f7ac47f21
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-26 15:04:39 +0000

    Add package textmeld/0.1.0/textmeld-0.1.0.tar.gz

---
+++ packages/textmeld/textmeld-0.1.0.tar.gz/textmeld-0.1.0/textmeld/constants.py
@@ -0,0 +1,20 @@
+from typing import Final
+
+READABLE_EXTENSIONS: Final[list[str]] = [
+    ".md",
+    ".txt",
+    ".yaml",
+    ".yml",
+    ".json",
+    ".py",
+    ".js",
+    ".ts",
+    ".jsx",
+    ".tsx",
+    ".html",
+    ".css",
+]
+
+DEFAULT_MAX_DEPTH: Final[int] = 999
+GITIGNORE_FILENAME: Final[str] = ".gitignore"
+README_FILENAME: Final[str] = "README.md"
