5c2497f3608d330271e8e2f2c81a5e9c4111bcf2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-26 04:05:02 +0000

    Add package aioccl/2024.12.6/aioccl-2024.12.6.tar.gz

---
+++ packages/aioccl/aioccl-2024.12.6.tar.gz/aioccl-2024.12.6/aioccl/device.py
@@ -0,0 +1,151 @@
+"""CCL device mapping."""
+
+from __future__ import annotations
+
+import logging
+import time
+from typing import Callable
+
+from .sensor import CCLSensor, CCL_SENSORS
+
+_LOGGER = logging.getLogger(__name__)
+
+CCL_DEVICE_INFO_TYPES = ("serial_no", "mac_address", "model", "fw_ver")
+
+
+class CCLDevice:
+    """Mapping for a CCL device."""
+    _binary_sensors: dict[str, CCLSensor] | None = {}
+    _device_id: str | None = None
+    _fw_ver: str | None = None
+    _last_updated_time: float | None = None
+    _mac_address: str | None = None
+    _model: str | None = None
+    _new_binary_sensor_callbacks = set()
+    _new_sensors: list[CCLSensor] | None = []
+    _new_sensor_callbacks = set()
+    _passkey = ''
+    _sensors: dict[str, CCLSensor] | None = {}
+    _serial_no: str | None = None
+    _update_callbacks = set()
+    
+
+    def __init__(self, passkey: str):
+        """Initialize a CCL device."""
+        self._passkey = passkey
+
+    @property
+    def passkey(self) -> str:
+        """Return the passkey."""
+        return self._passkey
+
+    @property
+    def device_id(self) -> str | None:
+        """Return the device ID."""
+        try:
+            self._device_id = self._mac_address.replace(":", "").lower()[-6:]
+        except Exception:  # pylint: disable=broad-exception-caught
+            return None
+        return self._device_id
+
+    @property
+    def name(self) -> str | None:
+        """Return the display name."""
+        if self._device_id is not None:
+            return self._model + " - " + self._device_id
+        return self._model
+
+    @property
+    def mac_address(self) -> str | None:
+        """Return the MAC address."""
+        return self._mac_address
+
+    @property
+    def model(self) -> str | None:
+        """Return the model."""
+        return self._model
+
+    @property
+    def fw_ver(self) -> str | None:
+        """Return the firmware version."""
+        return self._fw_ver
+
+    @property
+    def binary_sensors(self) -> dict[str, CCLSensor] | None:
+        """Store binary sensor data under this device."""
+        return self._binary_sensors
+
+    @property
+    def sensors(self) -> dict[str, CCLSensor] | None:
+        """Store sensor data under this device."""
+        return self._sensors
+
+    def update_info(self, info: dict[str, None | str]) -> None:
+        """Add or update device info."""
+        self._mac_address = info.get("mac_address")
+        self._model = info.get("model")
+        self._fw_ver = info.get("fw_ver")
+
+    def update_sensors(self, sensors: dict[str, None | str | int | float]) -> None:
+        """Add or update all sensor values."""
+        for key, value in sensors.items():
+            if CCL_SENSORS.get(key).binary:
+                if key not in self._binary_sensors:
+                    self._binary_sensors[key] = CCLSensor(key)
+                    self._new_sensors.append(self._binary_sensors[key])
+                self._binary_sensors[key].value = value
+            else:
+                if key not in self._sensors:
+                    self._sensors[key] = CCLSensor(key)
+                    self._new_sensors.append(self._sensors[key])
+                self._sensors[key].value = value
+        self._publish_new_sensors()
+        self._publish_updates()
+        self._last_updated_time = time.monotonic()
+        _LOGGER.debug("Sensors Updated: %s", self._last_updated_time)
+
+    def register_update_cb(self, callback: Callable[[], None]) -> None:
+        """Register callback, called when Sensor changes state."""
+        self._update_callbacks.add(callback)
+
+    def remove_update_cb(self, callback: Callable[[], None]) -> None:
+        """Remove previously registered callback."""
+        self._update_callbacks.discard(callback)
+
+    def _publish_updates(self) -> None:
+        """Schedule call all registered callbacks."""
+        try:
+            for callback in self._update_callbacks:
+                callback()
+        except Exception as err:  # pylint: disable=broad-exception-caught
+            _LOGGER.warning("Error while publishing sensor updates: %s", err)
+
+    def register_new_binary_sensor_cb(self, callback: Callable[[], None]) -> None:
+        """Register callback, called when Sensor changes state."""
+        self._new_binary_sensor_callbacks.add(callback)
+
+    def remove_new_binary_sensor_cb(self, callback: Callable[[], None]) -> None:
+        """Remove previously registered callback."""
+        self._new_binary_sensor_callbacks.discard(callback)
+
+    def register_new_sensor_cb(self, callback: Callable[[], None]) -> None:
+        """Register callback, called when Sensor changes state."""
+        self._new_sensor_callbacks.add(callback)
+
+    def remove_new_sensor_cb(self, callback: Callable[[], None]) -> None:
+        """Remove previously registered callback."""
+        self._new_sensor_callbacks.discard(callback)
+
+    def _publish_new_sensors(self) -> None:
+        """Schedule call all registered callbacks."""
+        for sensor in self._new_sensors[:]:
+            try:
+                if sensor.binary:
+                    for callback in self._new_binary_sensor_callbacks:
+                        callback(sensor)
+                else:
+                    for callback in self._new_sensor_callbacks:
+                        callback(sensor)
+                self._new_sensors.remove(sensor)
+            except Exception as err:  # pylint: disable=broad-exception-caught
+                _LOGGER.warning("Error while publishing new sensors: %s", err)
---
+++ packages/aioccl/aioccl-2024.12.6.tar.gz/aioccl-2024.12.6/aioccl/server.py
@@ -0,0 +1,98 @@
+"""CCL API server and handler."""
+
+from __future__ import annotations
+
+import logging
+
+from aiohttp import web
+
+from .device import CCLDevice, CCL_DEVICE_INFO_TYPES
+from .sensor import CCL_SENSORS
+
+_LOGGER = logging.getLogger(__name__)
+
+
+class CCLServer:
+    """Represent a CCL server manager."""
+
+    LISTEN_PORT = 42373
+
+    devices: dict[str, CCLDevice] = {}
+
+    @staticmethod
+    def register(device: CCLDevice) -> None:
+        """Register a device with a passkey."""
+        CCLServer.devices.setdefault(device.passkey, device)
+        _LOGGER.debug("Device registered: %s", device)
+    
+    @staticmethod
+    async def handler(request: web.BaseRequest | web.Request) -> web.Response:
+        """Handle POST requests for data updating."""
+        _body: dict[str, None | str | int | float] = {}
+        _device: CCLDevice = None
+        _info: dict[str, None | str] = {}
+        _passkey: str = ''
+        _sensors: dict[str, None | str | int | float] = {}
+        _status: None | int = None
+        _text: None | str = None
+
+        try:
+            _passkey = request.path[-8:]
+            for passkey in CCLServer.devices:
+                if passkey == _passkey:
+                    _device = CCLServer.devices[_passkey]
+                    break
+            assert isinstance(_device, CCLDevice), 404
+
+            assert request.content_type == "application/json", 400
+            assert 0 < request.content_length <= 5000, 400
+
+            _body = await request.json()
+
+        except Exception as err:  # pylint: disable=broad-exception-caught
+            _status = err.args[0]
+            if _status == 400:
+                _text = "400 Bad Request"
+            elif _status == 404:
+                _text = "404 Not Found"
+            else:
+                _status = 500
+                _text = "500 Internal Server Error"
+            _LOGGER.debug("Request exception occured: %s", err)
+            return web.Response(status=_status, text=_text)
+
+        
+        for key, value in _body.items():
+            if key in CCL_DEVICE_INFO_TYPES:
+                _info.setdefault(key, value)
+            elif key in CCL_SENSORS:
+                _sensors.setdefault(key, value)
+
+        _device.update_info(_info)
+        _device.update_sensors(_sensors)
+        _status = 200
+        _text = "200 OK"
+        _LOGGER.debug("Request processed: %s", _passkey)
+        return web.Response(status=_status, text=_text)
+
+    app = web.Application()
+    app.add_routes([web.get('/{passkey}', handler)])
+    runner = web.AppRunner(app)
+
+    @staticmethod
+    async def run() -> None:
+        """Try to run the API server."""
+        try:
+            _LOGGER.debug("Trying to start the API server.")
+            await CCLServer.runner.setup()
+            site = web.TCPSite(CCLServer.runner, port=CCLServer.LISTEN_PORT)
+            await site.start()
+        except Exception as err:  # pylint: disable=broad-exception-caught
+            _LOGGER.warning("Failed to run the API server: %s", err)
+        else:
+            _LOGGER.debug("Successfully started the API server.")
+
+    @staticmethod
+    async def stop() -> None:
+        """Stop running the API server."""
+        await CCLServer.runner.cleanup()
