afcc74c086be6dac1a4ea0ab48d19916b3f42a6a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-26 15:04:48 +0000

    Add package fake-bpy-module/20241226/fake_bpy_module-20241226.tar.gz

---
+++ packages/fake-bpy-module/fake_bpy_module-20241226.tar.gz/fake_bpy_module-20241226/gpu_extras/batch/__init__.pyi
@@ -0,0 +1,24 @@
+import typing
+import collections.abc
+import typing_extensions
+import gpu.types
+
+def batch_for_shader(
+    shader: gpu.types.GPUShader,
+    type: str,
+    content: dict[str, gpu.types.Buffer],
+    *,
+    indices=None,
+) -> gpu.types.GPUBatch:
+    """Return a batch already configured and compatible with the shader.
+
+        :param shader: shader for which a compatible format will be computed.
+        :type shader: gpu.types.GPUShader
+        :param type: "'POINTS', 'LINES', 'TRIS' or 'LINES_ADJ'".
+        :type type: str
+        :param content: Maps the name of the shader attribute with the data to fill the vertex buffer.
+    For the dictionary values see documentation for `gpu.types.GPUVertBuf.attr_fill` data argument.
+        :type content: dict[str, gpu.types.Buffer]
+        :return: compatible batch
+        :rtype: gpu.types.GPUBatch
+    """
