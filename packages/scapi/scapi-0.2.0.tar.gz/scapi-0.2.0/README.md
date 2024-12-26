b3b331bf0efb3edc38d1860445b0e4dcf7c8cdb0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-26 14:04:23 +0000

    Add package scapi/0.2.0/scapi-0.2.0.tar.gz

---
+++ packages/scapi/scapi-0.2.0.tar.gz/scapi-0.2.0/README.md
@@ -0,0 +1,11 @@
+# scapi
+[ScratchAttach](https://github.com/TimMcCool/scratchattach )より高機能を目指して。
+
+特徴
+- `async` `await` を使ったAPI
+- 型ヒント
+
+# インストール
+`pip install -U scapi`
+
+Wikiは[こちら](https://github.com/kakeruzoku/scapi/wiki )
---
+++ packages/scapi/scapi-0.2.0.tar.gz/scapi-0.2.0/scapi/sites/studio.py
@@ -0,0 +1,143 @@
+import datetime
+import random
+from typing import AsyncGenerator, TYPE_CHECKING
+
+
+from ..others import common as common
+from ..others import error as exception
+from . import base,activity
+from .comment import Comment
+
+if TYPE_CHECKING:
+    from .session import Session
+    from .user import User
+
+class Studio(base._BaseSiteAPI):
+    raise_class = exception.StudioNotFound
+    id_name = "id"
+
+    def __str__(self):
+        return f"<Studio id:{self.id} title:{self.title} Session:{self.Session}>"
+
+    def __init__(
+        self,
+        ClientSession:common.ClientSession,
+        id:int,
+        scratch_session:"Session|None"=None,
+        **entries
+    ):
+        super().__init__("get",f"https://api.scratch.mit.edu/studios/{id}",ClientSession,scratch_session)
+
+        self.id = id
+        self.title:str = None
+        self.description:str = None
+        self.author_id:int = None
+
+        self.open_to_all:bool = None
+        self.comments_allowed:bool = None
+
+        self._created:str = None
+        self._modified:str = None
+        self.created:datetime.datetime = None
+        self.modified:datetime.datetime = None
+
+        self.follower_count:int = None
+        self.manager_count:int = None
+        self.project_count:int = None
+
+    def _update_from_dict(self, data:dict):
+        self.title = data.get("title",self.title)
+        self.description = data.get("description",self.description)
+        self.author_id = data.get("host",self.author_id)
+
+        self.open_to_all = data.get("open_to_all",self.open_to_all)
+        self.comments_allowed = data.get("comments_allowed",self.comments_allowed)
+
+        _history:dict = data.get("history",{})
+        self._created = _history.get("created",self._created)
+        self.created = common.to_dt(self._created)
+        self._modified = _history.get("modified",self._modified)
+        self.modified = common.to_dt(self._modified)
+
+        _stats:dict = data.get("stats",{})
+        self.follower_count = _stats.get("followers",self.follower_count)
+        self.manager_count = _stats.get("managers",self.manager_count)
+        self.project_count = _stats.get("projects",self.project_count)
+
+    @property
+    def image_url(self) -> str:
+        return f"https://cdn2.scratch.mit.edu/get_image/gallery/{self.id}_170x100.png"
+    
+    @property
+    def url(self) -> str:
+        return f"https://scratch.mit.edu/studios/{self.id}/"
+
+    async def get_comment_by_id(self,id:int) -> Comment:
+        return await base.get_object(
+            self.ClientSession,{"place":self,"id":id,"data":None},Comment,self.Session
+        )
+
+    def get_comments(self, *, limit=40, offset=0) -> AsyncGenerator[Comment, None]:
+        return base.get_comment_iterator(
+            self,f"https://api.scratch.mit.edu/studios/{self.id}/comments",
+            limit=limit,offset=offset,add_params={"cachebust":random.randint(0,9999)}
+        )
+    
+    async def post_comment(self, content, *, parent_id="", commentee_id="") -> Comment:
+        self.has_session_raise()
+        data = {
+            "commentee_id": commentee_id,
+            "content": str(content),
+            "parent_id": parent_id,
+        }
+        header = self.ClientSession._header|{
+            "referer":self.url
+        }
+        resp = (await self.ClientSession.post(
+            f"https://api.scratch.mit.edu/proxy/comments/studio/{self.id}/",
+            header=header,json=data
+        )).json()
+        return Comment(
+            self.ClientSession,{"place":self,"data":resp,"id":resp["id"]},self.Session
+        )
+    
+    async def activity(self, *, limit=40, offset=0) -> AsyncGenerator[activity.Activity, None]:
+        c = 0
+        dt = str(datetime.datetime.now(datetime.timezone.utc))
+        for _ in range(offset,offset+limit,40):
+            r = await common.api_iterative(
+                self.ClientSession,f"https://api.scratch.mit.edu/studios/{self.id}/activity",limit=40,
+                add_params={"dateLimit":dt.replace("+00:00","Z")}
+            )
+            if len(r) == 0: return
+            for j in r:
+                c = c + 1
+                if c == limit: return
+                _obj = activity.Activity(self.ClientSession)
+                _obj._update_from_studio(self,j)
+                dt = str(_obj.datetime - datetime.timedelta(seconds=1))
+                yield _obj
+    
+async def get_studio(studio_id:int,*,ClientSession=None) -> Studio:
+    ClientSession = common.create_ClientSession(ClientSession)
+    return await base.get_object(ClientSession,studio_id,Studio)
+
+def create_Partial_Studio(studio_id:int,*,ClientSession:common.ClientSession|None=None,session:"Session|None"=None) -> Studio:
+    ClientSession = common.create_ClientSession(ClientSession)
+    return Studio(ClientSession,studio_id,session)
+
+def explore_studios(*, query:str="*", mode:str="trending", language:str="en", limit:int=40, offset:int=0,ClientSession:common.ClientSession=None) -> AsyncGenerator["Studio",None]:
+    ClientSession = common.create_ClientSession(ClientSession)
+    return base.get_object_iterator(
+        ClientSession, f"https://api.scratch.mit.edu/explore/studios",
+        None,Studio,limit=limit,offset=offset,
+        add_params={"language":language,"mode":mode,"q":query}
+    )
+
+def search_studios(query:str, *, mode:str="trending", language:str="en", limit:int=40, offset:int=0,ClientSession:common.ClientSession=None) -> AsyncGenerator["Studio",None]:
+    ClientSession = common.create_ClientSession(ClientSession)
+    return base.get_object_iterator(
+        ClientSession, f"https://api.scratch.mit.edu/search/studios",
+        None,Studio,limit=limit,offset=offset,
+        add_params={"language":language,"mode":mode,"q":query}
+    )
