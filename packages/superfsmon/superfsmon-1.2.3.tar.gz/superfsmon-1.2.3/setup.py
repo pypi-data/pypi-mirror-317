fc7ff56d130dd0cba99ed66ac9cdd60973f71f13
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-26 04:04:57 +0000

    Add package superfsmon/1.2.3/superfsmon-1.2.3.tar.gz

---
+++ packages/superfsmon/superfsmon-1.2.3.tar.gz/superfsmon-1.2.3/setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup
+
+setup(name='superfsmon',
+      version='1.2.3',
+      license='MIT',
+      description='Supervisor plugin to watch a directory and restart '
+                  'programs on changes',
+      author='Tim Schumacher',
+      author_email='tim@tschumacher.net',
+      url='https://github.com/timakro/superfsmon',
+      install_requires=[
+          'supervisor >= 4.0.0     ; sys_platform != "win32"',
+          'supervisor-win >= 4.0.0 ; sys_platform == "win32"',
+          'watchdog',
+      ],
+      packages=['superfsmon'],
+      entry_points={
+          'console_scripts': ['superfsmon=superfsmon:main']
+      })
