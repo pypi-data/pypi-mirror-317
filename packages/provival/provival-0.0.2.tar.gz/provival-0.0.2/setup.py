daa9b561e75882d6e80c316205894f43cf0f18d2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-26 14:04:16 +0000

    Add package provival/0.0.2/provival-0.0.2.tar.gz

---
+++ packages/provival/provival-0.0.2.tar.gz/provival-0.0.2/setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup
+
+setup(
+    name='provival',
+    version='0.0.2',    
+    description='Provival',
+    url='https://github.com/davidchen0420',
+    author='David Chen',
+    author_email='davidc.chen@mail.utoronto.ca',
+    license='BSD 2-clause',
+    packages=['provival'],
+    install_requires=['decoupler>=1.8.0',
+                      'pandas>=2.2.3',   
+                      'scipy>=1.14.1'],
+    include_package_data=True,
+package_data={"provival": ["data/SC_DB_Human.pickle", "data/SC_DB_Mouse.pickle", "data/SC_DB_Metadata.pickle"]},
+    classifiers=[
+        'Development Status :: 1 - Planning',
+        'Intended Audience :: Science/Research',
+        'License :: OSI Approved :: BSD License',  
+        'Operating System :: POSIX :: Linux',        
+        'Programming Language :: Python :: 3',
+    ],
+)
