8c1df1482c56678a2f0ad900adea0df840911ae7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-26 14:04:11 +0000

    Add package makenote/4.6/makenote-4.6-py3-none-any.whl

---
+++ packages/makenote/makenote-4.6-py3-none-any.whl/makenote/cli.py
@@ -0,0 +1,233 @@
+import argparse
+import configparser
+import os
+import shutil
+import sys
+
+from makenote import __version__
+from makenote.dbmanager import (
+    add_note,
+    get_note,
+    import_database,
+    list_tables,
+    make_book,
+    merge_databases_by_name,
+    set_category,
+    show_table_with_category,
+    table_exists,
+    tail_show_table,
+    update_entry,
+    export_database_json
+)
+from makenote.migrations import migrate_if_needed
+
+# read config file
+# TODO: try to read config from another local dir first. then go to default file
+
+
+possible_config_filenames = [
+    "./makenote.conf",
+    os.path.expanduser("~") + ".local/share/makenote/makenote.conf",
+    os.path.dirname(__file__) + "/makenote.conf",
+]
+for possible_name in possible_config_filenames:
+    if not os.path.exists(possible_name):
+        continue
+    else:
+        config_filename = possible_name
+
+migrate_if_needed(config_filename)
+
+config = configparser.ConfigParser()
+config.read(config_filename)
+
+# database file is stored here.
+diaryFileDir = os.path.abspath(
+    config["FILES"]["diaryFileDir"].replace("~/", f'{os.getenv("HOME")}/')
+)
+diaryFileName = os.path.join(diaryFileDir, config["FILES"]["default_table_name"])
+
+default_table_name = config["FILES"]["default_table_name"]
+# default table name
+
+show_jalali = config["SHOW_STYLE"].getboolean("show_jalali")
+
+if sys.stdout.isatty() is True:
+    # this number is like an option for how the show record output is styled
+    show_style = 2
+else:
+    show_style = 1
+
+
+parser = argparse.ArgumentParser(
+    prefix_chars="-",
+    prog="makenote",
+    formatter_class=argparse.RawDescriptionHelpFormatter,
+    description="add notes to diary or show them",
+    epilog="""examples:
+    makenote -t journals it was a nice day today!
+    makenote -s""",
+)
+
+parser.add_argument(
+    "-s",
+    "--show",
+    dest="show",
+    help="show notes from certain category",
+    nargs="?",
+    const=-1,
+    type=int,
+    metavar="CATEGORY",
+)
+parser.add_argument(
+    "-C",
+    "--category",
+    dest="set_category",
+    help="set category of notes",
+    default=-1,
+    type=int,
+    metavar="CATEGORY",
+)
+parser.add_argument(
+    "-T",
+    "--tail",
+    dest="tail",
+    help="show last items of table",
+    nargs="?",
+    type=int,
+    const=10,
+    metavar="LIMIT",
+)
+# parser.add_argument("-d", '--default', dest='default',
+#                     help="set default table", default=None)
+parser.add_argument(
+    "-c",
+    "--create",
+    dest="create_table",
+    help="create table",
+    default=None,
+    metavar="TABLE_NAME",
+)
+parser.add_argument(
+    "-l",
+    "--list",
+    dest="list_tables",
+    help="list tables",
+    default=None,
+    action="store_true",
+)
+parser.add_argument(
+    "-t",
+    "--table",
+    dest="table_name",
+    help="specify the notebook",
+    default=default_table_name,
+)
+parser.add_argument(
+    "-m",
+    "--merge",
+    help="merge two databases",
+    default=None,
+    nargs=3,
+    metavar=("FIRST", "SECOND", "OUTPUT"),
+)
+parser.add_argument(
+    "-x", "--export", help="export database into file", default=None, metavar="FILENAME"
+)
+parser.add_argument(
+    "-i",
+    "--import",
+    help="import database",
+    default=None,
+    dest="import_file",
+    metavar="FILENAME",
+)
+# parser.add_argument("-q", "--query",  help="search for text. in regex",
+#                     default=None, dest='query')
+parser.add_argument("text", help="text", default=None, nargs="*")
+
+parser.add_argument(
+    "-u",
+    "--update",
+    help="edit note. add entry number. last note is edited if no number is given",
+    default=None,
+    const="-1",
+    nargs="?",
+    metavar="note_id",
+    type=int,
+)
+parser.add_argument(
+    "-V", "--version", action="version", version="%(prog)s " + __version__
+)
+args = parser.parse_args()
+
+
+os.makedirs(os.path.dirname(diaryFileDir), exist_ok=True)
+# connect to sqlite file
+
+if args.show is not None:
+    show_table_with_category(diaryFileDir, args.table_name, category=args.set_category)
+elif args.tail:
+    tail_show_table(diaryFileDir, args.table_name, limit=args.tail)
+elif args.list_tables:
+    list_tables(diaryFileDir)
+elif args.create_table:
+    make_book(diaryFileDir, args.create_table)
+elif args.export:
+    export_database_json(diaryFileDir, args.table_name, args.export)
+elif args.import_file:
+    import_database(args.import_file, diaryFileName)
+elif args.merge:
+    firstdb, seconddb, outdb = args.merge
+    merge_databases_by_name(firstdb, seconddb, outdb)
+# elif args.default:
+#     default_table_name = args.default
+#     table_name = args.default
+
+elif args.set_category != -1:
+    if args.update is not None:
+        set_category(diaryFileDir, args.table_name, args.update, args.set_category)
+
+else:
+    # note will be added to this table
+    table_name = args.table_name
+    if not table_exists(diaryFileDir, table_name):
+        print(f"table {table_name} does not exist")
+        print("do you want to create it? (y/N)")
+        do_you_want_to_create = input()
+        if do_you_want_to_create.lower() in ["y", "yes"]:
+            make_book(diaryFileDir, table_name)
+        else:
+            exit(1)
+
+    if len(args.text) > 0:
+        note_text = " ".join(args.text)
+    else:
+        if args.update:
+            previous_text = get_note(diaryFileDir, args.table_name, args.update)
+        else:
+            previous_text = ""
+
+        try:
+            from prompt_toolkit import PromptSession, prompt
+            from prompt_toolkit.key_binding import KeyBindings
+
+            session = PromptSession()
+            bindings = KeyBindings()
+
+            @bindings.add("c-d")
+            def _(event):
+                "Exit when `c-d` is pressed."
+                session.history.append_string(event.app.current_buffer.text)
+                event.app.exit(result=event.app.current_buffer.text)
+
+            note_text = prompt(
+                multiline=True, default=previous_text, key_bindings=bindings
+            )
+        except KeyboardInterrupt:
+            exit(1)
+
+    if args.update:
+        update_entry(diaryFileDir, args.table_name, args.update, note_text)
+    else:
+        add_note(diaryFileDir, table_name, note_text)
---
+++ packages/makenote/makenote-4.6-py3-none-any.whl/makenote/convert_old_db_to_new.py
@@ -0,0 +1,148 @@
+import sqlite3
+import os
+import datetime
+from makenote.dbmanager import make_book, add_note
+import configparser
+import shutil
+
+def list_tables(sqlite_cursor: sqlite3.Cursor):
+    try:
+        # get list of tables
+        records = sqlite_cursor.execute(
+            'SELECT name from sqlite_master where type= "table"')
+        
+        tables = []
+
+        for r in records:
+            tables.append(r[0])
+        
+        return tables
+    # if there was an error, print error text and exit
+    except sqlite3.OperationalError as error_text:
+        print(error_text)
+        exit(1)
+
+def show_table(sqlite_cursor, table_name):
+    try:
+        # get records from sqlite
+        records = sqlite_cursor.execute(f"SELECT * FROM {table_name};")
+        # print them all
+        i = 0
+        records_list = []
+        for r in records:
+            records_list.append((r[0], r[1]))
+
+        return records_list
+    # if there was an error, print error text and exit
+    except sqlite3.OperationalError as error_text:
+        print(error_text)
+        exit(1)
+
+def merge_databases(cursor1:sqlite3.Cursor, cursor2:sqlite3.Cursor, cursor_out:sqlite3.Cursor):
+
+    def add_table(cursor_in, cursor_out, table_name):
+        cursor_out.execute(f'''CREATE TABLE IF NOT EXISTS {table_name} (date datetime, note text)''')
+        table_data = cursor_in.execute(f"select * from {table_name};").fetchall()
+
+        for entry in table_data:
+            cursor_out.execute(f"INSERT INTO {table_name} VALUES (?, ?)", (entry[0], entry[1]))
+  
+
+    def merge_tables(cursor1:sqlite3.Cursor, cursor2:sqlite3.Cursor, cursor_out:sqlite3.Cursor, table_name):
+
+
+        table_1 = cursor1.execute(f"select * from {table_name};").fetchall()
+        table_2 = cursor2.execute(f"select * from {table_name};").fetchall()
+
+        table_out = []
+        last_index_table2 = 0
+        for entry_1 in table_1:
+            for entry_2 in table_2[last_index_table2:]:
+                # print(x[0], y[0], x[0] > y[0])
+                if entry_1[0] > entry_2[0]:
+                    table_out.append(entry_2)
+                    # print(i)
+                    last_index_table2 += 1
+                else:
+                    break
+            table_out.append(entry_1)
+        for entry_2 in table_2[last_index_table2:]:
+            table_out.append(entry_2)
+
+
+        cursor_out.execute(f'''CREATE TABLE IF NOT EXISTS {table_name} (date datetime, note text)''')
+        for entry in table_out:
+            cursor_out.execute(f"INSERT INTO {table_name} VALUES (?, ?)", (entry[0], entry[1]))
+    
+
+    tables_1 = cursor1.execute('SELECT name from sqlite_master where type= "table"').fetchall()
+    tables_2 = cursor2.execute('SELECT name from sqlite_master where type= "table"').fetchall()
+    for table_name in tables_1:
+        if table_name in tables_2:
+            merge_tables(cursor1, cursor2, cursor_out, table_name[0])
+        else:
+            add_table(cursor1, cursor_out, table_name[0])
+    
+    for table_name in tables_2:
+        if table_name not in tables_1:
+            add_table(cursor2, cursor_out, table_name[0])
+
+
+def convert_old_db_to_new(old_database_filename:str, new_database_directory_name:str):
+
+    cur_old = sqlite3.Connection(old_database_filename).cursor()
+    tables = list_tables(cur_old)
+
+    os.makedirs(os.path.dirname(new_database_directory_name), exist_ok=True)
+    for table_name in tables:
+        make_book(new_database_directory_name, table_name)
+        entries = show_table(cur_old, table_name)
+        for date, note in entries:
+            date_and_time = datetime.datetime.fromisoformat(date)
+            add_note(new_database_directory_name, table_name, note, date_and_time=date_and_time)
+            print(date_and_time, date)
+
+
+def is_db_version1(database_filename):
+    # print(database_filename)
+    cur = sqlite3.Connection(database_filename).cursor()
+    tables = list_tables(cur)
+    for table in tables:
+        records = cur.execute(f"SELECT * FROM {table};")
+        for r in records:
+            if len(r) == 2:
+                return True
+    return False
+
+
+
+def check_for_old_dbs(database_directory:str)->list:
+    old_files = []
+    for filename in os.listdir(database_directory):
+        if filename.endswith('.db'):
+            file_path = os.path.realpath(os.path.join(database_directory, filename))
+            if is_db_version1(file_path):
+                old_files.append(file_path)
+    return old_files
+
+def convert_diaryFile(database_directory):
+    database_directory = os.path.realpath(database_directory)
+    os.makedirs(database_directory, exist_ok=True)
+    if 'diaryFile.db' in os.listdir(database_directory):
+        old_file_path = os.path.realpath(os.path.join(database_directory, 'diaryFile.db'))
+        new_file_path = os.path.realpath(os.path.join(database_directory, 'diaryFile.db.bak'))
+        shutil.move(old_file_path, new_file_path)
+        convert_old_db_to_new(new_file_path, database_directory)
+        print("database migration from v1.0 to v2.0 done")
+
+        
+def migrate_if_needed(config_filename):            
+    config = configparser.ConfigParser()
+    config.read(config_filename)
+    # database file is stored here.
+
+    if config['DATABASE'].getfloat('last_version') < 2.0:
+        diaryFileDir = os.path.abspath(config['FILES']['diaryFileDir'].replace("~/", f'{os.getenv("HOME")}/'))
+        convert_diaryFile(diaryFileDir)
+        config['DATABASE']['last_version'] = "2.0"
+        config.write(open(config_filename, 'w'))
