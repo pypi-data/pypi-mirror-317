3145b3fa472319a921cb1ccc11b2b9164dc18bd2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-26 15:05:21 +0000

    Add package pyravel/0.1.3/pyravel-0.1.3.tar.gz

---
+++ packages/pyravel/pyravel-0.1.3.tar.gz/pyravel-0.1.3/setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='pyravel',  # Changed name to match command
+    version='0.1.3',
+    author='Your Name',
+    author_email='your.email@example.com',
+    description='A Python package that builds files from the command line.',
+    packages=find_packages(),
+    entry_points={
+        'console_scripts': [
+            'pyravel=framework.cli:main',
+        ],
+    },
+    install_requires=[],
+    python_requires=">=3.12",
+)
