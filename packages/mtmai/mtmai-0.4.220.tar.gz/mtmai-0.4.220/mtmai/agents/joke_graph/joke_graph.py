5618f41e78f89d899e9a2b955d782bf1736bb349
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-26 14:04:44 +0000

    Add package mtmai/0.4.220/mtmai-0.4.220.tar.gz

---
+++ packages/mtmai/mtmai-0.4.220.tar.gz/mtmai-0.4.220/mtmai/agents/joke_graph/joke_graph.py
@@ -0,0 +1,22 @@
+from langgraph.graph import END, START, StateGraph
+from mtmai.agents.joke_graph.nodes.joke_writer_node import JokeWriterNode
+from mtmaisdk.clients.rest.models import JokeGraphState
+
+
+class JokeGraph:
+    @property
+    def name(self):
+        return "jokeGraph"
+
+    @property
+    def description(self):
+        return "笑话生成器(用于测试)"
+
+    async def build_graph(self):
+        builder = StateGraph(JokeGraphState)
+
+        builder.add_node("joke_writer", JokeWriterNode())
+        builder.add_edge(START, "joke_writer")
+        builder.add_edge("joke_writer", END)
+
+        return builder
