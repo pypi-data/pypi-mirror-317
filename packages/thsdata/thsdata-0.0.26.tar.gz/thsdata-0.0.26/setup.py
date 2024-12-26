6008f838ddc90be66b2ac647a48d766738b1fedb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-26 03:08:14 +0000

    Add package thsdata/0.0.26/thsdata-0.0.26.tar.gz

---
+++ packages/thsdata/thsdata-0.0.26.tar.gz/thsdata-0.0.26/setup.py
@@ -0,0 +1,47 @@
+import codecs
+import os
+from setuptools import setup, find_packages
+
+# these things are needed for the README.md show on pypi
+here = os.path.abspath(os.path.dirname(__file__))
+
+with codecs.open(os.path.join(here, "README.md"), encoding="utf-8") as fh:
+    long_description = "\n" + fh.read()
+
+
+VERSION = '0.0.26'
+DESCRIPTION = 'a python package for ... '
+LONG_DESCRIPTION = ' a python package for ...  '
+
+def package_files(directory):
+    paths = []
+    for (path, directories, filenames) in os.walk(directory):
+        for filename in filenames:
+            paths.append(os.path.join('..', path, filename))
+    return paths
+
+extra_files = package_files('libs')
+
+# Setting up
+setup(
+    name="thsdata",
+    version=VERSION,
+    author="",
+    author_email="",
+    description=DESCRIPTION,
+    long_description_content_type="text/markdown",
+    long_description=long_description,
+    packages=find_packages(),
+    package_data={'': extra_files},
+    include_package_data=True,
+    install_requires=[],
+    keywords=['python','thsdata'],
+    classifiers=[
+        "Development Status :: 1 - Planning",
+        "Intended Audience :: Developers",
+        "Programming Language :: Python :: 3",
+        "Operating System :: Unix",
+        "Operating System :: MacOS :: MacOS X",
+        "Operating System :: Microsoft :: Windows",
+    ]
+)
