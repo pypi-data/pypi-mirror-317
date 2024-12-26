00c311681fd40de7706e4ea29c62f7d70574d25c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-26 03:08:01 +0000

    Add package priority-manager/0.1.19/priority_manager-0.1.19-py3-none-any.whl

---
+++ packages/priority-manager/priority_manager-0.1.19-py3-none-any.whl/priority_manager/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+import re
+import sys
+from priority_manager.main import cli
+
+__version__ = "0.1.19"
+
+if __name__ == '__main__':
+    sys.exit(cli())
