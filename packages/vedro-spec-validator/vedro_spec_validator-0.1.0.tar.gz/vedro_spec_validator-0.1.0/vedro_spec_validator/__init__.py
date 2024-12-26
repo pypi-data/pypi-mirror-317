63fb37b8fc823e9f1629a45f7d5053112b1069bb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-26 15:05:02 +0000

    Add package vedro-spec-validator/0.1.0/vedro_spec_validator-0.1.0.tar.gz

---
+++ packages/vedro-spec-validator/vedro_spec_validator-0.1.0.tar.gz/vedro_spec_validator-0.1.0/vedro_spec_validator/__init__.py
@@ -0,0 +1,5 @@
+from ._spec_validator import validate_spec
+from ._spec_validator_plugin import SpecValidator, SpecValidatorPlugin
+
+__all__ = ("SpecValidator", "SpecValidatorPlugin", "validate_spec")
+__version__ = "0.1.0"
