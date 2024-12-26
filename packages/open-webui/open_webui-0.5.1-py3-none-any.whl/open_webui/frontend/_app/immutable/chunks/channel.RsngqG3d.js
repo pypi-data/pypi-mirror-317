f1e62fad4ba2eab093505f7caf4ae3665caa0b99
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-26 15:04:52 +0000

    Add package open-webui/0.5.1/open_webui-0.5.1-py3-none-any.whl

---
+++ packages/open-webui/open_webui-0.5.1-py3-none-any.whl/open_webui/frontend/_app/immutable/chunks/channel.RsngqG3d.js
@@ -0,0 +1,2 @@
+import{U as a,C as n}from"./Markdown.ClPR0zBx.js";const t=(r,o)=>a.lang.round(n.parse(r)[o]);export{t as c};
+//# sourceMappingURL=channel.RsngqG3d.js.map
---
+++ packages/open-webui/open_webui-0.5.1-py3-none-any.whl/open_webui/frontend/_app/immutable/entry/start.C7kxTyTe.js
@@ -0,0 +1,2 @@
+import{a as t}from"../chunks/entry.D_mVbzNT.js";export{t as start};
+//# sourceMappingURL=start.C7kxTyTe.js.map
