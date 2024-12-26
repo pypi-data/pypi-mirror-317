12491a72bf9603d36ef82e86b27705c894be6ba3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-26 04:04:30 +0000

    Add package declare-cloudinfra-standard-template-library/0.0.3rc72.post1/declare_cloudinfra_standard_template_library-0.0.3rc72.post1-py3-none-any.whl

---
+++ packages/declare-cloudinfra-standard-template-library/declare_cloudinfra_standard_template_library-0.0.3rc72.post1-py3-none-any.whl/declare_cloudinfra_standard_template_library/__init__.py
@@ -0,0 +1,8 @@
+#   -------------------------------------------------------------
+#   Copyright (c) Declare CloudInfra Limited Liability Company. All rights reserved.
+#   Licensed under the MIT License. See LICENSE in project root for information.
+#   -------------------------------------------------------------
+"""Python Package Template"""
+from __future__ import annotations
+
+__version__ = "0.0.3""-rc72-post1"
