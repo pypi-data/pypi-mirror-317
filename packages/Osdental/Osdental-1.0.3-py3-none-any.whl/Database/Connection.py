61e6410ac0f35706df7dd462fd29a0960b7a702b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-26 14:04:19 +0000

    Add package Osdental/1.0.3/Osdental-1.0.3-py3-none-any.whl

---
+++ packages/Osdental/Osdental-1.0.3-py3-none-any.whl/Database/Connection.py
@@ -0,0 +1,23 @@
+import os
+from dotenv import load_dotenv
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker, Session
+from Helpers.Message import DB_ENV_ERROR, DB_CONNECTION_ERROR
+
+load_dotenv()
+
+class DBConnection:
+    
+    def __init__(self):
+        self.connection_string = os.getenv('DATABASE')
+        if not self.connection_string:
+            raise ValueError(DB_ENV_ERROR)
+        
+        self.engine = create_engine(self.connection_string)
+        if self.engine is None:
+            raise ValueError(DB_CONNECTION_ERROR)
+        
+        self.session = sessionmaker(autocommit=False, autoflush=False, bind=self.engine)
+
+    def get_session(self) -> Session:
+        return self.session()
