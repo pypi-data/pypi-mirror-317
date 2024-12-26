2bd96d163d91b0d5b233ef659f689b2df267f434
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-26 03:08:11 +0000

    Add package Radial-Bias/0.1.2/radial_bias-0.1.2.tar.gz

---
+++ packages/Radial-Bias/radial_bias-0.1.2.tar.gz/radial_bias-0.1.2/setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='Radial-Bias',
+    version='0.1.2',
+    description='A simple Python project library.',
+    author='Raunak',
+    author_email='raunakgola9082@gmail.com',
+    packages=find_packages(where="src"),
+    package_dir={"":"src"},
+    install_requires=[ "requests >=2.25.1",
+    "numpy >=1.20.0"],  # List your dependencies here
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
---
+++ packages/Radial-Bias/radial_bias-0.1.2.tar.gz/radial_bias-0.1.2/tests/test_module.py
@@ -0,0 +1,11 @@
+import unittest
+from Radial_bias_Raunak_gola import module
+
+class TestAddFunction(unittest.TestCase):
+    def test_add(self):
+        self.assertEqual(module.add(1, 2), 3)
+        self.assertEqual(module.add(-1, 1), 0)
+        self.assertEqual(module.add(-1, -1), -2)
+
+if __name__ == '__main__':
+    unittest.main()
