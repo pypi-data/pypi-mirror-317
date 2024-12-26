13668001429c7053f26d4a0ef6145e8ba477d8ee
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-26 15:05:09 +0000

    Add package sgl-kernel/0.0.2.post9/sgl_kernel-0.0.2.post9-cp311-cp311-manylinux2014_x86_64.whl

---
+++ packages/sgl-kernel/sgl_kernel-0.0.2.post9-cp311-cp311-manylinux2014_x86_64.whl/sgl_kernel/ops/__init__.py
@@ -0,0 +1,19 @@
+from .moe_align_block_size import moe_align_block_size as _moe_align_block_size
+
+
+def moe_align_block_size(
+    topk_ids,
+    num_experts,
+    block_size,
+    sorted_token_ids,
+    experts_ids,
+    num_tokens_post_pad,
+):
+    _moe_align_block_size(
+        topk_ids,
+        num_experts,
+        block_size,
+        sorted_token_ids,
+        experts_ids,
+        num_tokens_post_pad,
+    )
