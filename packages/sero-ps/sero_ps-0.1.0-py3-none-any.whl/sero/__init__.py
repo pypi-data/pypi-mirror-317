24626570682de1ee69bec927fe56528ca03e135e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-26 03:08:07 +0000

    Add package sero-ps/0.1.0/sero_ps-0.1.0-py3-none-any.whl

---
+++ packages/sero-ps/sero_ps-0.1.0-py3-none-any.whl/sero/__init__.py
@@ -0,0 +1,8 @@
+__version__ = "0.1.0"
+__author__ = "Thirupathi"
+__email__ = "suryathirupks@gmail.com"
+
+from .prime_sero import soe
+from .scale_sero import scale_feature
+
+__all__ = ['soe', 'scale_feature']
