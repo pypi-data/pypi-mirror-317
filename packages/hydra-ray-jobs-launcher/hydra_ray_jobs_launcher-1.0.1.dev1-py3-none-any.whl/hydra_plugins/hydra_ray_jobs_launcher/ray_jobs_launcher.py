8cddf6c4bdcce8220cc3bb6b1624012b7f74e26e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-26 04:05:02 +0000

    Add package hydra-ray-jobs-launcher/1.0.1.dev1/hydra_ray_jobs_launcher-1.0.1.dev1-py3-none-any.whl

---
+++ packages/hydra-ray-jobs-launcher/hydra_ray_jobs_launcher-1.0.1.dev1-py3-none-any.whl/hydra_plugins/hydra_ray_jobs_launcher/ray_jobs_launcher.py
@@ -0,0 +1,54 @@
+# Copyright (c) Facebook, Inc. and its affiliates. All Rights Reserved
+import logging
+import sys
+from typing import Any, Optional, Sequence
+
+import ray
+from omegaconf import DictConfig, OmegaConf
+from ray.job_submission import JobSubmissionClient
+
+from hydra.core.utils import JobReturn
+from hydra.plugins.launcher import Launcher
+from hydra.types import HydraContext, TaskFunction
+
+from .config import RayJobsLauncherConf
+
+log = logging.getLogger(__name__)
+
+
+class RayJobsLauncher(Launcher):
+    def __init__(self, **params: Any) -> None:
+        """Ray Jobs Launcher
+
+        Launches jobs using Ray Jobs. For details, refer to:
+        https://docs.ray.io/en/latest/cluster/running-applications/job-submission/index.html
+        """
+        self.config: Optional[DictConfig] = None
+        self.task_function: Optional[TaskFunction] = None
+        self.hydra_context: Optional[HydraContext] = None
+
+        self.client_conf = OmegaConf.structured(RayJobsLauncherConf(**params))
+
+    def setup(
+        self,
+        *,
+        hydra_context: HydraContext,
+        task_function: TaskFunction,
+        config: DictConfig,
+    ) -> None:
+        self.config = config
+        self.task_function = task_function
+        self.hydra_context = hydra_context
+        self.original_invocation_path = sys.argv[0]
+
+        ray.init(ignore_reinit_error=True)
+        self.client = JobSubmissionClient("auto")
+
+    def launch(
+        self, job_overrides: Sequence[Sequence[str]], initial_job_idx: int
+    ) -> Sequence[JobReturn]:
+        from . import _core
+
+        return _core.launch(
+            launcher=self, job_overrides=job_overrides, initial_job_idx=initial_job_idx
+        )
