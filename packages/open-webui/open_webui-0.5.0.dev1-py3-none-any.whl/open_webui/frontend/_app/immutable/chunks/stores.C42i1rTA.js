c2bc6239a4fdeb2f63dabae18799f29ef9c93311
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-26 04:05:07 +0000

    Add package open-webui/0.5.0.dev1/open_webui-0.5.0.dev1-py3-none-any.whl

---
+++ packages/open-webui/open_webui-0.5.0.dev1-py3-none-any.whl/open_webui/frontend/_app/immutable/chunks/stores.C42i1rTA.js
@@ -0,0 +1,2 @@
+import{s as e}from"./entry.DG6P9C3k.js";const r=()=>{const s=e;return{page:{subscribe:s.page.subscribe},navigating:{subscribe:s.navigating.subscribe},updated:s.updated}},b={subscribe(s){return r().page.subscribe(s)}};export{b as p};
+//# sourceMappingURL=stores.C42i1rTA.js.map
---
+++ packages/open-webui/open_webui-0.5.0.dev1-py3-none-any.whl/open_webui/frontend/_app/immutable/entry/start.DRz5pbgt.js
@@ -0,0 +1,2 @@
+import{a as t}from"../chunks/entry.DG6P9C3k.js";export{t as start};
+//# sourceMappingURL=start.DRz5pbgt.js.map
