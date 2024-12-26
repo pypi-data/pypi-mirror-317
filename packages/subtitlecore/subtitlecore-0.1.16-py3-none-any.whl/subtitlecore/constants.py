1c3e660522a652556e42b8cb6aed63eae4dd31ea
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-26 03:07:59 +0000

    Add package subtitlecore/0.1.16/subtitlecore-0.1.16-py3-none-any.whl

---
+++ packages/subtitlecore/subtitlecore-0.1.16-py3-none-any.whl/subtitlecore/constants.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+"""
+Attributes:
+  SPACY_PKG (dict): key is language abbreviation, and value is spacy package name
+"""
+
+SPACY_PKG = {
+  "en": "en_core_web_trf",
+  "es": "es_dep_news_trf",
+  "de": "de_dep_news_trf",
+}
