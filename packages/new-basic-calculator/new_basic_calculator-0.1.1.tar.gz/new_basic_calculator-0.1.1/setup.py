58c38823e15971a4a4115a49647a3f0e49b3baa9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-26 03:07:44 +0000

    Add package new-basic-calculator/0.1.1/new_basic_calculator-0.1.1.tar.gz

---
+++ packages/new-basic-calculator/new_basic_calculator-0.1.1.tar.gz/new_basic_calculator-0.1.1/setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='new_basic_calculator',
+    version='0.1.1',
+    description='A basic calculator SDK',
+    author='snowffer',
+    author_email='snowffer@xxx.com',
+    packages=find_packages(where='./src'),
+    package_dir={'': './src'},
+    install_requires=[
+        'pandas',
+        # Add other dependencies here
+    ],
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.10',
+)
