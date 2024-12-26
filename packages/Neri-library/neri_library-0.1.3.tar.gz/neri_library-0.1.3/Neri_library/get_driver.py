0688899e8224af7685f57fa9d0f2a7fc7024064d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-26 04:05:11 +0000

    Add package Neri-library/0.1.3/neri_library-0.1.3.tar.gz

---
+++ packages/Neri-library/neri_library-0.1.3.tar.gz/neri_library-0.1.3/Neri_library/get_driver.py
@@ -0,0 +1,46 @@
+from selenium_stealth import stealth
+import undetected_chromedriver as uc
+import random
+import sys
+import os
+
+# 1. Alterar o User-Agent
+def set_user_agent():
+    user_agents = [
+        "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/115.0.0.0 Safari/537.36",
+        "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/116.0.0.0 Safari/537.36",
+        "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/117.0.0.0 Safari/537.36"
+    ]
+    return random.choice(user_agents)
+
+# 2. Configurar e iniciar o navegador com undetected_chromedriver
+def launch_browser(download_dir):
+
+    # Configurações para o Chrome
+    options = uc.ChromeOptions()
+
+    # Alterar o User-Agent
+    options.add_argument(f"user-agent={set_user_agent()}")
+
+    # Default's
+    profile = {
+        'download.prompt_for_download': False,
+        'download.directory_upgrade': True,
+    }
+
+    options.add_experimental_option('prefs', profile)
+
+    # Configurações para reduzir detecção
+    options.add_argument("--disable-blink-features=AutomationControlled")
+    options.add_argument("--disable-popup-blocking")
+    options.add_argument("--disable-infobars")
+    options.add_argument("--start-maximized")
+
+    # Inicializar o navegador com undetected_chromedriver
+    try:
+        driver = uc.Chrome(options=options, use_subprocess=True)
+    except Exception as e:
+        print(f"Erro ao iniciar o driver: {e}")
+        
+
+    return driver
---
+++ packages/Neri-library/neri_library-0.1.3.tar.gz/neri_library-0.1.3/setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="Neri_library",
+    version="0.1.3",
+    author="Neri",
+    author_email="gui.neriaz@gmail.com",
+    description="Me segue no insta ae @_._neri",
+    long_description_content_type="text/markdown",
+    packages=find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.7",
+    install_requires=[
+    "undetected-chromedriver",
+    "selenium-stealth",
+    "setuptools"
+    "selenium",
+    "requests",
+    ],
+)
