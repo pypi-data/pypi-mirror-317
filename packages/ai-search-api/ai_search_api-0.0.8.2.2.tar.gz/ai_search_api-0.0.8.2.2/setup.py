a0ba4a1fe87c122e831efbb840130055368f2a07
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-26 03:08:02 +0000

    Add package ai-search-api/0.0.8.2.2/ai_search_api-0.0.8.2.2.tar.gz

---
+++ packages/ai-search-api/ai_search_api-0.0.8.2.2.tar.gz/ai_search_api-0.0.8.2.2/setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="ai-search-api",
+    version="0.0.8.2.2",
+    packages=find_packages(),
+    install_requires=[],
+    description="search the future [test version]",
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    author="NeopadAI",
+    author_email="neopad.azienda@gmail.com",
+    url="https://github.com/neopad/ai-search",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
