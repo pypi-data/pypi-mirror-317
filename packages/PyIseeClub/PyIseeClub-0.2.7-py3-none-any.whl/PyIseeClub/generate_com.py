a9009fbd45fd41ce3237e6d9870284bf0b70ccf5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-26 15:05:53 +0000

    Add package PyIseeClub/0.2.7/PyIseeClub-0.2.7-py3-none-any.whl

---
+++ packages/PyIseeClub/PyIseeClub-0.2.7-py3-none-any.whl/PyIseeClub/generate_com.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+def generate_sequence(r, n):
+    """
+        r: Str
+        n: sequence num
+        eg: generate_sequence('IP001', 2)
+            out: ['IP001', 'IP002']
+    """
+    if n == 1:
+        return [r]
+    # 提取前缀（不包含最后两个字符的其余部分）
+    prefix = r[:-2]
+    # 提取最后两位数字并转换为整数
+    last_two_digits = int(r[-2:])
+    # 生成范围内的序列列表
+    sequence = [f"{prefix}{str(i).zfill(2)}" for i in range(last_two_digits, n+1)]
+    # 生成最终的字符串，格式为 '开始-结束'
+    # return f"{r}-{sequence[-1]}"
+    return sequence
+
