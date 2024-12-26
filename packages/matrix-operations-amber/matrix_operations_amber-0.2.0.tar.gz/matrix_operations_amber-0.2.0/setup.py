3c11c7b58453a3756223207440fbfe895e8b6002
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-26 03:08:01 +0000

    Add package matrix-operations-amber/0.2.0/matrix_operations_amber-0.2.0.tar.gz

---
+++ packages/matrix-operations-amber/matrix_operations_amber-0.2.0.tar.gz/matrix_operations_amber-0.2.0/setup.py
@@ -0,0 +1,49 @@
+from setuptools import setup, find_packages
+
+# 读取README.md文件作为长描述
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setup(
+    # 包的名称
+    name='matrix_operations_amber',
+
+    # 包的版本
+    version='0.2.0',
+
+    # 作者
+    author='Amber Hou',
+
+    # 作者的联系邮箱
+    author_email='1471147972@qq.com',
+
+    # 包的描述
+    description='A simple package for matrix operations',
+
+    # 包的长描述，从README.md读取
+    long_description=long_description,
+
+    # 长描述的格式，这里指定为markdown
+    long_description_content_type='text/markdown',
+
+    # 包的Python版本兼容性
+    python_requires='>=3.6',
+
+    # 包含的包和子包
+    packages=find_packages(),
+
+    # 包的分类
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Education',
+        'Intended Audience :: Science/Research',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+        'Topic :: Scientific/Engineering :: Mathematics',
+    ],
+
+)
