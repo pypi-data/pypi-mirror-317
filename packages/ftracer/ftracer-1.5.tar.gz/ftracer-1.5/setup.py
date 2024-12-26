54042e2198e0f42de3e6786dc841645f0d897c95
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-26 03:08:15 +0000

    Add package ftracer/1.5/ftracer-1.5.tar.gz

---
+++ packages/ftracer/ftracer-1.5.tar.gz/ftracer-1.5/setup.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python3
+
+import setuptools
+
+with open('README.md', 'r') as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="ftracer",
+    version="1.5",
+    author="Hiroyuki Ohsaki",
+    author_email="ohsaki@lsnl.jp",
+    description=
+    "Decorator functions to trace invocation of functions and class methods.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/h-ohsaki/ftracer",
+    packages=setuptools.find_packages(),
+    install_requires=['ansiterm'],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
+        "Operating System :: OS Independent",
+    ],
+)
