3cb6b2ace362b7e8e68245b23a9cee8c888107ad
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-26 03:08:00 +0000

    Add package numo/0.1.3/numo-0.1.3.tar.gz

---
+++ packages/numo/numo-0.1.3.tar.gz/numo-0.1.3/src/__init__.py
@@ -0,0 +1,5 @@
+"""Numo package for numerical operations and conversions."""
+from src.application.numo import Numo
+
+__version__ = "0.1.3"
+__all__ = ["Numo"] 
---
+++ packages/numo/numo-0.1.3.tar.gz/numo-0.1.3/tests/__init__.py
@@ -0,0 +1,1 @@
+"""Test suite for Numo package.""" 
