b7950b9507cb870ae7da3ba60a31cb49e4b80c78
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-27 15:04:34 +0000

    Add package Ramtrix/0.0.9/Ramtrix-0.0.9.tar.gz

---
+++ packages/Ramtrix/Ramtrix-0.0.9.tar.gz/Ramtrix-0.0.9/setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup
+
+VERSION = '0.0.9' 
+DESCRIPTION = 'Ripoff Numpy'
+LONG_DESCRIPTION = 'A library for manipulating matrices while boiling your RAM'
+
+# Setting up
+setup(
+        name="Ramtrix", 
+        version=VERSION,
+        author="Ram",
+        description=DESCRIPTION,
+        long_description=LONG_DESCRIPTION,
+
+        keywords=['python', 'matrices', 'row reduction', 'linear algebra'],
+        classifiers= [
+            "Development Status :: 3 - Alpha",
+            "Intended Audience :: Education",
+            "Programming Language :: Python :: 3",
+            "Operating System :: MacOS :: MacOS X",
+            "Operating System :: Microsoft :: Windows",
+        ]
+)
