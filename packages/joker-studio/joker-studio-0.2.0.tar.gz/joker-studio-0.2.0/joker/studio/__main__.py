6ee8f02c32e8b77ac14cbd4c1071acce96317b23
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-27 03:07:37 +0000

    Add package joker-studio/0.2.0/joker-studio-0.2.0.tar.gz

---
+++ packages/joker-studio/joker-studio-0.2.0.tar.gz/joker-studio-0.2.0/joker/studio/__main__.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python3
+# coding: utf-8
+
+from volkanic.cmdline import CommandRegistry
+
+cmddef = """\
+conv        joker.studio.ffmpeg.conv
+crop        joker.studio.ffmpeg.crop
+fade        joker.studio.ffmpeg.fade
+split       joker.studio.ffmpeg.split
+sub         joker.studio.ffmpeg.subtitle
+thumb       joker.studio.ffmpeg.thumb:thumb
+poster      joker.studio.ffmpeg.thumb:poster
+avatar      joker.studio.misc.avatar
+ico         joker.studio.misc.avatar:mkico
+imb         joker.studio.misc.avatar:mkimb
+iwh         joker.studio.misc.avatar:report_wh_ratios
+imt         joker.studio.misc.margin
+ren         joker.studio.misc.rename
+vid         joker.studio.misc.vident
+"""
+
+_prog = "python3 -m joker.sudio"
+registry = CommandRegistry.from_cmddef(cmddef, _prog)
+
+if __name__ == "__main__":
+    registry()
