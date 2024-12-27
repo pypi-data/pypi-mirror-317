c0a37575b03a1c297fc0469c10536f1558778cf6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-27 14:04:20 +0000

    Add package django-static-base/0.4.1/django_static_base-0.4.1.tar.gz

---
+++ packages/django-static-base/django_static_base-0.4.1.tar.gz/django_static_base-0.4.1/src/static_base/static/base/js/plugins/whitespace.js
@@ -0,0 +1,10 @@
+(function (a) {
+    a.fn.removeWhitespace = function () {
+        this.contents()
+            .filter(function () {
+                return this.nodeType == 3 && !/\S/.test(this.nodeValue);
+            })
+            .remove();
+        return this;
+    };
+})(jQuery);
