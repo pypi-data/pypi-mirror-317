98288ca38c9e25716de1be13560e03941a42cc48
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-27 15:04:25 +0000

    Add package regex-enumerator/0.9.1/regex_enumerator-0.9.1.tar.gz

---
+++ packages/regex-enumerator/regex_enumerator-0.9.1.tar.gz/regex_enumerator-0.9.1/tests/test_not_capturing_groups.py
@@ -0,0 +1,9 @@
+from regex_enumerator import RegexEnumerator
+from .test_function import f_finite, f_infinite
+
+
+def test_not_capturing_groups():
+    regex = r'(?:a)(b)\1'
+    possibilities = ['abb']
+
+    f_finite(regex, possibilities)
