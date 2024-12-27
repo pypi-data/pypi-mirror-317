c6c059a63447d47a7e66549bbbebcadd3f1d6269
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-27 04:04:17 +0000

    Add package tabular-orchestrated/0.3.2b0/tabular_orchestrated-0.3.2b0.tar.gz

---
+++ packages/tabular-orchestrated/tabular_orchestrated-0.3.2b0.tar.gz/tabular_orchestrated-0.3.2b0/tabular_orchestrated/dc/__init__.py
@@ -0,0 +1,9 @@
+from tabular_orchestrated.dc.dc_data import DCDataComp, DCTrainTestComp
+
+__all__ = [
+    "DCDataComp",
+    "DCTrainTestComp",
+    "DCModelCompV2",
+]
+
+from tabular_orchestrated.dc.dc_model_v2 import DCModelCompV2
