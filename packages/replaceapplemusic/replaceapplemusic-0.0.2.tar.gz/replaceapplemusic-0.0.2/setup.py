30d0d7860429952b4ffa0900145a2195b06ba66e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-27 04:04:22 +0000

    Add package replaceapplemusic/0.0.2/replaceapplemusic-0.0.2.tar.gz

---
+++ packages/replaceapplemusic/replaceapplemusic-0.0.2.tar.gz/replaceapplemusic-0.0.2/setup.py
@@ -0,0 +1,28 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="replaceapplemusic",
+    version="0.0.2",
+    py_modules=["main", "bridge", "file_reader"],
+    packages=find_packages(),
+    install_requires=[
+        "appscript",
+        "mutagen",
+    ],
+    entry_points={
+        "console_scripts": [
+            "ramusic=main:main",
+            "replaceapplemusic=main:main",
+        ],
+    },
+    author="realtvop",
+    description="A Python utility to replace source files of songs in Apple Music while preserving metadata",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    url="https://github.com/realtvop/ReplaceAppleMusic",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Operating System :: MacOS",
+    ],
+    python_requires=">=3.6",
+) 
