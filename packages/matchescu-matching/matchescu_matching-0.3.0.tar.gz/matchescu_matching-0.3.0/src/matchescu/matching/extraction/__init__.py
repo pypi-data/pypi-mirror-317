01deceb149e81df7d22571d39c5889f3aadbc251
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-27 03:07:46 +0000

    Add package matchescu-matching/0.3.0/matchescu_matching-0.3.0.tar.gz

---
+++ packages/matchescu-matching/matchescu_matching-0.3.0.tar.gz/matchescu_matching-0.3.0/src/matchescu/matching/extraction/__init__.py
@@ -0,0 +1,5 @@
+from matchescu.matching.extraction._csv import CsvDataSource
+from matchescu.matching.extraction._traits import Traits
+
+
+__all__ = ["CsvDataSource", "Traits"]
---
+++ packages/matchescu-matching/matchescu_matching-0.3.0.tar.gz/matchescu_matching-0.3.0/src/matchescu/matching/extraction/_traits.py
@@ -0,0 +1,57 @@
+import locale
+
+from typing import Any, Callable, Iterator, Union
+
+from matchescu.typing import Record, Trait
+
+
+def _process_string(value: Any) -> str:
+    return str(value)
+
+
+def _process_int(value: Any) -> int:
+    return int(value)
+
+
+def _process_float(value: Any) -> float:
+    return float(value)
+
+
+def _process_currency(value: Any) -> float | None:
+    if value is None:
+        return None
+    str_val = str(value).replace(",", "").strip()
+    return locale.atof(str_val.lstrip("$"))
+
+
+class RecordExtractionTrait:
+    def __init__(self, mapping: Callable[[Any], Any], keys: list[int | str]) -> None:
+        self.__mapping = mapping
+        self.__keys = keys
+
+    def __call__(self, item: Record) -> tuple:
+        return tuple(self.__mapping(item[key]) for key in self.__keys)
+
+
+class Traits:
+    def __init__(self):
+        self._traits = []
+
+    def string(self, keys: list[Union[int, str]]) -> "Traits":
+        self._traits.append(RecordExtractionTrait(_process_string, keys))
+        return self
+
+    def int(self, keys: list[Union[int, str]]) -> "Traits":
+        self._traits.append(RecordExtractionTrait(_process_int, keys))
+        return self
+
+    def float(self, keys: list[Union[int, str]]) -> "Traits":
+        self._traits.append(RecordExtractionTrait(_process_float, keys))
+        return self
+
+    def currency(self, keys: list[Union[int, str]]) -> "Traits":
+        self._traits.append(RecordExtractionTrait(_process_currency, keys))
+        return self
+
+    def __iter__(self) -> Iterator[Trait]:
+        return iter(self._traits)
