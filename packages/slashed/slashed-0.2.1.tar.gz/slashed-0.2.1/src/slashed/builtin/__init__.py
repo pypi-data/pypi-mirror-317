65fc45cf71454345ce0b467d38581345583009ac
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-27 14:04:08 +0000

    Add package slashed/0.2.1/slashed-0.2.1.tar.gz

---
+++ packages/slashed/slashed-0.2.1.tar.gz/slashed-0.2.1/src/slashed/builtin/__init__.py
@@ -0,0 +1,9 @@
+"""Built-in commands for Slashed."""
+
+from slashed.base import BaseCommand
+from slashed.builtin.help_cmd import help_cmd, show_command_cmd, exit_cmd
+
+
+def get_builtin_commands() -> list[BaseCommand]:
+    """Get list of built-in commands."""
+    return [help_cmd, show_command_cmd, exit_cmd]
