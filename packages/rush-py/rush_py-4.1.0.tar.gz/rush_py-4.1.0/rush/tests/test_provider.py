9e4e5ed104b90453505564d1337d2ecb3eea1ff6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-27 15:04:21 +0000

    Add package rush-py/4.1.0/rush_py-4.1.0.tar.gz

---
+++ packages/rush-py/rush_py-4.1.0.tar.gz/rush_py-4.1.0/rush/tests/test_provider.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+import asyncio
+import os
+
+from rush import Provider
+from rush.provider import build_provider_with_functions
+
+
+def test_provider_init():
+    # only run if RUSH_TOKEN is set
+    if "RUSH_TOKEN" in os.environ:
+        print("initing")
+        asyncio.run(build_provider_with_functions())
+    else:
+        print(os.environ)
+        print("Skipping test_provider_init")
+        Provider("")
