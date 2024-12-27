24abedf2bfa6629729fcaa03425d73fa382c1736
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-27 04:04:57 +0000

    Add package knify/1.6.70/knify-1.6.70.tar.gz

---
+++ packages/knify/knify-1.6.70.tar.gz/knify-1.6.70/knify/help.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+# -*- coding:utf-8 -*-
+# Author: qicongsheng
+def get_pip_name():
+    return 'knify'
+
+def get_version():
+    return '1.6.70'
+
+def print_version():
+    print('''treasure %s
+Development tools for python.''' % get_version())
