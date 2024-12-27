a746a975a089bb3418981831ea3ee027e56daf3b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-27 04:04:46 +0000

    Add package SQLGetterSetter/0.2.1.3.1/SQLGetterSetter-0.2.1.3.1.tar.gz

---
+++ packages/SQLGetterSetter/SQLGetterSetter-0.2.1.3.1.tar.gz/SQLGetterSetter-0.2.1.3.1/setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup, find_packages
+
+with open("README.md", "r") as f:
+    description = f.read()
+
+setup(
+    name = 'SQLGetterSetter',
+    version = '0.2.1.3.1',
+    packages = find_packages(),
+    install_requires =[
+        #library like 'pymongo = 3.2.1',
+
+    ],
+    entry_points = {
+        "console_scripts": [
+            "SQL-tester = SQLGetterSetter:hello",
+
+        ],
+    },
+    long_description=description,
+    long_description_content_type = "text/markdown",
+)
