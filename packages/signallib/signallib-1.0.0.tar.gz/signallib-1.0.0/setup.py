6cf4793305e05bcf59bfadd7b5e662faffbb9d70
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-27 14:04:21 +0000

    Add package signallib/1.0.0/signallib-1.0.0.tar.gz

---
+++ packages/signallib/signallib-1.0.0.tar.gz/signallib-1.0.0/setup.py
@@ -0,0 +1,30 @@
+
+from setuptools import setup, find_packages
+import codecs
+import os
+
+here = os.path.abspath(os.path.dirname(__file__))
+
+with codecs.open(os.path.join(here, "README.md"), encoding="utf-8") as fh:
+    long_description = "\n" + fh.read()
+
+VERSION = '1.0.0'
+DESCRIPTION = 'A Python library for signals and observers'
+
+# Setting up
+setup(
+    name="signallib",
+    version=VERSION,
+    author="Oarabile Koore",
+    description=DESCRIPTION,
+    long_description_content_type="text/markdown",
+    long_description=long_description,
+    packages=find_packages(),
+    install_requires=[],
+    keywords=['python', 'reactivity', 'signals', 'observers'],
+    classifiers=[
+        "Operating System :: Unix",
+        "Operating System :: MacOS :: MacOS X",
+        "Operating System :: Microsoft :: Windows",
+    ]
+)
