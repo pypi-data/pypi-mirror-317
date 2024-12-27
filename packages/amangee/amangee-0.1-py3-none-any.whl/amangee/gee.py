5a6d411f28e953c90499dfb3dcdfcc1540c4b99b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-27 03:07:33 +0000

    Add package amangee/0.1/amangee-0.1-py3-none-any.whl

---
+++ packages/amangee/amangee-0.1-py3-none-any.whl/amangee/gee.py
@@ -0,0 +1,7 @@
+# gee.py
+def download_satellite_data(aoi, start_date, end_date):
+    """
+    Download satellite data from GEE for a given area of interest (AOI).
+    """
+    return f"Downloading data for AOI: {aoi}, from {start_date} to {end_date}."
+
---
+++ packages/amangee/amangee-0.1-py3-none-any.whl/amangee/hydrology.py
@@ -0,0 +1,6 @@
+# hydrology.py
+def calculate_evapotranspiration(ndvi, temperature, solar_radiation):
+    """
+    Calculate evapotranspiration using NDVI, temperature, and solar radiation.
+    """
+    return f"Calculated ET with NDVI={ndvi}, Temp={temperature}, Solar={solar_radiation}."
