c3065da70b476a336d5a4da005691542c1fe69b0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-27 16:04:15 +0000

    Add package pbfetch/0.3.2/pbfetch-0.3.2-py3-none-any.whl

---
+++ packages/pbfetch/pbfetch-0.3.2-py3-none-any.whl/pbfetch/parse/computer_name.py
@@ -0,0 +1,25 @@
+def parse_comp_name():
+    file = "/sys/class/dmi/id"
+    name = "/product_name"
+    version = "/product_version"
+    try:
+        with open(file + name) as comp_name:
+            name = comp_name.read().strip()
+
+        with open(file + version) as comp_version:
+            version = comp_version.read().strip()
+
+        if name == "System Product Name":
+            name = "custom built"
+
+        if version == "System Version":
+            version = ""
+
+        if version:
+            return f"{name} ({version})"
+
+        return name
+
+    except Exception as e:
+        print(f"Parse Computer Name Error: {e}")
+        return None
---
+++ packages/pbfetch/pbfetch-0.3.2-py3-none-any.whl/pbfetch/parse/font.py
@@ -0,0 +1,42 @@
+from subprocess import Popen, PIPE
+
+
+def parse_font():
+    try:
+        font = Popen(
+            "fc-match Monospace",
+            shell=True,
+            stdout=PIPE,
+            stderr=PIPE,
+        )
+        font = str(font.communicate()[0])
+        font = font[3 : len(font) - 4]
+        font = font.split('"')[1]
+
+        if font:
+            return font
+        else:
+            return "not found"
+
+    except Exception:
+        pass
+
+    try:
+        font = Popen(
+            "zgrep FONT /proc/config.gz",
+            shell=True,
+            stdout=PIPE,
+            stderr=PIPE,
+        )
+        font = str(font.communicate()[0])
+        font = font[3 : len(font) - 4]
+
+        font = font.split(r"\n")
+
+        for line in font[::-1]:
+            if "=y" in line:
+                return line[12 : len(line) - 2]
+
+    except Exception as e:
+        print(f"Parse Font Error: {e}")
+        return None
