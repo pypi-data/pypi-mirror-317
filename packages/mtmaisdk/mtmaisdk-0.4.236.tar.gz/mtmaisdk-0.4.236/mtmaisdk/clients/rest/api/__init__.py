3e1ca085b95384def487a3d874d885f107f1f399
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-27 04:04:19 +0000

    Add package mtmaisdk/0.4.236/mtmaisdk-0.4.236.tar.gz

---
+++ packages/mtmaisdk/mtmaisdk-0.4.236.tar.gz/mtmaisdk-0.4.236/mtmaisdk/clients/rest/api/__init__.py
@@ -0,0 +1,30 @@
+# flake8: noqa
+
+# import apis into api package
+from mtmaisdk.clients.rest.api.api_token_api import APITokenApi
+from mtmaisdk.clients.rest.api.event_api import EventApi
+from mtmaisdk.clients.rest.api.github_api import GithubApi
+from mtmaisdk.clients.rest.api.healthcheck_api import HealthcheckApi
+from mtmaisdk.clients.rest.api.log_api import LogApi
+from mtmaisdk.clients.rest.api.metadata_api import MetadataApi
+from mtmaisdk.clients.rest.api.rate_limits_api import RateLimitsApi
+from mtmaisdk.clients.rest.api.sns_api import SNSApi
+from mtmaisdk.clients.rest.api.slack_api import SlackApi
+from mtmaisdk.clients.rest.api.step_run_api import StepRunApi
+from mtmaisdk.clients.rest.api.tenant_api import TenantApi
+from mtmaisdk.clients.rest.api.user_api import UserApi
+from mtmaisdk.clients.rest.api.worker_api import WorkerApi
+from mtmaisdk.clients.rest.api.workflow_api import WorkflowApi
+from mtmaisdk.clients.rest.api.workflow_run_api import WorkflowRunApi
+from mtmaisdk.clients.rest.api.agent_api import AgentApi
+from mtmaisdk.clients.rest.api.artifact_api import ArtifactApi
+from mtmaisdk.clients.rest.api.assisant_api import AssisantApi
+from mtmaisdk.clients.rest.api.blog_api import BlogApi
+from mtmaisdk.clients.rest.api.chat_api import ChatApi
+from mtmaisdk.clients.rest.api.default_api import DefaultApi
+from mtmaisdk.clients.rest.api.doc_api import DocApi
+from mtmaisdk.clients.rest.api.mtmai_api import MtmaiApi
+from mtmaisdk.clients.rest.api.prompt_api import PromptApi
+from mtmaisdk.clients.rest.api.subscribe_api import SubscribeApi
+from mtmaisdk.clients.rest.api.tools_api import ToolsApi
+
