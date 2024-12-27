820e5c1322783d6b7003fd155793c60466945eb7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-27 04:04:54 +0000

    Add package open-mpic-core/3.0.5/open_mpic_core-3.0.5.tar.gz

---
+++ packages/open-mpic-core/open_mpic_core-3.0.5.tar.gz/open_mpic_core-3.0.5/open_mpic_core/mpic_coordinator/domain/remote_check_call_configuration.py
@@ -0,0 +1,10 @@
+from open_mpic_core.common_domain.check_request import BaseCheckRequest
+from open_mpic_core.common_domain.enum.check_type import CheckType
+from open_mpic_core.mpic_coordinator.domain.remote_perspective import RemotePerspective
+
+
+class RemoteCheckCallConfiguration:
+    def __init__(self, check_type: CheckType, perspective: RemotePerspective, check_request: BaseCheckRequest):
+        self.check_type = check_type
+        self.perspective = perspective
+        self.check_request = check_request
