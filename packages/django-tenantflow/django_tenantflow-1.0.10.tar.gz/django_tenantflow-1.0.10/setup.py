2519e696cf10f1f4d78c5dc43737239a68a55263
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-27 15:04:44 +0000

    Add package django-tenantflow/1.0.10/django_tenantflow-1.0.10.tar.gz

---
+++ packages/django-tenantflow/django_tenantflow-1.0.10.tar.gz/django_tenantflow-1.0.10/setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="django-tenantflow",
+    version="1.0.10",
+    packages=find_packages(),
+    include_package_data=True,
+    install_requires=[
+        "django>=5.0",
+        "psycopg2-binary",
+    ],
+    license="MIT",
+    description="A Django library to enable semi-isolated multitenancy in your project with users outside the tenant.",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    url="https://github.com/ConspiraciXn/django-tenantflow",
+    author="Jared Soto",
+    author_email="jared.sl@icloud.com",
+    classifiers=[
+        "Framework :: Django",
+        "Programming Language :: Python :: 3.12",
+        "License :: OSI Approved :: MIT License",
+    ],
+)
