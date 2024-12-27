7773d0a7aba659c770c85ed970df2b33b6cedcbb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-27 16:04:17 +0000

    Add package cocoman/0.1.0/cocoman-0.1.0.tar.gz

---
+++ packages/cocoman/cocoman-0.1.0.tar.gz/cocoman-0.1.0/cocoman/core/__init__.py
@@ -0,0 +1,1 @@
+from cocoman.core.spiders import Spider
---
+++ packages/cocoman/cocoman-0.1.0.tar.gz/cocoman-0.1.0/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="cocoman",
+    version="0.1.0",
+    author="wauo",
+    author_email="markadc@126.com",
+    description="Spider",
+    packages=find_packages(),
+    python_requires=">=3.10",
+)
