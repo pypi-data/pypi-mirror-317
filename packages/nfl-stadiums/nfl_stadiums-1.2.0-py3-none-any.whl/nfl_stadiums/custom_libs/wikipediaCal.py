d9d659dae65d3cdf51b64cf9571788d984fb2e16
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-27 05:04:29 +0000

    Add package nfl-stadiums/1.2.0/nfl_stadiums-1.2.0-py3-none-any.whl

---
+++ packages/nfl-stadiums/nfl_stadiums-1.2.0-py3-none-any.whl/nfl_stadiums/custom_libs/wikipediaCal.py
@@ -0,0 +1,10 @@
+scrape_cal = {
+    "currentStadiumsTitle": "list of current stadiums",
+    "currentStadiumsTitleInfo": "The title of the section on wikipedia page https://en.wikipedia.org/wiki/List_of_current_NFL_stadiums pertaining to the current stadiums utilized by current NFL teams.",
+    "currentStadiumsNumFromHeading": 2,
+    "currentStadiumsNumFromHeadingInfo": "E.g., if current stadiums is the 2nd table in the section, 2. If it is the 1st, then 1.",
+    "additionalStadiumsTitle": "special event stadiums",
+    "additionalStadiumsTitleInfo": "The title of the section on wikipedia page https://en.wikipedia.org/wiki/List_of_current_NFL_stadiums pertaining to neutral fields that teams utilize for special events (e.g., global or hall of fame game)",
+    "additionalStadiumsNumFromHeading": 1,
+    "additionalStadiumsNumFromHeadingInfo": "E.g., if additional stadiums is the 2nd table in the section, 2. If it is the 1st, then 1."
+}
