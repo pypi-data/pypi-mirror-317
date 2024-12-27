1af4031167afad15824808f7b29bc71cbff8afb6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-27 15:04:47 +0000

    Add package manjpfb/0.0.6/manjpfb-0.0.6.tar.gz

---
+++ packages/manjpfb/manjpfb-0.0.6.tar.gz/manjpfb-0.0.6/manjpfb/CHECKSUM-SHA3-256.py
@@ -0,0 +1,7 @@
+'''
+SHA3-256(/Volumes/thder_data/home_mike/svnwork/current/manjpfb/head/public/pypi/manjpfb/__init__.py)= 34a7067d8a7077e10e91e7560a8cc9edab46b5636ebd47a84d485e545b4512fd
+SHA3-256(/Volumes/thder_data/home_mike/svnwork/current/manjpfb/head/public/pypi/manjpfb/__main__.py)= 557200c9b010f88c828c67b516c14f344b6d7dfb72c5b6e1d216d5d7df6fc210
+SHA3-256(/Volumes/thder_data/home_mike/svnwork/current/manjpfb/head/public/pypi/manjpfb/man_common.py)= 6bc20dca90946fc54a8a8a8f694574d3a1ee47ae4338b89f8f710979fca6fdea
+SHA3-256(/Volumes/thder_data/home_mike/svnwork/current/manjpfb/head/public/pypi/manjpfb/mman.py)= fc73253c1ae38b8e32dc5805f0e18822de7ed29873a9b419826b5a5e80c3ed7b
+SHA3-256(/Volumes/thder_data/home_mike/svnwork/current/manjpfb/head/public/pypi/manjpfb/manjpfb.py)= 70e8476a187342b675fc5574f814c9b9e74b206be8c943a9505f9c9bb5932f2e
+'''
