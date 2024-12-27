8032ff6563a07d047ccc34e3b6ebbade297a1355
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-27 14:04:16 +0000

    Add package FTL-Extract/0.5.0/ftl_extract-0.5.0-py3-none-any.whl

---
+++ packages/FTL-Extract/ftl_extract-0.5.0-py3-none-any.whl/ftl_extract/cli.py
@@ -0,0 +1,100 @@
+from __future__ import annotations
+
+from pathlib import Path
+from time import perf_counter_ns
+
+import click
+
+from ftl_extract.const import COMMENT_KEYS_MODE, DEFAULT_FTL_FILE, IGNORE_KWARGS
+from ftl_extract.ftl_extractor import extract
+
+
+@click.command()
+@click.argument("code_path", type=click.Path(exists=True, path_type=Path))
+@click.argument("output_path", type=click.Path(path_type=Path))
+@click.option(
+    "--language",
+    "-l",
+    multiple=True,
+    default=("en",),
+    show_default=True,
+    help="Language of translation.",
+)
+@click.option(
+    "--i18n_keys",
+    "-k",
+    default=("i18n", "L", "LazyProxy", "LazyFilter"),
+    multiple=True,
+    show_default=True,
+    help="Names of function that is used to get translation.",
+)
+@click.option(
+    "--ignore-attributes",
+    default=("set_locale", "use_locale", "use_context", "set_context"),
+    multiple=True,
+    show_default=True,
+    help="Ignore attributes, like `i18n.set_locale`.",
+)
+@click.option(
+    "--expand-ignore-attributes",
+    "-a",
+    multiple=True,
+    help="Expand default|targeted ignore attributes.",
+)
+@click.option(
+    "--ignore-kwargs",
+    default=IGNORE_KWARGS,
+    multiple=True,
+    show_default=True,
+    help="Ignore kwargs, like `when` from `aiogram_dialog.I18nFormat(..., when=...)`.",
+)
+@click.option(
+    "--comment-junks",
+    is_flag=True,
+    default=False,
+    show_default=True,
+    help="Comments Junk elements.",
+)
+@click.option(
+    "--default-ftl-file",
+    default=DEFAULT_FTL_FILE,
+    show_default=True,
+    type=click.Path(path_type=Path),
+)
+@click.option(
+    "--comment-keys-mode",
+    default="comment",
+    show_default=True,
+    help="Comment keys mode.",
+    type=click.Choice(COMMENT_KEYS_MODE),
+)
+@click.version_option()
+def cli_extract(
+    code_path: Path,
+    output_path: Path,
+    language: tuple[str, ...],
+    i18n_keys: tuple[str, ...],
+    ignore_attributes: tuple[str, ...],
+    expand_ignore_attributes: tuple[str, ...] | None = None,
+    ignore_kwargs: tuple[str, ...] = (),
+    comment_junks: bool = False,
+    default_ftl_file: Path = DEFAULT_FTL_FILE,
+    comment_keys_mode: str = "comment",
+) -> None:
+    click.echo(f"Extracting from {code_path}...")
+    start_time = perf_counter_ns()
+
+    extract(
+        code_path=code_path,
+        output_path=output_path,
+        language=language,
+        i18n_keys=i18n_keys,
+        ignore_attributes=ignore_attributes,
+        expand_ignore_attributes=expand_ignore_attributes,
+        ignore_kwargs=ignore_kwargs,
+        comment_junks=comment_junks,
+        default_ftl_file=default_ftl_file,
+        comment_keys_mode=comment_keys_mode,
+    )
+
+    click.echo(f"Done in {(perf_counter_ns() - start_time) * 1e-9:.3f}s.")
---
+++ packages/FTL-Extract/ftl_extract-0.5.0-py3-none-any.whl/ftl_extract/ftl_importer.py
@@ -0,0 +1,76 @@
+from __future__ import annotations
+
+from pathlib import Path
+from typing import TYPE_CHECKING
+
+from fluent.syntax import ast, parse
+
+from ftl_extract.matcher import FluentKey
+
+if TYPE_CHECKING:
+    from fluent.syntax.ast import Resource
+
+
+def import_from_ftl(
+    *,
+    path: Path,
+    locale: str,
+) -> tuple[dict[str, FluentKey], dict[str, FluentKey], Resource, list[FluentKey]]:
+    """Import `FluentKey`s from FTL."""
+    ftl_keys: dict[str, FluentKey] = {}
+    terms: dict[str, FluentKey] = {}
+    leave_as_is = []
+
+    resource = parse(path.read_text(encoding="utf-8"), with_spans=True)
+
+    for position, entry in enumerate(resource.body, start=0):
+        if isinstance(entry, ast.Message):
+            ftl_keys[entry.id.name] = FluentKey(
+                code_path=Path(),
+                key=entry.id.name,
+                translation=entry,
+                path=path,
+                locale=locale,
+                position=position,
+            )
+        elif isinstance(entry, ast.Term):
+            terms[entry.id.name] = FluentKey(
+                code_path=Path(),
+                key=entry.id.name,
+                translation=entry,
+                path=path,
+                locale=locale,
+                position=position,
+            )
+        else:
+            leave_as_is.append(
+                FluentKey(
+                    code_path=Path(),
+                    key="",
+                    translation=entry,
+                    path=path,
+                    locale=locale,
+                    position=position,
+                ),
+            )
+
+    return ftl_keys, terms, resource, leave_as_is
+
+
+def import_ftl_from_dir(
+    *,
+    path: Path,
+    locale: str,
+) -> tuple[dict[str, FluentKey], dict[str, FluentKey], list[FluentKey]]:
+    """Import `FluentKey`s from directory of FTL files."""
+    ftl_files = (path / locale).rglob("*.ftl") if path.is_dir() else [path]
+    ftl_keys: dict[str, FluentKey] = {}
+    terms: dict[str, FluentKey] = {}
+    leave_as_is = []
+
+    for ftl_file in ftl_files:
+        keys, terms, _, as_is_keys = import_from_ftl(path=ftl_file, locale=locale)
+        ftl_keys.update(keys)
+        leave_as_is.extend(as_is_keys)
+
+    return ftl_keys, terms, leave_as_is
