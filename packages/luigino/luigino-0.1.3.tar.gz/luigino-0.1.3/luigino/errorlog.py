aa978607cbfbfe6b4dc4e9d54420ee147787921d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-27 15:04:34 +0000

    Add package luigino/0.1.3/luigino-0.1.3.tar.gz

---
+++ packages/luigino/luigino-0.1.3.tar.gz/luigino-0.1.3/luigino/errorlog.py
@@ -0,0 +1,24 @@
+# 지정한 경로에 error.log 파일을 생성하고 에러 로그를 기록하는 모듈
+import os
+from common_task import CommonLuigiTask
+from functools import wraps
+
+def handle_exceptions(func):
+    @wraps(func)
+    def wrapper(self:CommonLuigiTask, *args, **kwargs):
+        try:
+            return func(self, *args, **kwargs)
+        except Exception as e:
+            write_log(self.output_dir(), f"{self.__class__.__name__}: {e}")
+            print(f"Error: {e}")
+            raise e
+    return wrapper
+
+def write_log(path, message):
+    """
+    지정한 경로에 error.log 파일을 생성하고 에러 로그를 기록하는 함수
+    :param path: 로그 파일을 생성할 경로
+    :param message: 기록할 에러 메시지
+    """
+    with open(os.path.join(path, 'error.log'), 'a') as f:
+        f.write(message + '\n')
