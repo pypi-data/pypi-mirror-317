5ebebb21ad42a57ca75a1e9126c5bf5f392c907e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-27 06:04:46 +0000

    Add package ovos-core/0.3.7a1/ovos-core-0.3.7a1.tar.gz

---
+++ packages/ovos-core/ovos-core-0.3.7a1.tar.gz/ovos-core-0.3.7a1/ovos_core/intent_services/adapt_service.py
@@ -0,0 +1,4 @@
+# backwards compat import
+from ovos_adapt.opm import AdaptPipeline as AdaptService
+from ovos_utils.log import log_deprecation
+log_deprecation("adapt service moved to 'ovos-adapt-pipeline-plugin'. this import is deprecated", "1.0.0")
---
+++ packages/ovos-core/ovos-core-0.3.7a1.tar.gz/ovos-core-0.3.7a1/ovos_core/intent_services/padacioso_service.py
@@ -0,0 +1,5 @@
+# backwards compat imports
+from padacioso.opm import PadaciosoPipeline as PadaciosoService, PadaciosoIntent
+from padacioso import IntentContainer as FallbackIntentContainer
+from ovos_utils.log import log_deprecation
+log_deprecation("adapt service moved to 'padacioso.opm'. this import is deprecated", "1.0.0")
