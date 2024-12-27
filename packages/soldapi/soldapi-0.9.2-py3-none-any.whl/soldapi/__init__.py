65bcea699180df07d5e2000429447858ae275891
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-27 15:04:27 +0000

    Add package soldapi/0.9.2/soldapi-0.9.2-py3-none-any.whl

---
+++ packages/soldapi/soldapi-0.9.2-py3-none-any.whl/soldapi/__init__.py
@@ -0,0 +1,20 @@
+"""
+Solid Operational Long-term Dynamics API
+
+Examples
+--------
+from soldapi.core import qwork
+duck1 = qwork.DuckLike(complex_object1)
+duck2 = qwork.DuckLike(complex_object2)
+print(duck1 - duck2)
+print(duck2 - duck1)
+"""
+
+import logging
+
+# from .core import qwork  # Chores like pathing, time utils.
+
+
+__all__ = []
+
+logging.getLogger("soldapi").addHandler(logging.NullHandler())
