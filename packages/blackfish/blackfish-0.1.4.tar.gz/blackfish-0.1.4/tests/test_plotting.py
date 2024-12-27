efdd30037faa3b92fdc350579fa2cd8c3b545e77
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-27 05:04:33 +0000

    Add package blackfish/0.1.4/blackfish-0.1.4.tar.gz

---
+++ packages/blackfish/blackfish-0.1.4.tar.gz/blackfish-0.1.4/tests/test_plotting.py
@@ -0,0 +1,13 @@
+from pathlib import Path
+
+import plotly.graph_objects as go
+
+from blackfish.plotting import plotly_template
+
+ROOT = Path(__file__).parent
+
+
+def test_plotly_template_works():
+    template = plotly_template()
+    fig = go.Figure(template=template)
+    assert isinstance(fig, go.Figure)
