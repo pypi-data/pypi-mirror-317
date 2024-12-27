dea8dd5b1199341971ccbe23c0d00a01b0fe6cbc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-27 06:04:41 +0000

    Add package awspacha/0.1.0/awspacha-0.1.0.tar.gz

---
+++ packages/awspacha/awspacha-0.1.0.tar.gz/awspacha-0.1.0/setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='awspacha',
+    version='0.1.0',
+    author='Ed Condori',
+    author_email='edcondoricc@gmail.com',
+    description='Paquete para operaciones con Spark y S3',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    url='https://github.com/edcondoric/awspacha',
+    packages=find_packages(where='src'),
+    package_dir={'': 'src'},
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.7',
+    install_requires=[
+        'pyspark',
+        'boto3',
+        'awsglue',
+    ],
+)
