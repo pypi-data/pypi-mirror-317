d5640ed34ed6efe6164ffcf2315902b99e9499db
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-27 15:04:24 +0000

    Add package pokercraft-local-bankroll/0.4.0/pokercraft_local_bankroll-0.4.0-cp313-cp313t-manylinux_2_17_ppc64le.manylinux2014_ppc64le.whl

---
+++ packages/pokercraft-local-bankroll/pokercraft_local_bankroll-0.4.0-cp313-cp313t-manylinux_2_17_ppc64le.manylinux2014_ppc64le.whl/pokercraft_local_bankroll/pokercraft_local_bankroll.pyi
@@ -0,0 +1,40 @@
+class BankruptcyMetric:
+    """
+    Represents a bankruptcy metric.
+    """
+
+    def len(self) -> int:
+        """
+        Get the number of simulations performed so far.
+        """
+        ...
+
+    def get_bankruptcy_rate(self) -> float:
+        """
+        Get the bankruptcy rate. This is not cached.
+        """
+        ...
+
+    def get_survival_rate(self) -> float:
+        """
+        Get the profitable rate. This is not cached.
+        """
+        ...
+
+    def get_profitable_rate(self) -> float:
+        """
+        Get the profitable rate. This is not cached.
+        """
+        ...
+
+def calculate(
+    initial_capital: float,
+    relative_return_results: list[float],
+    max_iteration: int,
+    profit_exit_multiplier: float,
+    simulation_count: int,
+) -> BankruptcyMetric:
+    """
+    Calculate the bankruptcy metric.
+    """
+    ...
