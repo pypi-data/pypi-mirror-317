a65609f03f4b835696069a37b314c5c26b82a89c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-27 05:04:32 +0000

    Add package mlinfra/0.0.25/mlinfra-0.0.25.tar.gz

---
+++ packages/mlinfra/mlinfra-0.0.25.tar.gz/mlinfra-0.0.25/src/mlinfra/modules/cloud/aws/helm_chart/main.tf
@@ -0,0 +1,21 @@
+resource "helm_release" "helm_chart" {
+  name             = var.name
+  namespace        = var.namespace
+  create_namespace = var.create_namespace
+  repository       = var.repository
+  chart            = var.chart
+  version          = var.chart_version
+  cleanup_on_fail  = var.cleanup_on_fail
+  atomic           = true
+  timeout          = var.timeout
+  values           = [var.values]
+
+  dynamic "set" {
+    for_each = var.set
+    content {
+      name  = set.value.name
+      value = set.value.value
+      type  = "auto"
+    }
+  }
+}
