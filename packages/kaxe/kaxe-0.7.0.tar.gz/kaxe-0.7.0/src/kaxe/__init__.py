e232fe22dfc013321b6789e478a2f8b4a796a11e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-27 04:05:10 +0000

    Add package kaxe/0.7.0/kaxe-0.7.0.tar.gz

---
+++ packages/kaxe/kaxe-0.7.0.tar.gz/kaxe-0.7.0/src/kaxe/__init__.py
@@ -0,0 +1,21 @@
+
+import logging
+
+from .plot import *
+from .chart import *
+from .data import data
+from .objects import *
+from .core import koundTeX, setDefaultColors
+from .core import resetColor as resetColors
+from .core import symbol as Symbol
+from .core.window import Window, AttrObject, AttrMap
+
+try:
+    ipy_str = str(type(get_ipython()))
+    if 'zmqshell' in ipy_str:
+        logging.basicConfig(level=logging.CRITICAL)
+    if 'terminal' in ipy_str:
+        logging.basicConfig(level=logging.CRITICAL)
+except:
+    logging.basicConfig(level=logging.INFO)
+
---
+++ packages/kaxe/kaxe-0.7.0.tar.gz/kaxe-0.7.0/src/kaxe/plot/__init__.py
@@ -0,0 +1,15 @@
+
+from .themes import Themes
+from .log import LogPlot, BoxLogPlot, LOGPLOT
+from .standard import Plot, XYPLOT
+from .polar import PolarPlot, POLARPLOT
+from .box import BoxPlot
+from .empty import EmptyPlot, EmptyWindow
+from .d3 import Plot3D, PlotCenter3D, PlotFrame3D, PlotEmpty3D, XYZPLOT
+
+
+class identities:
+    XYPLOT = XYPLOT
+    POLAR = POLARPLOT
+    XYZPLOT = XYZPLOT
+    LOGPLOT = LOGPLOT
