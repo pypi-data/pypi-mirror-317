1c1b0235f3b8c1077f4e4ac083d8a5f4b0a58c46
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-27 15:04:42 +0000

    Add package django-tenantflow/1.0.5/django_tenantflow-1.0.5-py3-none-any.whl

---
+++ packages/django-tenantflow/django_tenantflow-1.0.5-py3-none-any.whl/tenantflow/models/__init__.py
@@ -0,0 +1,7 @@
+from .account import AbstractAccount
+from .user_account import AbstractUserAccount
+
+__all__ = [
+    "AbstractAccount",
+    "AbstractUserAccount",
+]
---
+++ packages/django-tenantflow/django_tenantflow-1.0.5-py3-none-any.whl/tenantflow/models/user_account.py
@@ -0,0 +1,23 @@
+from django.db import models
+
+class AbstractUserAccount(models.Model):
+    """
+    Abstract model to represent the relationship between a user and an account in the project.
+    """
+    user = models.ForeignKey(
+        'auth.User',
+        on_delete=models.CASCADE,
+        related_name="user_accounts",
+        help_text="User associated with the account"
+    )
+    created_at = models.DateTimeField(auto_now_add=True, help_text="Creation date")
+    updated_at = models.DateTimeField(auto_now=True, help_text="Last update date")
+
+    def __str__(self):
+        return f"{self.user.username} - {self.account.name}"
+
+    class Meta:
+        abstract = True
+        unique_together = ('user')
+        verbose_name = "User-Account Relationship"
+        verbose_name_plural = "User-Account Relationships"
