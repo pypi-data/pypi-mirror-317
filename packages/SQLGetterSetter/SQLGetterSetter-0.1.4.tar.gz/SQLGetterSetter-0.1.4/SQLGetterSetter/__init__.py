ea3ca8ffffcab508943d8dbcc0f06bb85867bc51
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-27 04:04:45 +0000

    Add package SQLGetterSetter/0.1.4/SQLGetterSetter-0.1.4.tar.gz

---
+++ packages/SQLGetterSetter/SQLGetterSetter-0.1.4.tar.gz/SQLGetterSetter-0.1.4/SQLGetterSetter/__init__.py
@@ -0,0 +1,31 @@
+from .main import hello
+
+
+class MainClass:
+    def __init__(self, connection):
+        # pass
+        self.connection = connection
+    
+    def select(self,key = None):
+        cursor = self.connection.cursor()
+        try:
+            cursor.execute("SELECT * FROM users")
+            rows = cursor.fetchall()
+            print("Data retrieved successfully:")
+            for row in rows:
+                print(row)
+        except Error as e:
+            print(f"Error: {e}")
+                
+    def insert(self, data):
+        cursor = self.connection.cursor()
+        try:
+            query = "INSERT INTO users (first_name, last_name, email, age) VALUES (%s, %s, %s, %s)"
+            cursor.executemany(query, data)
+            self.connection.commit()  # Commit the transaction
+            print("Data inserted successfully.")
+        except Error as e:
+            print(f"Error: {e}")
+            self.connection.rollback()  # Rollback in case of error
+
+    
