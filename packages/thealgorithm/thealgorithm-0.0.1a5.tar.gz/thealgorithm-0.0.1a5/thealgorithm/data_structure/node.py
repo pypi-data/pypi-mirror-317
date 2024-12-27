0d37718342de984ef7855547fc8d2e9b27e35e44
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-27 03:07:43 +0000

    Add package thealgorithm/0.0.1a5/thealgorithm-0.0.1a5.tar.gz

---
+++ packages/thealgorithm/thealgorithm-0.0.1a5.tar.gz/thealgorithm-0.0.1a5/thealgorithm/data_structure/node.py
@@ -0,0 +1,12 @@
+class ListNode:
+    def __init__(self, x):
+        self.val = x
+        self.next = None
+        self.prev = None
+
+
+class TreeNode:
+    def __init__(self, x):
+        self.val = x
+        self.left = None
+        self.right = None
