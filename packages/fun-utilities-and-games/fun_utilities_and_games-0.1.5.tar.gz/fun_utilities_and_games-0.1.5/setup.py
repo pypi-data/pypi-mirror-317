0964922aa236f695793cef20234f6ff86b25772a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-27 15:04:40 +0000

    Add package fun-utilities-and-games/0.1.5/fun_utilities_and_games-0.1.5.tar.gz

---
+++ packages/fun-utilities-and-games/fun_utilities_and_games-0.1.5.tar.gz/fun_utilities_and_games-0.1.5/setup.py
@@ -0,0 +1,33 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='fun_utilities_and_games',
+    version='0.1.5',  
+    packages=find_packages(), 
+    install_requires=['pygame', 'tk', ],
+    author='Sriganth',
+    author_email='sriganthperiyannan@example.com',
+    description='A collection of fun utilities and text-based games in Python.',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown', 
+    url='https://github.com/Sriganth-byte/fun_utilities_and_games',  
+    project_urls={  
+        'Bug Tracker': 'https://github.com/Sriganth-byte/fun_utilities_and_games/issues',
+        'Source Code': 'https://github.com/Sriganth-byte/fun_utilities_and_games',
+    },
+    classifiers=[  
+        'Development Status :: 1 - Planning',
+        'Intended Audience :: Developers',
+        'Topic :: Games/Entertainment :: Puzzle Games',
+        'License :: OSI Approved :: MIT License',  
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.9',
+        'Programming Language :: Python :: 3.10',
+        'Programming Language :: Python :: 3.11',
+        'Programming Language :: Python :: Implementation :: CPython',
+        'Programming Language :: Python :: Implementation :: PyPy',
+    ],
+    python_requires='>=3.7', 
+    keywords='games utilities text-based python snake hangman tic-tac-toe puzzles',  
+    license='MIT',
+)
