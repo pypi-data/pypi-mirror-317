8644e47cd2663d28f2da329c13ef296dfbbc7c32
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-27 14:04:08 +0000

    Add package types-aiobotocore-sso-oidc/2.16.1/types_aiobotocore_sso_oidc-2.16.1.tar.gz

---
+++ packages/types-aiobotocore-sso-oidc/types_aiobotocore_sso_oidc-2.16.1.tar.gz/types_aiobotocore_sso_oidc-2.16.1/types_aiobotocore_sso_oidc/client.py
@@ -0,0 +1,162 @@
+"""
+Type annotations for sso-oidc service Client.
+
+[Documentation](https://youtype.github.io/types_aiobotocore_docs/types_aiobotocore_sso_oidc/client/)
+
+Usage::
+
+    ```python
+    from aiobotocore.session import get_session
+    from types_aiobotocore_sso_oidc.client import SSOOIDCClient
+
+    session = get_session()
+    async with session.create_client("sso-oidc") as client:
+        client: SSOOIDCClient
+    ```
+
+Copyright 2024 Vlad Emelianov
+"""
+
+from __future__ import annotations
+
+import sys
+from types import TracebackType
+from typing import Any, Mapping
+
+from aiobotocore.client import AioBaseClient
+from botocore.client import ClientMeta
+from botocore.errorfactory import BaseClientExceptions
+from botocore.exceptions import ClientError as BotocoreClientError
+
+from .type_defs import (
+    CreateTokenRequestRequestTypeDef,
+    CreateTokenResponseTypeDef,
+    CreateTokenWithIAMRequestRequestTypeDef,
+    CreateTokenWithIAMResponseTypeDef,
+    RegisterClientRequestRequestTypeDef,
+    RegisterClientResponseTypeDef,
+    StartDeviceAuthorizationRequestRequestTypeDef,
+    StartDeviceAuthorizationResponseTypeDef,
+)
+
+if sys.version_info >= (3, 12):
+    from typing import Self, Unpack
+else:
+    from typing_extensions import Self, Unpack
+
+
+__all__ = ("SSOOIDCClient",)
+
+
+class Exceptions(BaseClientExceptions):
+    AccessDeniedException: type[BotocoreClientError]
+    AuthorizationPendingException: type[BotocoreClientError]
+    ClientError: type[BotocoreClientError]
+    ExpiredTokenException: type[BotocoreClientError]
+    InternalServerException: type[BotocoreClientError]
+    InvalidClientException: type[BotocoreClientError]
+    InvalidClientMetadataException: type[BotocoreClientError]
+    InvalidGrantException: type[BotocoreClientError]
+    InvalidRedirectUriException: type[BotocoreClientError]
+    InvalidRequestException: type[BotocoreClientError]
+    InvalidRequestRegionException: type[BotocoreClientError]
+    InvalidScopeException: type[BotocoreClientError]
+    SlowDownException: type[BotocoreClientError]
+    UnauthorizedClientException: type[BotocoreClientError]
+    UnsupportedGrantTypeException: type[BotocoreClientError]
+
+
+class SSOOIDCClient(AioBaseClient):
+    """
+    [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/sso-oidc.html#SSOOIDC.Client)
+    [Show types-aiobotocore documentation](https://youtype.github.io/types_aiobotocore_docs/types_aiobotocore_sso_oidc/client/)
+    """
+
+    meta: ClientMeta
+
+    @property
+    def exceptions(self) -> Exceptions:
+        """
+        SSOOIDCClient exceptions.
+
+        [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/sso-oidc.html#SSOOIDC.Client)
+        [Show types-aiobotocore documentation](https://youtype.github.io/types_aiobotocore_docs/types_aiobotocore_sso_oidc/client/#exceptions)
+        """
+
+    def can_paginate(self, operation_name: str) -> bool:
+        """
+        [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/sso-oidc/client/can_paginate.html)
+        [Show types-aiobotocore documentation](https://youtype.github.io/types_aiobotocore_docs/types_aiobotocore_sso_oidc/client/#can_paginate)
+        """
+
+    async def generate_presigned_url(
+        self,
+        ClientMethod: str,
+        Params: Mapping[str, Any] = ...,
+        ExpiresIn: int = 3600,
+        HttpMethod: str = ...,
+    ) -> str:
+        """
+        [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/sso-oidc/client/generate_presigned_url.html)
+        [Show types-aiobotocore documentation](https://youtype.github.io/types_aiobotocore_docs/types_aiobotocore_sso_oidc/client/#generate_presigned_url)
+        """
+
+    async def create_token(
+        self, **kwargs: Unpack[CreateTokenRequestRequestTypeDef]
+    ) -> CreateTokenResponseTypeDef:
+        """
+        Creates and returns access and refresh tokens for clients that are
+        authenticated using client secrets.
+
+        [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/sso-oidc/client/create_token.html)
+        [Show types-aiobotocore documentation](https://youtype.github.io/types_aiobotocore_docs/types_aiobotocore_sso_oidc/client/#create_token)
+        """
+
+    async def create_token_with_iam(
+        self, **kwargs: Unpack[CreateTokenWithIAMRequestRequestTypeDef]
+    ) -> CreateTokenWithIAMResponseTypeDef:
+        """
+        Creates and returns access and refresh tokens for clients and applications that
+        are authenticated using IAM entities.
+
+        [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/sso-oidc/client/create_token_with_iam.html)
+        [Show types-aiobotocore documentation](https://youtype.github.io/types_aiobotocore_docs/types_aiobotocore_sso_oidc/client/#create_token_with_iam)
+        """
+
+    async def register_client(
+        self, **kwargs: Unpack[RegisterClientRequestRequestTypeDef]
+    ) -> RegisterClientResponseTypeDef:
+        """
+        Registers a client with IAM Identity Center.
+
+        [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/sso-oidc/client/register_client.html)
+        [Show types-aiobotocore documentation](https://youtype.github.io/types_aiobotocore_docs/types_aiobotocore_sso_oidc/client/#register_client)
+        """
+
+    async def start_device_authorization(
+        self, **kwargs: Unpack[StartDeviceAuthorizationRequestRequestTypeDef]
+    ) -> StartDeviceAuthorizationResponseTypeDef:
+        """
+        Initiates device authorization by requesting a pair of verification codes from
+        the authorization service.
+
+        [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/sso-oidc/client/start_device_authorization.html)
+        [Show types-aiobotocore documentation](https://youtype.github.io/types_aiobotocore_docs/types_aiobotocore_sso_oidc/client/#start_device_authorization)
+        """
+
+    async def __aenter__(self) -> Self:
+        """
+        [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/sso-oidc.html#SSOOIDC.Client)
+        [Show types-aiobotocore documentation](https://youtype.github.io/types_aiobotocore_docs/types_aiobotocore_sso_oidc/client/)
+        """
+
+    async def __aexit__(
+        self,
+        exc_type: type[BaseException] | None,
+        exc_val: BaseException | None,
+        exc_tb: TracebackType | None,
+    ) -> None:
+        """
+        [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/sso-oidc.html#SSOOIDC.Client)
+        [Show types-aiobotocore documentation](https://youtype.github.io/types_aiobotocore_docs/types_aiobotocore_sso_oidc/client/)
+        """
