1c79c66b02b12ea2dc2ddad3ee4dd5febd590ca8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-27 04:05:24 +0000

    Add package Babaji/2.0.1/Babaji-2.0.1-py3-none-any.whl

---
+++ packages/Babaji/Babaji-2.0.1-py3-none-any.whl/Babaji/demo.py
@@ -0,0 +1,10 @@
+from Babaji import HeightPredictor
+
+# Initialize the predictor (no need to pass URLs)
+predictor = HeightPredictor()
+
+# Predict height for a given weight
+  # Example input
+predicted_height = predictor.predict(78)
+print(predicted_height)
+
