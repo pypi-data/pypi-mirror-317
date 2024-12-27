56092e4b58e0d37275a08bb05855943a993a306a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-27 16:04:15 +0000

    Add package printk/0.3.2/printk-0.3.2.tar.gz

---
+++ packages/printk/printk-0.3.2.tar.gz/printk-0.3.2/setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup, find_packages
+
+# read the contents of your README file
+from os import path
+this_directory = path.abspath(path.dirname(__file__))
+with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
+    
+    
+setup(
+    # readme file
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    # package information
+    
+    name="printk",
+    version="0.3.2",
+    packages=find_packages(),
+    description="A colorful print tool for python",
+    author="bruce_cui",
+    author_email="summer56567@163.com",
+    install_requires=[
+        # 依赖列表
+        "termcolor >= 2.3.0",
+    ],
+)
