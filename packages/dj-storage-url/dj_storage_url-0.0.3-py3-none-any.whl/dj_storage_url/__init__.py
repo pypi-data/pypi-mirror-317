e9a8b593c1b6a56c9fcac66b008d47a372e4d1ed
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-28 04:05:01 +0000

    Add package dj-storage-url/0.0.3/dj_storage_url-0.0.3-py3-none-any.whl

---
+++ packages/dj-storage-url/dj_storage_url-0.0.3-py3-none-any.whl/dj_storage_url/__init__.py
@@ -0,0 +1,59 @@
+from urllib.parse import parse_qs, urlparse, urlsplit
+
+
+def strtobool(value):
+    value = value.lower()
+    if value in ("t", "true", "1"):
+        return True
+    elif value in ("f", "false", "0"):
+        return False
+    else:
+        raise ValueError("invalid truth value %r" % (value,))
+
+
+def file_system_storage(url):
+    parsed_url = urlparse(url)
+    return {
+        "BACKEND": "django.core.files.storage.FileSystemStorage",
+        "OPTIONS": {
+            "location": "".join((parsed_url.netloc, parsed_url.path)),
+        },
+    }
+
+
+def memory_storage(_):
+    return {"BACKEND": "django.core.files.storage.MemoryStorage", "OPTIONS": {}}
+
+
+def s3_storage(url):
+    parsed_url = urlparse(url)
+    querystring = parse_qs(parsed_url.query)
+    return {
+        "BACKEND": "storages.backends.s3boto3.S3Boto3Storage",
+        "OPTIONS": {
+            "bucket_name": parsed_url.netloc,
+            **({"location": parsed_url.path} if parsed_url.path else {}),
+            **(
+                {"gzip": strtobool(querystring["gzip"][0])}
+                if "gzip" in querystring
+                else {}
+            ),
+        },
+    }
+
+
+def unknown_storage(url):
+    scheme, _ = url.split("://")
+    return {"BACKEND": scheme, "OPTIONS": {}}
+
+
+SCHEME_TO_CONFIG = {
+    "file": file_system_storage,
+    "memory": memory_storage,
+    "s3": s3_storage,
+}
+
+
+def parse(url):
+    scheme, *_ = urlsplit(url)
+    return SCHEME_TO_CONFIG.get(scheme, unknown_storage)(url)
