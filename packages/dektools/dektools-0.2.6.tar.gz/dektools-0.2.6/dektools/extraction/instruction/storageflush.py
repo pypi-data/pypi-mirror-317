c52e578be4216d2846798e63ff1986ec8b91457b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-28 14:04:25 +0000

    Add package dektools/0.2.6/dektools-0.2.6.tar.gz

---
+++ packages/dektools/dektools-0.2.6.tar.gz/dektools-0.2.6/dektools/extraction/instruction/storageflush.py
@@ -0,0 +1,14 @@
+from .base import InstructionBase
+from ..produce import DefaultProduce
+
+
+class StorageFlushInstruction(InstructionBase):
+    head = '$<'
+    produce_cls = DefaultProduce
+
+    def run(self, variables, storage):
+        if self.value is None:
+            reset = True
+        else:
+            reset = self.instruction_set.eval_expression(None, self.value, variables)
+        return [storage.flush(self.produce_cls.wrapper(self.key), reset)]
