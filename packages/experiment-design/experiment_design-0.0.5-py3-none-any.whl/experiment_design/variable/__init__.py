986da2c0f60f9f7f8873690077953675ef1c426a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-28 15:04:49 +0000

    Add package experiment-design/0.0.5/experiment_design-0.0.5-py3-none-any.whl

---
+++ packages/experiment-design/experiment_design-0.0.5-py3-none-any.whl/experiment_design/variable/__init__.py
@@ -0,0 +1,13 @@
+from experiment_design.variable.variable import (
+    ContinuousVariable,
+    DiscreteVariable,
+    Variable,
+    create_continuous_uniform_variables,
+    create_discrete_uniform_variables,
+    create_variables_from_distributions,
+)
+from experiment_design.variable.space import (
+    ParameterSpace,
+    VariableCollection,
+    create_correlation_matrix,
+)
