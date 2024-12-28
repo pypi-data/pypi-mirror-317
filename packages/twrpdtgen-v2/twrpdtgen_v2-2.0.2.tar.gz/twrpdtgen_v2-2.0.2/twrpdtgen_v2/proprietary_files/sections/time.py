2a6e3762be296049452ad146c099e5e5f7c4eb88
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-28 04:04:33 +0000

    Add package twrpdtgen-v2/2.0.2/twrpdtgen_v2-2.0.2.tar.gz

---
+++ packages/twrpdtgen-v2/twrpdtgen_v2-2.0.2.tar.gz/twrpdtgen_v2-2.0.2/twrpdtgen_v2/proprietary_files/sections/time.py
@@ -0,0 +1,23 @@
+# BACKUP REPLACE FILE
+
+#
+# Copyright (C) 2022 The LineageOS Project
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+from twrpdtgen_v2.proprietary_files.section import Section, register_section
+
+class TimeSection(Section):
+	name = "Time services"
+	apps = [
+		"TimeService",
+	]
+	binaries = [
+		"time_daemon",
+	]
+	libraries = [
+		"libtime_genoff",
+	]
+
+register_section(TimeSection)
