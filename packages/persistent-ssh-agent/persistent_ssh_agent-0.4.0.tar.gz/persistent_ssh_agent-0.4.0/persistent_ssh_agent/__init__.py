12158c485d9d2bc2872de471c2aeccfcca454cdd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-28 14:04:42 +0000

    Add package persistent-ssh-agent/0.4.0/persistent_ssh_agent-0.4.0.tar.gz

---
+++ packages/persistent-ssh-agent/persistent_ssh_agent-0.4.0.tar.gz/persistent_ssh_agent-0.4.0/persistent_ssh_agent/__init__.py
@@ -0,0 +1,6 @@
+# Import third-party modules
+from persistent_ssh_agent.config import SSHConfig
+from persistent_ssh_agent.core import PersistentSSHAgent
+
+
+__all__ = ["PersistentSSHAgent", "SSHConfig"]
