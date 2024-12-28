e91d6ad591683f4b8ebf5c6ad4369f0c8f5e4ee1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-28 04:05:00 +0000

    Add package harlequelrah-fastapi/0.6.4/harlequelrah_fastapi-0.6.4-py3-none-any.whl

---
+++ packages/harlequelrah-fastapi/harlequelrah_fastapi-0.6.4-py3-none-any.whl/harlequelrah_fastapi/sqlapp/routes.py
@@ -0,0 +1,6 @@
+# from sqlalchemy.orm import Session
+# import schema
+# import crud
+# from fastapi import Depends, APIRouter
+# from typing import List
+# app_myapp = APIRouter(prefix="/myapps", tags=["myapp"])
