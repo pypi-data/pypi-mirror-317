4df8e8b8cfe81860902cb72906365f4114b447ee
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-28 04:04:27 +0000

    Add package twrpdtgen-v2/2.0.1/twrpdtgen_v2-2.0.1.tar.gz

---
+++ packages/twrpdtgen-v2/twrpdtgen_v2-2.0.1.tar.gz/twrpdtgen_v2-2.0.1/twrpdtgen_v2/proprietary_files/sections/alarm.py
@@ -0,0 +1,24 @@
+# BACKUP REPLACE FILE
+
+#
+# Copyright (C) 2022 The LineageOS Project
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+from twrpdtgen_v2.proprietary_files.section import Section, register_section
+
+class AlarmSection(Section):
+	name = "Alarm"
+	interfaces = [
+		"vendor.qti.hardware.alarm",
+	]
+	apps = [
+		"PowerOffAlarm",
+	]
+	binaries = [
+		"power_off_alarm",
+		"poweroffm64",
+	]
+
+register_section(AlarmSection)
---
+++ packages/twrpdtgen-v2/twrpdtgen_v2-2.0.1.tar.gz/twrpdtgen_v2-2.0.1/twrpdtgen_v2/proprietary_files/sections/weaver.py
@@ -0,0 +1,17 @@
+# BACKUP REPLACE FILE
+
+#
+# Copyright (C) 2022 The LineageOS Project
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+from twrpdtgen_v2.proprietary_files.section import Section, register_section
+
+class WeaverSection(Section):
+	name = "Weaver"
+	interfaces = [
+		"android.hardware.weaver",
+	]
+
+register_section(WeaverSection)
