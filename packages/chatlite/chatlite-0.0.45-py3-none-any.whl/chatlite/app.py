b7a3d30262aa4eca336cd4fd79d992ebb58fa056
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-28 14:04:27 +0000

    Add package chatlite/0.0.45/chatlite-0.0.45-py3-none-any.whl

---
+++ packages/chatlite/chatlite-0.0.45-py3-none-any.whl/chatlite/app.py
@@ -0,0 +1,54 @@
+from typing import Optional
+from chatlite.core._types._hf_type import HFModelType
+from chatlite.core.config import ModelConfig
+from chatlite.core import ChatServer
+
+def create_server(
+    model_type: str="local",
+    model_name: Optional[HFModelType]|str = None,
+    api_key: Optional[str] = None,
+    temperature: float = 0.0,
+    max_tokens: int = 4000,
+    base_url: Optional[str] = None,
+    **kwargs
+) -> ChatServer:
+    """Create a unified server instance"""
+    config = ModelConfig.create(
+        model_type=model_type,
+        model_name=model_name,
+        api_key=api_key,
+        temperature=temperature,
+        max_tokens=max_tokens,
+        base_url=base_url,
+        **kwargs
+    )
+    return ChatServer(config)
+
+def server(
+    model_type: str="local",
+    model_name: Optional[HFModelType]|str = None,
+    api_key: Optional[str] = None,
+    temperature: float = 0.0,
+    max_tokens: int = 4000,
+    base_url: Optional[str] = None,
+    **kwargs
+):
+    """Create a default server instance"""
+    _app = create_server(
+        model_type=model_type,
+        model_name=model_name,
+        api_key=api_key,
+        temperature=temperature,
+        max_tokens=max_tokens,
+        base_url=base_url,
+        **kwargs
+    )
+    _app.run()
+
+if __name__ == '__main__':
+    server(
+        model_type='huggingface',
+        model_name="NousResearch/Hermes-3-Llama-3.1-8B"
+    # base_url="http://192.168.170.76:11434/v1",
+    #     model_name="qwen2.5:7b-instruct"
+    )
