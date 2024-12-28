e6e06a96171afca5af636a2d5eb7d00fb3df86bd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-28 14:05:01 +0000

    Add package fake-bpy-module/20241228/fake_bpy_module-20241228-py3-none-any.whl

---
+++ packages/fake-bpy-module/fake_bpy_module-20241228-py3-none-any.whl/mathutils/interpolate/__init__.pyi
@@ -0,0 +1,16 @@
+"""
+The Blender interpolate module
+
+"""
+
+import typing
+import collections.abc
+import typing_extensions
+
+def poly_3d_calc(veclist, pt) -> list[float]:
+    """Calculate barycentric weights for a point on a polygon.
+
+    :param veclist: Sequence of 3D positions.
+    :param pt: 2D or 3D position.   :type pt: Sequence[float]   :return: list of per-vector weights.
+    :rtype: list[float]
+    """
