457ed4ba4d7aa5d7fac847b29bfb3883a5f062a3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-28 04:04:27 +0000

    Add package grid-data-models/1.3.3/grid_data_models-1.3.3.tar.gz

---
+++ packages/grid-data-models/grid_data_models-1.3.3.tar.gz/grid_data_models-1.3.3/src/gdm/distribution/components/base/distribution_component_base.py
@@ -0,0 +1,20 @@
+""" This module contains class for managing basic fields for distribution assets."""
+
+from typing_extensions import Annotated
+from typing import Optional
+from abc import ABC
+
+from pydantic import Field
+from infrasys import Component
+
+from gdm.distribution.components.distribution_feeder import DistributionFeeder
+from gdm.distribution.components.distribution_substation import DistributionSubstation
+
+
+class DistributionComponentBase(Component, ABC):
+    """Interface for simple distribution component."""
+
+    substation: Annotated[
+        Optional[DistributionSubstation], Field(None, description="Name of the substation.")
+    ]
+    feeder: Annotated[Optional[DistributionFeeder], Field(None, description="Name of the feeder.")]
