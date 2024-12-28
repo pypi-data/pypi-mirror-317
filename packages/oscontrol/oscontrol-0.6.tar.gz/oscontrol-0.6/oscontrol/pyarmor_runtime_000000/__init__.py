f6ca3f55d682cca333be6c72b87622328dd8544b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-28 14:04:53 +0000

    Add package oscontrol/0.6/oscontrol-0.6.tar.gz

---
+++ packages/oscontrol/oscontrol-0.6.tar.gz/oscontrol-0.6/oscontrol/pyarmor_runtime_000000/__init__.py
@@ -0,0 +1,2 @@
+# Pyarmor 9.0.6 (trial), 000000, 2024-12-28T00:48:47.034344
+from .pyarmor_runtime import __pyarmor__
---
+++ packages/oscontrol/oscontrol-0.6.tar.gz/oscontrol-0.6/setup.py
@@ -0,0 +1,17 @@
+   # setup.py
+from setuptools import setup, find_packages
+
+setup(
+       name='oscontrol',
+       version='0.6',
+       packages=find_packages(),
+       include_package_data=True,
+       description='A library for interacting with subsystems.',
+       author='Queezy52',
+       python_requires="==3.13.*",
+       classifiers=[
+           'Programming Language :: Python :: 3',
+           'License :: OSI Approved :: MIT License',
+           'Operating System :: OS Independent',
+       ]
+)
