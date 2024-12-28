6f49717223021a0438babb80166e33f81e525e0c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-28 03:06:57 +0000

    Add package kegscraper/0.1.1/kegscraper-0.1.1.tar.gz

---
+++ packages/kegscraper/kegscraper-0.1.1.tar.gz/kegscraper-0.1.1/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='kegscraper',
+    version='v0.1.1',
+    packages=['kegscraper'],
+    url='https://github.com/BigPotatoPizzaHey/kegscraper/',
+    license='',
+    author='BigPotatoPizzaHey',
+    author_email='',
+    description=''
+)
