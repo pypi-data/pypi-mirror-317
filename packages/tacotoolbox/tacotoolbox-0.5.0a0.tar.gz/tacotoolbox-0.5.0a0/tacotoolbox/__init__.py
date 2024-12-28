e6109df203b5b8b3045583a8006ecd112c0402c0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-28 04:04:33 +0000

    Add package tacotoolbox/0.5.0a0/tacotoolbox-0.5.0a0.tar.gz

---
+++ packages/tacotoolbox/tacotoolbox-0.5.0a0.tar.gz/tacotoolbox-0.5.0a0/tacotoolbox/__init__.py
@@ -0,0 +1,16 @@
+import pytortilla as tortilla
+
+from tacotoolbox import datamodel, edit
+from tacotoolbox.create import create, taco2tortilla, tortilla2taco
+from tacotoolbox.sanity import sanity_check
+
+__all__ = [
+    "datamodel",
+    "create",
+    "tortilla2taco",
+    "taco2tortilla",
+    "tortilla",
+    "edit",
+    "sanity_check",
+]
+__version__ = "0.5.0a"
