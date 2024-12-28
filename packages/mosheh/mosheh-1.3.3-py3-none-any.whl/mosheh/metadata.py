a0ae31d01de3daac56563f4be9fdd169694f46f7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-28 04:04:58 +0000

    Add package mosheh/1.3.3/mosheh-1.3.3-py3-none-any.whl

---
+++ packages/mosheh/mosheh-1.3.3-py3-none-any.whl/mosheh/metadata.py
@@ -0,0 +1,11 @@
+__author__ = 'LucasGonc'
+__copyright__ = 'Copyright (c) 2024 Lucas Gonçalves da Silva'
+__credits__ = ['LucasGonc']
+__license__ = 'MIT'
+__maintainer__ = 'LucasGonc'
+__email__ = 'lucasgoncsilva04@gmail.com'
+__repository__ = 'https://github.com/LucasGoncSilva/mosheh'
+__keywords__ = ['CLI', 'Python', 'documentation', 'MkDocs', 'automation', 'generation']
+__version__ = '1.3.3'
+__date__ = '2024-12-27'
+__status__ = 'Production'
