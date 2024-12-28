66c424aae6749f732cfe5cbfa13fd9159f4ec144
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-28 04:04:33 +0000

    Add package eox-tagging/9.0.0/eox_tagging-9.0.0.tar.gz

---
+++ packages/eox-tagging/eox_tagging-9.0.0.tar.gz/eox_tagging-9.0.0/eox_tagging/edxapp_wrappers/backends/course_overview_i_v1.py
@@ -0,0 +1,11 @@
+"""
+Backend CourseOverview file, here are all the methods from
+openedx.core.djangoapps.content.course_overviews.
+"""
+from openedx.core.djangoapps.content.course_overviews.models import \
+    CourseOverview  # pylint: disable=import-outside-toplevel, import-error
+
+
+def get_course_overview():
+    """Backend to get course overview."""
+    return CourseOverview
