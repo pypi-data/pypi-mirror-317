fe478ec4377a24c9544740f51d75207cfe107044
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-28 03:08:07 +0000

    Add package stofory-sdk/0.3.56/stofory_sdk-0.3.56.tar.gz

---
+++ packages/stofory-sdk/stofory_sdk-0.3.56.tar.gz/stofory_sdk-0.3.56/stofory_sdk/lib/mappings/__init__.py
@@ -0,0 +1,10 @@
+from .mappings import LocaleMapper, CurrencyMapper, ParameterTypeMapper, ModifierOperatorMapper, ReversedLocaleMapper, ReversedCurrencyMapper
+
+__all__ = [
+    "LocaleMapper",
+    "CurrencyMapper",
+    "ParameterTypeMapper",
+    "ModifierOperatorMapper",
+    "ReversedLocaleMapper",
+    "ReversedCurrencyMapper"
+]
