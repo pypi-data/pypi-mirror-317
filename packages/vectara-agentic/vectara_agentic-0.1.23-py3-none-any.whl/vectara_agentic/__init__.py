d01419ba722a74564de334abec034aafc676af53
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-28 14:04:27 +0000

    Add package vectara-agentic/0.1.23/vectara_agentic-0.1.23-py3-none-any.whl

---
+++ packages/vectara-agentic/vectara_agentic-0.1.23-py3-none-any.whl/vectara_agentic/__init__.py
@@ -0,0 +1,23 @@
+"""
+vectara_agentic package.
+"""
+
+# Define the package version
+__version__ = "0.1.23"
+
+# Import classes and functions from modules
+# from .module1 import Class1, function1
+# from .module2 import Class2, function2
+
+
+# Any initialization code
+def initialize_package():
+    """print a message when the package is initialized."""
+    print(f"Initializing vectara-agentic version {__version__}...")
+
+
+initialize_package()
+
+
+# Define the __all__ variable
+# __all__ = ['Class1', 'function1', 'Class2', 'function2']
