3a67b4f3b120958f5ad80d022ac76cdbc3b024d5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-28 04:04:55 +0000

    Add package FRENPY/0.2.9/frenpy-0.2.9.tar.gz

---
+++ packages/FRENPY/frenpy-0.2.9.tar.gz/frenpy-0.2.9/setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='frenpy',
+    packages=find_packages(include=['frenpy']),
+    version='0.2.9',
+    description='French python',
+    author='slohwnix',
+    install_requires=[]
+)
