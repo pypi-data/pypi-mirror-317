8f6912595682ddd4ebec78de9f37e491a9250c6e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-28 14:04:44 +0000

    Add package fagflag/1.0.0/fagflag-1.0.0.tar.gz

---
+++ packages/fagflag/fagflag-1.0.0.tar.gz/fagflag-1.0.0/setup.py
@@ -0,0 +1,27 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="fagflag",
+    version="1.0.0",
+    description="Generate pride flags as bitmap or vector images",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    author="disuko",
+    author_email="disuko@redpandastudios.net",
+    url="https://disuko.gay",
+    license="MIT",
+    packages=find_packages(),
+    include_package_data=True,
+    install_requires=["Pillow"],
+    entry_points={
+        "console_scripts": [
+            "fagflag=fagflag.__main__:main",
+        ],
+    },
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.6",
+)
