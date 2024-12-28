5463f50f64ce282acc2c722f2e5f9479a0599f75
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-28 04:05:02 +0000

    Add package awspacha/0.2.0/awspacha-0.2.0.tar.gz

---
+++ packages/awspacha/awspacha-0.2.0.tar.gz/awspacha-0.2.0/src/awspacha/__init__.py
@@ -0,0 +1,6 @@
+from .awsglue import GlueManager
+from .main import hello
+from .crawler import CrawlerManager
+from .athena import AthenaManager
+from .sqlfile import SQLFileManager
+from .operacion import Operacion
