b331e150948cd654e35bc6c091a6b9e5128b7079
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-28 03:07:27 +0000

    Add package dozersim/0.2.1/dozersim-0.2.1-py3-none-any.whl

---
+++ packages/dozersim/dozersim-0.2.1-py3-none-any.whl/dozersim/sources/timesource.py
@@ -0,0 +1,27 @@
+from __future__ import annotations
+
+from dataclasses import dataclass, field
+
+import numpy as np
+
+from dozersim.path.path import Path
+from dozersim.settings.settings import Settings
+from dozersim.modelling.model import Model
+
+
+@dataclass(slots=True)
+class TimeSourceSettings(Settings):
+    TimeStart: float = None
+    TimeEnd: float = None
+    SampleTime: float = None
+
+
+def evaluate_time_source(paths: tuple[Path], setting: TimeSourceSettings, model: Model):
+    for path in paths:
+        path.variables.add('time', np.arange(setting.TimeStart, setting.TimeEnd, setting.SampleTime), model, 'Time')
+
+
+class TimeSource(Model):
+
+    def __init__(self, name: str = 'time', settings: TimeSourceSettings = TimeSourceSettings(), eval_fun=evaluate_time_source):
+        super().__init__(name=name, settings=settings, eval_fun=eval_fun)
---
+++ packages/dozersim/dozersim-0.2.1-py3-none-any.whl/dozersim/visualizations/__init__.py
@@ -0,0 +1,2 @@
+from .visualization import Visualization, show_plots
+from .plotting import plot_results_3d, plot_results_scatter, plot_results_2d
