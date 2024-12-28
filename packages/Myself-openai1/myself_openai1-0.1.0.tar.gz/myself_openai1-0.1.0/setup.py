5a40bf94ba3883b465a26bcbb4a683999cbe6dd2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-28 14:04:58 +0000

    Add package Myself-openai1/0.1.0/myself_openai1-0.1.0.tar.gz

---
+++ packages/Myself-openai1/myself_openai1-0.1.0.tar.gz/myself_openai1-0.1.0/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='Myself_openai1',
+    version='0.1.0',
+    description='access a selfopenai',
+    author='林泽铃',
+    author_email='168679540@qq.com',
+    url='https://gitee.com/lin-zeling-27l/openai.git',
+    packages=find_packages(),  # 自动查找包
+)
