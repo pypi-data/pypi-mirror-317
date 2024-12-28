da2e66a08c8ab94d3dba92b98d80c87bc4453b47
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-28 15:04:39 +0000

    Add package kuzu/0.7.2.dev9/kuzu-0.7.2.dev9.tar.gz

---
+++ packages/kuzu/kuzu-0.7.2.dev9.tar.gz/sdist/kuzu-source/src/include/storage/storage_extension.h
@@ -0,0 +1,34 @@
+#pragma once
+
+#include "main/attached_database.h"
+
+namespace kuzu {
+namespace binder {
+struct AttachOption;
+}
+
+namespace storage {
+
+using attach_function_t = std::unique_ptr<main::AttachedDatabase> (*)(std::string dbPath,
+    std::string dbName, main::ClientContext* clientContext,
+    const binder::AttachOption& attachOption);
+
+class StorageExtension {
+public:
+    explicit StorageExtension(attach_function_t attachFunction)
+        : attachFunction{std::move(attachFunction)} {}
+    virtual bool canHandleDB(std::string /*dbType*/) const { return false; }
+
+    std::unique_ptr<main::AttachedDatabase> attach(std::string dbName, std::string dbPath,
+        main::ClientContext* clientContext, const binder::AttachOption& attachOption) const {
+        return attachFunction(dbName, dbPath, clientContext, attachOption);
+    }
+
+    virtual ~StorageExtension() = default;
+
+private:
+    attach_function_t attachFunction;
+};
+
+} // namespace storage
+} // namespace kuzu
