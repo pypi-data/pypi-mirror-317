3d8ea6abc4823730da912fea6781f62492b4da22
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-28 14:04:54 +0000

    Add package ClearIO/1.0.11/cleario-1.0.11.tar.gz

---
+++ packages/ClearIO/cleario-1.0.11.tar.gz/cleario-1.0.11/PackageAndDeploy.ps1
@@ -0,0 +1,10 @@
+#[String] $VEnvPythonPath = [IO.Path]::Combine([IO.Path]::GetDirectoryName($PSCommandPath), ".venv", "Scripts/python.exe")
+#& $VEnvPythonPath -m build
+#& $VEnvPythonPath -m twine upload --skip-existing --repository pypi dist/*
+
+
+#& "C:/Program Files/Python312/python.exe" -m pip install --upgrade pkginfo
+#& "C:/Program Files/Python312/python.exe" -m pip install --upgrade twine
+#& "C:/Program Files/Python312/python.exe" -m pip install --upgrade hatchling
+& "C:/Program Files/Python312/python.exe" -m build
+& "C:/Program Files/Python312/python.exe" -m twine upload --skip-existing --repository pypi dist/*
