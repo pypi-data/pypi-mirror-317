44b6d040b05444873caae72dfdf19e8e55f65cae
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-28 05:04:57 +0000

    Add package code-executor-py/0.0.1/code_executor_py-0.0.1.tar.gz

---
+++ packages/code-executor-py/code_executor_py-0.0.1.tar.gz/code_executor_py-0.0.1/test_script.py
@@ -0,0 +1,44 @@
+import os
+import pandas as pd
+from code_executor_py import VenvExecutor
+from langchain_openai import ChatOpenAI
+
+
+os.environ['OPENAI_API_KEY'] = 'sk-hgJ1UiRTg86R33AFEfscUWidCc2Ex-bqbuYESH2OTlT3BlbkFJXQFJuH9fHt9r3SBR377_BUP7b66CGxoDFRcp-Q43UA'
+
+llm = ChatOpenAI(
+        model="gpt-4o",
+    )
+
+func_code = """
+
+from sklearn.preprocessing import StandardScaler
+import pandas as pd
+
+def hello():
+    print("world")
+
+def hello2():
+    print("world")
+
+def process_data(data_df):
+    a = hello()
+    scaler = StandardScaler()
+    return pd.DataFrame(scaler.fit_transform(data_df), columns=data_df.columns)
+    """
+
+executor = VenvExecutor(
+    # llm=some_llm
+) 
+# Create executable function
+process_data = executor.create_executable(func_code)
+
+
+# Prepare test data
+test_data = pd.DataFrame({'A': [1, 2, 3], 'B': [4, 5, 6]})
+
+# Execute the function and get the result
+result = process_data(data_df =  test_data)
+
+print(result)
+
