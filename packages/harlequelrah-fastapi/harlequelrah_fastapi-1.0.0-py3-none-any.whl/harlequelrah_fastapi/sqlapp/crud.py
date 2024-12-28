9d413c670a5539b8c99ced8eea9bd57166b20f37
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-28 04:04:45 +0000

    Add package harlequelrah-fastapi/1.0.0/harlequelrah_fastapi-1.0.0-py3-none-any.whl

---
+++ packages/harlequelrah-fastapi/harlequelrah_fastapi-1.0.0-py3-none-any.whl/harlequelrah_fastapi/sqlapp/crud.py
@@ -0,0 +1,10 @@
+# from fastapi.responses import JSONResponse
+# from sqlalchemy.sql import func
+# from fastapi import HTTPException, status, Depends, Response
+# from harlequelrah_fastapi.exception.custom_http_exception import CustomHttpException
+# from myrpoject.myapp.model import MyModel
+# from myproject.myapp.schema import MyCreateModel, MyUpdateModel
+# from sqlalchemy.orm import Session
+# from harlequelrah_fastapi.utility.utils import update_entity
+
+
---
+++ packages/harlequelrah-fastapi/harlequelrah_fastapi-1.0.0-py3-none-any.whl/harlequelrah_fastapi/sqlapp/cruds.py
@@ -0,0 +1,11 @@
+# from sqlalchemy.sql import func
+# from fastapi import HTTPException, status, Depends, Response
+# from myapp.models import
+# from myapp.schemas import
+# from  settings.database import authentication
+# from sqlalchemy.orm import Session
+# from harlequelrah_fastapi.utility.utils import update_entity
+# dependencies = [
+#     Depends(authentication.get_session),
+#     Depends(authentication.get_current_user),
+# ]
