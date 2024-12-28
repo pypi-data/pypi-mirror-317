7ae8925905813ca5728c32791e5f3795da5d7e74
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-28 05:05:33 +0000

    Add package codesters3/1.0.1/codesters3-1.0.1-py3-none-any.whl

---
+++ packages/codesters3/codesters3-1.0.1-py3-none-any.whl/codesters/run.py
@@ -0,0 +1,43 @@
+#!/usr/bin/python
+import os
+import sys
+import random
+
+from .manager import Manager
+from .environment import Environment
+
+class App(object):
+
+    def __init__(self, filename):
+        self.manager = Manager()
+        self.canvas = Manager.canvas
+        self.root = Manager.root
+        self.filename = filename
+        self.canvas.pack()
+        self.do()
+        self.move_one()
+        self.animate = None
+
+    def do(self):
+        global stage
+        stage = Environment()
+        #filepath = str(os.getcwd())+ '/' + self.filename
+        filepath = self.filename
+        try:
+            exec(compile(open(filepath, "rb").read(), filepath, 'exec'), globals())
+        except IOError:
+            print("There is no file named "+filepath+". Please try again!")
+        except SyntaxError:
+            print(filepath+" is not a codesters Python file. Please try again!")
+
+    def move_one(self):
+        self.manager.run()
+        self.animate = Manager.root.after(5, self.move_one)
+
+#Workaround; for right now, this like has to be in the program being run, rather than __init__.py as it should.
+def run(filename):
+    app = App(filename)
+    app.root.mainloop()
+    sys.exit(0)
+
+#run(sys.argv[1])
