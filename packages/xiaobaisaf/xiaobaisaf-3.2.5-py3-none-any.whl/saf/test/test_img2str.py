10a1ab1f85b54b055abedfb7df482fce1a8dbcee
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-28 03:08:33 +0000

    Add package xiaobaisaf/3.2.5/xiaobaisaf-3.2.5-py3-none-any.whl

---
+++ packages/xiaobaisaf/xiaobaisaf-3.2.5-py3-none-any.whl/saf/test/test_img2str.py
@@ -0,0 +1,13 @@
+#! /usr/bin/env python
+'''
+@Author: xiaobaiTser
+@Time  : 2022/8/28 23:40
+@File  : test_element.py
+'''
+from selenium import webdriver as selenium_webdriver
+from saf.utils.ElementUtils import find_element
+from saf.utils.CaptchaUtils import image2str
+
+c = selenium_webdriver.Chrome()
+c.get('http://mail.xiaobai.com:8080/Center/Index/login')
+find_element(driver=c, value='//*[@name="captcha"]').send_keys(image2str(driver=c, value='//*[@id="captcha"]'))
