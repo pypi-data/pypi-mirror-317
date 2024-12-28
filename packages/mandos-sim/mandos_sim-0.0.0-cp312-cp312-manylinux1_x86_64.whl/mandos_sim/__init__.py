78bbe09df74989830b982df7e5edd3a491cb432b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-28 04:04:50 +0000

    Add package mandos-sim/0.0.0/mandos_sim-0.0.0-cp312-cp312-manylinux1_x86_64.whl

---
+++ packages/mandos-sim/mandos_sim-0.0.0-cp312-cp312-manylinux1_x86_64.whl/mandos_sim/__init__.py
@@ -0,0 +1,10 @@
+from mandos_cpp import Model
+from mandos_cpp import Deformable3D
+from mandos_cpp import RigidBodyCloud
+from mandos_cpp import RigidBodyCloud3D
+from mandos_cpp import RigidBodyGlobal3D
+from mandos_cpp import StepParameters
+from mandos_cpp import SurfaceMesh
+from mandos_cpp import energies
+from mandos_cpp import step
+import parallel_transport
