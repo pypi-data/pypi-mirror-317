f2349441b44a5bcc2d22192f1b1206b28d96d3fc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-28 04:05:00 +0000

    Add package status-codes/1.0.0/status_codes-1.0.0.tar.gz

---
+++ packages/status-codes/status_codes-1.0.0.tar.gz/status_codes-1.0.0/setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup, find_packages
+
+with open("README.md", "r") as f:
+    description = f.read()
+
+setup(
+    name='status_codes',
+    version='1.0.0',
+    packages=find_packages(),
+    install_requires=[
+        # Add dependencies here.
+    ],
+    long_description=description,
+    long_description_content_type="text/markdown",
+)
