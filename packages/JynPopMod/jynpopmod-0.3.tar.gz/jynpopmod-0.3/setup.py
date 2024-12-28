f47914c4b8e52f878dacb9cd3a1dcca0cb8ce31f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-28 04:04:35 +0000

    Add package JynPopMod/0.3/jynpopmod-0.3.tar.gz

---
+++ packages/JynPopMod/jynpopmod-0.3.tar.gz/jynpopmod-0.3/setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='JynPopMod',
+    version='0.3',
+    packages=find_packages(),
+    install_requires=[],
+    author='Jynoqtra',
+    author_email='Jynoqtra@gmail.com',
+    description='JynPopMod Python Module',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    url='https://github.com/Jynoqtra/JynPopMod',
+    classifiers=[  
+        'Programming Language :: Python :: 3',
+        'License :: Other/Proprietary License',
+        'Intended Audience :: Developers',
+    ],
+)
