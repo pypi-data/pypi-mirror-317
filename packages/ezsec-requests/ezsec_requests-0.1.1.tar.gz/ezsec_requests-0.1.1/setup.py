e8a1e0ca505db9cc38ea4c0a76454074c45433d3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-28 03:07:53 +0000

    Add package ezsec-requests/0.1.1/ezsec_requests-0.1.1.tar.gz

---
+++ packages/ezsec-requests/ezsec_requests-0.1.1.tar.gz/ezsec_requests-0.1.1/setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup
+
+setup(
+    name='ezsec-requests',
+    version='0.1.1',    
+    description='A set of pre-formatted request sending functions to interact with ezsec-api',
+    url='https://www.ezsec-api.com',
+    author='EZSEC LLC',
+    author_email='mbultman@ezsec-api.com',
+    license='MIT License',
+    packages=['ezsec_requests'],
+    install_requires=[],
+
+    classifiers=[
+        'Development Status :: 1 - Planning',
+        'Programming Language :: Python :: 3',
+    ],
+)
