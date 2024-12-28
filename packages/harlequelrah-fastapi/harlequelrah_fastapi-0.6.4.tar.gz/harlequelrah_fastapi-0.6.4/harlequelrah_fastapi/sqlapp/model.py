b8df766642e9f076e4f0a2e722ed7e5ff604449f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-28 04:05:00 +0000

    Add package harlequelrah-fastapi/0.6.4/harlequelrah_fastapi-0.6.4.tar.gz

---
+++ packages/harlequelrah-fastapi/harlequelrah_fastapi-0.6.4.tar.gz/harlequelrah_fastapi-0.6.4/harlequelrah_fastapi/sqlapp/model.py
@@ -0,0 +1,16 @@
+# from sqlalchemy import (
+#     Boolean,
+#     Column,
+#     DECIMAL,
+#     Integer,
+#     String,
+#     DateTime,
+#     ForeignKey,
+#     Table,
+# )
+# from myproject.settings.database import Base
+
+# from sqlalchemy.sql import func
+# from sqlalchemy.orm import relationship
+
+
