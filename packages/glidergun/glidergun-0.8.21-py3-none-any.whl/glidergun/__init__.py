b50760c369477173f975e5234038845963ee30c8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-28 04:05:00 +0000

    Add package glidergun/0.8.21/glidergun-0.8.21-py3-none-any.whl

---
+++ packages/glidergun/glidergun-0.8.21-py3-none-any.whl/glidergun/__init__.py
@@ -0,0 +1,17 @@
+# ruff: noqa: F401
+from glidergun._display import animate
+from glidergun._functions import (
+    create,
+    distance,
+    maximum,
+    mean,
+    minimum,
+    pca,
+    std,
+)
+from glidergun._grid import Grid, con, grid, standardize
+from glidergun._literals import BaseMap, ColorMap, DataType, ResamplingMethod
+from glidergun._mosaic import Mosaic, mosaic
+from glidergun._prediction import load_model
+from glidergun._stack import Stack, stack
+from glidergun._types import CellSize, Extent, PointValue
