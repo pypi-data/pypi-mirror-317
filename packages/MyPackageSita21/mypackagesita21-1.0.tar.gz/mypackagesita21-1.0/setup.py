ebf05e4bd576ef3dd0fc97f16aabfdb6bc5b4428
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-28 03:07:27 +0000

    Add package MyPackageSita21/1.0/mypackagesita21-1.0.tar.gz

---
+++ packages/MyPackageSita21/mypackagesita21-1.0.tar.gz/mypackagesita21-1.0/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup,find_packages
+
+setup(
+    name = 'MyPackageSita21',
+    version='1.00',
+    packages=find_packages(),
+    description='a simple python package to greet people',
+    author='sita',
+    author_email='sparcha@yahoo.com',
+    
+    
+    )
+
