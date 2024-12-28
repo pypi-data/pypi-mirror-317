c67d95eee5e948ee5babe8247ed1bfab972812dc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-28 14:04:21 +0000

    Add package sysfunc/0.2/sysfunc-0.2.tar.gz

---
+++ packages/sysfunc/sysfunc-0.2.tar.gz/sysfunc-0.2/setup.py
@@ -0,0 +1,17 @@
+   # setup.py
+from setuptools import setup, find_packages
+
+setup(
+       name='sysfunc',
+       version='0.2',
+       packages=find_packages(),
+       include_package_data=True,
+       description='A library for interacting with system functions',
+       author='Hosne432',
+       python_requires="==3.10.*",
+       classifiers=[
+           'Programming Language :: Python :: 3',
+           'License :: OSI Approved :: MIT License',
+           'Operating System :: OS Independent',
+       ]
+)
---
+++ packages/sysfunc/sysfunc-0.2.tar.gz/sysfunc-0.2/sysfunc/pyarmor_runtime_000000/__init__.py
@@ -0,0 +1,2 @@
+# Pyarmor 9.0.6 (trial), 000000, 2024-12-28T02:50:42.947627
+from .pyarmor_runtime import __pyarmor__
