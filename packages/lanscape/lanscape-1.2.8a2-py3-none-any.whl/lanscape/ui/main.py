d9d0c63fe16352152f62c465c4e714b6e143ed0c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-28 15:04:53 +0000

    Add package lanscape/1.2.8a2/lanscape-1.2.8a2-py3-none-any.whl

---
+++ packages/lanscape/lanscape-1.2.8a2-py3-none-any.whl/lanscape/ui/main.py
@@ -0,0 +1,87 @@
+
+import threading
+import webbrowser
+import time
+import logging
+import traceback
+import os
+from ..libraries.logger import configure_logging
+from ..libraries.runtime_args import parse_args, RuntimeArgs
+# do this so any logs generated on import are displayed
+args = parse_args()
+configure_logging(args.loglevel, args.logfile)
+
+from ..libraries.version_manager import get_installed_version, is_update_available
+from .app import start_webserver
+
+
+log = logging.getLogger('core')
+# determine if the execution is an instance of a flask reload
+# happens on file change with reloader enabled
+IS_FLASK_RELOAD = os.environ.get("WERKZEUG_RUN_MAIN")
+
+
+
+
+def main():
+    if not IS_FLASK_RELOAD:
+        log.info(f'LANscape v{get_installed_version()}')
+        try_check_update()
+    else: 
+        log.info('Flask reloaded app.')
+        
+        
+    try:
+        
+        no_gui(args)
+
+        log.info('Exiting...')
+    except Exception:
+        # showing error in debug only because this is handled gracefully
+        log.debug('Failed to start. Traceback below')
+        log.debug(traceback.format_exc())
+
+
+
+def try_check_update():
+    try: 
+        if is_update_available():
+            log.info('An update is available!')
+            log.info('Run "pip install --upgrade lanscape --no-cache" to supress this message.')
+    except:
+        log.debug(traceback.format_exc())
+        log.warning('Unable to check for updates.')
+    
+
+def open_browser(url: str,wait=2):
+    """
+    Open a browser window to the specified
+    url after waiting for the server to start
+    """
+    def do_open():
+        try:
+            time.sleep(wait)
+            webbrowser.open(url, new=2)
+        except:
+            srv_url = f"0.0.0.0:{url.split(':')[1]}"
+            log.debug(traceback.format_exc())
+            log.info(f'unable to open web browser, server running on {srv_url}')
+
+    threading.Thread(target=do_open).start()
+
+def no_gui(args: RuntimeArgs):
+    # determine if it was reloaded by flask debug reloader
+    # if it was, dont open the browser again
+    if not IS_FLASK_RELOAD:
+        open_browser(f'http://127.0.0.1:{args.port}')
+    
+    log.info(f'Server started: http://127.0.0.1:{args.port}')
+    
+    start_webserver(
+        args
+    )
+
+
+if __name__ == "__main__":
+    main()
+        
