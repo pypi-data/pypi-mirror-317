1acd363d6f0b148dae6098e6d5379423db2e2344
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-28 03:06:51 +0000

    Add package LogExportTable5/1.0.0/logexporttable5-1.0.0.tar.gz

---
+++ packages/LogExportTable5/logexporttable5-1.0.0.tar.gz/logexporttable5-1.0.0/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name='LogExportTable5',
+    version='1.0.0',
+    description='Log Export Table5',
+    author='SINTT',
+    author_email='bbhh66606@gmail.com',
+    packages=[''],  # список пакетов в вашей библиотеке
+    install_requires=[''],  # зависимости
+)
