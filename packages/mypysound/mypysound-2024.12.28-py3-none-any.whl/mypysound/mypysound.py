7a3c3ca0ad398186604b070198952243aaeefe0f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-28 14:04:49 +0000

    Add package mypysound/2024.12.28/mypysound-2024.12.28-py3-none-any.whl

---
+++ packages/mypysound/mypysound-2024.12.28-py3-none-any.whl/mypysound/mypysound.py
@@ -0,0 +1,21 @@
+def play(file):
+    import platform
+
+    platform = platform.system().lower()
+    if platform == "android":
+        from android.media import MediaPlayer
+        from os.path import dirname, join
+
+        player = MediaPlayer()
+        sound = file
+        player.setDataSource(sound)
+        player.prepare()
+        player.start()
+    if platform != "android" and platform != "ios":
+        from playsound3 import playsound
+
+        playsound(file)
+
+
+if __name__ == "__main__":
+    play(file="happy-birthday-whistled.wav")
