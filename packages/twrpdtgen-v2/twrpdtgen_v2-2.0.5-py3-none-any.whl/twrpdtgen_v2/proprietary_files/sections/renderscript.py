8397c7f7434e0c26ec9b5ca8ef77663e785a4de4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-28 04:04:42 +0000

    Add package twrpdtgen-v2/2.0.5/twrpdtgen_v2-2.0.5-py3-none-any.whl

---
+++ packages/twrpdtgen-v2/twrpdtgen_v2-2.0.5-py3-none-any.whl/twrpdtgen_v2/proprietary_files/sections/renderscript.py
@@ -0,0 +1,22 @@
+# BACKUP REPLACE FILE
+
+#
+# Copyright (C) 2022 The LineageOS Project
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+from twrpdtgen_v2.proprietary_files.section import Section, register_section
+
+class RenderscriptSection(Section):
+	name = "RenderScript"
+	interfaces = [
+		"android.hardware.renderscript",
+	]
+	libraries = [
+		"libRSDriver_adreno",
+		"librs_adreno",
+		"librs_adreno_sha1",
+	]
+
+register_section(RenderscriptSection)
