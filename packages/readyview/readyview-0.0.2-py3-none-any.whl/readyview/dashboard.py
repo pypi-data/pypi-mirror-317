0b5530f88c1376e59b475812c7ffb0aa046eb75b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-28 14:04:21 +0000

    Add package readyview/0.0.2/readyview-0.0.2-py3-none-any.whl

---
+++ packages/readyview/readyview-0.0.2-py3-none-any.whl/readyview/dashboard.py
@@ -0,0 +1,17 @@
+import rest_session
+from switch import Switch
+import os
+
+class Dashboard:
+	def __init__(self, organization_id=None, api_key=None):
+		self.organization_id = organization_id or os.environ.get("READYVIEW_DASHBOARD_ORG_ID")
+		self.api_key = api_key or os.environ.get("READYVIEW_DASHBOARD_API_KEY")
+		self._rest = rest_session.RestSession(self.organization_id, self.api_key)
+		self.switch = Switch(self._rest)		
+
+	def display_info(self):
+		return (
+			f"Org ID: {self.organization_id}\n"
+			f"API Key: {self.api_key}\n"
+		)
+		
