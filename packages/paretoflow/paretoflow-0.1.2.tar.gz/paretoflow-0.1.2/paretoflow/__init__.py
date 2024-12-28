fc836a1e186b62a7dc1dadc37c00cb476d39d3f9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-28 14:04:30 +0000

    Add package paretoflow/0.1.2/paretoflow-0.1.2.tar.gz

---
+++ packages/paretoflow/paretoflow-0.1.2.tar.gz/paretoflow-0.1.2/paretoflow/__init__.py
@@ -0,0 +1,24 @@
+"""
+ParetoFlow: A Python Library for Multi-Objective Optimization by using
+Generative Flows and Multi Predictors Guidance to approximate the Pareto Front.
+"""
+
+__version__ = "0.1.2"
+__author__ = "Ye Yuan, Can Chen"
+__credits__ = (
+    "Mila - Quebec AI Institute, McGill University Cyber Physical System Lab (CPSL)"
+)
+
+from paretoflow.flow import train_flow_matching
+from paretoflow.flow_net import FlowMatching, VectorFieldNet
+from paretoflow.multiple_model_predictor import train_proxies
+from paretoflow.multiple_model_predictor_net import MultipleModels
+from paretoflow.paretoflow_sample import ParetoFlowSampler
+from paretoflow.utils import (
+    min_max_denormalize_x,
+    min_max_normalize_x,
+    to_integers,
+    to_logits,
+    z_score_denormalize_x,
+    z_score_normalize_x,
+)
