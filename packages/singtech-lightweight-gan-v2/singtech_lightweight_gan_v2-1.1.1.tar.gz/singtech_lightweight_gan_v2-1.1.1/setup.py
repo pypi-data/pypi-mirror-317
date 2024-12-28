4f1b87c4ffc44d8c97171a7b7afe623400878f6d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-28 05:04:55 +0000

    Add package singtech-lightweight-gan-v2/1.1.1/singtech_lightweight_gan_v2-1.1.1.tar.gz

---
+++ packages/singtech-lightweight-gan-v2/singtech_lightweight_gan_v2-1.1.1.tar.gz/singtech_lightweight_gan_v2-1.1.1/setup.py
@@ -0,0 +1,45 @@
+import sys
+from setuptools import setup, find_packages
+
+sys.path[0:0] = ['lightweight_gan']
+from version import __version__
+
+setup(
+  name = 'singtech-lightweight-gan-v2',
+  packages = find_packages(),
+  entry_points={
+    'console_scripts': [
+      'lightweight_gan = lightweight_gan.cli:main',
+    ],
+  },
+  version = __version__,
+  license='MIT',
+  description = 'Lightweight GAN',
+  author = 'Shalu Singh',
+  author_email = 'singhtech.10@gmail.com',
+  url = 'https://github.com/shalusingh-tech/lightweight-gan',
+  keywords = [
+    'artificial intelligence',
+    'deep learning',
+    'generative adversarial networks'
+  ],
+  install_requires=[
+    'adabelief-pytorch',
+    'einops>=0.3',
+    'fire',
+    'kornia>=0.5.4',
+    'numpy',
+    'pillow',
+    'retry',
+    'torch>=1.10',
+    'torchvision',
+    'tqdm'
+  ],
+  classifiers=[
+    'Development Status :: 4 - Beta',
+    'Intended Audience :: Developers',
+    'Topic :: Scientific/Engineering :: Artificial Intelligence',
+    'License :: OSI Approved :: MIT License',
+    'Programming Language :: Python :: 3.6',
+  ],
+)
