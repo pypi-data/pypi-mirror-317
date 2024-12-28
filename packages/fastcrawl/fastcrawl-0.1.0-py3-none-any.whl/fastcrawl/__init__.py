4c51744744904a0b4915271481cd968cb7ef9c39
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-28 03:07:25 +0000

    Add package fastcrawl/0.1.0/fastcrawl-0.1.0-py3-none-any.whl

---
+++ packages/fastcrawl/fastcrawl-0.1.0-py3-none-any.whl/fastcrawl/__init__.py
@@ -0,0 +1,10 @@
+from .base_crawler import BaseCrawler
+from .base_pipeline import BasePipeline
+from .models import (
+    CrawlerHttpClientSettings,
+    CrawlerLoggingSettings,
+    CrawlerSettings,
+    CrawlerStats,
+    Request,
+    Response,
+)
