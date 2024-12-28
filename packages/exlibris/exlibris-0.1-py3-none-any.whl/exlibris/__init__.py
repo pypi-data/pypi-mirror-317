6e1e1c2a53f39360d91efb13a44a62f1f6659938
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-28 14:04:43 +0000

    Add package exlibris/0.1/exlibris-0.1-py3-none-any.whl

---
+++ packages/exlibris/exlibris-0.1-py3-none-any.whl/exlibris/__init__.py
@@ -0,0 +1,16 @@
+#def initialize():
+#    return pickle.loads(importlib_resources.read_binary(__name__, 'chartsdata.pkl'))
+
+#def load_datasets():
+#    return importlib_resources.read_text(__name__, 'stats.csv')
+
+from .stats import (
+    Stats
+)
+from .dataset import (
+    Dataset
+)
+
+from .stats_gsgp import (
+    stats_gsgp
+)
