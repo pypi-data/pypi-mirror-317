43ff5e925a6a80f4b1af8f6a70cdc39d025391f8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-28 04:04:50 +0000

    Add package logicbank/1.20.16/logicbank-1.20.16.tar.gz

---
+++ packages/logicbank/logicbank-1.20.16.tar.gz/logicbank-1.20.16/logic_bank/exceptions.py
@@ -0,0 +1,13 @@
+class LBActivateException(Exception):
+
+    message = "LogicBank Activation error"
+    def __init__(self, invalid_rules = [], missing_attributes = []):
+        LBActivateException = invalid_rules
+        self.missing_attributes = missing_attributes
+        self.message = f'LBActivateException: \n{LBActivateException}\n{self.missing_attributes}\n{self.message}'
+        super().__init__(self.message)
+
+    ''' 
+    def __str__(self):
+        return f'LBActivateException: \n{LBActivateException}\n{self.missing_attributes}\n{self.message}'
+    '''
