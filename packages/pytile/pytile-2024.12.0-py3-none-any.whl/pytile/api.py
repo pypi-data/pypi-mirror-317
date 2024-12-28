3b50db23d8df66cbe548f47266870f46f5849251
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-28 04:04:59 +0000

    Add package pytile/2024.12.0/pytile-2024.12.0-py3-none-any.whl

---
+++ packages/pytile/pytile-2024.12.0-py3-none-any.whl/pytile/api.py
@@ -0,0 +1,179 @@
+"""Define an object to work directly with the API."""
+
+from __future__ import annotations
+
+import asyncio
+from time import time
+from typing import Any, cast
+from uuid import uuid4
+
+from aiohttp import ClientSession
+from aiohttp.client_exceptions import ClientError
+
+from .const import LOGGER
+from .errors import InvalidAuthError, RequestError
+from .tile import Tile
+
+API_URL_SCAFFOLD = "https://production.tile-api.com/api/v1"
+
+DEFAULT_API_VERSION = "1.0"
+DEFAULT_APP_ID = "ios-tile-production"
+DEFAULT_APP_VERSION = "2.89.1.4774"
+DEFAULT_LOCALE = "en-US"
+DEFAULT_TIMEOUT = 10
+DEFAULT_USER_AGENT = "Tile/4774 CFNetwork/1312 Darwin/21.0.0"
+
+
+class API:
+    """Define the API management object."""
+
+    def __init__(  # pylint: disable=too-many-arguments
+        self,
+        email: str,
+        password: str,
+        session: ClientSession,
+        *,
+        client_uuid: str | None = None,
+        locale: str = DEFAULT_LOCALE,
+    ) -> None:
+        """Initialize.
+
+        Args:
+            email: An email address for a Tile account.
+            password: The account password.
+            session: An optional aiohttp ClientSession.
+            client_uuid: An optional UUID to identify this API object.
+            locale: An optional locale.
+        """
+        self._client_established: bool = False
+        self._email: str = email
+        self._locale: str = locale
+        self._password: str = password
+        self._session: ClientSession = session
+        self._session_expiry: int | None = None
+        self.client_uuid: str = client_uuid if client_uuid else str(uuid4())
+        self.user_uuid: str | None = None
+
+    async def _async_request(
+        self, method: str, endpoint: str, **kwargs: dict[str, Any]
+    ) -> dict[str, Any]:
+        """Make an API request.
+
+        Args:
+            method: An HTTP method.
+            endpoint: A relative API endpoint.
+            **kwargs: Additional kwargs to send with the request.
+
+        Returns:
+            An API response payload.
+
+        Raises:
+            InvalidAuthError: Raised upon invalid credentials.
+            RequestError: Raised upon an underlying HTTP error.
+        """
+        if self._session_expiry and self._session_expiry <= int(time() * 1000):
+            await self.async_init()
+
+        kwargs.setdefault("headers", {})
+        kwargs["headers"]["User-Agent"] = DEFAULT_USER_AGENT
+        kwargs["headers"]["tile_api_version"] = DEFAULT_API_VERSION
+        kwargs["headers"]["tile_app_id"] = DEFAULT_APP_ID
+        kwargs["headers"]["tile_app_version"] = DEFAULT_APP_VERSION
+        kwargs["headers"]["tile_client_uuid"] = self.client_uuid
+
+        try:
+            async with self._session.request(
+                method, f"{API_URL_SCAFFOLD}/{endpoint}", **kwargs
+            ) as resp:
+                resp.raise_for_status()
+                data = await resp.json()
+        except ClientError as err:
+            if "401" in str(err):
+                raise InvalidAuthError("Invalid credentials") from err
+            raise RequestError(f"Error requesting data from {endpoint}: {err}") from err
+
+        LOGGER.debug("Data received from /%s: %s", endpoint, data)
+
+        return cast(dict[str, Any], data)
+
+    async def async_get_tiles(self) -> dict[str, Tile]:
+        """Get all active Tiles from the user's account.
+
+        Returns:
+            A dictionary of Tile UUIDs to Tile objects.
+        """
+        states = await self._async_request("get", "tiles/tile_states")
+
+        details_tasks = {
+            tile_uuid: self._async_request("get", f"tiles/{tile_uuid}")
+            for tile_uuid in [tile["tile_id"] for tile in states["result"]]
+        }
+
+        results = await asyncio.gather(*details_tasks.values(), return_exceptions=True)
+
+        data = {}
+        for tile_uuid, result in zip(details_tasks, results):
+            if isinstance(result, RequestError) and "412" in str(result):
+                # Tile Labels will return an HTTP 412 because they don't have
+                # additional details; we can safely ignore these errors and still
+                # track the Tile (without additional details):
+                continue
+            if isinstance(result, BaseException):
+                LOGGER.error("Error requesting details for %s: %s", tile_uuid, result)
+                continue
+            data[tile_uuid] = Tile(self._async_request, result)
+
+        return data
+
+    async def async_init(self) -> None:
+        """Create a Tile session."""
+        # Invalidate the existing session expiry datetime (if it exists) so that the
+        # next few requests don't fail:
+        self._session_expiry = None
+
+        if not self._client_established:
+            await self._async_request(
+                "put",
+                f"clients/{self.client_uuid}",
+                data={
+                    "app_id": DEFAULT_APP_ID,
+                    "app_version": DEFAULT_APP_VERSION,
+                    "locale": self._locale,
+                },
+            )
+            self._client_established = True
+
+        resp = await self._async_request(
+            "post",
+            f"clients/{self.client_uuid}/sessions",
+            data={"email": self._email, "password": self._password},
+        )
+
+        if not self.user_uuid:
+            self.user_uuid = resp["result"]["user"]["user_uuid"]
+        self._session_expiry = resp["result"]["session_expiration_timestamp"]
+
+
+async def async_login(
+    email: str,
+    password: str,
+    session: ClientSession,
+    *,
+    client_uuid: str | None = None,
+    locale: str = DEFAULT_LOCALE,
+) -> API:
+    """Return an authenticated client.
+
+    Args:
+        email: An email address for a Tile account.
+        password: The account password.
+        session: An optional aiohttp ClientSession.
+        client_uuid: An optional UUID to identify this API object.
+        locale: An optional locale.
+
+    Returns:
+        An authenticated API object.
+    """
+    api: API = API(email, password, session, client_uuid=client_uuid, locale=locale)
+    await api.async_init()
+    return api
---
+++ packages/pytile/pytile-2024.12.0-py3-none-any.whl/pytile/tile.py
@@ -0,0 +1,278 @@
+"""Define a Tile object."""
+
+from __future__ import annotations
+
+from collections.abc import Awaitable, Callable
+from datetime import datetime, timezone
+from typing import Any, cast
+
+from .const import LOGGER
+
+
+class Tile:
+    """Define a Tile."""
+
+    def __init__(
+        self,
+        async_request: Callable[..., Awaitable[dict[str, Any]]],
+        tile_data: dict[str, Any],
+    ) -> None:
+        """Initialize.
+
+        Args:
+            async_request: The request method from the Client object.
+            tile_data: A dictionary of Tile data.
+        """
+        self._async_request = async_request
+        self._tile_data = tile_data
+
+        self._last_timestamp: datetime | None = None
+        self._lost_timestamp: datetime | None = None
+        self._save_timestamps(tile_data)
+
+    def __str__(self) -> str:
+        """Return the string representation of the Tile.
+
+        Returns:
+            A string representation.
+        """
+        return f"<Tile uuid={self.uuid} name={self.name}>"
+
+    @property
+    def accuracy(self) -> float | None:
+        """Return the accuracy of the last measurement.
+
+        Returns:
+            The accuracy (if it exists).
+        """
+        if (last_state := self._tile_data["result"].get("last_tile_state")) is None:
+            return None
+        return cast(float, last_state["h_accuracy"])
+
+    @property
+    def altitude(self) -> float | None:
+        """Return the last detected altitude.
+
+        Returns:
+            The altitude (if it exists).
+        """
+        if (last_state := self._tile_data["result"].get("last_tile_state")) is None:
+            return None
+        return cast(float, last_state["altitude"])
+
+    @property
+    def archetype(self) -> str:
+        """Return the archetype.
+
+        Returns:
+            The archetype.
+        """
+        return cast(str, self._tile_data["result"]["archetype"])
+
+    @property
+    def dead(self) -> bool:
+        """Return whether the Tile is dead.
+
+        Returns:
+            The dead status.
+        """
+        return cast(bool, self._tile_data["result"]["is_dead"])
+
+    @property
+    def firmware_version(self) -> str:
+        """Return the firmware version.
+
+        Returns:
+            The firmware version.
+        """
+        return cast(str, self._tile_data["result"]["firmware_version"])
+
+    @property
+    def hardware_version(self) -> str:
+        """Return the hardware version.
+
+        Returns:
+            The hardware version.
+        """
+        return cast(str, self._tile_data["result"]["hw_version"])
+
+    @property
+    def kind(self) -> str:
+        """Return the type of Tile.
+
+        Returns:
+            The type.
+        """
+        return cast(str, self._tile_data["result"]["tile_type"])
+
+    @property
+    def last_timestamp(self) -> datetime | None:
+        """Return the timestamp of the last location measurement.
+
+        Returns:
+            The timestamp (if it exists).
+        """
+        return self._last_timestamp
+
+    @property
+    def latitude(self) -> float | None:
+        """Return the last detected latitude.
+
+        Returns:
+            The latitude (if it exists).
+        """
+        if (last_state := self._tile_data["result"].get("last_tile_state")) is None:
+            return None
+        return cast(float, last_state["latitude"])
+
+    @property
+    def longitude(self) -> float | None:
+        """Return the last detected longitude.
+
+        Returns:
+            The longitude (if it exists).
+        """
+        if (last_state := self._tile_data["result"].get("last_tile_state")) is None:
+            return None
+        return cast(float, last_state["longitude"])
+
+    @property
+    def lost(self) -> bool:
+        """Return whether the Tile is lost.
+
+        Since the Tile API can sometimes fail to return last_tile_state data, if it's
+        missing here, we return True (indicating the Tile *is* lost).
+
+        Returns:
+            The lost status.
+        """
+        if (last_state := self._tile_data["result"].get("last_tile_state")) is None:
+            return True
+        return cast(bool, last_state["is_lost"])
+
+    @property
+    def lost_timestamp(self) -> datetime | None:
+        """Return the timestamp when the Tile was last in a "lost" state.
+
+        Returns:
+            The timestamp (if it exists).
+        """
+        return self._lost_timestamp
+
+    @property
+    def name(self) -> str:
+        """Return the name.
+
+        Returns:
+            The name.
+        """
+        return cast(str, self._tile_data["result"]["name"])
+
+    @property
+    def ring_state(self) -> str | None:
+        """Return the ring state.
+
+        Returns:
+            The ring state (if it exists).
+        """
+        if (last_state := self._tile_data["result"].get("last_tile_state")) is None:
+            return None
+        return cast(str, last_state["ring_state"])
+
+    @property
+    def uuid(self) -> str:
+        """Return the UUID.
+
+        Returns:
+            The UUID.
+        """
+        return cast(str, self._tile_data["result"]["tile_uuid"])
+
+    @property
+    def visible(self) -> bool:
+        """Return whether the Tile is visible.
+
+        Returns:
+            The visibility.
+        """
+        return cast(bool, self._tile_data["result"]["visible"])
+
+    @property
+    def voip_state(self) -> str | None:
+        """Return the VoIP state.
+
+        Returns:
+            The VoIP state (if it exists).
+        """
+        if (last_state := self._tile_data["result"].get("last_tile_state")) is None:
+            return None
+        return cast(str, last_state["voip_state"])
+
+    def _save_timestamps(self, tile_data: dict[str, Any]) -> None:
+        """Save UTC timestamps from a Tile data set.
+
+        Args:
+            tile_data: A dictionary of Tile data.
+        """
+        if (last_state := tile_data["result"].get("last_tile_state")) is None:
+            LOGGER.warning("Missing last_tile_state; can't report location info")
+            self._last_timestamp = None
+            self._lost_timestamp = None
+            return
+
+        self._last_timestamp = datetime.fromtimestamp(
+            last_state["timestamp"] / 1000, tz=timezone.utc
+        ).replace(tzinfo=None)
+        self._lost_timestamp = datetime.fromtimestamp(
+            last_state["lost_timestamp"] / 1000, tz=timezone.utc
+        ).replace(tzinfo=None)
+
+    def as_dict(self) -> dict[str, Any]:
+        """Return dictionary version of this Tile.
+
+        Returns:
+            A dictionary representation of the Tile.
+        """
+        return {
+            "accuracy": self.accuracy,
+            "altitude": self.altitude,
+            "archetype": self.archetype,
+            "dead": self.dead,
+            "firmware_version": self.firmware_version,
+            "hardware_version": self.hardware_version,
+            "kind": self.kind,
+            "last_timestamp": self.last_timestamp,
+            "latitude": self.latitude,
+            "longitude": self.longitude,
+            "lost": self.lost,
+            "lost_timestamp": self.lost_timestamp,
+            "name": self.name,
+            "ring_state": self.ring_state,
+            "uuid": self.uuid,
+            "visible": self.visible,
+            "voip_state": self.voip_state,
+        }
+
+    async def async_history(
+        self, start_datetime: datetime, end_datetime: datetime
+    ) -> dict[str, Any]:
+        """Get the latest measurements from the Tile.
+
+        Returns:
+            A dictionary containing the requested history.
+        """
+        return await self._async_request(
+            "get",
+            f"tiles/location/history/{self.uuid}",
+            params={
+                "aggregation": "False",
+                "end_ts": round(end_datetime.timestamp() * 1000),
+                "start_ts": round(start_datetime.timestamp() * 1000),
+            },
+        )
+
+    async def async_update(self) -> None:
+        """Get the latest measurements from the Tile."""
+        data = await self._async_request("get", f"tiles/{self.uuid}")
+        self._save_timestamps(data)
+        self._tile_data = data
