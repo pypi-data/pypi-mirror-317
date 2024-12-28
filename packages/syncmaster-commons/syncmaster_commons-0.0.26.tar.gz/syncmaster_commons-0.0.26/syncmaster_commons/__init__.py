4260324005415e94c6cb609457e78924dd060401
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-28 03:07:05 +0000

    Add package syncmaster-commons/0.0.26/syncmaster_commons-0.0.26.tar.gz

---
+++ packages/syncmaster-commons/syncmaster_commons-0.0.26.tar.gz/syncmaster_commons-0.0.26/syncmaster_commons/__init__.py
@@ -0,0 +1,13 @@
+__version__ = "0.0.26"
+
+from .abstract import SMBaseClass
+from .agents import AgentRequestPayload
+from .gupshup import GupshupIncomingPayLoad
+from .keys import KEYS
+
+__all__ = [
+    "AgentRequestPayload",
+    "GupshupIncomingPayLoad",
+    "KEYS",
+    "SMBaseClass",
+]
