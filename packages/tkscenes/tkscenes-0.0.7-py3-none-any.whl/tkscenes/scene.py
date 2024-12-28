02945af092ea938d1a4b8a2844a7976b8835247f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-28 04:04:26 +0000

    Add package tkscenes/0.0.7/tkscenes-0.0.7-py3-none-any.whl

---
+++ packages/tkscenes/tkscenes-0.0.7-py3-none-any.whl/tkscenes/scene.py
@@ -0,0 +1,27 @@
+from tkscenes.child import Child as Child
+
+
+class Scene:
+    def __init__(self):
+        self.children: dict[str, Child] = {}
+
+    def __setitem__(self, key, value):
+        self.children[key] = Child(value)
+
+    def __getitem__(self, item):
+        return self.children[item]
+
+    def load(self):
+        for child in self.children.values():
+            child.render()
+
+    def unload(self):
+        for child in self.children.values():
+            child.unrender()
+
+    def destroy(self):
+        keys = list(self.children.keys())
+
+        for key in keys:
+            self.children[key].destroy()
+            del self.children[key]
