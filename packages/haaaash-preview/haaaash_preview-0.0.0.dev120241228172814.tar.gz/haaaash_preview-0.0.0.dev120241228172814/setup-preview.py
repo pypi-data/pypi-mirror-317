0c714767effdb3b072d0c6bb60a3c5ebd1ef12c6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-29 04:04:41 +0000

    Add package haaaash-preview/0.0.0.dev120241228172814/haaaash_preview-0.0.0.dev120241228172814.tar.gz

---
+++ packages/haaaash-preview/haaaash_preview-0.0.0.dev120241228172814.tar.gz/haaaash_preview-0.0.0.dev120241228172814/setup-preview.py
@@ -0,0 +1,33 @@
+from setuptools import setup, find_packages
+
+filepath = 'README.md'
+
+setup(
+    name='haaaash-preview',
+    version='0.0.0.dev120241228172814',
+    author='Gudupao',
+    author_email='official@gudupao.top',
+    description='hash 批量计算器 预览版',
+    long_description=open(filepath, encoding='utf-8').read(),
+    long_description_content_type='text/markdown',
+    packages=find_packages(),
+    install_requires=[
+        'PySide6>=6.5.0'
+    ],
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3.9',
+        'Programming Language :: Python :: 3.10',
+        'Programming Language :: Python :: 3.11',
+        'Programming Language :: Python :: 3.12',
+        'Programming Language :: Python :: 3.13',
+    
+    ],
+    entry_points={
+        'console_scripts': [
+            'haaaash=haaaash.__main__:main',
+        ],
+    },
+)
