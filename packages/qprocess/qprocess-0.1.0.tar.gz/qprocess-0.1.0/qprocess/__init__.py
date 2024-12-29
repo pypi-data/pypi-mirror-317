68e4c60787db723493e601cfe3391ddfe40b85fb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-29 03:13:43 +0000

    Add package qprocess/0.1.0/qprocess-0.1.0.tar.gz

---
+++ packages/qprocess/qprocess-0.1.0.tar.gz/qprocess-0.1.0/qprocess/__init__.py
@@ -0,0 +1,6 @@
+# __init__.py
+from .gaussian import checker_gaussian, checker_normaltermination, checker_job, collector_job, collector_DateNormalTermination, collector_es, collector_geom, collector_disctance_matrix, standard_orientation, cpu_collector, function_collector
+from .mrcc import checker_mrcc, checker_normaltermination, checker_job, collector_job, collector_DateNormalTermination, collector_es, collector_geom, collector_osc_length
+
+# Define what gets imported with "from my_library import *"
+__all__ = []
---
+++ packages/qprocess/qprocess-0.1.0.tar.gz/qprocess-0.1.0/setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="qprocess",  # Unique name on PyPI
+    version="0.1.0",
+    description="A Python library for processing Quantum Chemical program output files",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    author="Bogdan Virag",
+    author_email="viragbogdan@edu.bme.hu",
+    url="https://github.com/fehergandalf/Gaussian.git",  # GitHub or other project URL
+    packages=find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.9",
+    install_requires=[
+            "pandas", "numpy"
+    ],
+)
