1bcdc6127c8f56d11fa0d5e92765a6077d15531d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-29 03:13:25 +0000

    Add package django-meta-og/1.1.1/django_meta_og-1.1.1.tar.gz

---
+++ packages/django-meta-og/django_meta_og-1.1.1.tar.gz/django_meta_og-1.1.1/django_meta_og/templatetags/django_meta_og.py
@@ -0,0 +1,27 @@
+from django import template
+from django.conf import settings
+
+from django_meta_og.models import Content
+
+from ..utils import get_or_create_fnc
+
+register = template.Library()
+
+
+@register.simple_tag()
+def django_meta_og_prefix():
+    prefixes = set()
+    if hasattr(settings, "META_OG_PREFIX_IN_TEMLATES"):
+        prefixes.update(settings.META_OG_PREFIX_IN_TEMLATES)
+    for meta in Content.objects.all():
+        prefixes.add((meta.property.namespace.prefix, meta.property.namespace.uri))
+    return "\n".join([f"{prefix}: {uri}" for prefix, uri in sorted(prefixes)])
+
+
+@register.simple_tag(takes_context=True)
+def django_meta_og_dynamic_content(context: dict, value: str) -> str:
+    if hasattr(settings, "PAGE_META_OG_DYNAMIC_CONTENT"):
+        path_and_description = settings.PAGE_META_OG_DYNAMIC_CONTENT.get(value)
+        if path_and_description:
+            return get_or_create_fnc(path_and_description[0])(context["request"])
+    return value
