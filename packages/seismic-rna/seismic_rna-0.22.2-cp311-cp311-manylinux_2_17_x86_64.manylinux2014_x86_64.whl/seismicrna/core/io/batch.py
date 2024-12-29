bb07631ba4282b5212e8b9b457e38423d8170592
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-29 14:04:28 +0000

    Add package seismic-rna/0.22.2/seismic_rna-0.22.2-cp311-cp311-manylinux_2_17_x86_64.manylinux2014_x86_64.whl

---
+++ packages/seismic-rna/seismic_rna-0.22.2-cp311-cp311-manylinux_2_17_x86_64.manylinux2014_x86_64.whl/seismicrna/core/io/batch.py
@@ -0,0 +1,43 @@
+import re
+from abc import ABC
+
+from .file import BrickleIO
+from ..batch import MutsBatch, ReadBatch
+from ..seq import Region
+
+
+class ReadBatchIO(ReadBatch, BrickleIO, ABC):
+    """ Pickled file of a batch of data. """
+
+    @classmethod
+    def btype(cls):
+        btype, = re.match("^([a-z_]*)batchio", cls.__name__.lower()).groups()
+        return btype
+
+
+class MutsBatchIO(MutsBatch, ReadBatchIO, ABC):
+    """ Pickled file of a batch of mutational data. """
+
+    def __init__(self, *args, region: Region, **kwargs):
+        super().__init__(*args, **kwargs, region=region, ref=region.ref)
+
+########################################################################
+#                                                                      #
+# © Copyright 2024, the Rouskin Lab.                                   #
+#                                                                      #
+# This file is part of SEISMIC-RNA.                                    #
+#                                                                      #
+# SEISMIC-RNA is free software; you can redistribute it and/or modify  #
+# it under the terms of the GNU General Public License as published by #
+# the Free Software Foundation; either version 3 of the License, or    #
+# (at your option) any later version.                                  #
+#                                                                      #
+# SEISMIC-RNA is distributed in the hope that it will be useful, but   #
+# WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANT- #
+# ABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General     #
+# Public License for more details.                                     #
+#                                                                      #
+# You should have received a copy of the GNU General Public License    #
+# along with SEISMIC-RNA; if not, see <https://www.gnu.org/licenses>.  #
+#                                                                      #
+########################################################################
