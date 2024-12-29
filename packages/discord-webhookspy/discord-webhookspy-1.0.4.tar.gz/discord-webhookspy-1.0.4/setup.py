f3708329ed0a9f725880340ab6e5eb4801370bca
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-29 03:13:45 +0000

    Add package discord-webhookspy/1.0.4/discord-webhookspy-1.0.4.tar.gz

---
+++ packages/discord-webhookspy/discord-webhookspy-1.0.4.tar.gz/discord-webhookspy-1.0.4/setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="discord-webhookspy",  # Paketinizin adı
+    version="1.0.4",
+    description="A simple Python library for Discord webhooks",
+    author="AlperenS",
+    author_email="ajaxseson1@gmail.com",
+    packages=find_packages(),
+    install_requires=[
+        "requests",  # Bağımlılıkları buraya ekleyin
+    ],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
