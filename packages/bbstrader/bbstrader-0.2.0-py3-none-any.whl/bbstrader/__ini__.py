88a75fa94b2a693d02e588dec674eb9e2bd17ef7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-29 03:13:13 +0000

    Add package bbstrader/0.2.0/bbstrader-0.2.0-py3-none-any.whl

---
+++ packages/bbstrader/bbstrader-0.2.0-py3-none-any.whl/bbstrader/__ini__.py
@@ -0,0 +1,18 @@
+"""
+Simplified Investment & Trading Toolkit
+=======================================
+
+"""
+__author__      = "Bertin Balouki SIMYELI"
+__copyright__   = "2023-2024 Bertin Balouki SIMYELI"
+__email__       = "bertin@bbstrader.com"
+__license__     = "MIT"
+__version__     = '0.2.0'
+
+
+from bbstrader import btengine
+from bbstrader import metatrader
+from bbstrader import models
+from bbstrader import trading
+from bbstrader import tseries
+from .config import config_logger
