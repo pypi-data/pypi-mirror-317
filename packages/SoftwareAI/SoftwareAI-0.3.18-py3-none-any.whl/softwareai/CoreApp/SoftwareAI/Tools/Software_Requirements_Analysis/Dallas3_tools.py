5c5db624748be82771b480c744ca1674e6384c14
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-29 03:13:43 +0000

    Add package SoftwareAI/0.3.18/SoftwareAI-0.3.18-py3-none-any.whl

---
+++ packages/SoftwareAI/SoftwareAI-0.3.18-py3-none-any.whl/softwareai/CoreApp/SoftwareAI/Tools/Software_Requirements_Analysis/Dallas3_tools.py
@@ -0,0 +1,20 @@
+
+
+tools_Dallas3 = [
+{"type": "file_search"},    
+{
+
+        "type": "function",
+            "function": {
+            "name": "pegar_hora",
+            "description": "Retorna a data e hora atual no formato YYYY-MM-DD HH:MM:SS.",
+            "parameters": {
+            "type": "object",
+            "properties": {},
+            "required": []
+            }
+        }
+ } 	
+]
+
+                
