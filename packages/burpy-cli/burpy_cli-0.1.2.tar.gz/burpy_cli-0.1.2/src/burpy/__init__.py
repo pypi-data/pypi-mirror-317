52b640b8d1c0482eedcd5aed9d228843cdcab4d6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-29 04:04:36 +0000

    Add package burpy-cli/0.1.2/burpy_cli-0.1.2.tar.gz

---
+++ packages/burpy-cli/burpy_cli-0.1.2.tar.gz/burpy_cli-0.1.2/src/burpy/__init__.py
@@ -0,0 +1,11 @@
+from .core.burpy import Burpy, Context, TextFormat, format_text
+
+__version__ = "0.1.0"
+
+__all__ = [
+    "Burpy",
+    "Context",
+    "TextFormat",
+    "format_text",
+    "__version__"
+]
