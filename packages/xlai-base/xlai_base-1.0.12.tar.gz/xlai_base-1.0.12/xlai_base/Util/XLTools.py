e53a2f244abb8ff9d03cf722f034538f4fe7b0c7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-29 15:04:37 +0000

    Add package xlai-base/1.0.12/xlai_base-1.0.12.tar.gz

---
+++ packages/xlai-base/xlai_base-1.0.12.tar.gz/xlai_base-1.0.12/xlai_base/Util/XLTools.py
@@ -0,0 +1,60 @@
+# coding=utf8
+import re
+
+_find_unsafe = re.compile(r'[^\w@%+=:,./-]', re.ASCII).search
+
+
+def quote(s):
+    """Return a shell-escaped version of the string *s*."""
+    if not s:
+        return "''"
+    if _find_unsafe(s) is None:
+        return s
+
+    # use single quotes, and put single quotes into double quotes
+    # the string $'b is then quoted as '$'"'"'b'
+    return "'" + s.replace("'", "'\"'\"'") + "'"
+
+
+def request_to_curl(request, compressed=False, verify=True):
+    """
+    Returns string with curl command by provided request object
+
+    Parameters
+    ----------
+    compressed : bool
+        If `True` then `--compressed` argument will be added to result
+        :param verify:
+        :param compressed:
+        :param request:
+    """
+    parts = [
+        ('curl', None),
+        ('-X', request.method),
+    ]
+
+    for k, v in sorted(request.headers.items()):
+        parts += [('-H', '{0}: {1}'.format(k, v))]
+
+    if request.body:
+        body = request.body
+        if isinstance(body, bytes):
+            body = body.decode('utf-8')
+        parts += [('-d', body)]
+
+    if compressed:
+        parts += [('--compressed', None)]
+
+    if not verify:
+        parts += [('--insecure', None)]
+
+    parts += [(None, request.url)]
+
+    flat_parts = []
+    for k, v in parts:
+        if k:
+            flat_parts.append(quote(k))
+        if v:
+            flat_parts.append(quote(v))
+
+    return ' '.join(flat_parts)
