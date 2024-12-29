df767cca8064782368023cb6c961d60e7f7cba0c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-29 03:13:13 +0000

    Add package bbstrader/0.2.0/bbstrader-0.2.0.tar.gz

---
+++ packages/bbstrader/bbstrader-0.2.0.tar.gz/bbstrader-0.2.0/bbstrader/metatrader/__init__.py
@@ -0,0 +1,6 @@
+
+from bbstrader.metatrader.account import *
+from bbstrader.metatrader.rates import *
+from bbstrader.metatrader.risk import *
+from bbstrader.metatrader.trade import *
+from bbstrader.metatrader.utils import *
---
+++ packages/bbstrader/bbstrader-0.2.0.tar.gz/bbstrader-0.2.0/bbstrader/models/__init__.py
@@ -0,0 +1,10 @@
+"""
+The `models` module provides a foundational framework for implementing various quantitative finance models. 
+
+It is designed to be a versatile base module for different types of models used in financial analysis and trading. 
+"""
+from bbstrader.models.risk import *
+from bbstrader.models.optimization import *
+from bbstrader.models.portfolio import *
+from bbstrader.models.factors import *
+from bbstrader.models.ml import *
