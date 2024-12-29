44d0874a8c8d236f2dcbd9c157264cbfadbe13c9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-29 04:04:49 +0000

    Add package kitchenai-python-sdk/1.4.0/kitchenai_python_sdk-1.4.0.tar.gz

---
+++ packages/kitchenai-python-sdk/kitchenai_python_sdk-1.4.0.tar.gz/kitchenai_python_sdk-1.4.0/kitchenai_python_sdk/api/__init__.py
@@ -0,0 +1,8 @@
+# flake8: noqa
+
+# import apis into api package
+from kitchenai_python_sdk.api.default_api import DefaultApi
+from kitchenai_python_sdk.api.embeddings_api import EmbeddingsApi
+from kitchenai_python_sdk.api.file_api import FileApi
+from kitchenai_python_sdk.api.query_api import QueryApi
+
