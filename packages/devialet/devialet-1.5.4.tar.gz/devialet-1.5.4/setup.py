dc0621e34a4184a11be266328e9239e338e32080
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-29 03:13:32 +0000

    Add package devialet/1.5.4/devialet-1.5.4.tar.gz

---
+++ packages/devialet/devialet-1.5.4.tar.gz/devialet-1.5.4/setup.py
@@ -0,0 +1,30 @@
+from distutils.core import setup
+
+setup(
+    name='devialet',
+    packages=['devialet'],
+    version='1.5.4',
+    license='MIT',
+    description='Devialet API',
+    long_description_content_type="text/markdown",
+    long_description='Devialet API',
+    author='fwestenberg',
+    author_email='',
+    url='https://github.com/fwestenberg/devialet',
+    download_url='https://github.com/fwestenberg/devialet/releases/latest',
+    keywords=['Devialet', 'Home-Assistant'],
+    install_requires=[
+        'aiohttp', 'async_upnp_client'
+    ],
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Developers',
+        'Topic :: Software Development :: Build Tools',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9'
+    ],
+)
