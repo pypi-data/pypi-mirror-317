6fbfb495e51f83bb4ab7c560bdf16dfa84b0fe55
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-29 03:12:56 +0000

    Add package inventree-rexel-plugin/0.0.2/inventree_rexel_plugin-0.0.2.tar.gz

---
+++ packages/inventree-rexel-plugin/inventree_rexel_plugin-0.0.2.tar.gz/inventree_rexel_plugin-0.0.2/setup.py
@@ -0,0 +1,53 @@
+# -*- coding: utf-8 -*-
+
+
+import setuptools  # type: ignore
+
+from inventree_rexel.version import REXEL_PLUGIN_VERSION
+
+with open('README.md', encoding='utf-8') as f:
+    long_description = f.read()
+
+
+setuptools.setup(
+    name="inventree-rexel-plugin",
+
+    version=REXEL_PLUGIN_VERSION,
+
+    author="Philip van der honing",
+
+    author_email="philipvanderhoning@gmail.com",
+
+    description="rexel product import plugin for InvenTree",
+
+    long_description=long_description,
+
+    long_description_content_type='text/markdown',
+
+    keywords="inventree rexel product import inventory",
+
+    url="https://github.com/philipboy881/inventree-rexel-plugin",
+
+    license="MIT",
+
+    packages=setuptools.find_packages(),
+
+    install_requires=[
+        'requests',
+        'beautifulsoup4',
+        'python-dotenv',
+    ],
+
+    setup_requires=[
+        "wheel",
+        "twine",
+    ],
+
+    python_requires=">=3.6",
+
+    entry_points={
+        "inventree_plugins": [
+            "rexelPlugin = inventree_rexel.rexel_plugin:RexelPlugin"
+        ]
+    },
+)
