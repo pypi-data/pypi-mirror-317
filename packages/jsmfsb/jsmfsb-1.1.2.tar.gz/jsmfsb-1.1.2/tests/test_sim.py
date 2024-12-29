01d370d58140fe8c2c0b1fa8b5330039c9ab0602
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-29 04:04:48 +0000

    Add package jsmfsb/1.1.2/jsmfsb-1.1.2.tar.gz

---
+++ packages/jsmfsb/jsmfsb-1.1.2.tar.gz/jsmfsb-1.1.2/tests/test_sim.py
@@ -0,0 +1,33 @@
+# test_sim.py
+
+import jax
+import jax.numpy as jnp
+import jsmfsb
+
+
+def test_simts():
+    lv = jsmfsb.models.lv()
+    step = lv.step_gillespie()
+    k0 = jax.random.key(42)
+    out = jsmfsb.sim_time_series(k0, lv.m, 0, 10, 0.1, step)
+    assert out.shape == (100, 2)
+
+
+def test_simsample():
+    lv = jsmfsb.models.lv()
+    step = lv.step_gillespie()
+    k0 = jax.random.key(42)
+    out = jsmfsb.sim_sample(k0, 20, lv.m, 0, 10, step)
+    assert out.shape == (20, 2)
+
+
+def test_simsamples():
+    lv = jsmfsb.models.lv()
+    step = lv.step_gillespie()
+    k0 = jax.random.key(42)
+    out = jsmfsb.sim_sample(k0, 20, lv.m, 0, 10, step)
+    out_b = jsmfsb.sim_sample(k0, 20, lv.m, 0, 10, step, batch_size=5)
+    assert jnp.all(out == out_b)
+
+
+# eof
