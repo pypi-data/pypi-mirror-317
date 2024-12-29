86f16144974698cab2eafe256e2e2a364f4e1598
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-29 14:04:24 +0000

    Add package EToolsbyEndie/1.1.0/etoolsbyendie-1.1.0.tar.gz

---
+++ packages/EToolsbyEndie/etoolsbyendie-1.1.0.tar.gz/etoolsbyendie-1.1.0/README.md
@@ -0,0 +1,38 @@
+
+![Logo](https://i.ibb.co/80xDDsg/ETools-Logo.png)
+
+
+# ETools 
+
+ETools is an open-source python package that allows you to many things with ease, using command prompt.
+
+
+### Authors
+
+- [**Wyatt Johnson**](https://github.com/realendie)
+
+
+### Features
+
+- Ability to download any YouTube video free of charge.
+- Ability to convert files to most standard file types.
+
+### Installation
+
+To install ETools YouTube Downloader run the command:
+
+```
+pip install EToolsbyEndie
+```   
+
+## How to Use
+
+**YouTube Video Downloading:**
+
+To download a YouTube video run the command
+```download_video``` in command prompt.
+
+**File Type Conversion:**
+
+To convert a file type run the command
+```convert_file``` in command prompt.
---
+++ packages/EToolsbyEndie/etoolsbyendie-1.1.0.tar.gz/etoolsbyendie-1.1.0/setup.py
@@ -0,0 +1,48 @@
+from setuptools import setup, find_packages
+
+with open("README.md", "r") as file:
+    long_description = file.read()
+
+setup(
+    name="EToolsbyEndie",
+    url="https://github.com/realendie/ETools_By_Endie",
+    author="Wyatt Johnson",
+    author_email="enderprooffical@gmail.com",
+    license="MIT",
+    license_files="LICENSE",
+    version="1.1.0",
+    packages=find_packages(),
+    install_requires=[
+        "darkdetect==0.8.0",
+        "packaging==24.2",
+        "yt-dlp",
+        "ffmpeg",
+    ],
+    keywords=[
+        "python",
+        "utility",
+        "youtube",
+        "tools",
+        "downloader",
+        "video",
+        "audio",
+        "converter",
+        "mp3",
+        "mp4",
+        "youtube-dl",
+        "yt-dlp",
+        "ffmpeg",
+    ],
+    short_description="ETools is an open-source python package that allows you to many things with ease, using command prompt.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    entry_points={
+        "console_scripts": [
+            "download_video = ETools:download_video",
+            "convert_file = ETools:convert_file",
+        ]
+    },
+)
+
+#  python setup.py sdist bdist_wheel
+#  twine upload dist/*
