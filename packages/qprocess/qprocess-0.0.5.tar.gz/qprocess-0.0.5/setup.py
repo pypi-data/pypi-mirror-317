031f30bdc650f6bd43d0740bd63d68a60f4943fa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-29 04:04:48 +0000

    Add package qprocess/0.0.5/qprocess-0.0.5.tar.gz

---
+++ packages/qprocess/qprocess-0.0.5.tar.gz/qprocess-0.0.5/setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="qprocess",  # Unique name on PyPI
+    version="0.0.5",
+    description="A Python library for processing Quantum Chemical program output files",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    author="Bogdan Virag",
+    author_email="viragbogdan@edu.bme.hu",
+    url="https://github.com/fehergandalf/Gaussian.git",  # GitHub or other project URL
+    packages=find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.6",
+    install_requires=[
+            "pandas", "numpy"
+    ],
+)
