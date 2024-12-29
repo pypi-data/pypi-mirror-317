c3ddef554ecd2616f337d2182b1e964d6d6dc669
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-29 03:12:59 +0000

    Add package nexy/0.0.18/nexy-0.0.18-py3-none-any.whl

---
+++ packages/nexy/nexy-0.0.18-py3-none-any.whl/nexy/app.py
@@ -0,0 +1,22 @@
+from fastapi import FastAPI
+from fastapi.staticfiles import StaticFiles
+from .router import Router
+import sys
+from pathlib import Path
+
+
+def Nexy(title: str = None ,**args):
+    if title is None:
+        title = Path.cwd().name 
+
+    app:FastAPI = FastAPI(title=title,**args)
+    app.mount("/", StaticFiles(directory="public"), name="public")
+
+    # Configurer le cache
+    cache_dir = Path('./__pycache__/nexy')
+    cache_dir.mkdir(exist_ok=True)
+    sys.pycache_prefix = str(cache_dir)
+
+    Router(appModule=app)
+    return app
+
