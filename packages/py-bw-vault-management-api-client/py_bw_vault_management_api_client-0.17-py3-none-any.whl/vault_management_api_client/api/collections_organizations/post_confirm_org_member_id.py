1ea8feee48e3102789767f287b5c5c4a418050ee
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-29 03:13:15 +0000

    Add package py-bw-vault-management-api-client/0.17/py_bw_vault_management_api_client-0.17-py3-none-any.whl

---
+++ packages/py-bw-vault-management-api-client/py_bw_vault_management_api_client-0.17-py3-none-any.whl/vault_management_api_client/api/collections_organizations/post_confirm_org_member_id.py
@@ -0,0 +1,122 @@
+from http import HTTPStatus
+from typing import Any, Dict, Optional, Union
+
+import httpx
+
+from ... import errors
+from ...client import AuthenticatedClient, Client
+from ...types import UNSET, Response
+
+
+def _get_kwargs(
+    id: str,
+    *,
+    organization_id: str,
+) -> Dict[str, Any]:
+    params: Dict[str, Any] = {}
+
+    params["organizationId"] = organization_id
+
+    params = {k: v for k, v in params.items() if v is not UNSET and v is not None}
+
+    _kwargs: Dict[str, Any] = {
+        "method": "post",
+        "url": f"/confirm/org-member/{id}",
+        "params": params,
+    }
+
+    return _kwargs
+
+
+def _parse_response(*, client: Union[AuthenticatedClient, Client], response: httpx.Response) -> Optional[Any]:
+    if response.status_code == HTTPStatus.OK:
+        return None
+    if response.status_code == HTTPStatus.BAD_REQUEST:
+        return None
+    if response.status_code == HTTPStatus.NOT_FOUND:
+        return None
+    if response.status_code == HTTPStatus.INTERNAL_SERVER_ERROR:
+        return None
+    if client.raise_on_unexpected_status:
+        raise errors.UnexpectedStatus(response.status_code, response.content)
+    else:
+        return None
+
+
+def _build_response(*, client: Union[AuthenticatedClient, Client], response: httpx.Response) -> Response[Any]:
+    return Response(
+        status_code=HTTPStatus(response.status_code),
+        content=response.content,
+        headers=response.headers,
+        parsed=_parse_response(client=client, response=response),
+    )
+
+
+def sync_detailed(
+    id: str,
+    *,
+    client: Union[AuthenticatedClient, Client],
+    organization_id: str,
+) -> Response[Any]:
+    """Confirm a member to a specified Organization.
+
+     Confirm a member to a specified Organization by specifying a user identifier (e.g.
+    `6b39c966-c776-4ba9-9489-ae320149af01`) in the path and the Organization identifier (e.g.
+    `b64d6e40-adf2-4f46-b4d2-acd40147548a`) as a query parameter.
+
+    Args:
+        id (str):
+        organization_id (str):
+
+    Raises:
+        errors.UnexpectedStatus: If the server returns an undocumented status code and Client.raise_on_unexpected_status is True.
+        httpx.TimeoutException: If the request takes longer than Client.timeout.
+
+    Returns:
+        Response[Any]
+    """
+
+    kwargs = _get_kwargs(
+        id=id,
+        organization_id=organization_id,
+    )
+
+    response = client.get_httpx_client().request(
+        **kwargs,
+    )
+
+    return _build_response(client=client, response=response)
+
+
+async def asyncio_detailed(
+    id: str,
+    *,
+    client: Union[AuthenticatedClient, Client],
+    organization_id: str,
+) -> Response[Any]:
+    """Confirm a member to a specified Organization.
+
+     Confirm a member to a specified Organization by specifying a user identifier (e.g.
+    `6b39c966-c776-4ba9-9489-ae320149af01`) in the path and the Organization identifier (e.g.
+    `b64d6e40-adf2-4f46-b4d2-acd40147548a`) as a query parameter.
+
+    Args:
+        id (str):
+        organization_id (str):
+
+    Raises:
+        errors.UnexpectedStatus: If the server returns an undocumented status code and Client.raise_on_unexpected_status is True.
+        httpx.TimeoutException: If the request takes longer than Client.timeout.
+
+    Returns:
+        Response[Any]
+    """
+
+    kwargs = _get_kwargs(
+        id=id,
+        organization_id=organization_id,
+    )
+
+    response = await client.get_async_httpx_client().request(**kwargs)
+
+    return _build_response(client=client, response=response)
---
+++ packages/py-bw-vault-management-api-client/py_bw_vault_management_api_client-0.17-py3-none-any.whl/vault_management_api_client/api/collections_organizations/post_move_itemid_organization_id.py
@@ -0,0 +1,129 @@
+from http import HTTPStatus
+from typing import Any, Dict, Optional, Union
+
+import httpx
+
+from ... import errors
+from ...client import AuthenticatedClient, Client
+from ...models.post_move_itemid_organization_id_body import PostMoveItemidOrganizationIdBody
+from ...types import Response
+
+
+def _get_kwargs(
+    itemid: str,
+    organization_id: str,
+    *,
+    body: PostMoveItemidOrganizationIdBody,
+) -> Dict[str, Any]:
+    headers: Dict[str, Any] = {}
+
+    _kwargs: Dict[str, Any] = {
+        "method": "post",
+        "url": f"/move/{itemid}/{organization_id}",
+    }
+
+    _body = body.to_dict()
+
+    _kwargs["json"] = _body
+    headers["Content-Type"] = "application/json"
+
+    _kwargs["headers"] = headers
+    return _kwargs
+
+
+def _parse_response(*, client: Union[AuthenticatedClient, Client], response: httpx.Response) -> Optional[Any]:
+    if response.status_code == HTTPStatus.OK:
+        return None
+    if response.status_code == HTTPStatus.BAD_REQUEST:
+        return None
+    if response.status_code == HTTPStatus.NOT_FOUND:
+        return None
+    if response.status_code == HTTPStatus.INTERNAL_SERVER_ERROR:
+        return None
+    if client.raise_on_unexpected_status:
+        raise errors.UnexpectedStatus(response.status_code, response.content)
+    else:
+        return None
+
+
+def _build_response(*, client: Union[AuthenticatedClient, Client], response: httpx.Response) -> Response[Any]:
+    return Response(
+        status_code=HTTPStatus(response.status_code),
+        content=response.content,
+        headers=response.headers,
+        parsed=_parse_response(client=client, response=response),
+    )
+
+
+def sync_detailed(
+    itemid: str,
+    organization_id: str,
+    *,
+    client: Union[AuthenticatedClient, Client],
+    body: PostMoveItemidOrganizationIdBody,
+) -> Response[Any]:
+    """Move an item to a Collection.
+
+     Move an item to a Collection by specifying an `itemid` and `organizationId` in the path, and
+    specifying one or more Collection identifiers in the request body.
+
+    Args:
+        itemid (str):
+        organization_id (str):
+        body (PostMoveItemidOrganizationIdBody):
+
+    Raises:
+        errors.UnexpectedStatus: If the server returns an undocumented status code and Client.raise_on_unexpected_status is True.
+        httpx.TimeoutException: If the request takes longer than Client.timeout.
+
+    Returns:
+        Response[Any]
+    """
+
+    kwargs = _get_kwargs(
+        itemid=itemid,
+        organization_id=organization_id,
+        body=body,
+    )
+
+    response = client.get_httpx_client().request(
+        **kwargs,
+    )
+
+    return _build_response(client=client, response=response)
+
+
+async def asyncio_detailed(
+    itemid: str,
+    organization_id: str,
+    *,
+    client: Union[AuthenticatedClient, Client],
+    body: PostMoveItemidOrganizationIdBody,
+) -> Response[Any]:
+    """Move an item to a Collection.
+
+     Move an item to a Collection by specifying an `itemid` and `organizationId` in the path, and
+    specifying one or more Collection identifiers in the request body.
+
+    Args:
+        itemid (str):
+        organization_id (str):
+        body (PostMoveItemidOrganizationIdBody):
+
+    Raises:
+        errors.UnexpectedStatus: If the server returns an undocumented status code and Client.raise_on_unexpected_status is True.
+        httpx.TimeoutException: If the request takes longer than Client.timeout.
+
+    Returns:
+        Response[Any]
+    """
+
+    kwargs = _get_kwargs(
+        itemid=itemid,
+        organization_id=organization_id,
+        body=body,
+    )
+
+    response = await client.get_async_httpx_client().request(**kwargs)
+
+    return _build_response(client=client, response=response)
