860b44b86185dbc7424fbe5ee6f4a94d2ee83a1e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-29 15:04:38 +0000

    Add package cusbara/0.0.0/cusbara-0.0.0.tar.gz

---
+++ packages/cusbara/cusbara-0.0.0.tar.gz/cusbara-0.0.0/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='cusbara',
+    version='0.0.0',
+    packages=find_packages(),
+    install_requires=[
+        "setuptools==69.5.1"
+    ],
+)
