fa271b794b7489f85504ed62aa65f8336f2754f0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-29 03:13:32 +0000

    Add package kindle-to-markdown/0.0.3/kindle_to_markdown-0.0.3-py3-none-any.whl

---
+++ packages/kindle-to-markdown/kindle_to_markdown-0.0.3-py3-none-any.whl/kindle_to_markdown/languages.py
@@ -0,0 +1,40 @@
+# -*- coding: utf-8 -*-
+"""Language definitions for Kindle annotation file."""
+
+SUPPORTED_LANGUAGES: dict = {
+    'en': {
+        'textmark': 'Highlight',
+        'note': 'Note',
+        'bookmark': 'Bookmark',
+        'page': 'Page',
+        'position': 'Location',
+    },
+    'de': {
+        'textmark': 'Markierung',
+        'note': 'Notiz',
+        'bookmark': 'Lesezeichen',
+        'page': 'Seite',
+        'position': 'Position',
+    },
+    'fr': {
+        'textmark': 'Surlignement',
+        'note': 'Note',
+        'bookmark': 'Signet',
+        'page': 'Page',
+        'position': 'Emplacement',
+    },
+    'it': {
+        'textmark': 'Evidenziazione',
+        'note': 'Nota',
+        'bookmark': 'Segnalibro',
+        'page': 'Pagina',
+        'position': 'Posizione',
+    },
+    'es': {
+        'textmark': 'Subrayar',
+        'note': 'Nota',
+        'bookmark': 'Marcador',
+        'page': 'Página',
+        'position': 'Posición',
+    },
+}
