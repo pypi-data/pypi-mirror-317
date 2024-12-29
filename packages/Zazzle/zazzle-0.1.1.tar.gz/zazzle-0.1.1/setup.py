1dd8b37b14cf2ed3c019b7159ed07dcb55594b25
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-29 04:04:30 +0000

    Add package Zazzle/0.1.1/zazzle-0.1.1.tar.gz

---
+++ packages/Zazzle/zazzle-0.1.1.tar.gz/zazzle-0.1.1/setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup
+
+setup(
+    name='Zazzle',
+    packages=['zazzle'],
+    version='0.1.1',
+    author='BitNinja01',
+    author_email='bitninja01@pm.me',
+    description='An easy logging library.',
+    url='https://github.com/BitNinja01/Zazzle',
+    download_url='https://github.com/BitNinja01/zazzle/archive/refs/tags/0.1.1.tar.gz',
+    license='GNU General Public License v3 (GPLv3)',
+    classifiers=[
+        'Programming Language :: Python :: 3.10',
+        'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
+        'Operating System :: OS Independent',
+        'Intended Audience :: Developers',
+        'Development Status :: 3 - Alpha',
+        'Natural Language :: English',
+        'Topic :: System :: Logging'
+    ],
+    python_requires='>=3.8',
+    zip_safe=True
+    )
