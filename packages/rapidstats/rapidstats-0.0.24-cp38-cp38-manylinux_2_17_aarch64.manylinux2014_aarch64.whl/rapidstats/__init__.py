f28bbfb53a08f0c794f26ee05acd343a8f84896a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-29 04:04:49 +0000

    Add package rapidstats/0.0.24/rapidstats-0.0.24-cp38-cp38-manylinux_2_17_aarch64.manylinux2014_aarch64.whl

---
+++ packages/rapidstats/rapidstats-0.0.24-cp38-cp38-manylinux_2_17_aarch64.manylinux2014_aarch64.whl/rapidstats/__init__.py
@@ -0,0 +1,16 @@
+from ._bootstrap import Bootstrap
+from ._corr import correlation_matrix
+from ._distributions import norm
+from ._metrics import (
+    adverse_impact_ratio,
+    adverse_impact_ratio_at_thresholds,
+    brier_loss,
+    confusion_matrix,
+    confusion_matrix_at_thresholds,
+    max_ks,
+    mean,
+    mean_squared_error,
+    predicted_positive_ratio_at_thresholds,
+    roc_auc,
+    root_mean_squared_error,
+)
