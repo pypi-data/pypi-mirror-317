f4ea164d592a0687ae4454bb8cd37da832f49532
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-29 03:12:52 +0000

    Add package mc-server-tool/0.0.8/mc_server_tool-0.0.8.tar.gz

---
+++ packages/mc-server-tool/mc_server_tool-0.0.8.tar.gz/mc_server_tool-0.0.8/mc_server_tool/__init__.py
@@ -0,0 +1,1 @@
+from .src.execute import main
---
+++ packages/mc-server-tool/mc_server_tool-0.0.8.tar.gz/mc_server_tool-0.0.8/mc_server_tool/src/globals.py
@@ -0,0 +1,14 @@
+import random
+import os
+
+#Here are stored all defaults!
+
+DEFAULT_PACKAGE = "Vanilla"
+DEFAULT_PATH = os.path.expanduser("~")
+DEFAULT_RAM = 4  # Gigabyte
+DEFAULT_PORT = 25565
+DEFAULT_TIMEOUT = 15  # Seconds
+
+USER_AGENTS = ["", ""] # TODO Add random User Agents
+
+RANDOM_USER_AGENT = random.choice(USER_AGENTS)
