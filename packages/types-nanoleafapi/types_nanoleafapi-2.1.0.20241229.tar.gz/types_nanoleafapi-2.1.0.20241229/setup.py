dab069aa5d7bea25189b0a3669852c7ced8c2e26
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-29 14:04:12 +0000

    Add package types-nanoleafapi/2.1.0.20241229/types_nanoleafapi-2.1.0.20241229.tar.gz

---
+++ packages/types-nanoleafapi/types_nanoleafapi-2.1.0.20241229.tar.gz/types_nanoleafapi-2.1.0.20241229/setup.py
@@ -0,0 +1,56 @@
+from setuptools import setup
+
+name = "types-nanoleafapi"
+description = "Typing stubs for nanoleafapi"
+long_description = '''
+## Typing stubs for nanoleafapi
+
+This is a [PEP 561](https://peps.python.org/pep-0561/)
+type stub package for the [`nanoleafapi`](https://github.com/MylesMor/nanoleafapi) package.
+It can be used by type-checking tools like
+[mypy](https://github.com/python/mypy/),
+[pyright](https://github.com/microsoft/pyright),
+[pytype](https://github.com/google/pytype/),
+[Pyre](https://pyre-check.org/),
+PyCharm, etc. to check code that uses `nanoleafapi`. This version of
+`types-nanoleafapi` aims to provide accurate annotations for
+`nanoleafapi==2.1.*`.
+
+This package is part of the [typeshed project](https://github.com/python/typeshed).
+All fixes for types and metadata should be contributed there.
+See [the README](https://github.com/python/typeshed/blob/main/README.md)
+for more details. The source for this package can be found in the
+[`stubs/nanoleafapi`](https://github.com/python/typeshed/tree/main/stubs/nanoleafapi)
+directory.
+
+This package was tested with
+mypy 1.14.0,
+pyright 1.1.389,
+and pytype 2024.10.11.
+It was generated from typeshed commit
+[`2cdda12df78275b98a5d3cdc8a92f93d596d9d5d`](https://github.com/python/typeshed/commit/2cdda12df78275b98a5d3cdc8a92f93d596d9d5d).
+'''.lstrip()
+
+setup(name=name,
+      version="2.1.0.20241229",
+      description=description,
+      long_description=long_description,
+      long_description_content_type="text/markdown",
+      url="https://github.com/python/typeshed",
+      project_urls={
+          "GitHub": "https://github.com/python/typeshed",
+          "Changes": "https://github.com/typeshed-internal/stub_uploader/blob/main/data/changelogs/nanoleafapi.md",
+          "Issue tracker": "https://github.com/python/typeshed/issues",
+          "Chat": "https://gitter.im/python/typing",
+      },
+      install_requires=[],
+      packages=['nanoleafapi-stubs'],
+      package_data={'nanoleafapi-stubs': ['__init__.pyi', 'digital_twin.pyi', 'discovery.pyi', 'nanoleaf.pyi', 'METADATA.toml', 'py.typed']},
+      license="Apache-2.0",
+      python_requires=">=3.8",
+      classifiers=[
+          "License :: OSI Approved :: Apache Software License",
+          "Programming Language :: Python :: 3",
+          "Typing :: Stubs Only",
+      ]
+)
