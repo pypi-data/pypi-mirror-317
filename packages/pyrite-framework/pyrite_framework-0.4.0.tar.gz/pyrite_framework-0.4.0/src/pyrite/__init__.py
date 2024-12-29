8e03ae8ce76f669ed4ebd16e810924d4fc906eee
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-29 04:04:34 +0000

    Add package pyrite-framework/0.4.0/pyrite_framework-0.4.0.tar.gz

---
+++ packages/pyrite-framework/pyrite_framework-0.4.0.tar.gz/pyrite_framework-0.4.0/src/pyrite/__init__.py
@@ -0,0 +1,15 @@
+import pygame
+
+from .display_settings import DisplaySettings  # noqa:F401
+
+DEFAULT_WINDOWED: DisplaySettings = DisplaySettings()
+"""
+Basic windowed display, 800x600
+"""
+DEFAULT_FULLSCREEN: DisplaySettings = DisplaySettings(
+    resolution=(0, 0), flags=pygame.FULLSCREEN
+)
+"""
+Fullscreen view, native resolution.
+Actual resolution supplied by pygame.
+"""
---
+++ packages/pyrite-framework/pyrite_framework-0.4.0.tar.gz/pyrite_framework-0.4.0/tests/test_game.py
@@ -0,0 +1,18 @@
+import unittest
+
+from src.pyrite.game import Game
+
+
+class Test(Game):
+
+    pass
+
+
+class TestGame(unittest.TestCase):
+
+    pass
+
+
+if __name__ == "__main__":
+
+    unittest.main()
