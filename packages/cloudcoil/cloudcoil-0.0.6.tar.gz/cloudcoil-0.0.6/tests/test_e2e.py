15623b79a176bddc36b138654b040a8ac023bb54
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-29 14:04:17 +0000

    Add package cloudcoil/0.0.6/cloudcoil-0.0.6.tar.gz

---
+++ packages/cloudcoil/cloudcoil-0.0.6.tar.gz/cloudcoil-0.0.6/tests/test_e2e.py
@@ -0,0 +1,17 @@
+"""Tests for cloudcoil package."""
+
+import pytest
+
+from cloudcoil.apimachinery import ObjectMeta
+from cloudcoil.kinds.core import v1 as corev1
+
+
+@pytest.mark.configure_test_cluster(cluster_name="test-cloudcoil", remove=False)
+def test_e2e(test_client_set):
+    with test_client_set:
+        assert corev1.Service.get("kubernetes", "default").metadata.name == "kubernetes"
+        output = corev1.Namespace(metadata=ObjectMeta(generate_name="test-")).create()
+        name = output.metadata.name
+        assert corev1.Namespace.get(name).metadata.name == name
+        assert output.remove().metadata.name == name
+        assert corev1.Namespace.delete(name).status.phase == "Terminating"
