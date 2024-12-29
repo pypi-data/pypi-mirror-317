c20f6933f02b46acb3ee8a476b79295d1bd9e58a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-29 14:04:30 +0000

    Add package gomailer/1.0.0/gomailer-1.0.0.tar.gz

---
+++ packages/gomailer/gomailer-1.0.0.tar.gz/gomailer-1.0.0/src/contacts.py
@@ -0,0 +1,22 @@
+import requests
+
+
+class Contacts:
+    base_url = 'https://users.go-mailer.com'
+
+    def __init__(self, api_key):
+        self.api_key = api_key
+
+    def synchronize(email, data={}):
+        if not email:
+            raise Exception('Email must be specified.')
+        elif not bool(data):
+            raise Exception('User data must be specified.')
+
+        body = data
+        body['email'] = email
+        headers = {'Authorization': 'Bearer {self.api_key}'}
+
+        response = requests.post(
+            "{self.base_url}/api/contacts", json=body, headers=headers)
+        return response
