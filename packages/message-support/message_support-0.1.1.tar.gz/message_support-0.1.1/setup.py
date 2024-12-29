0646a46715680bc2384f56dc41def60fcc087ff1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-29 03:13:41 +0000

    Add package message-support/0.1.1/message_support-0.1.1.tar.gz

---
+++ packages/message-support/message_support-0.1.1.tar.gz/message_support-0.1.1/setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="message-support",
+    version="0.1.1",
+    packages=find_packages(),
+    install_requires=["requests"],
+    description="Python library for interacting with message.support's API.",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    author="Message.Support",
+    url="https://message.support",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.6",
+)
