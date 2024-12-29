2b4f6d0abd97545d089d0a4bae23b874d77db082
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-29 14:04:21 +0000

    Add package toolbox-pyspark/1.0.0/toolbox_pyspark-1.0.0-py3-none-any.whl

---
+++ packages/toolbox-pyspark/toolbox_pyspark-1.0.0-py3-none-any.whl/toolbox_pyspark/utils/exceptions.py
@@ -0,0 +1,14 @@
+class ColumnDoesNotExistError(Exception):
+    pass
+
+
+class InvalidPySparkDataTypeError(Exception):
+    pass
+
+
+class InvalidDataFrameNameError(Exception):
+    pass
+
+
+class PySparkVersionError(Exception):
+    pass
