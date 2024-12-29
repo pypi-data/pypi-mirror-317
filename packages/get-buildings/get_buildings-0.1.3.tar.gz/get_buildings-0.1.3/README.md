294c7b53c3193815abbffc0505eb2a6246957946
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-29 04:04:41 +0000

    Add package get-buildings/0.1.3/get_buildings-0.1.3.tar.gz

---
+++ packages/get-buildings/get_buildings-0.1.3.tar.gz/get_buildings-0.1.3/README.md
@@ -0,0 +1,8 @@
+# Get Buildings Downloader
+
+A Python tool for downloading and processing building footprints using bounding box data.
+
+## Installation
+
+```bash
+pip install get_buildings
---
+++ packages/get-buildings/get_buildings-0.1.3.tar.gz/get_buildings-0.1.3/setup.py
@@ -0,0 +1,31 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="get_buildings",
+    version="0.1.3",
+    author="Araz Shah",
+    author_email="araz.shah@gmail.com",
+    description="A tool for downloading and processing building footprints using bounding box data.",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    url="https://github.com/arazshah/get_buildings",
+    packages=find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.6",
+    install_requires=[
+        "pandas",
+        "shapely",
+        "geopandas",
+        "mercantile",
+        "tqdm",
+    ],
+    entry_points={
+        "console_scripts": [
+            "get_buildings=get_buildings:main",
+        ],
+    },
+)
