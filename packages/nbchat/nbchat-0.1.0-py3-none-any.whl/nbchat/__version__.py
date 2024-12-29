9c8c573d92333620318918755040328150882769
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-29 03:13:44 +0000

    Add package nbchat/0.1.0/nbchat-0.1.0-py3-none-any.whl

---
+++ packages/nbchat/nbchat-0.1.0-py3-none-any.whl/nbchat/__version__.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+
+VERSION = (0, 1, 0)
+PRERELEASE = None  # alpha, beta or rc
+REVISION = None
+
+
+def generate_version(version, prerelease=None, revision=None):
+    version_parts = [".".join(map(str, version))]
+    if prerelease is not None:
+        version_parts.append("-{}".format(prerelease))
+    if revision is not None:
+        version_parts.append(".{}".format(revision))
+    return "".join(version_parts)
+
+
+__version__ = generate_version(VERSION, prerelease=PRERELEASE, revision=REVISION)
