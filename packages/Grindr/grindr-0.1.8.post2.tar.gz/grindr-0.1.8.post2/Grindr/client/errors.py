ffeac535c7898c8df12b5c213dc4c0b66bb94094
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-29 14:04:27 +0000

    Add package Grindr/0.1.8.post2/grindr-0.1.8.post2.tar.gz

---
+++ packages/Grindr/grindr-0.1.8.post2.tar.gz/grindr-0.1.8.post2/Grindr/client/errors.py
@@ -0,0 +1,31 @@
+import httpx
+
+
+class AlreadyConnectedError(RuntimeError):
+    pass
+
+
+class AuthenticationDetailsMissingError(RuntimeError):
+    pass
+
+
+class GrindrRequestError(RuntimeError):
+
+    def __init__(self, response: httpx.Response | None, *args):
+        self.response = response
+        super().__init__(*args)
+
+
+class LoginFailedResponse(GrindrRequestError):
+
+    def __init__(self, response: httpx.Response | None, *args):
+        self.response = response
+        super().__init__(*args)
+
+
+class AccountBannedError(LoginFailedResponse):
+    pass
+
+
+class CloudflareWAFResponse(LoginFailedResponse):
+    pass
