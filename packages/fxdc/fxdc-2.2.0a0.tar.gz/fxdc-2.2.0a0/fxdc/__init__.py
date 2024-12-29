33e768a92a9af7f46f1f401be63dc28b5c80da5d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-29 04:04:29 +0000

    Add package fxdc/2.2.0a0/fxdc-2.2.0a0.tar.gz

---
+++ packages/fxdc/fxdc-2.2.0a0.tar.gz/fxdc-2.2.0a0/fxdc/__init__.py
@@ -0,0 +1,18 @@
+from .read import load, loads
+from .parsedata import *
+from .config import Config
+from .writedata import ParseObject
+from .write import dumps, dump
+from .defaultclasses import load_default_classes
+__all__ = [
+    "load",
+    "loads",
+    "FxDCObject",
+    "Config",
+    "Parser",
+    "ParseObject",
+    "dumps",
+    "dump",
+]
+
+load_default_classes()
