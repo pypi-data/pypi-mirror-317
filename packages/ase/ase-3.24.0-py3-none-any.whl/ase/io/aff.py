11b034b61ce88f24600a212bf3a45c28fe79196e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-29 04:04:56 +0000

    Add package ase/3.24.0/ase-3.24.0-py3-none-any.whl

---
+++ packages/ase/ase-3.24.0-py3-none-any.whl/ase/io/aff.py
@@ -0,0 +1,6 @@
+from ase.io.ulm import DummyWriter, Reader, Writer
+from ase.io.ulm import InvalidULMFileError as InvalidAFFError
+from ase.io.ulm import open as affopen
+
+__all__ = ['affopen', 'InvalidAFFError',
+           'Reader', 'Writer', 'DummyWriter']
