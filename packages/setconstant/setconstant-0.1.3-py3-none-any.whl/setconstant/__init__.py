9a0580a1844f06f8f63336be89887a6cd5d748f0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-29 03:13:33 +0000

    Add package setconstant/0.1.3/setconstant-0.1.3-py3-none-any.whl

---
+++ packages/setconstant/setconstant-0.1.3-py3-none-any.whl/setconstant/__init__.py
@@ -0,0 +1,7 @@
+from .setconstant import setconstant
+
+const_i = setconstant.const_i
+const_f = setconstant.const_f
+const_s = setconstant.const_s
+get_constant = setconstant.get_constant
+delete_constant = setconstant.delete_constant
