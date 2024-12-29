3b18bf16778ac43f05adf0e65d4bba609d9bbf95
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-29 14:04:17 +0000

    Add package cloudcoil/0.0.6/cloudcoil-0.0.6-py3-none-any.whl

---
+++ packages/cloudcoil/cloudcoil-0.0.6-py3-none-any.whl/cloudcoil/client/__init__.py
@@ -0,0 +1,5 @@
+from ._api_client import APIClient, AsyncAPIClient
+from ._client_set import ClientSet
+from ._resource import Resource, ResourceList
+
+__all__ = ["ClientSet", "Resource", "APIClient", "AsyncAPIClient", "ResourceList"]
