c96faee04d3e8d7f186e9f5ad6794b4e1c62d2ec
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-29 03:13:04 +0000

    Add package mloptiflow/0.0.15/mloptiflow-0.0.15-py3-none-any.whl

---
+++ packages/mloptiflow/mloptiflow-0.0.15-py3-none-any.whl/mloptiflow/templates/tabular_classification/app.py
@@ -0,0 +1,10 @@
+import streamlit as st
+
+
+def main():
+    st.title("Tabular Classification Project Template")
+    st.write("This is a template for a tabular classification project.")
+
+
+if __name__ == "__main__":
+    main()
