5a8d262de47bfd9d3a5195714b4cc4f576d275f2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-29 14:04:34 +0000

    Add package deimos-client/0.1.8/deimos_client-0.1.8-py2.py3-none-any.whl

---
+++ packages/deimos-client/deimos_client-0.1.8-py2.py3-none-any.whl/deimos_client/__init__.py
@@ -0,0 +1,4 @@
+from .utils import get_job_type, encode_image
+from .base_client_handler import JobStatus, JobMetricTag, BaseClientHandler, JobDefinition
+from .file_client import get_file_client
+from .file_management import FileManagement
