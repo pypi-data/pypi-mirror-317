dede20bcd9dbb67bebd4cb7e5865fb63720d618d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-29 14:04:10 +0000

    Add package fluid-api-agent/0.0.7/fluid_api_agent-0.0.7.tar.gz

---
+++ packages/fluid-api-agent/fluid_api_agent-0.0.7.tar.gz/fluid_api_agent-0.0.7/fluid_api_agent/__init__.py
@@ -0,0 +1,7 @@
+from fluid_api_agent.main import fluid_api_request, fluid_api_request_sync, batch_fluid_api_request  # noqa: E402
+
+__all__ = [
+    "fluid_api_request",
+    "fluid_api_request_sync", 
+    "batch_fluid_api_request"
+]
