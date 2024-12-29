21bfb5e0594e37e0b392e1a4d6fe842be60fa485
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-29 15:04:41 +0000

    Add package tibia/2.2.0/tibia-2.2.0.tar.gz

---
+++ packages/tibia/tibia-2.2.0.tar.gz/tibia-2.2.0/tibia/iterable/__init__.py
@@ -0,0 +1,31 @@
+from . import aio, lazy, threaded
+from .eager import (
+    filter,
+    first,
+    group_by,
+    join,
+    map,
+    reduce,
+    reduce_to,
+    skip,
+    sort_asc,
+    sort_desc,
+    take,
+)
+
+__all__ = [
+    "aio",
+    "lazy",
+    "threaded",
+    "filter",
+    "first",
+    "group_by",
+    "join",
+    "map",
+    "reduce",
+    "reduce_to",
+    "skip",
+    "sort_asc",
+    "sort_desc",
+    "take",
+]
