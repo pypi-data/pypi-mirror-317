52478c663ed4008baf389de21a8348b988f437e7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-29 03:13:20 +0000

    Add package c-ckpt/0.1/c_ckpt-0.1.tar.gz

---
+++ packages/c-ckpt/c_ckpt-0.1.tar.gz/c_ckpt-0.1/setup.py
@@ -0,0 +1,45 @@
+import os
+import zipfile
+from setuptools import setup, find_packages
+from setuptools.command.install import install
+
+setup(
+    name='c_ckpt',
+    version='0.1',
+    description='Protein chemical shift prediction based on Protein Language Model',
+    author='Zhu He',
+    author_email='2260913071@qq.com',
+    url='https://github.com/doorpro/predict-chemical-shifts-from-protein-sequence.git',
+    long_description = open('README.md',encoding="utf-8").read(),
+    long_description_content_type='text/markdown',
+    zip_safe=True,
+    packages=find_packages(),
+    include_package_data=True,
+    package_data={
+        'reg_c': [
+            'reg_c.pth'
+        ]
+    },
+    entry_points = {
+        'console_scripts': [
+            'plm-cs=plm_cs.CS_predict:main'
+        ]
+    },
+    install_requires=[
+        'torch == 2.5.0',
+        'torchaudio == 2.5.0',
+        'torchvision == 0.20.0',
+        'fair-esm == 2.0.0',
+        'numpy == 2.1.2',
+        'biopython == 1.84',
+        'pandas == 2.2.3'
+    ],
+    classifiers = [
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Natural Language :: English",
+        "Operating System :: OS Independent",
+        "Topic :: Scientific/Engineering :: Bio-Informatics",
+        "Topic :: Scientific/Engineering :: Artificial Intelligence"
+    ]
+)
