6dd21d6e012c98e159dafd94937f47c782840c9c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-29 03:12:53 +0000

    Add package vaddpack/1.2.4/vaddpack-1.2.4.tar.gz

---
+++ packages/vaddpack/vaddpack-1.2.4.tar.gz/vaddpack-1.2.4/vaddpack/HelloVaddi.py
@@ -0,0 +1,8 @@
+def __init__(self):
+    self.name = "MyClass"
+
+def helloWorld(self,str):
+    return "Hello Vaddi Package :: "+ str
+    
+def newWelcomeMessage():
+    return "New Vadd Welcome package"
