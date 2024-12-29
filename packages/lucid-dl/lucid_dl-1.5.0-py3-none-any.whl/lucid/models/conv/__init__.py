1f036f72747e4958aedff8b8814819a1715f1cbb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-29 14:04:20 +0000

    Add package lucid-dl/1.5.0/lucid_dl-1.5.0-py3-none-any.whl

---
+++ packages/lucid-dl/lucid_dl-1.5.0-py3-none-any.whl/lucid/models/conv/__init__.py
@@ -0,0 +1,6 @@
+from lucid.models.conv.lenet import *
+from lucid.models.conv.alex import *
+from lucid.models.conv.zfnet import *
+from lucid.models.conv.vgg import *
+from lucid.models.conv.inception import *
+from lucid.models.conv.inception_res import *
