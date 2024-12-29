bb19dd0d3606c160ee09c388f6328ce1e2b8d173
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-29 03:13:38 +0000

    Add package CureQ/1.1.0/CureQ-1.1.0-py3-none-any.whl

---
+++ packages/CureQ/CureQ-1.1.0-py3-none-any.whl/CureQ/__init__.py
@@ -0,0 +1,3 @@
+# The init file can be left empty or you could provide a nice message as well as some variables describing your Library
+Message = "Have fun with all our functions by analyzing your MEA files!"
+Developers = "Developed by Joram van Beem and Jesse Antonissenfor the CureQ research consortium."
