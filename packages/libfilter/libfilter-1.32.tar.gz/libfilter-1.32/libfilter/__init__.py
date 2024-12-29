c8d6950d1173e2b63aec11b5655f70bb5da555e4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-29 04:04:26 +0000

    Add package libfilter/1.32/libfilter-1.32.tar.gz

---
+++ packages/libfilter/libfilter-1.32.tar.gz/libfilter-1.32/libfilter/__init__.py
@@ -0,0 +1,15 @@
+from .base_classes import *
+from .audio_filters import *
+from .oscillators import *
+from .samplers import *
+from .other import *
+from .transformers import *
+from .data_encoders import *
+from .modulators import *
+from .encoders import *
+from .decoders import *
+
+__lib__ = "libfilter"
+__lib_creator__ = "radio95"
+__lib_version__ = 1.32
+__version__ = __lib_version__
