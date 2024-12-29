d50d0d9e41ab67fe1122a52b93a1b175c5077cb9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-29 14:04:33 +0000

    Add package gomailer/1.0.2/gomailer-1.0.2.tar.gz

---
+++ packages/gomailer/gomailer-1.0.2.tar.gz/gomailer-1.0.2/setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="gomailer",
+    version="1.0.2",
+    packages=find_packages(),
+    author="Go-Mailer Limited",
+    author_email="info@go-mailer.com",
+    description="The Go-Mailer Utility library",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
