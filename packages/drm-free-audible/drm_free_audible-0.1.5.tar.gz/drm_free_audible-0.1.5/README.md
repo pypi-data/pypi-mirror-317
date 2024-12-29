70a4cd37f2d94102ac4c4de5723b95a6dc16936a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-29 03:12:54 +0000

    Add package drm-free-audible/0.1.5/drm_free_audible-0.1.5.tar.gz

---
+++ packages/drm-free-audible/drm_free_audible-0.1.5.tar.gz/drm_free_audible-0.1.5/README.md
@@ -0,0 +1,4 @@
+# DRM free Audbile
+
+Python script to remove DRM from my Audible files, so that they can be played in
+devices like Garmin sports watches.
---
+++ packages/drm-free-audible/drm_free_audible-0.1.5.tar.gz/drm_free_audible-0.1.5/src/drm_free_audible/main.py
@@ -0,0 +1,42 @@
+"""Module sets up logging and runs the main application."""
+
+import logging
+import logging.config
+from pathlib import Path
+import yaml
+from importlib import resources
+
+
+def setup_logging():
+    """Set up logging configuration from a YAML file."""
+    # Ensure the logs directory exists
+    logs_dir = Path.home() / "logs"
+    logs_dir.mkdir(parents=True, exist_ok=True)
+
+    # Load the logging configuration
+    with resources.open_text("drm_free_audible", "logging_config.yaml") as file:
+        config = yaml.safe_load(file)
+    logging.config.dictConfig(config)
+
+
+setup_logging()
+logger = logging.getLogger(__name__)
+
+import argparse
+
+from drm_free_audible.drm import create_drm_free_files
+
+
+def main() -> None:
+    """Set up logging and run the application."""
+    logger.debug("Program started.")
+    parser = argparse.ArgumentParser(description="Create DRM-free files from src to dst")
+    parser.add_argument("src", type=str, help="Source path")
+    parser.add_argument("dst", type=str, help="Destination path")
+    args = parser.parse_args()
+    create_drm_free_files(args.src, args.dst)
+    logger.info("Program completed.")
+
+
+if __name__ == "__main__":
+    main()
