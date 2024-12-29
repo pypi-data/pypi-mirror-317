e60ae4e319b046e97acc03b678f9bf1cae0e4313
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-29 15:04:41 +0000

    Add package tibia/2.2.0/tibia-2.2.0-py3-none-any.whl

---
+++ packages/tibia/tibia-2.2.0-py3-none-any.whl/tibia/__init__.py
@@ -0,0 +1,19 @@
+from .future import Future
+from .future_maybe import FutureMaybe
+from .future_result import FutureResult
+from .maybe import Empty, Maybe, Some
+from .result import Err, Ok, Result
+from .value import Value
+
+__all__ = (
+    "Future",
+    "FutureMaybe",
+    "FutureResult",
+    "Empty",
+    "Maybe",
+    "Some",
+    "Err",
+    "Ok",
+    "Result",
+    "Value",
+)
