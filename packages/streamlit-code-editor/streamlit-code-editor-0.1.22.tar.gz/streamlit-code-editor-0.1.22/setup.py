3a10561a3d43a757e032bb75befc703880e22979
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-30 14:04:57 +0000

    Add package streamlit-code-editor/0.1.22/streamlit-code-editor-0.1.22.tar.gz

---
+++ packages/streamlit-code-editor/streamlit-code-editor-0.1.22.tar.gz/streamlit-code-editor-0.1.22/setup.py
@@ -0,0 +1,27 @@
+import setuptools
+from pathlib import Path
+
+this_directory = Path(__file__).parent
+long_description = (this_directory / "PROJECT.md").read_text()
+
+
+setuptools.setup(
+    name="streamlit-code-editor",
+    version="0.1.22",
+    author="Anas Bouzid",
+    author_email="anasbouzid@gmail.com",
+    description="React-ace editor customized for Streamlit",
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    url="https://github.com/bouzidanas/streamlit-code-editor",
+    packages=setuptools.find_packages(),
+    include_package_data=True,
+    classifiers=[],
+    python_requires=">=3.6",
+    install_requires=[
+        # By definition, a Custom Component depends on Streamlit.
+        # If your component has other Python dependencies, list
+        # them here.
+        "streamlit >= 0.63",
+    ],
+)
