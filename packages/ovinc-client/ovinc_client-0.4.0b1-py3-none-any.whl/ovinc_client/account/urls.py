ab0e6ab0b32ab4f095a1224aa5d12224b752a469
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-30 15:05:57 +0000

    Add package ovinc-client/0.4.0b1/ovinc_client-0.4.0b1-py3-none-any.whl

---
+++ packages/ovinc-client/ovinc_client-0.4.0b1-py3-none-any.whl/ovinc_client/account/urls.py
@@ -0,0 +1,8 @@
+from rest_framework.routers import DefaultRouter
+
+from ovinc_client.account.views import UserInfoViewSet
+
+router = DefaultRouter()
+router.register("user_info", UserInfoViewSet, basename="user_info")
+
+urlpatterns = router.urls
