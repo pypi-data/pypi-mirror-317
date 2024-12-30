c032a306e9efd351fa68d01b0204e5e98b5f2a7b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-30 03:10:01 +0000

    Add package nonebot-adapter-wxmp/0.0.1/nonebot_adapter_wxmp-0.0.1.tar.gz

---
+++ packages/nonebot-adapter-wxmp/nonebot_adapter_wxmp-0.0.1.tar.gz/nonebot_adapter_wxmp-0.0.1/nonebot/adapters/wxmp/event/__init__.py
@@ -0,0 +1,17 @@
+from .base import *
+from .offical import *
+from .miniprogram import *
+
+__all__ = [
+    "Event",
+    "MessageEvent",
+    "MiniprogramEvent",
+    "OfficalAccountEvent",
+
+    "AuthorizationChangeEvent",
+    "MiniprogramMessageEvent",
+    "OfficalAccountMessageEvent",
+    "SubscribeEvent",
+    "UnSubscribeEvent",
+    "MenuClickEvent",
+]
