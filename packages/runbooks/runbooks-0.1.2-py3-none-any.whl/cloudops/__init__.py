627968f9b42273389d6b6348480cea0680a46590
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-30 15:05:55 +0000

    Add package runbooks/0.1.2/runbooks-0.1.2-py3-none-any.whl

---
+++ packages/runbooks/runbooks-0.1.2-py3-none-any.whl/cloudops/__init__.py
@@ -0,0 +1,10 @@
+"""
+CloudOps: CloudOps Toolkit
+
+Provides utility functions for math operations, AWS S3 interactions, and data management.
+"""
+
+__version__ = "0.1.2"
+
+from .toolkit import add, divide, multiply, subtract
+# from .s3 import S3Client
