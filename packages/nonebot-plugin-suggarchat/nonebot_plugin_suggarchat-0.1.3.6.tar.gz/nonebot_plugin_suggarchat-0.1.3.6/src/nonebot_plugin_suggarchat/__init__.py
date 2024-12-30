3d3d429c26d8fac54b0deb8c049534b5798c9c0f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-30 03:10:14 +0000

    Add package nonebot-plugin-suggarchat/0.1.3.6/nonebot_plugin_suggarchat-0.1.3.6.tar.gz

---
+++ packages/nonebot-plugin-suggarchat/nonebot_plugin_suggarchat-0.1.3.6.tar.gz/nonebot_plugin_suggarchat-0.1.3.6/src/nonebot_plugin_suggarchat/__init__.py
@@ -0,0 +1,26 @@
+
+from nonebot import get_plugin_config
+from nonebot.plugin import PluginMetadata
+from .conf import __KERNEL_VERSION__
+from .config import Config
+from .conf import *
+from .resources import *
+from .suggar import *
+from .API import *
+
+__plugin_meta__ = PluginMetadata(
+    name="nonebot_plugin_suggarchat",
+    description="Plugin for the Suggar chat framework compatible with Nonebot2.",
+    usage="use OpenAI API to chat",
+    config=Config,
+    homepage="https://github.com/JohnRichard4096/nonebot_plugin_suggarchat/",
+    type="application",
+    author="JohnRichard4096",
+    version=__KERNEL_VERSION__,
+)
+
+config = get_plugin_config(Config)
+
+
+
+
