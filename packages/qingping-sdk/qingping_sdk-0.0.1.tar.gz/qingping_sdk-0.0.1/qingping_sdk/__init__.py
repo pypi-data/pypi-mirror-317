76f6a1cad16e7961e3eecc0f01c45167b11e54c1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-30 15:05:02 +0000

    Add package qingping-sdk/0.0.1/qingping_sdk-0.0.1.tar.gz

---
+++ packages/qingping-sdk/qingping_sdk-0.0.1.tar.gz/qingping_sdk-0.0.1/qingping_sdk/__init__.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+from qingping_sdk.client import Client
+from qingping_sdk.connection import (
+    Connection,
+    Event,
+    build_history_data,
+    parse_history_data,
+)
+
+__version__ = "0.0.1"
