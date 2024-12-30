8c5d5bb87e7dda2e1cca0b469530a81872de9968
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-30 03:10:14 +0000

    Add package cw-test/1.0.3/cw_test-1.0.3.tar.gz

---
+++ packages/cw-test/cw_test-1.0.3.tar.gz/cw_test-1.0.3/setup.py
@@ -0,0 +1,18 @@
+
+from setuptools import setup, find_packages
+
+# read the contents of your README file
+from pathlib import Path
+this_directory = Path(__file__).parent
+long_description = (this_directory / "README.md").read_text()
+
+setup(
+    name="cw_test",
+    version= "1.0.3",
+    packages= find_packages(),
+    readme = "README.md",
+    # scripts = ["cw_test/manim_setup.py"],
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+
+)
