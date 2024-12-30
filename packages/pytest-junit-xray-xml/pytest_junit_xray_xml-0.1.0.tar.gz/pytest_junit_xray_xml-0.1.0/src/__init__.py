c82523908ff62f25ee2240dd61c3ca1b633ac5c3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-30 03:09:51 +0000

    Add package pytest-junit-xray-xml/0.1.0/pytest_junit_xray_xml-0.1.0.tar.gz

---
+++ packages/pytest-junit-xray-xml/pytest_junit_xray_xml-0.1.0.tar.gz/pytest_junit_xray_xml-0.1.0/src/__init__.py
@@ -0,0 +1,15 @@
+from .fixtures import (
+    record_test_evidence,
+    record_test_summary,
+    record_test_id,
+    record_test_description,
+    record_test_key
+)
+
+__all__ = [
+    "record_test_evidence",
+    "record_test_summary",
+    "record_test_id",
+    "record_test_description",
+    "record_test_key"
+]
