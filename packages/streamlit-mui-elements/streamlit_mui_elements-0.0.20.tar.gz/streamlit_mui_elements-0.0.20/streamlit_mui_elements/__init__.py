65436a65f31a8342f55cf346cff3f543ac792e69
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-30 15:05:27 +0000

    Add package streamlit-mui-elements/0.0.20/streamlit_mui_elements-0.0.20.tar.gz

---
+++ packages/streamlit-mui-elements/streamlit_mui_elements-0.0.20.tar.gz/streamlit_mui_elements-0.0.20/streamlit_mui_elements/__init__.py
@@ -0,0 +1,12 @@
+from streamlit_mui_elements.core.elements import ElementsManager
+from streamlit_mui_elements.core.exceptions import *
+from streamlit_mui_elements.modules.mui import mui
+from streamlit_mui_elements.modules.datagrid import data_grid
+from streamlit_mui_elements.modules.file_upload import file_upload
+
+def elements(key: str) -> ElementsManager:
+    """Create a Streamlit Elements frame using ElementsManager."""
+    return ElementsManager(key)
+
+__all__ = ['elements', 'mui', 'data_grid', 'file_upload', 'handle_csv_upload']
+
