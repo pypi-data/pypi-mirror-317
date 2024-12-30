27df1a17b79bca6f29bb3792fa0aa91c3720b476
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-30 04:04:44 +0000

    Add package fastcrawl/0.2.0/fastcrawl-0.2.0.tar.gz

---
+++ packages/fastcrawl/fastcrawl-0.2.0.tar.gz/fastcrawl-0.2.0/fastcrawl/models/log_settings.py
@@ -0,0 +1,27 @@
+from pathlib import Path
+from typing import Optional, Union
+
+from pydantic import BaseModel
+
+
+class LogSettings(BaseModel):
+    """Log settings model.
+
+    Attributes:
+        configure_globally (bool): Whether to configure logging globally. Default is True.
+        level (str): Log level. Default is "INFO".
+        format (str): Log format. Default is "%(asctime)s [%(name)s] %(levelname)s: %(message)s".
+        file (Optional[Union[Path, str]]): File to write logs to. Default is None.
+        level_asyncio (str): Log level for asyncio library. Default is "WARNING".
+        level_httpx (str): Log level for httpx library. Default is "WARNING".
+        level_httpcore (str): Log level for httpcore library. Default is "WARNING".
+
+    """
+
+    configure_globally: bool = True
+    level: str = "INFO"
+    format: str = "%(asctime)s [%(name)s] %(levelname)s: %(message)s"
+    file: Optional[Union[Path, str]] = None
+    level_asyncio: str = "WARNING"
+    level_httpx: str = "WARNING"
+    level_httpcore: str = "WARNING"
