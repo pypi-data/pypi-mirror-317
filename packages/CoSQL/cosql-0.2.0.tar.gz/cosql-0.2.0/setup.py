bb67e9d0127f6c0bb5d980a17219a76dcdb24bca
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-30 05:04:25 +0000

    Add package CoSQL/0.2.0/cosql-0.2.0.tar.gz

---
+++ packages/CoSQL/cosql-0.2.0.tar.gz/cosql-0.2.0/setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup, find_packages
+
+setup(
+    name = 'COSQL',
+    version = '0.2.0',
+    description = 'SQLite3 wrapper for asyncio',
+
+    author = 'Comet',
+    url = 'https://github.com/cwmet/COSQL',
+
+    packages = find_packages(exclude=[]),
+    python_requires = '>=3.12',
+    install_requires = ['asyncio'],
+)
