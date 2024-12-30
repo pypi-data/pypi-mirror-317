bd50a051fadc749dfca7b67024abf399fdbf1102
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-30 15:04:55 +0000

    Add package dg-itest/0.0.23a1/dg_itest-0.0.23a1-py3-none-any.whl

---
+++ packages/dg-itest/dg_itest-0.0.23a1-py3-none-any.whl/dg_itest/utils/keywords.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Time    : 2024/2/21 16:05
+# @Author  : yaitza
+# @Email   : yaitza@foxmial.com
+import time
+import random
+
+class KeyWords:
+	def currenttime(self):
+		"""
+		获取当前时间
+		:return: 当前时间, YYYYMMDDHHMMSS
+		"""
+		current_time = time.strftime('%Y%m%d%H%M%S', time.localtime(time.time()))
+		return current_time
+
+	def randomnum(self):
+		"""
+		获取随机数
+		:return: 随机数, 10000-99999
+		"""
+		random_num = random.randint(10000, 99999)
+		return str(random_num)
+
+
+if __name__ == '__main__':
+	change = KeyWords().randomnum()
+	print(change, type(change))
+	current_time = KeyWords().currenttime()
+	print(current_time, type(current_time))
