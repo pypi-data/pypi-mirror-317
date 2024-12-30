dfa55cccbe6ab0f64af27ee43288be06b6ebbb3c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-30 03:10:29 +0000

    Add package mongrate/0.0.5/mongrate-0.0.5.tar.gz

---
+++ packages/mongrate/mongrate-0.0.5.tar.gz/mongrate-0.0.5/mongodb_migrator/cli.py
@@ -0,0 +1,83 @@
+import click
+
+from mongodb_migrator.core import MongoDBMigrator, create_default_config
+
+
+@click.group()
+def cli():
+    """MongoDB Migrator CLI."""
+    pass
+
+
+@cli.command()
+@click.option(
+    "--config-file",
+    default="mongrate_config.yaml",
+    help="Path to the config file.",
+)
+def init(config_file):
+    """Initialize the migrations system."""
+    create_default_config(config_file)
+    migrator = MongoDBMigrator(config_file)
+    migrator.init()
+
+
+@cli.command()
+@click.argument("name")
+@click.option(
+    "--config-file",
+    default="mongrate_config.yaml",
+    help="Path to the config file.",
+)
+def create(name, config_file):
+    """Create a new migration file."""
+    migrator = MongoDBMigrator(config_file)
+    migrator.create_migration(name)
+
+
+@cli.command()
+@click.argument("target", required=False, default="all")
+@click.option(
+    "--config-file",
+    default="mongrate_config.yaml",
+    help="Path to the config file.",
+)
+def upgrade(target, config_file):
+    """Apply migrations up to a specific target."""
+    migrator = MongoDBMigrator(config_file)
+    if target == "all":
+        migrator.apply_migrations()
+    else:
+        migrator.apply_migrations(target)
+
+
+@cli.command()
+@click.argument("target", required=False, default="all")
+@click.option(
+    "--config-file",
+    default="mongrate_config.yaml",
+    help="Path to the config file.",
+)
+def downgrade(target, config_file):
+    """Rollback migrations down to a specific target."""
+    migrator = MongoDBMigrator(config_file)
+    if target == "all":
+        migrator.rollback_migrations()
+    else:
+        migrator.rollback_migrations(target)
+
+
+@cli.command()
+@click.option(
+    "--config-file",
+    default="mongrate_config.yaml",
+    help="Path to the config file.",
+)
+def history(config_file):
+    """Show the migration history."""
+    migrator = MongoDBMigrator(config_file)
+    migrator.show_migration_history()
+
+
+if __name__ == "__main__":
+    cli()
