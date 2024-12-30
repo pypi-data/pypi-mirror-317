496706eb6367cdc2cf2a054cc6c17f22e4250ef3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-30 14:04:59 +0000

    Add package licomp-proprietary/0.4.0/licomp_proprietary-0.4.0-py2.py3-none-any.whl

---
+++ packages/licomp-proprietary/licomp_proprietary-0.4.0-py2.py3-none-any.whl/licomp_proprietary/__main__.py
@@ -0,0 +1,28 @@
+#!/bin/env python3
+
+# SPDX-FileCopyrightText: 2024 Henrik Sandklef
+#
+# SPDX-License-Identifier: GPL-3.0-or-later
+
+from licomp.interface import UseCase
+from licomp.interface import Provisioning
+from licomp.main_base import LicompParser
+
+from licomp_proprietary.config import cli_name
+from licomp_proprietary.config import description
+from licomp_proprietary.config import epilog
+from licomp_proprietary.proprietary import LicompProprietary
+
+def main():
+    lp = LicompProprietary()
+    o_parser = LicompParser(lp,
+                            cli_name,
+                            description,
+                            epilog,
+                            UseCase.LIBRARY,
+                            Provisioning.BIN_DIST)
+    o_parser.run()
+
+
+if __name__ == '__main__':
+    main()
