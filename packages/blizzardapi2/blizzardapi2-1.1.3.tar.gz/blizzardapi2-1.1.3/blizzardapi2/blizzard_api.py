0d8d0a6e03029756519793eecf9b778e0f4fcff8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-30 14:04:55 +0000

    Add package blizzardapi2/1.1.3/blizzardapi2-1.1.3.tar.gz

---
+++ packages/blizzardapi2/blizzardapi2-1.1.3.tar.gz/blizzardapi2-1.1.3/blizzardapi2/blizzard_api.py
@@ -0,0 +1,24 @@
+"""blizzard_api.py file."""
+
+from .battlenet.battlenet_api import BattlenetApi
+from .diablo3.diablo3_api import Diablo3Api
+from .hearthstone.hearthstone_api import HearthstoneApi
+from .starcraft2.starcraft2_api import Starcraft2Api
+from .wow.wow_api import WowApi
+
+
+class BlizzardApi:
+    """Blizzard API class.
+
+    Attributes:
+        client_id: A string client id supplied by Blizzard.
+        client_secret: A string client secret supplied by Blizzard.
+    """
+
+    def __init__(self, client_id: str, client_secret: str) -> None:
+        """Init BlizzardApi."""
+        self.battlenet = BattlenetApi(client_id, client_secret)
+        self.diablo3 = Diablo3Api(client_id, client_secret)
+        self.hearthstone = HearthstoneApi(client_id, client_secret)
+        self.wow = WowApi(client_id, client_secret)
+        self.starcraft2 = Starcraft2Api(client_id, client_secret)
