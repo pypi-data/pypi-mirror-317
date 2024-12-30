2b42dd4b069ed9462f5ebeddaa319bb83c1c6af8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-30 03:10:10 +0000

    Add package tryon-tray/0.1.6/tryon_tray-0.1.6.tar.gz

---
+++ packages/tryon-tray/tryon_tray-0.1.6.tar.gz/tryon_tray-0.1.6/src/tryon_tray/services/vton/__init__.py
@@ -0,0 +1,13 @@
+"""Virtual try-on services."""
+
+from ..factory import ServiceFactory, ServiceType
+from .fashnai import FashnaiVTON
+from .klingai import KlingaiVTON
+from .replicate import ReplicateVTON
+from .vmodel import VModelVTON
+
+# Register VTON services
+ServiceFactory.register(ServiceType.VTON, "fashnai", FashnaiVTON)
+ServiceFactory.register(ServiceType.VTON, "klingai", KlingaiVTON)
+ServiceFactory.register(ServiceType.VTON, "replicate", ReplicateVTON)
+ServiceFactory.register(ServiceType.VTON, "vmodel", VModelVTON) 
