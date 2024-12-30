6a4e97b669d04b9f9591ff83a1d7c4b6d4c28f85
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-30 03:10:38 +0000

    Add package XeroGraph/0.0.9/XeroGraph-0.0.9-py3-none-any.whl

---
+++ packages/XeroGraph/XeroGraph-0.0.9-py3-none-any.whl/XeroGraph/__init__.py
@@ -0,0 +1,24 @@
+"""
+******************
+***   XeroGraph  ***
+******************
+
+A Python implementation of Little's MCAR test.
+"""
+__author__ = 'Julhash Kazi'
+__email__ = 'XeroGraph@kazilab.se'
+__description__ = 'XeroGraph'
+__version__ = '0.0.9'
+__url__ = 'https://www.kazilab.se'
+
+from .main import XeroAnalyzer as XA
+from .main import XeroAnalyzer as xa
+from .main import XeroAnalyzer as xeroanalyzer
+from .main import XeroAnalyzer as xero_analyzer
+from .main import XeroAnalyzer
+from .compare import XeroCompare as XC
+from .compare import XeroCompare as xc
+from .compare import XeroCompare as xerocompare
+from .compare import XeroCompare as xero_compare
+from .compare import XeroCompare
+from .utils import DATASETS, load_dataset
