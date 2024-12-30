a27cc68d180704e155d0e9c7010142714e943d1b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-30 15:04:57 +0000

    Add package spark-dataframe-tools/0.7.2/spark_dataframe_tools-0.7.2.tar.gz

---
+++ packages/spark-dataframe-tools/spark_dataframe_tools-0.7.2.tar.gz/spark_dataframe_tools-0.7.2/setup.py
@@ -0,0 +1,31 @@
+import os
+from distutils.core import setup
+
+from setuptools import find_packages
+
+setuppath = os.path.dirname(os.path.abspath(__file__))
+
+setup(
+    name='spark_dataframe_tools',
+    packages=find_packages(),
+    version='0.7.2',
+    description='spark_dataframe_tools',
+    long_description=open(os.path.join(setuppath, 'README.md')).read(),
+    long_description_content_type="text/markdown",
+    author='Jonathan Quiza',
+    author_email='jony327@gmail.com',
+    url='https://github.com/jonaqp/spark_dataframe_tools/',
+    download_url='https://github.com/jonaqp/spark_dataframe_tools/archive/main.zip',
+    keywords=['spark', 'dataframe'],
+    install_requires=open(os.path.join(setuppath, 'requirements.txt')).read().splitlines(),
+    dependency_links=[],
+    include_package_data=True,
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Intended Audience :: Developers',
+        'Intended Audience :: Science/Research',
+        'Topic :: Scientific/Engineering :: Artificial Intelligence',
+        'License :: OSI Approved :: Apache Software License',
+        'Programming Language :: Python :: 3.9',
+    ],
+)
