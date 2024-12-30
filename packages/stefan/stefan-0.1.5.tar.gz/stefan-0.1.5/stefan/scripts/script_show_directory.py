1f8c77328ae9a03aee3198d8376ce21eccc2c549
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-30 14:05:00 +0000

    Add package stefan/0.1.5/stefan-0.1.5.tar.gz

---
+++ packages/stefan/stefan-0.1.5.tar.gz/stefan-0.1.5/stefan/scripts/script_show_directory.py
@@ -0,0 +1,11 @@
+from stefan.execution_context import ExecutionContext
+from stefan.project_configuration import ProjectContext
+from stefan.tool.tool_show_directory import ShowDirectoryToolDefinition
+
+def script_show_directory(project_context: ProjectContext):
+    context = ExecutionContext.initial(current_agent=None, project_context=project_context)
+    tool = ShowDirectoryToolDefinition()
+    args = {
+        "directory": "."
+    }
+    print(tool.execute_tool(args, context))
