318ea597e3abb3159829421df265beee30b6b018
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-30 15:05:47 +0000

    Add package hockey-blast-common-lib/0.1.4/hockey-blast-common-lib-0.1.4.tar.gz

---
+++ packages/hockey-blast-common-lib/hockey-blast-common-lib-0.1.4.tar.gz/hockey-blast-common-lib-0.1.4/hockey_blast_common_lib/db_utils.py
@@ -0,0 +1,21 @@
+import sys
+import os
+
+from models import Human
+
+def get_fake_human_for_stats(session):
+    first_name = "Fake"
+    middle_name = "Stats"
+    last_name = "Human"
+
+    # Check if the human already exists
+    existing_human = session.query(Human).filter_by(first_name=first_name, middle_name=middle_name, last_name=last_name).first()
+    if existing_human:
+        return existing_human.id
+
+    # Create a new human
+    human = Human(first_name=first_name, middle_name=middle_name, last_name=last_name)
+    session.add(human)
+    session.commit()  # Commit to get the human.id
+
+    return human.id
