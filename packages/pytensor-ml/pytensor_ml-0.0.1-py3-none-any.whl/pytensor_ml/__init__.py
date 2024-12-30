898385f0bd74acce3ac2da52e8e5ad1875394764
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-30 14:05:09 +0000

    Add package pytensor-ml/0.0.1/pytensor_ml-0.0.1-py3-none-any.whl

---
+++ packages/pytensor-ml/pytensor_ml-0.0.1-py3-none-any.whl/pytensor_ml/__init__.py
@@ -0,0 +1,15 @@
+import logging
+import sys
+
+from pytensor_ml._version import get_versions
+
+_log = logging.getLogger(__name__)
+
+if not logging.root.handlers:
+    _log.setLevel(logging.INFO)
+    if len(_log.handlers) == 0:
+        handler = logging.StreamHandler(sys.stderr)
+        _log.addHandler(handler)
+
+
+__version__ = get_versions()["version"]
