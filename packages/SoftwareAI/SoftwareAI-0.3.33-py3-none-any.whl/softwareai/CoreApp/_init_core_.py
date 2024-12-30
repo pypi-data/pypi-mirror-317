3eddea784d1077afffbcce918804150262314fd8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-30 14:05:04 +0000

    Add package SoftwareAI/0.3.33/SoftwareAI-0.3.33-py3-none-any.whl

---
+++ packages/SoftwareAI/SoftwareAI-0.3.33-py3-none-any.whl/softwareai/CoreApp/_init_core_.py
@@ -0,0 +1,16 @@
+# IMPORT SoftwareAI Core 
+from softwareai.CoreApp.SoftwareAI.Core import (
+        AutenticateAgent,
+        ResponseAgent,
+        python_functions,
+        Agent_files,
+        Agent_files_update,
+        Agent_destilation,
+        OpenAIKeysinit,
+        FirebaseKeysinit
+)
+from softwareai.CoreApp._init_environment_ import (
+        init_env
+)
+
+#########################################
