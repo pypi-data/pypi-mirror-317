e6c8bc21ea481e10e8a9236760a52b58df317514
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-30 03:10:22 +0000

    Add package nexy/0.0.20.7/nexy-0.0.20.7-py3-none-any.whl

---
+++ packages/nexy/nexy-0.0.20.7-py3-none-any.whl/nexy/hooks.py
@@ -0,0 +1,10 @@
+
+from pathlib import Path
+from fastapi.responses import  HTMLResponse
+# def useView(content):
+#     return HTMLResponse(content=f"{content}")
+
+
+def useView():
+    Path(f"view/{name}/{file_path}")
+    return 
