c25c6d4b9944d3ea98247c6327eb0044596ce685
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-30 03:09:57 +0000

    Add package jetblack-fixparser/2.5.0/jetblack_fixparser-2.5.0.tar.gz

---
+++ packages/jetblack-fixparser/jetblack_fixparser-2.5.0.tar.gz/jetblack_fixparser-2.5.0/src/jetblack_fixparser/meta_data/message.py
@@ -0,0 +1,44 @@
+"""Meta data for the FIX message"""
+
+from typing import Mapping, Union
+
+from .message_member import MessageMemberMetaData
+
+MessageFieldMetaDataMapping = Mapping[
+    str,
+    Union[MessageMemberMetaData, 'MessageFieldMetaDataMapping']
+]
+
+
+class MessageMetaData:
+    """FIX message meta data"""
+
+    def __init__(
+            self,
+            name: str,
+            msgtype: bytes,
+            msgcat: str,
+            fields: MessageFieldMetaDataMapping
+    ) -> None:
+        """Initialise the message meta data
+
+        Args:
+            name (str): The name
+            msgtype (bytes): The type
+            msgcat (str): The category
+            fields (MessageFieldMetaDataMapping): The fields.
+        """
+        self.name = name
+        self.msgtype = msgtype
+        self.msgcat = msgcat
+        self.fields = fields
+
+    def __str__(self) -> str:
+        return (
+            'MessageMetaData: '
+            f'name="{self.name}", '
+            f'msgtype="{self.msgtype.decode("ascii")}", '
+            f'msgcat="{self.msgcat}"'
+        )
+
+    __repr__ = __str__
