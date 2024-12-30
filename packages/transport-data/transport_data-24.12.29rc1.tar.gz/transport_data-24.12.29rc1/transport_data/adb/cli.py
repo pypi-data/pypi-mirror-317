657930be55bbe59c45129ad9c63b5d3e4fb5a879
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-30 14:05:04 +0000

    Add package transport-data/24.12.29rc1/transport_data-24.12.29rc1.tar.gz

---
+++ packages/transport-data/transport_data-24.12.29rc1.tar.gz/transport_data-24.12.29rc1/transport_data/adb/cli.py
@@ -0,0 +1,59 @@
+"""CLI for :mod:`.adb`.
+
+.. runblock:: console
+
+   $ tdc adb --help
+
+.. runblock:: console
+
+   $ tdc add fetch --help
+
+.. runblock:: console
+
+   $ tdc adb convert --help
+"""
+
+import click
+
+from . import FILES, convert, fetch
+
+
+@click.group("adb")
+def main():
+    """Asian Development Bank (ADB) provider."""
+
+
+@main.command("fetch")
+@click.argument("part", nargs=-1)
+@click.option("--go", is_flag=True, help="Actually fetch.")
+@click.option("--all", "all_", is_flag=True, help="Fetch all files.")
+def fetch_cmd(part, all_, go):
+    """Fetch original data files."""
+    if not len(part):
+        if not all_:
+            print(f"Supply --all or 1+ of: {' '.join(FILES)}")
+            return
+
+        part = list(FILES.keys())
+
+    fetch(*part)
+
+
+@main.command("convert")
+@click.argument("part", nargs=-1)
+@click.option("--all", "all_", is_flag=True, help="Convert all parts.")
+def convert_cmd(part, all_):  # pragma: no cover
+    """Convert to SDMX data and structures."""
+    if not len(part):
+        if not all_:
+            print(f"Supply --all or 1+ of: {' '.join(FILES)}")
+            return
+
+        part = list(FILES.keys())
+
+    for p in part:
+        if p == "POL":
+            print(f"Skip PART = {p}; not currently supported")
+            continue
+
+        convert(p)
---
+++ packages/transport-data/transport_data-24.12.29rc1.tar.gz/transport_data-24.12.29rc1/transport_data/proto/cli.py
@@ -0,0 +1,30 @@
+"""CLI for :mod:`.proto`.
+
+.. runblock:: console
+
+   $ tdc proto --help
+
+"""
+
+import click
+
+
+@click.command("proto")
+def main():  # pragma: no cover
+    """TDC prototype debugging."""
+    # from transport_data import adb
+    from transport_data import jrc
+
+    # # commented: this fetches *all* data, and should not be done too often
+    # # jrc.extract_all()
+    #
+    # # commented: uncomment these statements to fetch and unpack just one data file
+    # path = jrc.fetch("AT")
+    # jrc.extract(path)
+
+    print(jrc.read("AT"))
+
+    # # commented: uncomment this statement to fetch ADB ATO data files
+    # adb.get()
+
+    # adb.convert_all()
