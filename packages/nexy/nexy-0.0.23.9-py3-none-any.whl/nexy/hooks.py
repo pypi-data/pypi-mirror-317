f299cab0d66cef979bf66b2594ada267ddef5214
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-30 05:04:22 +0000

    Add package nexy/0.0.23.9/nexy-0.0.23.9-py3-none-any.whl

---
+++ packages/nexy/nexy-0.0.23.9-py3-none-any.whl/nexy/hooks.py
@@ -0,0 +1,17 @@
+from fastapi.responses import  HTMLResponse
+from jinja2 import Environment, FileSystemLoader, TemplateNotFound
+from nexy.utils import find_page_html
+
+def useView(data):
+    path = find_page_html()
+    env = Environment(loader=FileSystemLoader("app"))   
+    
+    try:
+        template = env.get_template(path)
+        return template.render(**data)
+    
+    except TemplateNotFound:
+        return HTMLResponse(content=f"Template non trouvé : {path}/page.html", status_code=404)
+    except Exception as e:
+        return HTMLResponse(content=f"Erreur lors du rendu du template : {str(e)}", status_code=500)
+
