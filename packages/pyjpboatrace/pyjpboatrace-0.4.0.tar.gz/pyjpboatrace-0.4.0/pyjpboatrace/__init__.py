143a05594618786b456e9963e4dd652bb659e883
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-30 03:09:56 +0000

    Add package pyjpboatrace/0.4.0/pyjpboatrace-0.4.0.tar.gz

---
+++ packages/pyjpboatrace/pyjpboatrace-0.4.0.tar.gz/pyjpboatrace-0.4.0/pyjpboatrace/__init__.py
@@ -0,0 +1,28 @@
+from .pyjpboatrace import PyJPBoatrace
+from . import certification
+from . import const
+from . import drivers
+from . import exceptions
+from . import operator
+from . import pyjpboatrace
+from . import scraper
+from . import user_information
+from . import utils
+from . import validator
+
+
+__version__ = 'v0.4.0'
+
+__all__ = [
+    PyJPBoatrace.__name__,
+    certification.__name__,
+    const.__name__,
+    drivers.__name__,
+    exceptions.__name__,
+    operator.__name__,
+    pyjpboatrace.__name__,
+    scraper.__name__,
+    user_information.__name__,
+    utils.__name__,
+    validator.__name__,
+]
