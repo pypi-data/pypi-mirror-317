b536114c7faadb568d64445eff285b5302ebcbed
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-30 14:05:11 +0000

    Add package skyctl/1.1.13/skyctl-1.1.13-py2.py3-none-any.whl

---
+++ packages/skyctl/skyctl-1.1.13-py2.py3-none-any.whl/ctl/interactive_cli.py
@@ -0,0 +1,36 @@
+import cmd
+import sys
+
+from ctl.constants import GENERAL_ERROR_CODE
+from ctl.error import CLIError
+from ctl.terminal import cli  # 从另一个文件导入 cli
+
+
+class SkyCtlCmd(cmd.Cmd):
+    intro = "Welcome to SkyCtl CLI. Type 'help' or '?' for commands."
+    prompt = "(skyctl) "
+
+    def do_command(self, line):
+        """Execute a CLI command."""
+        try:
+            cli.main(args=line.split())  # 调用 cli 的 main 方法
+        except CLIError as e:
+            e.show()
+        except Exception as e:
+            cli_error = CLIError(str(e), GENERAL_ERROR_CODE)
+            cli_error.show()
+
+    def do_exit(self, line):
+        """Exit the CLI."""
+        print("Goodbye!")
+        return True
+
+
+if __name__ == '__main__':
+    try:
+        SkyCtlCmd().cmdloop()
+    except CLIError as e:
+        e.show()
+    except Exception as e:
+        cli_error = CLIError(str(e), GENERAL_ERROR_CODE)
+        cli_error.show()
