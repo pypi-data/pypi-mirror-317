f06f3bdc36402c704445bf8f01de9a1e3a22a40d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-30 05:04:47 +0000

    Add package scarf-sdk/0.1.1/scarf_sdk-0.1.1.tar.gz

---
+++ packages/scarf-sdk/scarf_sdk-0.1.1.tar.gz/scarf_sdk-0.1.1/setup.py
@@ -0,0 +1,27 @@
+from setuptools import find_packages, setup
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setup(
+    name="scarf-sdk",
+    version="0.1.1",
+    author="Scarf",
+    author_email="engineering@scarf.sh",
+    description="Python bindings for Scarf telemetry",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/scarf-sh/scarf-py",
+    packages=find_packages(),
+    python_requires=">=3.7",
+    install_requires=[
+        "requests>=2.25.0",
+    ],
+    extras_require={
+        "dev": [
+            "pytest>=7.0.0",
+            "pytest-cov>=4.0.0",
+            "ruff>=0.1.0",
+        ],
+    },
+)
