3ced85b837c1bd0562b9413e926f277e0ce50aaf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-30 05:04:35 +0000

    Add package pyprune/0.1.5/pyprune-0.1.5-py3-none-any.whl

---
+++ packages/pyprune/pyprune-0.1.5-py3-none-any.whl/pyprune/__init__.py
@@ -0,0 +1,11 @@
+from .backtracking import Backtracking, Choices, Grid  # noqa: F401
+from .subset import (  # noqa: F401
+    elements,
+    is_empty,
+    is_singleton,
+    num_elements,
+    remove,
+    remove_except,
+    smallest,
+    subset,
+)
