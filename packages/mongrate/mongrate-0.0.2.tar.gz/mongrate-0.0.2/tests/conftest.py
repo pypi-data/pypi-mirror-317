094401b40179464ceddf1152a6124f317a2901ea
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-30 03:09:50 +0000

    Add package mongrate/0.0.2/mongrate-0.0.2.tar.gz

---
+++ packages/mongrate/mongrate-0.0.2.tar.gz/mongrate-0.0.2/tests/conftest.py
@@ -0,0 +1,10 @@
+TEST_DEFAULT_CONFIG_FILE = "tests/test_mongodb_migrator_config.yaml"
+
+
+"""Run pytest with db connection
+    pytest tests/
+"""
+
+"""Run pytest without db connection
+    MOCK_DB=true pytest tests/
+"""
