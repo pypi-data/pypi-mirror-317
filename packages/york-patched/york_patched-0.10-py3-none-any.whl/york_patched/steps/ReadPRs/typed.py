a15ed0c7f33dbf45db2b18328aef679e2e1d8f20
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-30 15:06:04 +0000

    Add package york-patched/0.10/york_patched-0.10-py3-none-any.whl

---
+++ packages/york-patched/york_patched-0.10-py3-none-any.whl/york_patched/steps/ReadPRs/typed.py
@@ -0,0 +1,19 @@
+from typing_extensions import Annotated, List, TypedDict
+
+from york_patched.common.utils.step_typing import StepTypeConfig
+from york_patched.steps.ReadPRDiffs.typed import ReadPRDiffsOutputs
+
+
+class __ReadPRsRequiredInputs(TypedDict):
+    repo_slug: str
+
+
+class ReadPRsInputs(__ReadPRsRequiredInputs, total=False):
+    pr_state: str
+    scm_url: Annotated[str, StepTypeConfig(is_config=True)]
+    gitlab_api_key: Annotated[str, StepTypeConfig(is_config=True, or_op=["github_api_key"])]
+    github_api_key: Annotated[str, StepTypeConfig(is_config=True, or_op=["gitlab_api_key"])]
+
+
+class ReadPRsOutputs(TypedDict):
+    pr_texts: List[ReadPRDiffsOutputs]
