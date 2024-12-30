71c6b16c7482846e005cca7fd6308c02fd76483f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-30 03:10:14 +0000

    Add package cw-test/1.0.3/cw_test-1.0.3-py3-none-any.whl

---
+++ packages/cw-test/cw_test-1.0.3-py3-none-any.whl/cw_test/__init__.py
@@ -0,0 +1,10 @@
+
+ipy = get_ipython()
+
+def mscene_magic(line):
+
+  line_magic = f"-m cw_test {line}"
+
+  ipy.run_line_magic("run", line_magic)
+
+ipy.register_magic_function(mscene_magic, "line", "mscene")
