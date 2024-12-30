4b2dc3b002453299434d3cd05cfa18f54b87c26d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-30 15:04:56 +0000

    Add package askharrison/0.1.1/askharrison-0.1.1.tar.gz

---
+++ packages/askharrison/askharrison-0.1.1.tar.gz/askharrison-0.1.1/setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup, find_packages
+# install requirements from requirements.txt
+setup(
+    name="askharrison",
+    version="0.1.1",
+    packages=find_packages(),
+    description="A package for the AskHarrison, including various GenAI tools",
+    install_requires=[
+        "streamlit",
+        "requests",
+        "pyarxiv",
+        "transformers",
+        "ipykernel",
+        "pandas",
+        "numpy",
+        "arxiv",
+        "openai==1.29.0",
+        "tiktoken",
+        "Pillow",            
+        ]
+)
