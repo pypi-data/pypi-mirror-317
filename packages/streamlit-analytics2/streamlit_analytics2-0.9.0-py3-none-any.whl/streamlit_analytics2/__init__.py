5ab707ac58d8b451d1019b97c1ac11980f4f97b7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-30 04:04:43 +0000

    Add package streamlit-analytics2/0.9.0/streamlit_analytics2-0.9.0-py3-none-any.whl

---
+++ packages/streamlit-analytics2/streamlit_analytics2-0.9.0-py3-none-any.whl/streamlit_analytics2/__init__.py
@@ -0,0 +1,9 @@
+"""
+Streamlit Analytics 2
+Track & visualize user interactions with your streamlit app.
+"""
+
+from .main import counts, start_tracking, stop_tracking, track  # noqa: F401
+
+__version__ = "0.9.0"
+__name__ = "streamlit_analytics2"
