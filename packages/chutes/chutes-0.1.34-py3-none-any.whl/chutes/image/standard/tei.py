23dff16c7f4ee4c046ae255f1962a929c7a58b3e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-30 04:04:58 +0000

    Add package chutes/0.1.34/chutes-0.1.34-py3-none-any.whl

---
+++ packages/chutes/chutes-0.1.34-py3-none-any.whl/chutes/image/standard/tei.py
@@ -0,0 +1,11 @@
+TEI = "chutes/tei:1.6.0"
+
+# To build this yourself, you can use something like:
+# from chutes.image import Image  # noqa: E402
+# image = (
+#     Image(
+#         username="chutes", name="text-embeddings-inference", tag="1.6.0"
+#     )
+#     .from_base("ghcr.io/huggingface/text-embeddings-inference:1.6")
+#     .run_command("apt update && apt -y install python3 python3-venv python3-pip")
+# )
