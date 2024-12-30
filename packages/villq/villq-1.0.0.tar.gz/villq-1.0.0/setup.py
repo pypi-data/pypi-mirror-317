75d38c221aec6aa5b371c5a8f0cb1bcddbe3e9f8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-30 14:04:59 +0000

    Add package villq/1.0.0/villq-1.0.0.tar.gz

---
+++ packages/villq/villq-1.0.0.tar.gz/villq-1.0.0/setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup, find_packages
+
+setup(
+    name                           = "villq"                                                         ,
+    version                        = "1.0.0"                                                           ,
+    description                    = "This Module Creates a Purple Fade Effect on ASCII Art and Text." ,
+    long_description_content_type  = "text/markdown"                                                   ,
+    author                         = "Anvil"                                                           ,
+    author_email                   = "_@chimera.rip"                                                   ,
+    url                            = "https://github.com/vChimera/anvilly"                             ,
+    packages                       = find_packages()                                                   ,
+    classifiers                    = [
+        "Programming Language :: Python :: 3"                                                          ,
+        "License :: OSI Approved :: MIT License"                                                       ,
+        "Operating System :: OS Independent"                                                           ,
+    ]                                                                                                  ,
+    python_requires                = ">=3.6"                                                           ,
+)
