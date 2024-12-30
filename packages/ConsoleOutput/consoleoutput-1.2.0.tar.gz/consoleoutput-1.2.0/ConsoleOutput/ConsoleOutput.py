6a680e91d5acfebffc5e5dbb6c3eb4593242550f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-30 05:04:47 +0000

    Add package ConsoleOutput/1.2.0/consoleoutput-1.2.0.tar.gz

---
+++ packages/ConsoleOutput/consoleoutput-1.2.0.tar.gz/consoleoutput-1.2.0/ConsoleOutput/ConsoleOutput.py
@@ -0,0 +1,100 @@
+import os
+import sys
+from datetime import datetime
+import ctypes
+
+
+class Color:
+    F_COLOR = {
+        "NE": "\033[30m",  # Negro
+        "RO": "\033[31m",  # Rojo
+        "VE": "\033[32m",  # Verde
+        "AM": "\033[33m",  # Amarillo
+        "AZ": "\033[34m",  # Azul
+        "MA": "\033[35m",  # Magenta
+        "CI": "\033[36m",  # Cian
+        "BL": "\033[37m",  # Blanco
+    }
+
+    B_COLOR = {
+        "NE": "\033[40m",  # Fondo negro
+        "RO": "\033[41m",  # Fondo rojo
+        "VE": "\033[42m",  # Fondo verde
+        "AM": "\033[43m",  # Fondo amarillo
+        "AZ": "\033[44m",  # Fondo azul
+        "MA": "\033[45m",  # Fondo magenta
+        "CI": "\033[46m",  # Fondo cian
+        "BL": "\033[47m",  # Fondo blanco
+    }
+
+    @staticmethod
+    def reset():
+        return "\033[0m"  # Reset color
+
+
+class SalidaConsola:
+    @staticmethod
+    def habilitar_color_windows():
+        """Habilita soporte ANSI en CMD de Windows."""
+        if os.name == "nt":
+            kernel32 = ctypes.windll.kernel32
+            kernel32.SetConsoleMode(kernel32.GetStdHandle(-11), 7)
+
+    @staticmethod
+    def limpiar_consola():
+        """Limpia la consola."""
+        if os.name == "nt":
+            os.system("cls")
+        else:
+            os.system("clear")
+
+    @staticmethod
+    def imprimir(texto: str, color_texto: str = "BL", color_fondo: str = "NE"):
+        # Obtener los colores de texto y fondo
+        color_texto = Color.F_COLOR.get(color_texto, Color.F_COLOR["BL"])
+        color_fondo = Color.B_COLOR.get(color_fondo, Color.B_COLOR["NE"])
+        print(f'{color_fondo}{color_texto}{texto}{Color.reset()}')
+
+    class Alertas:
+        def __init__(self, time: bool = True):
+            self.time = time
+            self.color = Color()
+
+        @staticmethod
+        def get_time():
+            hora_actual = datetime.now()
+            return hora_actual.strftime("[%H:%M:%S]")
+
+        @staticmethod
+        def ajustar_mensaje(etiqueta: str):
+            return f"[ ! {etiqueta:^13} ! ]"
+
+        def Danger(self, text: str):
+            etiqueta = self.ajustar_mensaje("DANGER")
+            mensaje = f"{self.color.B_COLOR.get('RO')}{self.color.F_COLOR.get('AM')}{etiqueta} {Color.reset()} {self.get_time()} {self.color.B_COLOR.get('RO')}{self.color.F_COLOR.get('AM')} {text} {Color.reset()}"
+            print(mensaje)
+
+        def Risk(self, text: str):
+            etiqueta = self.ajustar_mensaje("RISK")
+            mensaje = f"{self.color.B_COLOR.get('AM')}{self.color.F_COLOR.get('RO')}{etiqueta} {Color.reset()} {self.get_time()} {self.color.B_COLOR.get('AM')}{self.color.F_COLOR.get('RO')} {text} {Color.reset()}"
+            print(mensaje)
+
+        def Warning(self, text: str):
+            etiqueta = self.ajustar_mensaje("WARNING")
+            mensaje = f"{self.color.B_COLOR.get('VE')}{self.color.F_COLOR.get('CI')}{etiqueta} {Color.reset()} {self.get_time()} {self.color.B_COLOR.get('VE')}{self.color.F_COLOR.get('CI')} {text} {Color.reset()}"
+            print(mensaje)
+
+        def Normal(self, text: str):
+            etiqueta = self.ajustar_mensaje("NORMAL")
+            mensaje = f"{self.color.B_COLOR.get('NE')}{self.color.F_COLOR.get('BL')}{etiqueta} {Color.reset()} {self.get_time()} {self.color.B_COLOR.get('NE')}{self.color.F_COLOR.get('BL')} {text} {Color.reset()}"
+            print(mensaje)
+
+        def Message(self, text: str):
+            etiqueta = self.ajustar_mensaje("MESSAGE")
+            mensaje = f"{self.color.B_COLOR.get('BL')}{self.color.F_COLOR.get('CI')}{etiqueta} {Color.reset()} {self.get_time()} {self.color.B_COLOR.get('BL')}{self.color.F_COLOR.get('CI')} {text} {Color.reset()}"
+            print(mensaje)
+
+        def Information(self, text: str):
+            etiqueta = self.ajustar_mensaje("INFORMATION")
+            mensaje = f"{self.color.B_COLOR.get('AZ')}{self.color.F_COLOR.get('VE')}{etiqueta} {Color.reset()} {self.get_time()} {self.color.B_COLOR.get('AZ')}{self.color.F_COLOR.get('VE')} {text} {Color.reset()}"
+            print(mensaje)
---
+++ packages/ConsoleOutput/consoleoutput-1.2.0.tar.gz/consoleoutput-1.2.0/setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="ConsoleOutput",
+    version="1.2.0",
+    author="Carlos-dev-G",
+    author_email="baa4tsdev@gmail.com",
+    description="Es una librería ligera y fácil de usar que permite aplicar estilos y colores a los textos que se imprimen en la consola",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    url="https://github.com/Carlos-dev-G/ConsoleOutput",
+    packages=find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.6",
+    install_requires=[],
+)
