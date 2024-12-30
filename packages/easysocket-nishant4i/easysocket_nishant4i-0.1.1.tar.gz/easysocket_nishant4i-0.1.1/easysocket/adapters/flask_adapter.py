54da14b4bf92f27770dab782d7766bf0a6ca8649
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-30 03:10:29 +0000

    Add package easysocket-nishant4i/0.1.1/easysocket_nishant4i-0.1.1.tar.gz

---
+++ packages/easysocket-nishant4i/easysocket_nishant4i-0.1.1.tar.gz/easysocket_nishant4i-0.1.1/easysocket/adapters/flask_adapter.py
@@ -0,0 +1,37 @@
+from flask import request
+from werkzeug.serving import WSGIServer
+from geventwebsocket.handler import WebSocketHandler
+import uuid
+import asyncio
+
+class FlaskAdapter:
+    def __init__(self, app, easysocket, path):
+        self.app = app
+        self.easysocket = easysocket
+        self.path = path
+        self.setup_routes()
+        
+    def setup_routes(self):
+        @self.app.route(self.path)
+        async def websocket():
+            if request.environ.get('wsgi.websocket'):
+                ws = request.environ['wsgi.websocket']
+                client_id = str(uuid.uuid4())
+                self.easysocket.clients[client_id] = ws
+                
+                try:
+                    while not ws.closed:
+                        message = ws.receive()
+                        if message:
+                            await self.easysocket.protocol.handle_message(ws, message)
+                except Exception as e:
+                    print(f"WebSocket error: {e}")
+                finally:
+                    if client_id in self.easysocket.clients:
+                        del self.easysocket.clients[client_id]
+                        
+            return ''
+        
+    def run(self, host='0.0.0.0', port=5000):
+        server = WSGIServer((host, port), self.app, handler_class=WebSocketHandler)
+        server.serve_forever()
---
+++ packages/easysocket-nishant4i/easysocket_nishant4i-0.1.1.tar.gz/easysocket_nishant4i-0.1.1/setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="easysocket-nishant4i",
+    version="0.1.1",
+    packages=find_packages(),
+    install_requires=[
+        "websockets>=10.0",
+        "python-socketio>=5.0.0",
+    ],
+    extras_require={
+        'flask': ['flask>=2.0.0', 'gevent-websocket>=0.10.1'],
+        'django': ['django>=3.2', 'channels>=3.0.0'],
+    },
+    author="Nishant Maurya",
+    author_email="mauryanishant2005@gmail.com",
+    description="A simple WebSocket library for Flask and Django",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+) 
