c214dafce3dd492e493dba49970e682276e0cb02
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-30 03:09:53 +0000

    Add package cw-test/1.0.2/cw_test-1.0.2.tar.gz

---
+++ packages/cw-test/cw_test-1.0.2.tar.gz/cw_test-1.0.2/setup.py
@@ -0,0 +1,11 @@
+
+from setuptools import setup, find_packages
+
+setup(
+    name="cw_test",
+    version= "1.0.2",
+    packages= find_packages(),
+    readme = "README.md",
+    # scripts = ["cw_test/manim_setup.py"],
+
+)
