2f6a56a07c82b32d29c433aed45b1a0d74eae298
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-30 14:05:00 +0000

    Add package forthright/0.0.5/forthright-0.0.5-py3-none-any.whl

---
+++ packages/forthright/forthright-0.0.5-py3-none-any.whl/forthright/__init__.py
@@ -0,0 +1,9 @@
+
+package_version = '0.0.5'
+__version__ = package_version
+
+# Import submodules
+from .forthright import forthright_server
+from .forthright import forthright_client
+
+__all__ = ['forthright_server', 'forthright_client']
