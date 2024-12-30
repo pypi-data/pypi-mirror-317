4f11ba81e8350c2798de73c567d4c98b53b27e43
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-30 05:04:36 +0000

    Add package supersynthetic-data-generator/0.1.3/supersynthetic-data-generator-0.1.3.tar.gz

---
+++ packages/supersynthetic-data-generator/supersynthetic-data-generator-0.1.3.tar.gz/supersynthetic-data-generator-0.1.3/setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="supersynthetic-data-generator",
+    version="0.1.3",
+    author="Ujwal Watgule",
+    author_email="ujwalwatgule@gmail.com",
+    description="A library to generate synthetic datasets for testing ML models",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    url="https://github.com/UjwalWtg/supersynthetic-data-generator",
+    packages=find_packages(),
+    install_requires=[
+        "numpy",
+        "pandas",
+        "faker"
+    ],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.6",
+)
