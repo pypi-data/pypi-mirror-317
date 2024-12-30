4bcebaf468778dba687738dd63d68d0ebae308be
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-30 15:06:06 +0000

    Add package About-ak-dev/1.0/about_ak_dev-1.0.tar.gz

---
+++ packages/About-ak-dev/about_ak_dev-1.0.tar.gz/about_ak_dev-1.0/about_me/about_me.py
@@ -0,0 +1,8 @@
+def Myname():
+    print("Ajith Kumar")
+
+def DOB():
+    print("12-10-2000")
+
+def Iam():
+    print("I am fulllstack developer")
---
+++ packages/About-ak-dev/about_ak_dev-1.0.tar.gz/about_ak_dev-1.0/setup.py
@@ -0,0 +1,3 @@
+from setuptools import setup, find_packages
+
+setup(name="About_ak_dev", version="1.0", packages=find_packages(),description="This package contains data about Ak dev")
