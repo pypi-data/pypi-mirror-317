f161fc5e63a8cea20fc87b1104abfedd94e511ae
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-30 04:04:51 +0000

    Add package cwtch/0.11.0/cwtch-0.11.0.tar.gz

---
+++ packages/cwtch/cwtch-0.11.0.tar.gz/cwtch-0.11.0/build.py
@@ -0,0 +1,21 @@
+import os
+
+from Cython.Build import cythonize
+from setuptools import Distribution, Extension
+
+
+cython_dir = os.path.join("src")
+extension = Extension(
+    "cwtch.core",
+    [
+        os.path.join(cython_dir, "core.pyx"),
+    ],
+    extra_compile_args=["-O2", "-std=c2x"],
+)
+
+ext_modules = cythonize([extension], include_path=[cython_dir])
+dist = Distribution({"ext_modules": ext_modules})
+
+dist.run_command("build_ext")
+build_ext_cmd = dist.get_command_obj("build_ext")
+build_ext_cmd.copy_extensions_to_source()
