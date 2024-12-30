67707f46ab9a0de3beee568bcfb56685f472d2b8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-30 05:04:20 +0000

    Add package py-progress-lib/0.8.1/py_progress_lib-0.8.1-py3-none-any.whl

---
+++ packages/py-progress-lib/py_progress_lib-0.8.1-py3-none-any.whl/py_progress/progress.py
@@ -0,0 +1,45 @@
+from colorama import Fore, Back, Style, init
+import time
+import math
+
+class TextStyle:
+    def __init__(self) -> None:
+        init()
+
+    def get_text(self, text: str, color: any = "", back: any = "") -> str:
+        return color + back + str(text) + Style.RESET_ALL
+
+class ProgressBar():
+    def __init__(self, part) -> None:
+        self.procent = 0
+        self.all = part
+        self.len = 60
+        self.old = ""
+
+        self.start_time = -1
+
+
+    
+    def progress(self, name):
+        ts = TextStyle()
+        print(f"\r {' ' * (self.len + len(self.old) + 10)}", end="")
+        self.procent += 1
+        procent = math.ceil((self.procent / self.all)  * 100)
+        proc = math.ceil(self.len / 100 *  procent)
+        bar = ts.get_text(text=" ", back=Back.WHITE) * proc + " " * (self.len - proc)
+        procent = ts.get_text(text=str(procent) + "%", color=Fore.GREEN)
+        print(f"\r {procent} |{bar}|: {ts.get_text(name, color=Fore.CYAN)}", end="")
+
+        self.old = name
+
+    def progress_with_time(self, name):
+
+        if self.start_time != -1:
+            curr_last = self.all - self.procent
+            time_to_finish = int(curr_last * (time.time() - self.start_time))
+            new_name = f"{name} || left: {time_to_finish}s"
+        else:
+            new_name = name
+
+        self.progress(name=new_name)
+        self.start_time = time.time()
