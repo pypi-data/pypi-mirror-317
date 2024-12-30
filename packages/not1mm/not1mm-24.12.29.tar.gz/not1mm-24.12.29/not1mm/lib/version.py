c0704a2dc281ab342bcc1cc6da84a04942225014
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-30 14:04:50 +0000

    Add package not1mm/24.12.29/not1mm-24.12.29.tar.gz

---
+++ packages/not1mm/not1mm-24.12.29.tar.gz/not1mm-24.12.29/not1mm/lib/version.py
@@ -0,0 +1,3 @@
+"""It's the version"""
+
+__version__ = "24.12.29"
---
+++ packages/not1mm/not1mm-24.12.29.tar.gz/not1mm-24.12.29/not1mm/plugins/arrl_rtty_ru.py
@@ -0,0 +1,291 @@
+"""ARRL plugin"""
+
+# pylint: disable=invalid-name, unused-argument, unused-variable, c-extension-no-member
+
+import datetime
+from decimal import Decimal
+from pathlib import Path
+
+from PyQt6 import QtWidgets
+from not1mm.lib.plugin_common import online_score_xml
+
+EXCHANGE_HINT = ""
+
+name = "ARRL RTTY Round Up"
+cabrillo_name = "ARRL-RTTY"
+mode = "BOTH"  # CW SSB BOTH RTTY
+columns = [
+    "YYYY-MM-DD HH:MM:SS",
+    "Call",
+    "Freq",
+    "Snt",
+    "Rcv",
+    "SentNr",
+    "RcvNr",
+    "Exchange1",
+    "CK",
+    "Prec",
+    "Sect",
+    "WPX",
+    "Power",
+    "M1",
+    "ZN",
+    "M2",
+    "PFX",
+    "PTS",
+    "Name",
+    "Comment",
+]
+
+# 1 once per contest, 2 work each band, 3 each band/mode, 4 no dupe checking
+dupe_type = 4
+
+
+def init_contest(self):
+    """setup plugin"""
+    set_tab_next(self)
+    set_tab_prev(self)
+    interface(self)
+
+
+def interface(self):
+    """Setup user interface"""
+
+
+def reset_label(self):
+    """reset label after field cleared"""
+
+
+def set_tab_next(self):
+    """Set TAB Advances"""
+    self.tab_next = {
+        self.callsign: self.sent,
+        self.sent: self.receive,
+        self.receive: self.other_1,
+        self.other_1: self.other_2,
+        self.other_2: self.callsign,
+    }
+
+
+def set_tab_prev(self):
+    """Set TAB Advances"""
+    self.tab_prev = {
+        self.callsign: self.other_2,
+        self.sent: self.callsign,
+        self.receive: self.sent,
+        self.other_1: self.receive,
+        self.other_2: self.other_1,
+    }
+
+
+def set_contact_vars(self):
+    """Contest Specific"""
+
+
+def predupe(self):
+    """called after callsign entered"""
+
+
+def prefill(self):
+    """Fill SentNR"""
+
+
+def points(self):
+    """Calc point"""
+
+
+def show_mults(self, rtc=None):
+    """Return display string for mults"""
+
+
+def show_qso(self):
+    """Return qso count"""
+    result = self.database.fetch_qso_count()
+    if result:
+        return int(result.get("qsos", 0))
+    return 0
+
+
+def get_points(self):
+    """Return raw points before mults"""
+    result = self.database.fetch_points()
+    if result:
+        return int(result.get("Points", 0))
+    return 0
+
+
+def calc_score(self):
+    """Return calculated score"""
+    result = self.database.fetch_points()
+
+
+def adif(self):
+    """
+    Creates an ADIF file of the contacts made.
+    """
+    now = datetime.datetime.now()
+    date_time = now.strftime("%Y-%m-%d_%H-%M-%S")
+    filename = (
+        str(Path.home())
+        + "/"
+        + f"{self.station.get('Call').upper()}_{cabrillo_name}_{date_time}.adi"
+    )
+    log = self.database.fetch_all_contacts_asc()
+    try:
+        with open(filename, "w", encoding="utf-8") as file_descriptor:
+            print("<ADIF_VER:5>2.2.0", end="\r\n", file=file_descriptor)
+            print("<EOH>", end="\r\n", file=file_descriptor)
+            for contact in log:
+                hiscall = contact.get("Call", "")
+                hisname = contact.get("Name", "")
+                the_date_and_time = contact.get("TS")
+                # band = contact.get("Band")
+                themode = contact.get("Mode")
+                frequency = str(Decimal(str(contact.get("Freq", 0))) / 1000)
+                sentrst = contact.get("SNT", "")
+                rcvrst = contact.get("RCV", "")
+                sentnr = str(contact.get("SentNr", "0"))
+                rcvnr = str(contact.get("NR", "0"))
+                grid = contact.get("GridSquare", "")
+                comment = contact.get("Comment", "")
+                loggeddate = the_date_and_time[:10]
+                loggedtime = the_date_and_time[11:13] + the_date_and_time[14:16]
+                print(
+                    f"<QSO_DATE:{len(''.join(loggeddate.split('-')))}:d>"
+                    f"{''.join(loggeddate.split('-'))}",
+                    end="\r\n",
+                    file=file_descriptor,
+                )
+
+                try:
+                    print(
+                        f"<TIME_ON:{len(loggedtime)}>{loggedtime}",
+                        end="\r\n",
+                        file=file_descriptor,
+                    )
+                except TypeError:
+                    ...
+
+                try:
+                    print(
+                        f"<CALL:{len(hiscall)}>{hiscall.upper()}",
+                        end="\r\n",
+                        file=file_descriptor,
+                    )
+                except TypeError:
+                    ...
+
+                try:
+                    if len(hisname):
+                        print(
+                            f"<NAME:{len(hisname)}>{hisname.title()}",
+                            end="\r\n",
+                            file=file_descriptor,
+                        )
+                except TypeError:
+                    ...
+
+                try:
+                    print(
+                        f"<MODE:{len(themode)}>{themode}",
+                        end="\r\n",
+                        file=file_descriptor,
+                    )
+                except TypeError:
+                    ...
+
+                try:
+                    print(
+                        f"<FREQ:{len(frequency)}>{frequency}",
+                        end="\r\n",
+                        file=file_descriptor,
+                    )
+                except TypeError:
+                    ...
+
+                try:
+                    print(
+                        f"<RST_SENT:{len(sentrst)}>{sentrst}",
+                        end="\r\n",
+                        file=file_descriptor,
+                    )
+                except TypeError:
+                    ...
+
+                try:
+                    print(
+                        f"<RST_RCVD:{len(rcvrst)}>{rcvrst}",
+                        end="\r\n",
+                        file=file_descriptor,
+                    )
+                except TypeError:
+                    ...
+
+                try:
+                    if sentnr != "0":
+                        print(
+                            f"<STX_STRING:{len(sentnr)}>{sentnr}",
+                            end="\r\n",
+                            file=file_descriptor,
+                        )
+                except TypeError:
+                    ...
+
+                try:
+                    if rcvnr != "0":
+                        print(
+                            f"<SRX_STRING:{len(rcvnr)}>{rcvnr}",
+                            end="\r\n",
+                            file=file_descriptor,
+                        )
+                except TypeError:
+                    ...
+
+                try:
+                    if len(grid) > 1:
+                        print(
+                            f"<GRIDSQUARE:{len(grid)}>{grid}",
+                            end="\r\n",
+                            file=file_descriptor,
+                        )
+                except TypeError:
+                    ...
+
+                try:
+                    if len(comment):
+                        print(
+                            f"<COMMENT:{len(comment)}>{comment}",
+                            end="\r\n",
+                            file=file_descriptor,
+                        )
+                except TypeError:
+                    ...
+
+                print("<EOR>", end="\r\n", file=file_descriptor)
+                print("", end="\r\n", file=file_descriptor)
+    except IOError:
+        ...
+
+
+def cabrillo(self):
+    """Generates Cabrillo file. Maybe."""
+
+
+def recalculate_mults(self):
+    """Recalculates multipliers after change in logged qso."""
+
+
+# States/Prov.	DXCC	state	country
+
+
+def get_mults(self):
+    """"""
+
+    mults = {}
+    mults["state"], mults["country"] = show_mults(self, rtc=True)
+    return mults
+
+
+def just_points(self):
+    """"""
+    return get_points(self)
