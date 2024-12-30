ea4abe1f5bd78e715a8d867758e8a62d9c89600a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-30 14:04:57 +0000

    Add package licomp-dwheeler/0.3.4/licomp_dwheeler-0.3.4.tar.gz

---
+++ packages/licomp-dwheeler/licomp_dwheeler-0.3.4.tar.gz/licomp_dwheeler-0.3.4/licomp_dwheeler/__main__.py
@@ -0,0 +1,28 @@
+#!/bin/env python3
+
+# SPDX-FileCopyrightText: 2024 Henrik Sandklef
+#
+# SPDX-License-Identifier: GPL-3.0-or-later
+
+from licomp.interface import UseCase
+from licomp.interface import Provisioning
+from licomp.main_base import LicompParser
+
+from licomp_dwheeler.config import cli_name
+from licomp_dwheeler.config import description
+from licomp_dwheeler.config import epilog
+from licomp_dwheeler.dwheeler import LicompDw
+
+def main():
+    ld = LicompDw()
+    o_parser = LicompParser(ld,
+                            cli_name,
+                            description,
+                            epilog,
+                            UseCase.LIBRARY,
+                            Provisioning.BIN_DIST)
+    o_parser.run()
+
+
+if __name__ == '__main__':
+    main()
